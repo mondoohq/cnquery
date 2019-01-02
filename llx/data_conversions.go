3abffed4b787897589f97007b1d5813ac3bf241d
Dominik Richter <dominik.richter@gmail.com>
2019-01-02 00:50:41 -0800

    add a ton of functions to convert between go types and llx primitives

---
+++ llx/data_conversions.go
@@ -0,0 +1,314 @@
+package llx
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+
+	"go.mondoo.io/mondoo/lumi"
+	"go.mondoo.io/mondoo/types"
+)
+
+type dataConverter func(interface{}, types.Type) (*Primitive, error)
+type primitiveConverter func(*Primitive) *RawData
+
+var dataConverters map[types.Type]dataConverter
+var primitiveConverters map[types.Type]primitiveConverter
+
+func init() {
+	dataConverters = map[types.Type]dataConverter{
+		types.Nil:          nil2result,
+		types.Bool:         bool2result,
+		types.Ref:          ref2result,
+		types.Int:          int2result,
+		types.Float:        float2result,
+		types.String:       string2result,
+		types.Regex:        regex2result,
+		types.ArrayLike:    array2result,
+		types.MapLike:      map2result,
+		types.ResourceLike: resource2result,
+		types.FunctionLike: function2result,
+	}
+
+	primitiveConverters = map[types.Type]primitiveConverter{
+		types.Nil:          pnil2raw,
+		types.Bool:         pbool2raw,
+		types.Ref:          pref2raw,
+		types.Int:          pint2raw,
+		types.Float:        pfloat2raw,
+		types.String:       pstring2raw,
+		types.Regex:        pregex2raw,
+		types.ArrayLike:    parray2raw,
+		types.MapLike:      pmap2raw,
+		types.ResourceLike: presource2raw,
+		types.FunctionLike: pfunction2raw,
+	}
+}
+
+func nil2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return NilPrimitive, nil
+}
+
+func bool2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return BoolPrimitive(value.(bool)), nil
+}
+
+func ref2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return RefPrimitive(value.(int32)), nil
+}
+
+func int2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return IntPrimitive(value.(int64)), nil
+}
+
+func float2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return FloatPrimitive(value.(float64)), nil
+}
+
+func string2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return StringPrimitive(value.(string)), nil
+}
+
+func regex2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return RegexPrimitive(value.(string)), nil
+}
+
+func array2result(value interface{}, typ types.Type) (*Primitive, error) {
+	arr := value.([]interface{})
+	res := make([]*Primitive, len(arr))
+	ct := typ.Child()
+	var err error
+	for i := range arr {
+		res[i], err = raw2primitive(arr[i], ct)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &Primitive{Type: string(typ), Array: res}, nil
+}
+
+func stringmap2result(value interface{}, typ types.Type) (*Primitive, error) {
+	m := value.(map[string]interface{})
+	res := make(map[string]*Primitive)
+	ct := typ.Child()
+	var err error
+	for k, v := range m {
+		res[k], err = raw2primitive(v, ct)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &Primitive{Type: string(typ), Map: res}, nil
+}
+
+func intmap2result(value interface{}, typ types.Type) (*Primitive, error) {
+	m := value.(map[int32]interface{})
+	res := make(map[string]*Primitive)
+	ct := typ.Child()
+	var err error
+	for k, v := range m {
+		res[strconv.FormatInt(int64(k), 10)], err = raw2primitive(v, ct)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &Primitive{Type: string(typ), Map: res}, nil
+}
+
+func map2result(value interface{}, typ types.Type) (*Primitive, error) {
+	switch typ.Key() {
+	case types.String:
+		return stringmap2result(value, typ)
+	case types.Int:
+		return intmap2result(value, typ)
+	default:
+		return nil, errors.New("only supports turning string or int maps into primitives, not " + typ.Label())
+	}
+}
+
+func resource2result(value interface{}, typ types.Type) (*Primitive, error) {
+	m := value.(lumi.ResourceType)
+	r := m.LumiResource()
+	v := r.Name + "\x00" + r.Id
+	return &Primitive{Type: string(typ), Value: []byte(v)}, nil
+}
+
+func function2result(value interface{}, typ types.Type) (*Primitive, error) {
+	return FunctionPrimitive(value.(int32)), nil
+}
+
+func raw2primitive(value interface{}, typ types.Type) (*Primitive, error) {
+	utyp := typ.Underlying()
+	c, ok := dataConverters[utyp]
+	if !ok {
+		rdata, ok := value.(*RawData)
+		if ok {
+			return raw2primitive(rdata.Value, rdata.Type)
+		}
+		return nil, errors.New("Cannot serialize data type " + typ.Label())
+	}
+	return c(value, typ)
+}
+
+// Result converts the raw data into a proto-compliant data structure that
+// can be sent over the wire. It converts the interface{} value of RawData
+// into a []byte structure that is easily serializable
+func (r *RawData) Result() *Result {
+	if r.Error != nil {
+		return &Result{
+			Error: r.Error.Error(),
+		}
+	}
+	data, err := raw2primitive(r.Value, r.Type)
+	if err != nil {
+		return &Result{Error: err.Error()}
+	}
+	return &Result{Data: data}
+}
+
+// Result converts the raw result into a proto-compliant data structure that
+// can be sent over the wire. See RawData.Result()
+func (r *RawResult) Result() *Result {
+	res := r.Data.Result()
+	res.CodeId = r.CodeID
+	res.Ref = r.Ref
+	return res
+}
+
+func pnil2raw(p *Primitive) *RawData {
+	return NilData
+}
+
+func pbool2raw(p *Primitive) *RawData {
+	return BoolData(bytes2bool(p.Value))
+}
+
+func pref2raw(p *Primitive) *RawData {
+	return RefData(int32(bytes2int(p.Value)))
+}
+
+func pint2raw(p *Primitive) *RawData {
+	return IntData(bytes2int(p.Value))
+}
+
+func pfloat2raw(p *Primitive) *RawData {
+	return FloatData(bytes2float(p.Value))
+}
+
+func pstring2raw(p *Primitive) *RawData {
+	return StringData(string(p.Value))
+}
+
+func pregex2raw(p *Primitive) *RawData {
+	return RegexData(string(p.Value))
+}
+
+func parray2raw(p *Primitive) *RawData {
+	d, err := args2resourceargs(p.Array)
+	return &RawData{Value: d, Error: err, Type: types.Type(p.Type)}
+}
+
+func pmap2raw(p *Primitive) *RawData {
+	d, err := primitive2map(p.Map)
+	return &RawData{Value: d, Error: err, Type: types.Type(p.Type)}
+}
+
+func presource2raw(p *Primitive) *RawData {
+	res := strings.SplitN(string(p.Value), "\x00", 2)
+	id := ""
+	if len(res) > 1 {
+		id = res[1]
+	}
+	return &RawData{Value: lumi.MockResource{
+		StaticResource: &lumi.Resource{
+			ResourceID: lumi.ResourceID{Name: res[0], Id: id},
+		},
+	}, Type: types.Type(p.Type)}
+}
+
+func pfunction2raw(p *Primitive) *RawData {
+	return &RawData{Value: int32(bytes2int(p.Value)), Type: types.Type(p.Type)}
+}
+
+// RawData converts the primtiive into the internal go-representation of the
+// data that can be used for computations
+func (p *Primitive) RawData() *RawData {
+	typ := types.Type(p.Type)
+	c, ok := primitiveConverters[typ.Underlying()]
+	if !ok {
+		return &RawData{Error: errors.New("Cannot convert primitive to value for primitive type " + typ.Label())}
+	}
+	return c(p)
+}
+
+func args2resourceargs(args []*Primitive) ([]interface{}, error) {
+	if args == nil {
+		return []interface{}{}, nil
+	}
+
+	res := make([]interface{}, len(args))
+	for i := range args {
+		cur := args[i].RawData()
+		if cur.Error != nil {
+			return nil, cur.Error
+		}
+		res[i] = cur.Value
+	}
+	return res, nil
+}
+
+func primitive2map(m map[string]*Primitive) (map[string]interface{}, error) {
+	if m == nil {
+		return map[string]interface{}{}, nil
+	}
+
+	res := make(map[string]interface{})
+	for k, v := range m {
+		if v == nil {
+			res[k] = nil
+			continue
+		}
+		cur := v.RawData()
+		if cur.Error != nil {
+			return nil, cur.Error
+		}
+		res[k] = cur.Value
+	}
+	return res, nil
+}
+
+// returns the resolved argument if it's a ref; otherwise just the argument
+// returns the reference if something else needs executing before it can be computed
+// returns an error otherwise
+func (c *LeiseExecutor) resolveValue(arg *Primitive, ref int32) (*RawData, int32, error) {
+	typ := types.Type(arg.Type)
+	switch typ.Underlying() {
+	case types.Ref:
+		srcRef := int32(bytes2int(arg.Value))
+		// check if the reference exists; if not connect it
+		res, ok := c.cache.Load(srcRef)
+		if !ok {
+			return c.connectRef(srcRef, ref)
+		}
+		return res.Result, 0, res.Result.Error
+
+	case types.ArrayLike:
+		res := make([]interface{}, len(arg.Array))
+		for i := range arg.Array {
+			c, ref, err := c.resolveValue(arg.Array[i], ref)
+			if ref != 0 || err != nil {
+				return nil, ref, err
+			}
+			res[i] = c.Value
+		}
+
+		// type is in arg.Value
+		return &RawData{
+			Type:  typ,
+			Value: res,
+		}, 0, nil
+	}
+
+	v := arg.RawData()
+	return v, 0, v.Error
+}
