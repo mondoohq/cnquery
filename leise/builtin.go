51dfaf4f5c86aae919e2005f43b4cf416611429a
Dominik Richter <dominik.richter@gmail.com>
2019-01-02 00:47:44 -0800

    add llx proto

---
+++ leise/builtin.go
@@ -0,0 +1,88 @@
+package leise
+
+import (
+	"errors"
+	"sort"
+
+	"go.mondoo.io/mondoo/leise/parser"
+	"go.mondoo.io/mondoo/lumi"
+	"go.mondoo.io/mondoo/types"
+)
+
+type compileHandler struct {
+	typ       func(types.Type) types.Type
+	signature FunctionSignature
+	compile   func(*compiler, types.Type, int32, string, *parser.Call) (types.Type, error)
+}
+
+var childType = func(t types.Type) types.Type { return t.Child() }
+var arrayBlockType = func(t types.Type) types.Type { return types.Array(types.Map(types.Int, types.Any)) }
+var intType = func(t types.Type) types.Type { return types.Int }
+
+var builtinFunctions map[types.Type]map[string]compileHandler
+
+func init() {
+	builtinFunctions = map[types.Type]map[string]compileHandler{
+		types.ArrayLike: map[string]compileHandler{
+			"[]":     compileHandler{typ: childType, signature: FunctionSignature{Required: 1, Args: []types.Type{types.Int}}},
+			"{}":     compileHandler{typ: arrayBlockType, signature: FunctionSignature{Required: 1, Args: []types.Type{types.FunctionLike}}},
+			"length": compileHandler{typ: intType, signature: FunctionSignature{}},
+		},
+		types.MapLike: map[string]compileHandler{
+			"[]":     compileHandler{typ: childType, signature: FunctionSignature{Required: 1, Args: []types.Type{types.String}}},
+			"length": compileHandler{typ: intType, signature: FunctionSignature{}},
+		},
+		types.ResourceLike: map[string]compileHandler{
+			// "":       compileHandler{compile: compileResourceDefault},
+			"where":  compileHandler{compile: compileResourceWhere, signature: FunctionSignature{Required: 1, Args: []types.Type{types.FunctionLike}}},
+			"length": compileHandler{compile: compileResourceLength, signature: FunctionSignature{}},
+		},
+	}
+}
+
+func builtinFunction(typ types.Type, id string) (*compileHandler, error) {
+	fh, ok := builtinFunctions[typ.Underlying()]
+	if !ok {
+		return nil, errors.New("Cannot find any functions for type '" + typ.Label() + "'")
+	}
+	c, ok := fh[id]
+	if !ok {
+		c, ok = fh[""]
+		if !ok {
+			return nil, errors.New("Cannot find function '" + id + "' for type '" + typ.Label() + "'")
+		}
+	}
+	return &c, nil
+}
+
+func fieldNames(resource *lumi.ResourceInfo) []string {
+	res := make([]string, len(resource.Fields))
+	idx := 0
+	for k := range resource.Fields {
+		res[idx] = k
+		idx++
+	}
+	return res
+}
+
+func availableFields(c *compiler, typ types.Type) []string {
+	m, ok := builtinFunctions[typ.Underlying()]
+	if !ok {
+		return nil
+	}
+
+	res := make([]string, len(m))
+	idx := 0
+	for k := range m {
+		res[idx] = k
+		idx++
+	}
+
+	if typ.IsResource() {
+		fieldNames := fieldNames(c.Schema.Resources[typ.Name()])
+		res = append(res, fieldNames...)
+	}
+	sort.Strings(res)
+
+	return res
+}
