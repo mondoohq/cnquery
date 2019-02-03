7a102a86c16464cb21554afc132068544c24cf23
Christoph Hartmann <chris@lollyrock.com>
2019-02-03 21:33:35 +0100

    🎉 read origin packages for debian/ubuntu packages

--- lumi/resources/parser/package_test.go
+++ lumi/resources/parser/package_test.go
@@ -62,6 +62,7 @@ func TestAlpineApkdbParser(t *testing.T) {
 		Version:     "1510953106:1.1.18-r2",
 		Arch:        "x86_64",
 		Description: "the musl c library (libc) implementation",
+		Origin:      "musl",
 	}
 	assert.Contains(t, m, p, "musl detected")
 
@@ -70,6 +71,7 @@ func TestAlpineApkdbParser(t *testing.T) {
 		Version:     "1510257703:2.6.3-r0",
 		Arch:        "x86_64",
 		Description: "libressl libcrypto library",
+		Origin:      "libressl",
 	}
 	assert.Contains(t, m, p, "libcrypto detected")
 
@@ -78,6 +80,7 @@ func TestAlpineApkdbParser(t *testing.T) {
 		Version:     "1510257703:2.6.3-r0",
 		Arch:        "x86_64",
 		Description: "libressl libssl library",
+		Origin:      "libressl",
 	}
 	assert.Contains(t, m, p, "libssl detected")
 
@@ -86,6 +89,7 @@ func TestAlpineApkdbParser(t *testing.T) {
 		Version:     "1515485577:2.8.2-r0",
 		Arch:        "x86_64",
 		Description: "Alpine Package Keeper - package manager for alpine",
+		Origin:      "apk-tools",
 	}
 	assert.Contains(t, m, p, "apk-tools detected")
 
@@ -94,6 +98,7 @@ func TestAlpineApkdbParser(t *testing.T) {
 		Version:     "1513075346:1.27.2-r7",
 		Arch:        "x86_64",
 		Description: "Size optimized toolbox of many common UNIX utilities",
+		Origin:      "busybox",
 	}
 	assert.Contains(t, m, p, "apk-tools detected")
 
@@ -102,6 +107,7 @@ func TestAlpineApkdbParser(t *testing.T) {
 		Version:     "1510075862:3.0.5-r2",
 		Arch:        "x86_64",
 		Description: "Alpine base dir structure and init scripts",
+		Origin:      "alpine-baselayout",
 	}
 	assert.Contains(t, m, p, "apk-tools detected")
 }
@@ -129,6 +135,7 @@ func TestDpkgParser(t *testing.T) {
 		Version: "2.31.1-0.4ubuntu3.1",
 		Arch:    "amd64",
 		Status:  "install ok installed",
+		Origin:  "util-linux",
 		Description: `collection of partitioning utilities
 This package contains the classic fdisk, sfdisk and cfdisk partitioning
 utilities from the util-linux suite.
