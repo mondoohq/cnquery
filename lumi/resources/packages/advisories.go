bb6bf401623c5e4987b328dcf5cbea5764685d53
Christoph Hartmann <chris@lollyrock.com>
2019-03-20 21:20:25 +0100

    🎉 agent registration flow

--- lumi/resources/packages/advisories.go
+++ lumi/resources/packages/advisories.go
@@ -3,41 +3,16 @@ package packages
 import (
 	"context"
 	"net/http"
-	"os"
 
+	"github.com/rs/zerolog/log"
+	"go.mondoo.io/mondoo/nexus/agents"
+
+	"go.mondoo.io/mondoo/falcon"
+	guard_cert_auth "go.mondoo.io/mondoo/falcon/guard/authentication/cert"
 	"go.mondoo.io/mondoo/lumi/resources/parser"
 	"go.mondoo.io/mondoo/motor/platform"
 	"go.mondoo.io/mondoo/vadvisor/api"
 )
-
-var MONDOO_API = "https://api.mondoo.app"
-
-// allow overwrite of the API url by an environment variable
-func init() {
-	if len(os.Getenv("MONDOO_API")) > 0 {
-		MONDOO_API = os.Getenv("MONDOO_API")
-	}
-}
-
-func GetCve(id string) (*api.CVE, error) {
-	sa, err := api.NewSecurityAdvisorClient(MONDOO_API, &http.Client{})
-	if err != nil {
-		return nil, err
-	}
-	cve, err := sa.GetCVE(context.TODO(), &api.CveIdentifier{Id: id})
-	if err != nil {
-		return nil, err
-	}
-	return cve, nil
-}
-
-func GetAdvisory(id string) (*api.Advisory, error) {
-	sa, err := api.NewSecurityAdvisorClient(MONDOO_API, &http.Client{})
-	if err != nil {
-		return nil, err
-	}
-	return sa.GetAdvisory(context.TODO(), &api.AdvisoryIdentifier{Id: id})
-}
 
 func ConvertPlatform(platform platform.Info) *api.Platform {
 	return &api.Platform{
@@ -62,12 +37,63 @@ func ConvertParserPackages(pkgs []parser.Package) []*api.Package {
 	return apiPkgs
 }
 
+type Scanner struct {
+	MondooApiUrl string
+	Agentid      string
+	Spaceid      string
+	Privatekey   *string
+}
+
+func (s Scanner) authentication() []falcon.ClientPlugin {
+	plugins := []falcon.ClientPlugin{}
+	if s.Privatekey != nil {
+		var certplugin falcon.ClientPlugin
+		config, err := agents.GetGuardClientConfig(s.Spaceid, s.Agentid, *s.Privatekey)
+		if err == nil {
+			certplugin, err = guard_cert_auth.NewClientPlugin(config)
+		}
+
+		if err == nil {
+			plugins = append(plugins, certplugin)
+		} else {
+			log.Error().Err(err).Msg("Cannot configure certificate authentication")
+		}
+	}
+
+	return plugins
+}
+
 // searches all advisories for given packages
-func Analyze(scanJob *api.ScanJob) (*api.Report, error) {
-	sa, err := api.NewSecurityAdvisorClient(MONDOO_API, &http.Client{})
+func (s *Scanner) Analyze(scanJob *api.ScanJob) (*api.Report, error) {
+	sa, err := api.NewSecurityAdvisorClient(s.MondooApiUrl, &http.Client{})
 	if err != nil {
 		return nil, err
 	}
 
+	auth := s.authentication()
+	for i := range auth {
+		sa.AddPlugin(auth[i])
+	}
+
 	return sa.Analyse(context.Background(), scanJob)
+}
+
+func (s *Scanner) GetCve(id string) (*api.CVE, error) {
+	sa, err := api.NewSecurityAdvisorClient(s.MondooApiUrl, &http.Client{})
+	if err != nil {
+		return nil, err
+	}
+	cve, err := sa.GetCVE(context.TODO(), &api.CveIdentifier{Id: id})
+	if err != nil {
+		return nil, err
+	}
+	return cve, nil
+}
+
+func (s *Scanner) GetAdvisory(id string) (*api.Advisory, error) {
+	sa, err := api.NewSecurityAdvisorClient(s.MondooApiUrl, &http.Client{})
+	if err != nil {
+		return nil, err
+	}
+	return sa.GetAdvisory(context.TODO(), &api.AdvisoryIdentifier{Id: id})
 }
