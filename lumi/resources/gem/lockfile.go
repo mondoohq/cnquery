5f5edfad83fb980d182e49b5f02fcb89ab1feca4
Christoph Hartmann <chris@lollyrock.com>
2019-04-08 22:39:10 +0200

    🎉 gem lockfile parser

---
+++ lumi/resources/gem/lockfile.go
@@ -0,0 +1,110 @@
+package gem
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"regexp"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+
+	"go.mondoo.io/mondoo/vadvisor/api"
+)
+
+var (
+	Specline     = regexp.MustCompile(`^\s*(.*)\s\((.*)\)\s*$`)
+	GIT          = "GIT"
+	PATH         = "PATH"
+	GEM          = "GEM"
+	DEPENDENCIES = "DEPENDENCIES"
+)
+
+func ParseGemfileLock(r io.Reader) ([]*api.Package, error) {
+	pkgs := []*api.Package{}
+	state := "INIT"
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// check if we get a state change
+		newState := strings.TrimSpace(line)
+		switch newState {
+		case GIT:
+			state = newState
+		case PATH:
+			state = newState
+		case GEM:
+			log.Debug().Msg("GEM state")
+			state = newState
+		case DEPENDENCIES:
+			state = newState
+		}
+
+		var err error
+		var pkg *api.Package
+		switch state {
+		case GIT:
+			fallthrough
+		case PATH:
+			fallthrough
+		case GEM:
+			pkg, err = parseSpecLine(line)
+			if err != nil {
+				log.Error().Err(err).Str("line", line).Msg("cannot parse gem package name")
+				continue
+			}
+			if pkg != nil {
+				pkgs = append(pkgs, pkg)
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return pkgs, nil
+}
+
+func parseSpecLine(line string) (*api.Package, error) {
+	// ignore everthing with 2 leading spaces, we do not need that info
+	whitespace := LeadingSpaces(line)
+	// We do not need to scan whitespace = 6 since those are just dependencies
+	// of the package. At this point, we do not need the package graph
+	if whitespace == 4 {
+		name, version, err := ParsePackagename(line)
+		if err != nil {
+			return nil, err
+		}
+
+		return &api.Package{
+			Name:      name,
+			Version:   version,
+			Format:    "gem",
+			Namespace: "gem",
+		}, nil
+	}
+	return nil, nil
+}
+
+func ParsePackagename(line string) (string, string, error) {
+	m := Specline.FindStringSubmatch(line)
+	if len(m) == 3 {
+		return m[1], m[2], nil
+	} else {
+		return "", "", errors.New("cannot parse " + line)
+	}
+}
+
+func LeadingSpaces(line string) int {
+	i := 0
+	for _, runeValue := range line {
+		if runeValue == ' ' {
+			i++
+		} else {
+			break
+		}
+	}
+	return i
+}
---
+++ lumi/resources/gem/lockfile_test.go
@@ -0,0 +1,56 @@
+package gem_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mondoo.io/mondoo/lumi/resources/gem"
+	"go.mondoo.io/mondoo/vadvisor/api"
+)
+
+func TestGemfileLockParser(t *testing.T) {
+	data, err := os.Open("./testdata/Gemfile.lock")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pkgs, err := gem.ParseGemfileLock(data)
+	assert.Nil(t, err)
+	assert.Equal(t, 230, len(pkgs))
+
+	assert.Contains(t, pkgs, &api.Package{
+		Name:      "actioncable",
+		Version:   "6.0.0.beta3",
+		Format:    "gem",
+		Namespace: "gem",
+	})
+
+	assert.Contains(t, pkgs, &api.Package{
+		Name:      "zeitwerk",
+		Version:   "2.0.0",
+		Format:    "gem",
+		Namespace: "gem",
+	})
+}
+
+func TestParsePackagename(t *testing.T) {
+	var name string
+	var version string
+	var err error
+
+	name, version, err = gem.ParsePackagename("actioncable (6.0.0.beta3)")
+	assert.Nil(t, err)
+	assert.Equal(t, "actioncable", name)
+	assert.Equal(t, "6.0.0.beta3", version)
+
+	name, version, err = gem.ParsePackagename("activerecord-jdbcsqlite3-adapter (52.1-java)")
+	assert.Nil(t, err)
+	assert.Equal(t, "activerecord-jdbcsqlite3-adapter", name)
+	assert.Equal(t, "52.1-java", version)
+
+	name, version, err = gem.ParsePackagename(" aws-sdk-kms (1.11.0)")
+	assert.Nil(t, err)
+	assert.Equal(t, "aws-sdk-kms", name)
+	assert.Equal(t, "1.11.0", version)
+}
