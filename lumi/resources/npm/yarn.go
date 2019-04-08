09e289b9c6c2f7b1843d8ecd8f1eb1e3d8cc65a0
Christoph Hartmann <chris@lollyrock.com>
2019-04-08 19:59:26 +0200

    package parser for package.json, package-lock.json, yarn.lock

---
+++ lumi/resources/npm/yarn.go
@@ -0,0 +1,80 @@
+package npm
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"regexp"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+	"go.mondoo.io/mondoo/vadvisor/api"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type YarnLockEntry struct {
+	Version      string
+	Resolved     string
+	Dependencies map[string]string
+}
+
+func ParseYarnLock(r io.Reader) ([]*api.Package, error) {
+	var b bytes.Buffer
+
+	// iterate and convert the format to yaml on the fly
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		reStr := regexp.MustCompile(`^(\s*.*)\s\"(.*)$`)
+		repStr := "${1}: \"$2"
+		line = reStr.ReplaceAllString(line, repStr)
+
+		b.Write([]byte(line))
+		b.Write([]byte("\n"))
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	var yarnLock map[string]YarnLockEntry
+
+	err := yaml.Unmarshal(b.Bytes(), &yarnLock)
+	if err != nil {
+		return nil, err
+	}
+
+	entries := []*api.Package{}
+
+	// add all dependencies
+	for k, v := range yarnLock {
+		name, _, err := ParseYarnPackageName(k)
+		if err != nil {
+			log.Error().Str("name", name).Msg("cannot parse yarn package name")
+			continue
+		}
+		entries = append(entries, &api.Package{
+			Name:      name,
+			Version:   v.Version,
+			Format:    "npm",
+			Namespace: "nodejs",
+		})
+	}
+
+	return entries, nil
+}
+
+func ParseYarnPackageName(name string) (string, string, error) {
+	// a yarn package line may include may items
+	pkgNames := strings.Split(name, ",")
+
+	if len(pkgNames) == 0 {
+		// something wrong
+		return "", "", errors.New("cannot parse yarn package name")
+	}
+
+	parse := regexp.MustCompile(`^(.*)@(.*)$`)
+	m := parse.FindStringSubmatch(strings.TrimSpace(pkgNames[0]))
+	return m[1], m[2], nil
+}
