cafbac5b7be5a6892814095c4e6cab9b3f96ddca
Christoph Hartmann <chris@lollyrock.com>
2019-04-16 10:43:23 +0200

    operating system os groups parser

---
+++ lumi/resources/groups/groups_test.go
@@ -0,0 +1,65 @@
+package groups_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mondoo.io/mondoo/lumi/resources/groups"
+	mock "go.mondoo.io/mondoo/motor/mock/toml"
+	"go.mondoo.io/mondoo/motor/types"
+)
+
+func TestParseEtcGroups(t *testing.T) {
+	mock, err := mock.New(&types.Endpoint{Backend: "mock", Path: "groups_unix.toml"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	f, err := mock.File("/etc/group")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Nil(t, err)
+
+	r, err := f.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+	assert.Nil(t, err)
+
+	m, err := groups.ParseEtcGroup(r)
+	assert.Nil(t, err)
+	assert.Equal(t, 23, len(m), "detected the right amount of services")
+
+	assert.Equal(t, "root", m[0].Name, "detected user name")
+	assert.Equal(t, int64(0), m[0].Gid, "detected gid")
+	assert.Equal(t, []string{}, m[0].Members, "user description")
+
+	assert.Equal(t, "vagrant", m[22].Name, "detected user name")
+	assert.Equal(t, int64(1000), m[22].Gid, "detected gid")
+	assert.Equal(t, []string{"vagrant"}, m[22].Members, "user description")
+}
+
+func TestParseDscacheutilResult(t *testing.T) {
+	mock, err := mock.New(&types.Endpoint{Backend: "mock", Path: "groups_osx.toml"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c, err := mock.RunCommand("dscacheutil -q group")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m, err := groups.ParseDscacheutilResult(c.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, 3, len(m), "detected the right amount of groups")
+
+	assert.Equal(t, int64(395), m[0].Gid, "detected group id")
+	assert.Equal(t, "com.apple.access_ftp", m[0].Name, "detected group name")
+	assert.Equal(t, []string{}, m[0].Members, "detected group members")
+
+	assert.Equal(t, int64(216), m[2].Gid, "detected group id")
+	assert.Equal(t, "_postgres", m[2].Name, "detected group name")
+	assert.Equal(t, []string{"_devicemgr", "_calendar", "_teamsserver", "_xserverdocs"}, m[2].Members, "detected group members")
+}
