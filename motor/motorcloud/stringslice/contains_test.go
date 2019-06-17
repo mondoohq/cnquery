5342caa6039465a9df532c40cdaa19ab26cffb57
Christoph Hartmann <chris@lollyrock.com>
2019-06-17 13:56:01 +0200

    refactor assets cli autopilot

---
+++ motor/motorcloud/stringslice/contains_test.go
@@ -0,0 +1,17 @@
+package stringslice_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mondoo.io/mondoo/motor/motorcloud/stringslice"
+)
+
+func TestContains(t *testing.T) {
+	assert.True(t, stringslice.Contains([]string{"ab", "aa"}, "ab"))
+	assert.False(t, stringslice.Contains([]string{"ab", "aa"}, "a"))
+	assert.False(t, stringslice.Contains([]string{"ab", "aa"}, "bs"))
+	assert.True(t, stringslice.Contains([]string{"hello", "world"}, "world"))
+	assert.True(t, stringslice.Contains([]string{"hello", "world"}, "hello"))
+	assert.False(t, stringslice.Contains([]string{"hello", "world"}, "john"))
+}
