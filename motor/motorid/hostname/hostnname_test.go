480b9a90aac6cd23f0740c4f680c28281c6e325c
Christoph Hartmann <chris@lollyrock.com>
2019-06-20 12:31:16 +0200

    refactor platform reference id detection

---
+++ motor/motorid/hostname/hostnname_test.go
@@ -0,0 +1,68 @@
+package hostname_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mondoo.io/mondoo/motor/motorid/hostname"
+	motor "go.mondoo.io/mondoo/motor/motoros"
+	"go.mondoo.io/mondoo/motor/motoros/mock/toml"
+	"go.mondoo.io/mondoo/motor/motoros/types"
+)
+
+func TestHostnameLinux(t *testing.T) {
+	trans, err := toml.New(&types.Endpoint{Backend: "mock", Path: "./testdata/hostname_linux.toml"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m, err := motor.New(trans)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hostame, err := hostname.Hostname(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "abefed34cc9c", hostame)
+}
+
+func TestHostnameWindows(t *testing.T) {
+	trans, err := toml.New(&types.Endpoint{Backend: "mock", Path: "./testdata/hostname_windows.toml"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m, err := motor.New(trans)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hostame, err := hostname.Hostname(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "WIN-ABCDEFGVHLD", hostame)
+}
+
+func TestHostnameMacos(t *testing.T) {
+	trans, err := toml.New(&types.Endpoint{Backend: "mock", Path: "./testdata/hostname_macos.toml"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m, err := motor.New(trans)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hostame, err := hostname.Hostname(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "moonshot.local", hostame)
+}
--- motor/motoros/resolver/resolver.go
+++ motor/motoros/resolver/resolver.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/rs/zerolog/log"
+	"go.mondoo.io/mondoo/motor/motorid/hostname"
 	motor "go.mondoo.io/mondoo/motor/motoros"
 	"go.mondoo.io/mondoo/motor/motoros/local"
 	"go.mondoo.io/mondoo/motor/motoros/mock"
@@ -15,73 +16,142 @@ import (
 	gossh "golang.org/x/crypto/ssh"
 )
 
-func New(endpoint *types.Endpoint) (*motor.Motor, string, error) {
-	trans, identifier, err := ResolveTransport(endpoint)
+func New(endpoint *types.Endpoint) (*motor.Motor, []string, error) {
+	m, identifier, err := ResolveTransport(endpoint)
 	if err != nil {
-		return nil, "", err
+		return nil, nil, err
 	}
-	m, err := motor.New(trans)
 	return m, identifier, err
 }
 
-func NewFromUrl(uri string) (*motor.Motor, string, error) {
+func NewFromUrl(uri string) (*motor.Motor, []string, error) {
 	t := &types.Endpoint{}
 	err := t.ParseFromURI(uri)
 	if err != nil {
-		return nil, "", err
+		return nil, nil, err
 	}
 	return New(t)
 }
 
-func NewWithUrlAndKey(uri string, key string) (*motor.Motor, string, error) {
+func NewWithUrlAndKey(uri string, key string) (*motor.Motor, []string, error) {
 	t := &types.Endpoint{
 		PrivateKeyPath: key,
 	}
 	err := t.ParseFromURI(uri)
 	if err != nil {
-		return nil, "", err
+		return nil, nil, err
 	}
 	return New(t)
 }
 
-func ResolveTransport(endpoint *types.Endpoint) (types.Transport, string, error) {
+func ResolveTransport(endpoint *types.Endpoint) (*motor.Motor, []string, error) {
+	var m *motor.Motor
+	var identifier []string
 	var err error
-	var identifier string
 
-	var trans types.Transport
 	switch endpoint.Backend {
 	case "mock":
 		log.Debug().Msg("connection> load mock transport")
-		trans, err = mock.New()
-	case "local", "nodejs":
+		trans, err := mock.New()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
+	case "nodejs":
+		log.Debug().Msg("connection> load nodejs transport")
+		// NOTE: while similar to local transport, the ids are completely different
+		trans, err := local.New()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
+	case "local":
 		log.Debug().Msg("connection> load local transport")
-		// TODO: we need to generate an artifact id
-		trans, err = local.New()
+		trans, err := local.New()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// NOTE: we need to be careful with hostname's since they are not required to be unique
+		hostname, hostErr := hostname.Hostname(m)
+		if hostErr == nil && len(hostname) > 0 {
+			identifier = append(identifier, "//platformid.api.mondoo.app/hostname/"+hostname)
+		}
 	case "tar":
 		log.Debug().Msg("connection> load tar transport")
 		// TODO: we need to generate an artifact id
-		trans, err = tar.New(endpoint)
+		trans, err := tar.New(endpoint)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
 	case "docker":
 		log.Debug().Msg("connection> load docker transport")
-		trans, identifier, err = ResolveDockerTransport(endpoint)
+		var id string
+		trans, id, err := ResolveDockerTransport(endpoint)
+		if err != nil {
+			return nil, nil, err
+		}
+		identifier = append(identifier, id)
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
 	case "ssh":
 		log.Debug().Msg("connection> load ssh transport")
-		sshTrans, sshErr := ssh.New(endpoint)
-		if sshErr == nil && sshTrans != nil {
-			fingerprint := gossh.FingerprintSHA256(sshTrans.HostKey)
-			fingerprint = strings.Replace(fingerprint, ":", "-", 1)
-			identifier = "//platformid.api.mondoo.app/runtime/ssh/hostkey/" + fingerprint
+		trans, sshErr := ssh.New(endpoint)
+		if sshErr != nil {
+			return nil, nil, err
 		}
-		trans = sshTrans
-		err = sshErr
+		if sshErr == nil && trans != nil {
+			fingerprint := gossh.FingerprintSHA256(trans.HostKey)
+			fingerprint = strings.Replace(fingerprint, ":", "-", 1)
+			identifier = append(identifier, "//platformid.api.mondoo.app/runtime/ssh/hostkey/"+fingerprint)
+		}
+
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
 	case "winrm":
 		log.Debug().Msg("connection> load winrm transport")
-		trans, err = winrm.New(endpoint)
+		trans, err := winrm.New(endpoint)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		m, err = motor.New(trans)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// NOTE: we need to be careful with hostname's since they are not required to be unique
+		hostname, hostErr := hostname.Hostname(m)
+		if hostErr == nil && len(hostname) > 0 {
+			identifier = append(identifier, "//platformid.api.mondoo.app/hostname/"+hostname)
+		}
 	case "":
-		return nil, "", errors.New("connection type is required, try `-t backend://` (docker://, local://, tar://, ssh://)")
+		return nil, nil, errors.New("connection type is required, try `-t backend://` (docker://, local://, tar://, ssh://)")
 	default:
-		return nil, "", errors.New("connection> unsupported backend, only docker://, local://, tar://, ssh:// are allowed'" + endpoint.Backend + "'")
+		return nil, nil, errors.New("connection> unsupported backend, only docker://, local://, tar://, ssh:// are allowed'" + endpoint.Backend + "'")
 	}
 
-	return trans, identifier, err
+	return m, identifier, err
 }
