2ad50139fd7b6a9844216c0fca66c7007fdac721
Christoph Hartmann <chris@lollyrock.com>
2019-07-10 13:53:41 +0200

    add support for aws ssm managed instances

---
+++ motor/motorcloud/aws/hybrid_instances.go
@@ -0,0 +1,120 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"go.mondoo.io/mondoo/motor/motorid/awsec2"
+	"go.mondoo.io/mondoo/nexus/assets"
+)
+
+func NewSSMManagedInstancesDiscovery(cfg aws.Config) (*SSMManagedInstances, error) {
+	return &SSMManagedInstances{config: cfg}, nil
+}
+
+type SSMManagedInstances struct {
+	config aws.Config
+}
+
+func (ssmi *SSMManagedInstances) List() ([]*assets.Asset, error) {
+	ctx := context.Background()
+	ssmsvc := ssm.New(ssmi.config)
+
+	identity, err := CheckIam(ssmi.config)
+	if err != nil {
+		return nil, err
+	}
+
+	account := *identity.Account
+
+	// check that all instances have ssm agent installed and are reachable
+	// it will return only those instances that are active in ssm
+	// e.g stopped instances are not reachable
+	platformFilter := string(ssm.InstanceInformationFilterKeyPlatformTypes)
+	resourceFilter := string(ssm.InstanceInformationFilterKeyResourceType)
+	isssmreq := ssmsvc.DescribeInstanceInformationRequest(&ssm.DescribeInstanceInformationInput{
+		Filters: []ssm.InstanceInformationStringFilter{
+			// TODO: we filter for linux instances until we support Windows
+			ssm.InstanceInformationStringFilter{Key: &platformFilter, Values: []string{string(ssm.PlatformTypeLinux)}},
+			// we only look for managed instanced
+			ssm.InstanceInformationStringFilter{Key: &resourceFilter, Values: []string{string(ssm.ResourceTypeManagedInstance)}},
+		},
+	})
+	isssmresp, err := isssmreq.Send(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not gather ssm information")
+	}
+
+	log.Debug().Msgf("%+v\n", *isssmresp)
+
+	instances := []*assets.Asset{}
+	for i := range isssmresp.InstanceInformationList {
+		instance := isssmresp.InstanceInformationList[i]
+
+		asset := &assets.Asset{
+			ReferenceIDs: []string{awsec2.MondooInstanceID(account, ssmi.config.Region, *instance.InstanceId)},
+			Name:         *instance.InstanceId,
+			Platform: &assets.Platform{
+				Kind:    assets.Kind_KIND_VIRTUAL_MACHINE,
+				Runtime: "aws ssm-managed",
+			},
+			// Connections: connections,
+			State:  mapSmmManagedPingStateCode(instance.PingStatus),
+			Labels: make(map[string]string),
+		}
+
+		tagreq := ssmsvc.ListTagsForResourceRequest(&ssm.ListTagsForResourceInput{
+			ResourceId:   instance.InstanceId,
+			ResourceType: ssm.ResourceTypeForTaggingManagedInstance,
+		})
+
+		tagresp, err := tagreq.Send(ctx)
+		if err != nil {
+			log.Warn().Err(err).Msg("could not gather ssm information")
+		} else if tagresp != nil {
+			log.Debug().Msgf("%+v\n", *tagresp)
+
+			for j := range tagresp.TagList {
+				tag := tagresp.TagList[j]
+				if tag.Key != nil {
+					key := *tag.Key
+					value := ""
+					if tag.Value != nil {
+						value = *tag.Value
+					}
+					asset.Labels[key] = value
+				}
+			}
+		}
+
+		// fetch aws specific metadata
+		asset.Labels["mondoo.app/region"] = ssmi.config.Region
+		if instance.InstanceId != nil {
+			asset.Labels["mondoo.app/instance"] = *instance.InstanceId
+		}
+		if instance.IPAddress != nil {
+			asset.Labels["mondoo.app/public-ip"] = *instance.IPAddress
+		}
+
+		instances = append(instances, asset)
+	}
+
+	return instances, nil
+}
+
+func mapSmmManagedPingStateCode(pingStatus ssm.PingStatus) assets.State {
+	switch pingStatus {
+	case ssm.PingStatusOnline:
+		return assets.State_STATE_RUNNING
+	case ssm.PingStatusConnectionLost:
+		return assets.State_STATE_PENDING
+	case ssm.PingStatusInactive:
+		return assets.State_STATE_STOPPED
+	default:
+		return assets.State_STATE_UNKNOWN
+	}
+}
