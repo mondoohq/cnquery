19a05b78bedf1f0c2e154bc269556f2b3dfb3138
Christoph Hartmann <chris@lollyrock.com>
2019-09-02 12:17:03 +0200

    improve ssh authentication
    - handle encrypted private keys
    - check if private key is already loaded via ssh-agent
    - show warn if a configured auth mechanism could not be initialized
    - always try remaining ssh auth mechanism

--- motor/motoros/ssh/session.go
+++ motor/motoros/ssh/session.go
@@ -1,10 +1,12 @@
 package ssh
 
 import (
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"net"
 
@@ -33,47 +35,91 @@ func sshClientConnection(hostconfig *types.Endpoint, hostKeyCallback ssh.HostKey
 	return ssh.Dial("tcp", fmt.Sprintf("%s:%s", hostconfig.Host, hostconfig.Port), sshConfig)
 }
 
+func authPrivateKey(privateKeyPath string, password string) (ssh.AuthMethod, error) {
+	log.Debug().Str("key", privateKeyPath).Msg("enabled ssh private key authentication")
+	if _, err := os.Stat(privateKeyPath); os.IsNotExist(err) {
+		return nil, errors.New("private key does not exist " + privateKeyPath)
+	}
+
+	pemBytes, err := ioutil.ReadFile(privateKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// check if the key is encrypted
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, errors.New("ssh: no key found")
+	}
+
+	var signer ssh.Signer
+	if strings.Contains(block.Headers["Proc-Type"], "ENCRYPTED") {
+		// we may want to support to parse password protected encrypted key
+		signer, err = ssh.ParsePrivateKeyWithPassphrase(pemBytes, []byte(password))
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// parse unencrypted key
+		signer, err = ssh.ParsePrivateKey(pemBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return ssh.PublicKeys(signer), nil
+}
+
+// hasAgentLoadedKey returns if the ssh agent has loaded the key file
+// This may not be 100% accurate. The key can be stored in multiple locations with the
+// same fingerprint. We cannot determine the fingerprint without decoding the encrypted
+// key, `ssh-keygen -lf /Users/chartmann/.ssh/id_rsa` seems to use the ssh agent to
+// determine the fingerprint without prompting for the password
+func hasAgentLoadedKey(list []*agent.Key, filename string) bool {
+	for i := range list {
+		if list[i].Comment == filename {
+			return true
+		}
+	}
+	return false
+}
+
 func authMethods(endpoint *types.Endpoint) ([]ssh.AuthMethod, error) {
 	auths := []ssh.AuthMethod{}
 
-	if endpoint.PrivateKeyPath != "" {
-		log.Debug().Str("key", endpoint.PrivateKeyPath).Msg("load private key")
-		if _, err := os.Stat(endpoint.PrivateKeyPath); os.IsNotExist(err) {
-			return auths, errors.New("private key does not exist " + endpoint.PrivateKeyPath)
-		}
+	sshAgentKeys := []*agent.Key{}
 
-		pemBytes, err := ioutil.ReadFile(endpoint.PrivateKeyPath)
-		if err != nil {
-			return auths, err
-		}
-
-		signer, err := ssh.ParsePrivateKey(pemBytes)
-		if err != nil {
-			return auths, err
-		}
-
-		auths = append(auths, ssh.PublicKeys(signer))
-	}
-
-	if endpoint.Password != "" {
-		log.Debug().Msg("use password authentication")
-		auths = append(auths, ssh.Password(endpoint.Password))
-	}
-
-	agentAuth := sshAgent()
-	if agentAuth != nil {
+	// enable ssh agent auth
+	if sshAgentConn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
 		log.Debug().Str("socket", os.Getenv("SSH_AUTH_SOCK")).Msg("enabled ssh agent authentication")
+		sshAgentClient := agent.NewClient(sshAgentConn)
+		agentAuth := ssh.PublicKeysCallback(sshAgentClient.Signers)
 		auths = append(auths, agentAuth)
+
+		// includes all loaded keys
+		list, err := sshAgentClient.List()
+		if err == nil {
+			sshAgentKeys = list
+		}
 	} else {
 		log.Debug().Msg("could not find valud ssh agent authentication")
 	}
 
-	return auths, nil
-}
-
-func sshAgent() ssh.AuthMethod {
-	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
-		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+	// use key auth, only load if the key was not found in ssh agent
+	if endpoint.PrivateKeyPath != "" && !hasAgentLoadedKey(sshAgentKeys, endpoint.PrivateKeyPath) {
+		priv, err := authPrivateKey(endpoint.PrivateKeyPath, endpoint.Password)
+		if err != nil {
+			log.Warn().Err(err).Str("key", endpoint.PrivateKeyPath).Msg("could not load private key, fallback to ssh agent")
+		} else {
+			auths = append(auths, priv)
+		}
 	}
-	return nil
+
+	// use password auth
+	if endpoint.Password != "" {
+		log.Debug().Msg("enabled ssh password authentication")
+		auths = append(auths, ssh.Password(endpoint.Password))
+	}
+
+	return auths, nil
 }
