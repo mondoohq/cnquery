c559d72e62cd58ef5defe2dba0c0cb7fad04fe33
Christoph Hartmann <chris@lollyrock.com>
2019-09-20 12:36:49 +0200

    detect esxi version

---
+++ motor/motoros/platform/parser_esxi_version_test.go
@@ -0,0 +1,21 @@
+package platform_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mondoo.io/mondoo/motor/motoros/platform"
+)
+
+func TestEsxiVersionParser(t *testing.T) {
+
+	m, err := platform.ParseEsxiRelease("VMware ESXi 6.7.0 build-13006603")
+	assert.Nil(t, err)
+
+	assert.Equal(t, "6.7.0 build-13006603", m)
+
+	m, err = platform.ParseEsxiRelease("VMware ESXi 6.7.0 build-13006603\n")
+	assert.Nil(t, err)
+
+	assert.Equal(t, "6.7.0 build-13006603", m)
+}
