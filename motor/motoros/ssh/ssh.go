82c796794c1657f242a113027e0f8b478da7b275
Christoph Hartmann <chris@lollyrock.com>
2019-11-07 20:32:14 +0100

    🎉 improve ssh connectivity
    - improve logging and output resolved endpoint
    - automatically fallback to scp if sftp has failed
    - more explicit resolution of the ssh_config option

--- motor/motoros/ssh/ssh.go
+++ motor/motoros/ssh/ssh.go
@@ -3,6 +3,7 @@ package ssh
 import (
 	"net"
 	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
 
@@ -21,26 +22,51 @@ import (
 func ReadSSHConfig(endpoint *types.Endpoint) *types.Endpoint {
 	host := endpoint.Host
 
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Debug().Err(err).Msg("Failed to determine user home directory")
+		return endpoint
+	}
+
+	sshUserConfigPath := filepath.Join(home, ".ssh", "config")
+	f, err := os.Open(sshUserConfigPath)
+	if err != nil {
+		log.Debug().Err(err).Str("file", sshUserConfigPath).Msg("Could not read ssh config")
+		return endpoint
+	}
+
+	cfg, err := ssh_config.Decode(f)
+	if err != nil {
+		log.Debug().Err(err).Str("file", sshUserConfigPath).Msg("Could not parse ssh config")
+		return endpoint
+	}
+
 	// optional step, tries to parse the ssh config to see if additional information
 	// is already available
-	hostname := ssh_config.Get(host, "HostName")
-	if len(hostname) > 0 {
+	hostname, err := cfg.Get(host, "HostName")
+	if err == nil && len(hostname) > 0 {
 		endpoint.Host = hostname
 	}
 
 	if len(endpoint.User) == 0 {
-		endpoint.User = ssh_config.Get(host, "User")
+		user, err := cfg.Get(host, "User")
+		if err == nil {
+			endpoint.User = user
+		}
 	}
 
 	if len(endpoint.Port) == 0 {
-		endpoint.Port = ssh_config.Get(host, "Port")
+		port, err := cfg.Get(host, "Port")
+		if err == nil {
+			endpoint.Port = port
+		}
 	}
 
 	if len(endpoint.PrivateKeyPath) == 0 {
-		entry := ssh_config.Get(host, "IdentityFile")
+		entry, err := cfg.Get(host, "IdentityFile")
 		// TODO: the ssh_config uses os/home but instead should be use go-homedir, could become a compile issue
 		// TODO: the problem is that the lib returns defaults and we cannot properly distingush
-		if ssh_config.Default("IdentityFile") != entry {
+		if err == nil && ssh_config.Default("IdentityFile") != entry {
 			// commonly ssh config included paths like ~
 			expanded, err := homedir.Expand(entry)
 			if err == nil {
@@ -95,31 +121,30 @@ func New(endpoint *types.Endpoint) (*SSHTransport, error) {
 		return nil
 	})
 	if err != nil {
+		log.Warn().Err(err).Str("transport", "ssh").Str("host", endpoint.Host).Str("port", endpoint.Port).Str("user", endpoint.User).Msg("could not establish ssh session")
 		return nil, err
 	}
 
-	log.Debug().Str("transport", "ssh").Msg("session established")
-
+	log.Debug().Str("transport", "ssh").Str("host", endpoint.Host).Str("port", endpoint.Port).Str("user", endpoint.User).Msg("ssh session established")
 	activateScp := false
 	if os.Getenv("MONDOO_SSH_SCP") == "on" {
 		activateScp = true
-		log.Info().Str("transport", "ssh").Msg("ssh uses scp (beta) instead of sftp for file transfer")
 	}
 
 	return &SSHTransport{
-		Endpoint:             endpoint,
-		SSHClient:            conn,
-		UseBetaScpFilesystem: activateScp,
-		HostKey:              hostkey,
+		Endpoint:         endpoint,
+		SSHClient:        conn,
+		UseScpFilesystem: activateScp,
+		HostKey:          hostkey,
 	}, nil
 }
 
 type SSHTransport struct {
-	Endpoint             *types.Endpoint
-	SSHClient            *ssh.Client
-	fs                   afero.Fs
-	UseBetaScpFilesystem bool
-	HostKey              ssh.PublicKey
+	Endpoint         *types.Endpoint
+	SSHClient        *ssh.Client
+	fs               afero.Fs
+	UseScpFilesystem bool
+	HostKey          ssh.PublicKey
 }
 
 func (t *SSHTransport) RunCommand(command string) (*types.Command, error) {
@@ -130,15 +155,22 @@ func (t *SSHTransport) RunCommand(command string) (*types.Command, error) {
 
 func (t *SSHTransport) FS() afero.Fs {
 	if t.fs == nil {
-		if t.UseBetaScpFilesystem {
-			t.fs = scp.NewFs(t.SSHClient)
-		} else {
+		// we always try to use sftp first (if scp is not user-enforced)
+		// and we also fallback to scp if sftp does not work
+		if !t.UseScpFilesystem {
 			fs, err := sftp.New(t.SSHClient)
 			if err != nil {
-				log.Error().Err(err).Msg("error during sftp initialization")
+				log.Error().Err(err).Msg("error during sftp initialization, enable fallback to scp")
+				// enable fallback
+				t.UseScpFilesystem = true
 			} else {
 				t.fs = fs
 			}
+		}
+
+		if t.UseScpFilesystem {
+			log.Info().Str("transport", "ssh").Msg("ssh uses scp (beta) instead of sftp for file transfer")
+			t.fs = scp.NewFs(t.SSHClient)
 		}
 	}
 	return t.fs
