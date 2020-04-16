910fabe0963c97ee59020c5e80bc715fd4564b33
Christoph Hartmann <chris@lollyrock.com>
2020-04-16 17:21:41 +0200

    kernel module resource

--- lumi/resources/kernel.go
+++ lumi/resources/kernel.go
@@ -1,9 +1,11 @@
 package resources
 
 import (
-	"errors"
+	"github.com/pkg/errors"
 
+	"github.com/rs/zerolog/log"
 	"go.mondoo.io/mondoo/lumi"
+	"go.mondoo.io/mondoo/lumi/resources/kernelmodule"
 	"go.mondoo.io/mondoo/lumi/resources/procfs"
 	"go.mondoo.io/mondoo/motor/motoros/fsutil"
 )
@@ -16,11 +18,10 @@ func (s *lumiKernel) init(args *lumi.Args) (*lumi.Args, error) {
 	}
 
 	supported := false
-	for _, f := range platform.Family {
-		if f == "linux" {
-			supported = true
-		}
+	if platform.IsFamily("linux") || platform.IsFamily("darwin") || platform.Name == "freebsd" {
+		supported = true
 	}
+
 	if supported == false {
 		return nil, errors.New("kernel resource is only supported for linux platforms")
 	}
@@ -29,7 +30,7 @@ func (s *lumiKernel) init(args *lumi.Args) (*lumi.Args, error) {
 }
 
 func (s *lumiKernel) id() (string, error) {
-	return "kernel_parameters", nil
+	return "kernel", nil
 }
 
 func (s *lumiKernel) GetParameters() (map[string]interface{}, error) {
@@ -62,4 +63,43 @@ func (s *lumiKernel) GetParameters() (map[string]interface{}, error) {
 	}
 
 	return res, nil
+}
+
+func (s *lumiKernel) GetModules() ([]interface{}, error) {
+	// find suitable kernel module manager
+	mm, err := kernelmodule.ResolveManager(s.Runtime.Motor)
+	if mm == nil || err != nil {
+		return nil, errors.Wrap(err, "Could not detect suiteable kernel module manager for platform")
+	}
+
+	// retrieve all kernel modules
+	kernelModules, err := mm.List()
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not retrieve kernel module list for platform")
+	}
+	log.Debug().Int("modules", len(kernelModules)).Msg("lumi[kernel.modules]> modules")
+
+	// create lumi kernel module entry resources for each entry
+	moduleEntries := make([]interface{}, len(kernelModules))
+	for i, kernelModule := range kernelModules {
+
+		// set init arguments for the lumi package resource
+		args := make(lumi.Args)
+		args["name"] = kernelModule.Name
+		args["size"] = kernelModule.Size
+
+		e, err := newKernel_module(s.Runtime, &args)
+		if err != nil {
+			log.Error().Err(err).Str("module", kernelModule.Name).Msg("lumi[kernel.modules]> could not create kernel module resource")
+			continue
+		}
+		moduleEntries[i] = e.(Kernel_module)
+	}
+
+	// return the kernel modules as new entries
+	return moduleEntries, nil
+}
+
+func (s *lumiKernel_module) id() (string, error) {
+	return s.Name()
 }
---
+++ lumi/resources/kernelmodule/parser.go
@@ -0,0 +1,96 @@
+package kernelmodule
+
+import (
+	"bufio"
+	"io"
+	"regexp"
+	"strings"
+)
+
+func ParseLsmod(r io.Reader) []*KernelModule {
+	res := []*KernelModule{}
+
+	lsmodEntry := regexp.MustCompile(`^(\S+)\s+(\S+)\s+(\S+)\s*(\S*)$`)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		m := lsmodEntry.FindStringSubmatch(line)
+		if len(m) == 5 {
+			if m[1] == "Module" {
+				continue
+			}
+
+			res = append(res, &KernelModule{
+				Name:   strings.TrimSpace(m[1]),
+				Size:   strings.TrimSpace(m[2]),
+				UsedBy: strings.TrimSpace(m[3]),
+			})
+		}
+	}
+
+	return res
+}
+
+func ParseLinuxProcModules(r io.Reader) []*KernelModule {
+	res := []*KernelModule{}
+
+	procModulesEntry := regexp.MustCompile(`^(\S+)\s(\S+)\s(\S+)\s(.*)$`)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		m := procModulesEntry.FindStringSubmatch(line)
+		if len(m) == 5 {
+			res = append(res, &KernelModule{
+				Name:   strings.TrimSpace(m[1]),
+				Size:   strings.TrimSpace(m[2]),
+				UsedBy: strings.TrimSpace(m[3]),
+			})
+		}
+	}
+
+	return res
+}
+
+func ParseKldstat(r io.Reader) []*KernelModule {
+	res := []*KernelModule{}
+
+	lsmodEntry := regexp.MustCompile(`^\s+(\S+)\s+(\S+)\s+(\S+)\s*(\S*)\s*(\S*)$`)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		m := lsmodEntry.FindStringSubmatch(line)
+		if len(m) == 6 {
+			res = append(res, &KernelModule{
+				Name:   strings.TrimSpace(m[5]),
+				Size:   strings.TrimSpace(m[4]),
+				UsedBy: strings.TrimSpace(m[2]),
+			})
+		}
+	}
+
+	return res
+}
+
+func ParseKextstat(r io.Reader) []*KernelModule {
+	res := []*KernelModule{}
+
+	lsmodEntry := regexp.MustCompile(`^\s+(\S+)\s+(\S+)\s+(\S+)\s*(\S*)\s*(\S*)\s*(\S*)`)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		m := lsmodEntry.FindStringSubmatch(line)
+		if len(m) == 7 {
+			res = append(res, &KernelModule{
+				Name:   strings.TrimSpace(m[6]),
+				Size:   strings.TrimSpace(m[4]),
+				UsedBy: strings.TrimSpace(m[2]),
+			})
+		}
+	}
+
+	return res
+}
