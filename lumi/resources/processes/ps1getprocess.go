93953ca57cf6f8a1101469d16a69d95759fe57c3
Christoph Hartmann <chris@lollyrock.com>
2020-04-16 11:45:38 +0200

    processes resource

---
+++ lumi/resources/processes/ps1getprocess.go
@@ -0,0 +1,176 @@
+package processes
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/rs/zerolog/log"
+	motor "go.mondoo.io/mondoo/motor/motoros"
+)
+
+const (
+	Ps1GetProcess = "Get-Process -IncludeUserName | Select-Object Name, Description, Id, PriorityClass, PM, NPM, CPU, VirtualMemorySize, Responding, SessionId, StartTime, TotalProcessorTime, UserName, Path | ConvertTo-Json"
+)
+
+// Get-Process -IncludeUserName | Select-Object -Property *
+// UserName                   : NT AUTHORITY\SYSTEM
+// Name                       : winlogon
+// Id                         : 584
+// PriorityClass              : High
+// FileVersion                : 10.0.17763.1 (WinBuild.160101.0800)
+// HandleCount                : 234
+// WorkingSet                 : 10424320
+// PagedMemorySize            : 2641920
+// PrivateMemorySize          : 2641920
+// VirtualMemorySize          : 100098048
+// TotalProcessorTime         : 00:00:00.0156250
+// SI                         : 1
+// Handles                    : 234
+// VM                         : 2203418320896
+// WS                         : 10424320
+// PM                         : 2641920
+// NPM                        : 11392
+// Path                       : C:\windows\system32\winlogon.exe
+// Company                    : Microsoft Corporation
+// CPU                        : 0.015625
+// ProductVersion             : 10.0.17763.1
+// Description                : Windows Logon Application
+// Product                    : Microsoft® Windows® Operating System
+// __NounName                 : Process
+// BasePriority               : 13
+// ExitCode                   :
+// HasExited                  : False
+// ExitTime                   :
+// Handle                     : 3492
+// SafeHandle                 : Microsoft.Win32.SafeHandles.SafeProcessHandle
+// MachineName                : .
+// MainWindowHandle           : 0
+// MainWindowTitle            :
+// MainModule                 : System.Diagnostics.ProcessModule (winlogon.exe)
+// MaxWorkingSet              : 1413120
+// MinWorkingSet              : 204800
+// Modules                    : {System.Diagnostics.ProcessModule (winlogon.exe), System.Diagnostics.ProcessModule (ntdll.dll), System.Diagnostics.ProcessModule (KERNEL32.DLL), System.Diagnostics.ProcessModule (KERNELBASE.dll)...}
+// NonpagedSystemMemorySize   : 11392
+// NonpagedSystemMemorySize64 : 11392
+// PagedMemorySize64          : 2641920
+// PagedSystemMemorySize      : 135128
+// PagedSystemMemorySize64    : 135128
+// PeakPagedMemorySize        : 3715072
+// PeakPagedMemorySize64      : 3715072
+// PeakWorkingSet             : 11091968
+// PeakWorkingSet64           : 11091968
+// PeakVirtualMemorySize      : 104349696
+// PeakVirtualMemorySize64    : 2203422572544
+// PriorityBoostEnabled       : True
+// PrivateMemorySize64        : 2641920
+// PrivilegedProcessorTime    : 00:00:00.0156250
+// ProcessName                : winlogon
+// ProcessorAffinity          : 1
+// Responding                 : True
+// SessionId                  : 1
+// StartInfo                  : System.Diagnostics.ProcessStartInfo
+// StartTime                  : 4/16/2020 8:24:41 AM
+// SynchronizingObject        :
+// Threads                    : {588, 924, 2788}
+// UserProcessorTime          : 00:00:00
+// VirtualMemorySize64        : 2203418320896
+// EnableRaisingEvents        : False
+// StandardInput              :
+// StandardOutput             :
+// StandardError              :
+// WorkingSet64               : 10424320
+// Site                       :
+// Container                  :
+type WindowsProcess struct {
+	ID                 int64
+	Name               string
+	Description        string
+	PriorityClass      int
+	PM                 int64
+	NPM                int64
+	CPU                float64
+	VirtualMemorySize  int64
+	Responding         bool
+	SessionId          int
+	StartTime          string
+	TotalProcessorTime WindowsTotalProcessorTime
+	UserName           string
+	Path               string
+}
+
+type WindowsTotalProcessorTime struct {
+	Ticks             int
+	Days              int
+	Hours             int
+	Milliseconds      int
+	Minutes           int
+	Seconds           int
+	TotalDays         float64
+	TotalHours        float64
+	TotalMilliseconds float64
+	TotalMinutes      float64
+	TotalSeconds      float64
+}
+
+func (p WindowsProcess) ToOSProcess() *OSProcess {
+	return &OSProcess{
+		Pid:     p.ID,
+		Command: p.Path,
+		// Uid:     p.UserName,
+	}
+}
+
+func ParseWindowsProcesses(r io.Reader) ([]WindowsProcess, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var processes []WindowsProcess
+	err = json.Unmarshal(data, &processes)
+	if err != nil {
+		return nil, err
+	}
+
+	return processes, nil
+}
+
+type WindowsProcessManager struct {
+	motor *motor.Motor
+}
+
+func (wpm *WindowsProcessManager) Name() string {
+	return "Windows Process Manager"
+}
+
+func (wpm *WindowsProcessManager) List() ([]*OSProcess, error) {
+	// TODO: wrap in powershell
+	c, err := wpm.motor.Transport.RunCommand(Ps1GetProcess)
+	if err != nil {
+		return nil, fmt.Errorf("processes> could not run command")
+	}
+
+	entries, err := ParseWindowsProcesses(c.Stdout)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Debug().Int("processes", len(entries)).Msg("found processes")
+
+	var ps []*OSProcess
+	for i := range entries {
+		ps = append(ps, entries[i].ToOSProcess())
+	}
+	return ps, nil
+}
+
+func (wpm *WindowsProcessManager) Exists(pid int64) (bool, error) {
+	return false, errors.New("not implemented")
+}
+
+func (wpm *WindowsProcessManager) Process(pid int64) (*OSProcess, error) {
+	return nil, errors.New("not implemented")
+}
