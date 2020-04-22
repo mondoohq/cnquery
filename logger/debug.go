7bdedfde4a1ad64f1bca22826956fc214e64c894
Dominik Richter <dominik.richter@gmail.com>
2020-04-22 14:59:53 -0700

    ✨ shell parse --command

--- logger/debug.go
+++ logger/debug.go
@@ -8,12 +8,17 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// DebugJSON prints a prettified JSON of the data to CLI
+// DebugJSON prints a prettified JSON of the data to CLI on debug mode
 func DebugJSON(obj interface{}) {
 	if !log.Debug().Enabled() {
 		return
 	}
 
+	fmt.Fprintln(os.Stderr, PrettyJSON(obj))
+}
+
+// PrettyJSON turns any object into its prettified json representation
+func PrettyJSON(obj interface{}) string {
 	s, _ := prettyjson.Marshal(obj)
-	fmt.Fprintln(os.Stderr, string(s))
+	return string(s)
 }
