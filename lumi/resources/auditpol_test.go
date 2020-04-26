892e306485ddf4a590c69aeaf0639ea63da6099b
Dominik Richter <dominik.richter@gmail.com>
2020-04-26 06:05:25 -0700

    add auditpol block test

--- lumi/resources/auditpol_test.go
+++ lumi/resources/auditpol_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.mondoo.io/mondoo/llx"
 )
 
 func TestResource_Auditpol(t *testing.T) {
@@ -40,11 +41,13 @@ func TestResource_Auditpol(t *testing.T) {
 		assert.Equal(t, false, res[0].Data.Value)
 	})
 
-	// // unclear how I should test that this is failing?
-	// t.Run("test a specific secpol systemaccess entry", func(t *testing.T) {
-	// 	res := testQuery(t, "auditpol.where(subcategory == 'Application Group Management').list { inclusionsetting == 'Success and Failure'}")
-	// 	assert.NotEmpty(t, res)
-	// 	assert.Empty(t, res[0].Result().Error)
-	// 	assert.Equal(t, false, res[0].Data.Value)
-	// })
+	t.Run("test a specific secpol systemaccess entry", func(t *testing.T) {
+		res := testQuery(t, "auditpol.where(subcategory == 'Application Group Management').list { inclusionsetting == 'Success and Failure'}")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		listResults := res[0].Data.Value.([]interface{})
+		blockRes := listResults[0].(map[string]interface{})
+		qres := blockRes["iMFZaUEieakYM9UF7rZamtZd6a/JDV9Bo0xa4laKyH4Ksjiw1R+tXflOQaf15p9ARoRFfo9XuaFC+rzw2vFVrg=="].(*llx.RawData)
+		assert.Equal(t, false, qres.Value)
+	})
 }
