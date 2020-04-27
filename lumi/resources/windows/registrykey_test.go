ac0f8bd552e480627c7822f0e83137d2e3441ca2
Christoph Hartmann <chris@lollyrock.com>
2020-04-27 15:40:37 +0200

    lumi registrykey resource

---
+++ lumi/resources/windows/registrykey_test.go
@@ -0,0 +1,34 @@
+package windows
+
+import (
+	"os"
+	"testing"
+
+	"github.com/tj/assert"
+)
+
+func TestWindowsRegistryKeyItemParser(t *testing.T) {
+	data, err := os.Open("./testdata/registrykey.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	items, err := ParseRegistryKeyItems(data)
+	assert.Nil(t, err)
+	assert.Equal(t, 7, len(items))
+	assert.Equal(t, "ConsentPromptBehaviorAdmin", items[0].Key)
+	assert.Equal(t, 4, items[0].Value.Kind)
+	assert.Equal(t, int64(5), items[0].Value.Number)
+	assert.Equal(t, "5", items[0].GetValue())
+}
+
+func TestWindowsRegistryKeyChildParser(t *testing.T) {
+	data, err := os.Open("./testdata/registrykey-children.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	items, err := ParseRegistryKeyChildren(data)
+	assert.Nil(t, err)
+	assert.Equal(t, 5, len(items))
+}
