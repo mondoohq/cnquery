be846fafee7fddfd6bdc8bd1b1f6c13c5b2bb8b1
Dominik Richter <dominik.richter@gmail.com>
2020-05-05 17:21:34 -0700

    🐛 fix truthy checks across all data

--- llx/rawdata_test.go
+++ llx/rawdata_test.go
@@ -26,3 +26,36 @@ func TestRawData_String(t *testing.T) {
 		assert.Equal(t, tests[i].res, tests[i].data.String())
 	}
 }
+
+func TestTruthy(t *testing.T) {
+	tests := []struct {
+		data *RawData
+		res  bool
+	}{
+		{BoolTrue, true},
+		{BoolFalse, false},
+		{IntData(0), false},
+		{IntData(123), true},
+		{FloatData(0), false},
+		{FloatData(1.23), true},
+		{StringData(""), false},
+		{StringData("b"), true},
+		{RegexData(""), false},
+		{RegexData("r"), true},
+		{ArrayData([]interface{}{}, types.Any), true},
+		{ArrayData([]interface{}{false}, types.Bool), false},
+		{ArrayData([]interface{}{true}, types.Bool), true},
+		{MapData(map[string]interface{}{}, types.Any), true},
+		{MapData(map[string]interface{}{"a": false}, types.Bool), false},
+		{MapData(map[string]interface{}{"a": true}, types.Bool), true},
+		{ResourceData(nil, "something"), true},
+	}
+
+	for i := range tests {
+		o := tests[i]
+		t.Run(o.data.String(), func(t *testing.T) {
+			is, _ := o.data.IsTruthy()
+			assert.Equal(t, o.res, is)
+		})
+	}
+}
