92995e9b3cfebd84c5a4e56e213489d0405d5e6f
Dominik Richter <dominik.richter@gmail.com>
2020-05-10 14:31:53 -0700

    ✨ be smart about tabs in ini-style files with space separation

--- lumi/resources/parsers/ini_test.go
+++ lumi/resources/parsers/ini_test.go
@@ -62,3 +62,69 @@ func TestIni(t *testing.T) {
 		})
 	}
 }
+
+func TestIni_SpaceDelim(t *testing.T) {
+	tests := []struct {
+		title   string
+		content string
+		res     map[string]interface{}
+	}{
+		{
+			"simple assignment",
+			"key value",
+			map[string]interface{}{
+				"": map[string]interface{}{
+					"key": "value",
+				},
+			},
+		},
+		{
+			"no assignment",
+			"keykey",
+			map[string]interface{}{
+				"": map[string]interface{}{
+					"keykey": "",
+				},
+			},
+		},
+		{
+			"newline comment",
+			"key\n# comment\n  # more comment\n\t# and one more\nvalue",
+			map[string]interface{}{
+				"": map[string]interface{}{
+					"key":   "",
+					"value": "",
+				},
+			},
+		},
+		{
+			"groups",
+			"key\n[some group]\nkey2 value",
+			map[string]interface{}{
+				"": map[string]interface{}{
+					"key": "",
+				},
+				"some group": map[string]interface{}{
+					"key2": "value",
+				},
+			},
+		},
+		{
+			"tabs",
+			"key\tvalue",
+			map[string]interface{}{
+				"": map[string]interface{}{
+					"key": "value",
+				},
+			},
+		},
+	}
+
+	for i := range tests {
+		cur := tests[i]
+		t.Run(cur.title, func(t *testing.T) {
+			res := ParseIni(cur.content, " ")
+			assert.Equal(t, cur.res, res.Fields)
+		})
+	}
+}
