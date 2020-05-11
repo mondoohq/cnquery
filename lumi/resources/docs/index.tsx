27b6d858a8f5bfad0ca49372612e2f5ffc9c04a6
Dominik Richter <dominik.richter@gmail.com>
2020-05-11 14:58:50 -0700

    🌠 lr docs

---
+++ lumi/resources/docs/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import 'whatwg-fetch';
+import { LrDocs } from './src/lrdocs/lrdocs';
+
+const theme = {
+  colors: {
+    primary: "#ff61a9",
+    secondary: "#61fff9",
+    fg: "#fff",
+    bg: "#444",
+    bgDarker: "#2b2b2b",
+  },
+};
+
+const Background = styled.div`
+  background: ${props => props.theme.colors.bg};
+  color: ${props => props.theme.colors.fg};
+  font-family: "Inconsolata", monospace;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+type AppState = {}
+class App extends React.Component<{}, AppState> {
+  state: AppState = {}
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        {this.renderThemed.bind(this)()}
+      </ThemeProvider>
+    )
+  }
+
+  renderThemed() {
+    return (
+      <Background>
+        <div style={{height: "100%", overflow: "auto"}}>
+          <LrDocs />
+        </div>
+      </Background>
+    )
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root')
+)
