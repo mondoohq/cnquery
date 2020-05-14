e11b3504f9f55ba9caeda4c8cf9cfef6b27cbdc1
Christoph Hartmann <chris@lollyrock.com>
2020-05-14 15:12:39 +0200

    add yaml.path resource

---
+++ lumi/resources/yaml.go
@@ -0,0 +1,79 @@
+package resources
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+
+	"k8s.io/client-go/util/jsonpath"
+	"sigs.k8s.io/yaml"
+)
+
+func (l *lumiYamlPath) id() (string, error) {
+	filepath, err := l.Filepath()
+	if err != nil {
+		return "", err
+	}
+
+	jsonpath, err := l.Jsonpath()
+	if err != nil {
+		return "", err
+	}
+
+	var id strings.Builder
+	id.WriteString(filepath)
+	if len(jsonpath) > 0 {
+		id.WriteString(" -jsonpath ")
+		id.WriteString(jsonpath)
+	}
+	return id.String(), nil
+}
+
+func (l *lumiYamlPath) GetResult() (string, error) {
+	// get file content
+	filepath, err := l.Filepath()
+	if err != nil {
+		return "", err
+	}
+
+	// get jsonpath value
+	jp, err := l.Jsonpath()
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: I could not get this running with lumi file resource, the content was never returned
+	f, err := l.Runtime.Motor.Transport.File(filepath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+
+	// unmarshal file content
+	var jsonInterface interface{}
+	err = yaml.Unmarshal([]byte(data), &jsonInterface)
+	if err != nil {
+		return "", err
+	}
+
+	// parse json path expression
+	j := jsonpath.New("lumiyamlpath")
+	j.AllowMissingKeys(false)
+	err = j.Parse(jp)
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	err = j.Execute(buf, jsonInterface)
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
---
+++ lumi/resources/yaml_test.go
@@ -0,0 +1,43 @@
+package resources
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/util/jsonpath"
+	"sigs.k8s.io/yaml"
+)
+
+func TestResource_Yaml(t *testing.T) {
+	t.Run("extract content", func(t *testing.T) {
+		res := testQuery(t, "yaml.path(filepath: '/root/pod.yaml', jsonpath: '{.kind}').result")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, "Pod", res[0].Data.Value)
+	})
+}
+
+func TestJsonPath(t *testing.T) {
+	mContent, err := ioutil.ReadFile("./testdata/pod.yaml")
+	require.NoError(t, err)
+
+	// load data
+	var jsonInterface interface{}
+	err = yaml.Unmarshal(mContent, &jsonInterface)
+	require.NoError(t, err)
+
+	// parse json path expression
+	j := jsonpath.New("jsonpath")
+	j.AllowMissingKeys(false)
+	err = j.Parse("{.kind}")
+	require.NoError(t, err)
+
+	buf := new(bytes.Buffer)
+	err = j.Execute(buf, jsonInterface)
+	require.NoError(t, err)
+	jsonpathResult := buf.String()
+	assert.Equal(t, "Pod", jsonpathResult)
+}
