51ddfc62cde83d08937e33a454f7978a75a7784e
Dominik Richter <dominik.richter@gmail.com>
2020-05-23 14:04:56 -0700

    ⭐ overhaul mondoo shell logger
    solves #2

--- logger/log.go
+++ logger/log.go
@@ -16,7 +16,7 @@ import (
 var Debug bool
 
 func init() {
-	Set(false, true)
+	Set("debug")
 	// uses cli logger by default
 	CliNoColorLogger()
 }
@@ -26,31 +26,42 @@ func SetWriter(w io.Writer) {
 	log.Logger = log.Output(w)
 }
 
-func UseJsonLogging() {
+// UseJSONLogging for global logger
+func UseJSONLogging() {
 	log.Logger = zerolog.New(os.Stderr).With().Timestamp().Logger()
 }
 
+// CliLogger sets the global logger to the console logger with color
 func CliLogger() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
 
+// CliNoColorLogger sets the global logger to the console logger without color
 func CliNoColorLogger() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, NoColor: true})
 }
 
 // Set will set up the logger
-func Set(prod bool, debug bool) {
-	Debug = debug
-	if debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	} else {
+func Set(level string) {
+	switch level {
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "info":
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	default:
+		log.Error().Msg("unknown log level: " + level)
 	}
 }
 
 // InitTestEnv will set all log configurations for a test environment
 // verbose and colorful
 func InitTestEnv() {
-	Set(false, true)
+	Set("debug")
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
