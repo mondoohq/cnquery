5f1924c98b40ea7e5a1dff27a99cb1b42eadc49c
Christoph Hartmann <chris@lollyrock.com>
2020-05-24 15:48:44 +0200

    🐛 case-insensitive registry key properties

--- lumi/resources/registrykey.go
+++ lumi/resources/registrykey.go
@@ -1,8 +1,11 @@
 package resources
 
 import (
+	"errors"
 	"strings"
 
+	"github.com/rs/zerolog/log"
+	"go.mondoo.io/mondoo/lumi"
 	"go.mondoo.io/mondoo/lumi/resources/powershell"
 	"go.mondoo.io/mondoo/lumi/resources/windows"
 )
@@ -96,4 +99,87 @@ func (k *lumiRegistrykey) GetChildren() ([]interface{}, error) {
 	}
 
 	return res, nil
+}
+
+func (p *lumiRegistrykeyProperty) id() (string, error) {
+	path, err := p.Path()
+	if err != nil {
+		return "", err
+	}
+
+	name, err := p.Name()
+	if err != nil {
+		return "", err
+	}
+
+	return path + " - " + name, nil
+}
+
+func (p *lumiRegistrykeyProperty) init(args *lumi.Args) (*lumi.Args, RegistrykeyProperty, error) {
+	log.Debug().Msg("reg key property")
+	pathRaw := (*args)["path"]
+	if pathRaw == nil {
+		return args, nil, nil
+	}
+
+	path, ok := pathRaw.(string)
+	if !ok {
+		return args, nil, nil
+	}
+
+	nameRaw := (*args)["name"]
+	if nameRaw == nil {
+		return args, nil, nil
+	}
+
+	name, ok := nameRaw.(string)
+	if !ok {
+		return args, nil, nil
+	}
+
+	// create resource here, but do not use it yet
+	obj, err := p.Runtime.CreateResource("registrykey", "path", path)
+	if err != nil {
+		return nil, nil, err
+	}
+	registryKey := obj.(Registrykey)
+
+	log.Debug().Msg("reg key property, parent exists")
+	exists, err := registryKey.Exists()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// set default values
+	(*args)["value"] = ""
+	(*args)["exists"] = false
+
+	// path exists
+	if exists {
+		log.Debug().Msg("reg key property, get properties")
+		properties, err := registryKey.Properties()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// search for property
+		for k := range properties {
+			if strings.ToLower(k) == strings.ToLower(name) {
+				(*args)["exists"] = true
+				(*args)["value"] = properties[k].(string)
+				break
+			}
+		}
+	}
+	return args, nil, nil
+}
+
+func (p *lumiRegistrykeyProperty) GetExists() (bool, error) {
+	// NOTE: will not be called since its set in the constructor
+	return false, errors.New("not implemented")
+}
+
+func (p *lumiRegistrykeyProperty) GetValue() (string, error) {
+	// NOTE: will not be called since its set in the constructor
+	return "", errors.New("not implemented")
 }
