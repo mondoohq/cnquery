f469f48c7ac5a584c31e65b5c31898d7ffbe6580
Christoph Hartmann <chris@lollyrock.com>
2020-06-24 16:52:56 +0200

    🌟 add bsd support for stat helper command

--- motor/motoros/statutil/stat.go
+++ motor/motoros/statutil/stat.go
@@ -39,11 +39,50 @@ func New(cmdRunner CommandRunner) *statHelper {
 // Stat helper implements the stat command for various unix systems
 // since this helper is used by transports itself, we cannot rely on the
 // platform detection mechanism (since it may rely on stat to determine the system)
+// therefore we implement the minimum required to detect the right stat parser
 type statHelper struct {
 	commandRunner CommandRunner
+	detected      bool
+	isunix        bool
+}
+
+var bsdunix = map[string]bool{
+	"openbsd":   true,
+	"dragonfly": true,
+	"freebsd":   true,
+	"netbsd":    true,
 }
 
 func (s *statHelper) Stat(name string) (os.FileInfo, error) {
+	// detect stat version
+	if !s.detected {
+		cmd, err := s.commandRunner.RunCommand("uname -s")
+		if err != nil {
+			log.Debug().Err(err).Str("file", name).Msg("could not stat the file")
+		}
+
+		data, err := ioutil.ReadAll(cmd.Stdout)
+		if err != nil {
+			return nil, err
+		}
+
+		// only switch to unix if we properly detected it, otherwise fallback to linux
+		val := strings.ToLower(strings.TrimSpace(string(data)))
+
+		isunix, ok := bsdunix[val]
+		if ok && isunix {
+			s.isunix = true
+		}
+		s.detected = true
+	}
+
+	if s.isunix {
+		return s.unix(name)
+	}
+	return s.linux(name)
+}
+
+func (s *statHelper) linux(name string) (os.FileInfo, error) {
 	lstat := "-L"
 	format := "--printf"
 	path := ShellEscape(name)
@@ -93,6 +132,74 @@ func (s *statHelper) Stat(name string) (os.FileInfo, error) {
 	}
 
 	mask, err := strconv.ParseUint(statsData[1], 16, 32)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not stat "+name)
+	}
+
+	mode := os.FileMode(uint32(mask) & 07777)
+
+	mtime, err := strconv.ParseInt(statsData[4], 10, 64)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not stat "+name)
+	}
+
+	return &types.FileInfo{
+		FSize:    int64(size),
+		FMode:    mode,
+		FIsDir:   mode.IsDir(),
+		FModTime: time.Unix(mtime, 0),
+		Uid:      uid,
+		Gid:      gid,
+	}, nil
+}
+
+func (s *statHelper) unix(name string) (os.FileInfo, error) {
+	lstat := "-L"
+	format := "-f"
+	path := ShellEscape(name)
+
+	var sb strings.Builder
+	sb.WriteString("stat ")
+	sb.WriteString(lstat)
+	sb.WriteString(" ")
+	sb.WriteString(format)
+	sb.WriteString(" '%z:%p:%u:%g:%a:%m'")
+	sb.WriteString(" ")
+	sb.WriteString(path)
+
+	cmd, err := s.commandRunner.RunCommand(sb.String())
+	if err != nil {
+		log.Debug().Err(err).Str("file", name).Msg("could not stat the file")
+	}
+
+	data, err := ioutil.ReadAll(cmd.Stdout)
+	if err != nil {
+		return nil, err
+	}
+
+	statsData := strings.Split(string(data), ":")
+	if len(statsData) != 6 {
+		log.Error().Str("name", name).Msg("could parse stat response")
+		return nil, os.ErrNotExist
+	}
+
+	size, err := strconv.Atoi(statsData[0])
+	if err != nil {
+		return nil, errors.Wrap(err, "could not stat "+name)
+	}
+
+	uid, err := strconv.ParseInt(statsData[2], 10, 64)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not stat "+name)
+	}
+
+	gid, err := strconv.ParseInt(statsData[3], 10, 64)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not stat "+name)
+	}
+
+	// NOTE: the base is 8 instead of 16 on linux systems
+	mask, err := strconv.ParseUint(statsData[1], 8, 32)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not stat "+name)
 	}
--- motor/motoros/statutil/stat_test.go
+++ motor/motoros/statutil/stat_test.go
@@ -28,3 +28,22 @@ func TestLinuxStatCmd(t *testing.T) {
 	assert.Equal(t, os.FileMode(0x180), fi.Mode())
 	assert.Equal(t, time.Unix(1590420240, 0), fi.ModTime())
 }
+
+func TestOpenbsdStatCmd(t *testing.T) {
+	filepath, _ := filepath.Abs("./testdata/openbsd.toml")
+	trans, err := mock.NewFromToml(&types.Endpoint{Backend: "mock", Path: filepath})
+	require.NoError(t, err)
+
+	statHelper := New(trans)
+
+	// get file stats
+	fi, err := statHelper.Stat("/etc/ssh/sshd_config")
+	require.NoError(t, err)
+
+	assert.Equal(t, int64(2259), fi.Size())
+	assert.Equal(t, false, fi.IsDir())
+	assert.Equal(t, "-rw-r--r--", fi.Mode().String())
+	assert.Equal(t, time.Unix(1592996018, 0), fi.ModTime())
+	assert.Equal(t, int64(0), fi.Sys().(*types.FileInfo).Uid)
+	assert.Equal(t, int64(0), fi.Sys().(*types.FileInfo).Gid)
+}
