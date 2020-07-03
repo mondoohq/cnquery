988b65b7098b4c6feb5bcf6e9327719e2ddad541
Christoph Hartmann <chris@lollyrock.com>
2020-07-03 12:24:27 +0200

    🌟 parse sshd config cipher, kexs and macs

--- lumi/resources/ssh_test.go
+++ lumi/resources/ssh_test.go
@@ -24,4 +24,25 @@ func TestResource_SSHD(t *testing.T) {
 		assert.Empty(t, res[0].Result().Error)
 		assert.Equal(t, "yes", res[0].Data.Value)
 	})
+
+	t.Run("parse ciphers", func(t *testing.T) {
+		res := testQuery(t, "sshd.config.ciphers")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, []interface{}{"chacha20-poly1305@openssh.com", "aes256-gcm@openssh.com", "aes128-gcm@openssh.com", "aes256-ctr", "aes192-ctr", "aes128-ctr"}, res[0].Data.Value)
+	})
+
+	t.Run("parse macs", func(t *testing.T) {
+		res := testQuery(t, "sshd.config.macs")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, []interface{}{"hmac-sha2-512-etm@openssh.com", "hmac-sha2-256-etm@openssh.com", "umac-128-etm@openssh.com", "hmac-sha2-512", "hmac-sha2-256"}, res[0].Data.Value)
+	})
+
+	t.Run("parse kexs", func(t *testing.T) {
+		res := testQuery(t, "sshd.config.kexs")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, []interface{}{"curve25519-sha256@libssh.org", "diffie-hellman-group-exchange-sha256"}, res[0].Data.Value)
+	})
 }
