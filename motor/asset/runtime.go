e7766512716a402baa3ef887b9954f5497f8371b
Christoph Hartmann <chris@lollyrock.com>
2020-07-27 14:27:00 +0200

    improve policy asset output

--- motor/asset/runtime.go
+++ motor/asset/runtime.go
@@ -15,3 +15,30 @@ const (
 	RUNTIME_VSPHERE_HOSTS    = "vsphere host" // esxi instances
 	RUNTIME_VSPHERE_VM       = "vsphere vm"   // vms running on esxi
 )
+
+func (x Kind) Name() string {
+	switch x {
+	case Kind_KIND_VIRTUAL_MACHINE_IMAGE:
+		return "virtual machine image"
+	case Kind_KIND_CONTAINER_IMAGE:
+		return "container image"
+	case Kind_KIND_CODE:
+		return "code"
+	case Kind_KIND_PACKAGE:
+		return "package"
+	case Kind_KIND_VIRTUAL_MACHINE:
+		return "virtual machine"
+	case Kind_KIND_CONTAINER:
+		return "container"
+	case Kind_KIND_PROCESS:
+		return "process"
+	case Kind_KIND_API:
+		return "api"
+	case Kind_KIND_BARE_METAL:
+		return "bare metal"
+	case Kind_KIND_UNKNOWN:
+		fallthrough
+	default:
+		return "unknown"
+	}
+}
