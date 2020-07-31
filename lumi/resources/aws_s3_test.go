eb8243b0f36f259142386cb52590ca390281179e
Christoph Hartmann <chris@lollyrock.com>
2020-07-31 15:03:04 +0200

    improve aws s3 resources to support buckets in multiple regions

--- lumi/resources/aws_s3_test.go
+++ lumi/resources/aws_s3_test.go
@@ -2,6 +2,7 @@ package resources_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func TestCloudtrail(t *testing.T) {
+func TestS3(t *testing.T) {
 
 	cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("mondoo-inc"))
 	if err != nil {
@@ -24,14 +25,59 @@ func TestCloudtrail(t *testing.T) {
 	require.NoError(t, err)
 	ctx := context.Background()
 
-	res, err := svc.ListBucketsRequest(&s3.ListBucketsInput{}).Send(ctx)
+	// res, err := svc.ListBucketsRequest(&s3.ListBucketsInput{}).Send(ctx)
+	// require.NoError(t, err)
+	// fmt.Printf("%v", res)
 
-	svc.GetBucketTaggingRequest(&s3.GetBucketTaggingInput{}).Send(ctx)
-	svc.GetBucketLocationRequest(&s3.GetBucketLocationInput{}).Send(ctx)
-	svc.GetBucketAclRequest(&s3.GetBucketAclInput{}).Send(ctx)
-	svc.GetBucketEncryptionRequest(&s3.GetBucketEncryptionInput{}).Send(ctx)
-	svc.GetBucketLocationRequest(&s3.GetBucketLocationInput{}).Send(ctx)
-	svc.GetBucketPolicyRequest(&s3.GetBucketPolicyInput{}).Send(ctx)
-	svc.GetBucketCorsRequest(&s3.GetBucketCorsInput{}).Send(ctx)
-	svc.GetBucketMetricsConfigurationRequest(&s3.GetBucketMetricsConfigurationInput{}).Send(ctx)
+	bucket := "multi-reagion-trail-test"
+	// // policy
+	// policy, err := svc.GetBucketPolicyRequest(&s3.GetBucketPolicyInput{
+	// 	Bucket: &bucket,
+	// }).Send(ctx)
+	// require.NoError(t, err)
+	// fmt.Printf("%v", policy)
+
+	// // tags
+	// tags, err := svc.GetBucketTaggingRequest(&s3.GetBucketTaggingInput{
+	// 	Bucket: &bucket,
+	// }).Send(ctx)
+	// // aws code NoSuchTagSetError in case no tag is set
+	// // require.NoError(t, err)
+	// fmt.Printf("%v", tags)
+
+	// cors
+	// cors, err := svc.GetBucketCorsRequest(&s3.GetBucketCorsInput{
+	// 	Bucket: &bucket,
+	// }).Send(ctx)
+	// // require.NoError(t, err)
+	// // if a bucket has no cors configuration, we get "NoSuchCORSConfiguration"
+	// fmt.Printf("%v", cors)
+
+	// acl
+	acl, err := svc.GetBucketAclRequest(&s3.GetBucketAclInput{
+		Bucket: &bucket,
+	}).Send(ctx)
+	// require.NoError(t, err)
+	fmt.Printf("%v", acl)
+
+	// encryption, err := svc.GetBucketEncryptionRequest(&s3.GetBucketEncryptionInput{
+	// 	Bucket: &bucket,
+	// }).Send(ctx)
+	// // require.NoError(t, err), returns "ServerSideEncryptionConfigurationNotFoundError" if not set
+	// fmt.Printf("%v", encryption)
+
+	location, err := svc.GetBucketLocationRequest(&s3.GetBucketLocationInput{
+		Bucket: &bucket,
+	}).Send(ctx)
+	// require.NoError(t, err)
+	// returns the region of the location
+	// us-east-1 returns ""
+	// https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetBucketLocation.html#API_GetBucketLocation_ResponseSyntax
+	fmt.Printf("%v", location)
+
+	metrics, err := svc.GetBucketMetricsConfigurationRequest(&s3.GetBucketMetricsConfigurationInput{
+		Bucket: &bucket,
+	}).Send(ctx)
+	// require.NoError(t, err)
+	fmt.Printf("%v", metrics)
 }
