fedfb7eea4598afbe857f711f4afb6fd1ef5d2d2
Christoph Hartmann <chris@lollyrock.com>
2020-08-04 11:56:32 +0200

    play with azure services

--- motor/discovery/azure/compute_test.go
+++ motor/discovery/azure/compute_test.go
@@ -4,13 +4,14 @@ package azure_test
 // 	"context"
 // 	"testing"
 
+// 	"github.com/stretchr/testify/assert"
 // 	"github.com/stretchr/testify/require"
 // 	"go.mondoo.io/mondoo/motor/discovery/azure"
-// 	"github.com/stretchr/testify/assert"
 // )
 
 // func TestAzureInstanceFetch(t *testing.T) {
-//  subscriptionid := "/subscriptions/abc/resourceGroups/name"
+// 	subscriptionid := "/subscriptions/123456/resourceGroups/name"
+
 // 	client, err := azure.NewCompute(subscriptionid)
 // 	require.NoError(t, err)
 
