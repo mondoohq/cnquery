ec29c7a4ebdf94516ccb8d5648add1151b4eb36a
Christoph Hartmann <chris@lollyrock.com>
2020-08-06 18:10:45 +0200

    add azure storage blob container

--- lumi/resources/azure_rm_storage.go
+++ lumi/resources/azure_rm_storage.go
@@ -71,3 +71,69 @@ func (a *lumiAzurerm) GetStorageAccounts() ([]interface{}, error) {
 func (a *lumiAzurermStorageAccount) id() (string, error) {
 	return a.Id()
 }
+
+func (a *lumiAzurermStorageAccount) GetContainers() ([]interface{}, error) {
+	at, err := azuretransport(a.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	// id is a azure resource od
+	id, err := a.Id()
+	if err != nil {
+		return nil, err
+	}
+
+	resourceID, err := at.ParseResourceID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := resourceID.Component("storageAccounts")
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	authorizer, err := at.Authorizer()
+	if err != nil {
+		return nil, err
+	}
+
+	client := storage.NewBlobContainersClient(resourceID.SubscriptionID)
+	client.Authorizer = authorizer
+
+	container, err := client.List(ctx, resourceID.ResourceGroup, account, "", "", "")
+	if err != nil {
+		return nil, err
+	}
+
+	res := []interface{}{}
+
+	for i := range container.Values() {
+		entry := container.Values()[i]
+
+		properties, err := jsonToDict(entry.ContainerProperties)
+		if err != nil {
+			return nil, err
+		}
+
+		lumiAzure, err := a.Runtime.CreateResource("azurerm.storage.container",
+			"id", toString(entry.ID),
+			"name", toString(entry.Name),
+			"etag", toString(entry.Etag),
+			"type", toString(entry.Type),
+			"properties", properties,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiAzure)
+	}
+
+	return res, nil
+}
+
+func (a *lumiAzurermStorageContainer) id() (string, error) {
+	return a.Id()
+}
