8b8585bdc392a6c285c2682b9aaf0ec547775b05
Christoph Hartmann <chris@lollyrock.com>
2020-08-09 17:54:08 +0200

    improve azure resources

--- motor/transports/azure/client.go
+++ motor/transports/azure/client.go
@@ -2,7 +2,6 @@ package azure
 
 import (
 	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 )
 
@@ -10,8 +9,8 @@ func (t *Transport) Authorizer() (autorest.Authorizer, error) {
 	return auth.NewAuthorizerFromCLI()
 }
 
-func (t *Transport) GraphAuthorizer() (autorest.Authorizer, error) {
-	return auth.NewAuthorizerFromCLIWithResource(azure.PublicCloud.ResourceIdentifiers.Graph)
+func (t *Transport) AuthorizerWithAudience(audience string) (autorest.Authorizer, error) {
+	return auth.NewAuthorizerFromCLIWithResource(audience)
 }
 
 func (t *Transport) ParseResourceID(id string) (*ResourceID, error) {
