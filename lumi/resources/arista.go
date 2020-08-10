c21e3ec9be6cba601ebf9cbdcf00ec83839ecf5b
Christoph Hartmann <chris@lollyrock.com>
2020-08-10 23:01:43 +0200

    🌟 lumi support for arista eos interface, interface status, version

--- lumi/resources/arista.go
+++ lumi/resources/arista.go
@@ -4,37 +4,38 @@ import (
 	"errors"
 
 	"go.mondoo.io/mondoo/lumi/resources/arista"
+	"go.mondoo.io/mondoo/motor/transports"
 	arista_transport "go.mondoo.io/mondoo/motor/transports/arista"
 )
 
-func (a *lumiAristaeos) id() (string, error) {
-	return "aristaeos", nil // TODO: consider encoding the host to ensure we can handle multiple instances
-}
-
-func (v *lumiAristaeosIpinterface) id() (string, error) {
-	return v.Name()
-}
-
-func (a *lumiAristaeos) getClientInstance() (*arista.Eos, error) {
-	at, ok := a.Runtime.Motor.Transport.(*arista_transport.Transport)
+func aristaClientInstance(t transports.Transport) (*arista.Eos, error) {
+	at, ok := t.(*arista_transport.Transport)
 	if !ok {
-		return nil, errors.New("aristaeos resource is not supported on this transport")
+		return nil, errors.New("arista.eos resource is not supported on this transport")
 	}
 
 	eos := arista.NewEos(at.Client())
 	return eos, nil
 }
 
-func (a *lumiAristaeos) GetRunningConfig() (string, error) {
-	eos, err := a.getClientInstance()
+func (a *lumiAristaEos) id() (string, error) {
+	return "arista.eos", nil
+}
+
+func (v *lumiAristaEosIpinterface) id() (string, error) {
+	return v.Name()
+}
+
+func (a *lumiAristaEos) GetRunningConfig() (string, error) {
+	eos, err := aristaClientInstance(a.Runtime.Motor.Transport)
 	if err != nil {
 		return "", err
 	}
 	return eos.RunningConfig(), nil
 }
 
-func (a *lumiAristaeos) GetSystemConfig() (map[string]interface{}, error) {
-	eos, err := a.getClientInstance()
+func (a *lumiAristaEos) GetSystemConfig() (map[string]interface{}, error) {
+	eos, err := aristaClientInstance(a.Runtime.Motor.Transport)
 	if err != nil {
 		return nil, err
 	}
@@ -48,8 +49,8 @@ func (a *lumiAristaeos) GetSystemConfig() (map[string]interface{}, error) {
 	return res, nil
 }
 
-func (a *lumiAristaeos) GetIpInterfaces() ([]interface{}, error) {
-	eos, err := a.getClientInstance()
+func (a *lumiAristaEos) GetIpInterfaces() ([]interface{}, error) {
+	eos, err := aristaClientInstance(a.Runtime.Motor.Transport)
 	if err != nil {
 		return nil, err
 	}
@@ -57,7 +58,7 @@ func (a *lumiAristaeos) GetIpInterfaces() ([]interface{}, error) {
 
 	lumiIfaces := make([]interface{}, len(ifaces))
 	for i, iface := range ifaces {
-		lumiService, err := a.Runtime.CreateResource("aristaeos.ipinterface",
+		lumiService, err := a.Runtime.CreateResource("arista.eos.ipinterface",
 			"name", iface.Name(),
 			"address", iface.Address(),
 			"mtu", iface.Mtu(),
@@ -69,4 +70,98 @@ func (a *lumiAristaeos) GetIpInterfaces() ([]interface{}, error) {
 	}
 
 	return lumiIfaces, nil
+}
+
+func (a *lumiAristaEos) GetVersion() (map[string]interface{}, error) {
+	eos, err := aristaClientInstance(a.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+	version := eos.ShowVersion()
+	return jsonToDict(version)
+}
+
+func (a *lumiAristaEos) GetInterfaces() ([]interface{}, error) {
+	eos, err := aristaClientInstance(a.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+	ifaces := eos.ShowInterface()
+
+	lumiIfaces := []interface{}{}
+	for k := range ifaces.Interfaces {
+		iface := ifaces.Interfaces[k]
+
+		address := []interface{}{}
+		for i := range iface.InterfaceAddress {
+			ifaceAddress, err := jsonToDict(iface.InterfaceAddress[i])
+			if err != nil {
+				return nil, err
+			}
+			address = append(address, ifaceAddress)
+		}
+
+		counters, err := jsonToDict(iface.InterfaceCounters)
+		if err != nil {
+			return nil, err
+		}
+
+		statistics, err := jsonToDict(iface.InterfaceStatistics)
+		if err != nil {
+			return nil, err
+		}
+
+		lumiIface, err := a.Runtime.CreateResource("arista.eos.interface",
+			"name", iface.Name,
+			"bandwidth", int64(iface.Bandwidth),
+			"burnedInAddress", iface.BurnedInAddress,
+			"description", iface.Description,
+			"forwardingModel", iface.ForwardingModel,
+			"hardware", iface.Hardware,
+			"interfaceAddress", address,
+			"interfaceCounters", counters,
+			"interfaceMembership", iface.InterfaceMembership,
+			"interfaceStatistics", statistics,
+			"interfaceStatus", iface.InterfaceStatus,
+			"l2Mtu", int64(iface.L2Mtu),
+			"lastStatusChangeTimestamp", int64(iface.LastStatusChangeTimestamp),
+			"lineProtocolStatus", iface.LineProtocolStatus,
+			"mtu", int64(iface.Mtu),
+			"physicalAddress", iface.PhysicalAddress,
+		)
+		if err != nil {
+			return nil, err
+		}
+		lumiIfaces = append(lumiIfaces, lumiIface)
+
+	}
+	return lumiIfaces, nil
+}
+
+func (a *lumiAristaEosInterface) id() (string, error) {
+	return a.Name()
+}
+
+func (a *lumiAristaEosInterface) GetStatus() (map[string]interface{}, error) {
+	eos, err := aristaClientInstance(a.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	ifaceName, err := a.Name()
+	if err != nil {
+		return nil, err
+	}
+
+	status, err := eos.ShowInterfacesStatus()
+	if err != nil {
+		return nil, err
+	}
+
+	entry, ok := status[ifaceName]
+	if !ok {
+		return nil, nil
+	}
+
+	return jsonToDict(entry)
 }
--- lumi/resources/arista/arista.go
+++ lumi/resources/arista/arista.go
@@ -23,6 +23,16 @@ func (eos *Eos) SystemConfig() map[string]string {
 	return sys.Get()
 }
 
+func (eos *Eos) ShowVersion() module.ShowVersion {
+	show := module.Show(eos.node)
+	return show.ShowVersion()
+}
+
+func (eos *Eos) ShowInterface() module.ShowInterface {
+	show := module.Show(eos.node)
+	return show.ShowInterfaces()
+}
+
 func (eos *Eos) IPInterfaces() []module.IPInterfaceConfig {
 	ifaceModule := module.IPInterface(eos.node)
 	interfaces := ifaceModule.GetAll()
@@ -33,4 +43,42 @@ func (eos *Eos) IPInterfaces() []module.IPInterfaceConfig {
 		res = append(res, iface)
 	}
 	return res
+}
+
+type showInterfacesStatus struct {
+	InterfaceStatuses map[string]InterfaceStatus
+}
+
+func (s *showInterfacesStatus) GetCmd() string {
+	return "show interfaces status"
+}
+
+type InterfaceStatus struct {
+	Bandwidth           int64    `json:"bandwidth"`
+	InterfaceType       string   `json:"interfaceType"`
+	Description         string   `json:"description"`
+	AutoNegotiateActive bool     `json:"autoNegotiateActive"`
+	Duplex              string   `json:"duplex"`
+	LinkStatus          string   `json:"linkStatus"`
+	LineProtocolStatus  string   `json:"lineProtocolStatus"`
+	VlanInformation     vlanInfo `json:"vlanInformation"`
+}
+
+type vlanInfo struct {
+	InterfaceMode            string `json:"interfaceMode"`
+	VlanID                   int64  `json:"vlanId"`
+	InterfaceForwardingModel string `json:"interfaceForwardingModel"`
+}
+
+func (eos *Eos) ShowInterfacesStatus() (map[string]InterfaceStatus, error) {
+	shIntRsp := &showInterfacesStatus{}
+
+	handle, _ := eos.node.GetHandle("json")
+	handle.AddCommand(shIntRsp)
+
+	if err := handle.Call(); err != nil {
+		return nil, err
+	}
+
+	return shIntRsp.InterfaceStatuses, nil
 }
