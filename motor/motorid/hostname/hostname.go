a3b3719d0c80556ce040816678b124c3c557f8a1
Christoph Hartmann <chris@lollyrock.com>
2020-08-27 15:27:25 +0200

    🐛 harmonize os hostname detection and use `hostname` command on windows

--- motor/motorid/hostname/hostname.go
+++ motor/motorid/hostname/hostname.go
@@ -11,31 +11,23 @@ import (
 
 func Hostname(t transports.Transport, p *platform.Platform) (string, error) {
 	var hostname string
-	switch {
-	case p.IsFamily(platform.FAMILY_UNIX):
-		cmd, err := t.RunCommand("hostname")
-		if err != nil {
-			return hostname, err
-		}
-		data, err := ioutil.ReadAll(cmd.Stdout)
-		if err != nil {
-			return hostname, err
-		}
 
-		hostname = string(data)
-	case p.IsFamily(platform.FAMILY_WINDOWS):
-		cmd, err := t.RunCommand("powershell -c \"$env:computername\"")
-		if err != nil {
-			return hostname, err
-		}
-		data, err := ioutil.ReadAll(cmd.Stdout)
-		if err != nil {
-			return hostname, err
-		}
-		hostname = string(data)
-	default:
+	if !p.IsFamily(platform.FAMILY_UNIX) && !p.IsFamily(platform.FAMILY_WINDOWS) {
 		return hostname, errors.New("your platform is not supported by hostname resource")
 	}
+
+	// NOTE: hostname command works more reliable than t.RunCommand("powershell -c \"$env:computername\"")
+	// since it will return a non-zero exit code.
+	cmd, err := t.RunCommand("hostname")
+	if err != nil {
+		return hostname, err
+	}
+	data, err := ioutil.ReadAll(cmd.Stdout)
+	if err != nil {
+		return hostname, err
+	}
+
+	hostname = string(data)
 
 	return strings.TrimSpace(hostname), nil
 }
