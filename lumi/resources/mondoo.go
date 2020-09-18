7277a00f639dc3929d2e16b89f6fdcb92bbc3e1c
Christoph Hartmann <chris@lollyrock.com>
2020-09-18 20:08:16 +0200

    🎉 expose job environment as part of the mondoo resource

--- lumi/resources/mondoo.go
+++ lumi/resources/mondoo.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 
 	"go.mondoo.io/mondoo"
+	"go.mondoo.io/mondoo/cli/execruntime"
 )
 
 func (s *lumiMondoo) id() (string, error) {
@@ -26,4 +27,21 @@ func (s *lumiMondoo) GetResources() ([]interface{}, error) {
 		res[i] = s
 	}
 	return res, nil
+}
+
+type runtimeEnv struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (s *lumiMondoo) GetJobEnvironment() (map[string]interface{}, error) {
+	// get the local agent runtime information
+	ciEnv := execruntime.Detect()
+
+	re := &runtimeEnv{
+		ID:   ciEnv.Namespace,
+		Name: ciEnv.Name,
+	}
+
+	return jsonToDict(re)
 }
