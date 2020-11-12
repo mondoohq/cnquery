8b0f4c0efa7a123b8a89a39f5884bfa9810dbef6
Dominik Richter <dominik.richter@gmail.com>
2020-11-12 00:09:39 -0800

    🔨 logger.DebugDumpJSON

--- logger/debug.go
+++ logger/debug.go
@@ -1,11 +1,14 @@
 package logger
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/hokaccha/go-prettyjson"
 	"github.com/rs/zerolog/log"
+	"go.mondoo.io/mondoo"
 )
 
 // DebugJSON prints a prettified JSON of the data to CLI on debug mode
@@ -29,4 +32,22 @@ func TraceJSON(obj interface{}) {
 func PrettyJSON(obj interface{}) string {
 	s, _ := prettyjson.Marshal(obj)
 	return string(s)
+}
+
+// DebugDumpJSON will write a JSON dump if the Debug mode is active and
+// the DumpLocal prefix is defined.
+func DebugDumpJSON(name string, obj interface{}) {
+	if !log.Debug().Enabled() || mondoo.DumpLocal == "" {
+		return
+	}
+
+	raw, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Error().Err(err).Msg("failed to dump JSON for resolved policy")
+	}
+
+	err = ioutil.WriteFile(mondoo.DumpLocal+name+".json", []byte(raw), 0644)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to dump JSON for resolved policy")
+	}
 }
