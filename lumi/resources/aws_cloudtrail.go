2892b5a57c829756debfdbcdb6dec32e34ccf10d
Victoria Jeffrey <vj@email.com>
2020-12-10 17:15:32 -0700

    🧹 reorganize aws cloudwatch resources; add multi-region and pagination; enable metric tests in aws policy

--- lumi/resources/aws_cloudtrail.go
+++ lumi/resources/aws_cloudtrail.go
@@ -2,16 +2,12 @@ package resources
 
 import (
 	"context"
-	"strconv"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudtrail"
-	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
-	"github.com/aws/aws-sdk-go-v2/service/sns"
 	"github.com/cockroachdb/errors"
-	"github.com/rs/zerolog/log"
 )
 
 func (t *lumiAwsCloudtrail) id() (string, error) {
@@ -24,12 +20,14 @@ func (t *lumiAwsCloudtrail) GetTrails() ([]interface{}, error) {
 		return nil, err
 	}
 
+	// no need to call for each region, this api gets all trails across all regions (afaict)
 	svc := at.Cloudtrail("")
 	ctx := context.Background()
 
+	// no pagination required
 	trailsResp, err := svc.DescribeTrailsRequest(&cloudtrail.DescribeTrailsInput{}).Send(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "could not gather aws iam virtual-mfa-devices")
+		return nil, errors.Wrap(err, "could not gather aws cloudtrail trails")
 	}
 
 	res := []interface{}{}
@@ -47,7 +45,6 @@ func (t *lumiAwsCloudtrail) GetTrails() ([]interface{}, error) {
 			}
 			s3Bucket = lumiAwsS3Bucket
 		}
-
 		lumiAwsCloudtrailTrail, err := t.Runtime.CreateResource("aws.cloudtrail.trail",
 			"arn", toString(trail.TrailARN),
 			"name", toString(trail.Name),
@@ -62,6 +59,7 @@ func (t *lumiAwsCloudtrail) GetTrails() ([]interface{}, error) {
 			"cloudWatchLogsLogGroupArn", toString(trail.CloudWatchLogsLogGroupArn),
 			// TODO: link to watch logs grou
 			"cloudWatchLogsRoleArn", toString(trail.CloudWatchLogsRoleArn),
+			"region", toString(trail.HomeRegion),
 		)
 		if err != nil {
 			return nil, err
@@ -78,12 +76,16 @@ func (t *lumiAwsCloudtrailTrail) id() (string, error) {
 }
 
 func (t *lumiAwsCloudtrailTrail) GetStatus() (interface{}, error) {
+	regionValue, err := t.Region()
+	if err != nil {
+		return nil, err
+	}
 	at, err := awstransport(t.Runtime.Motor.Transport)
 	if err != nil {
 		return nil, err
 	}
 
-	svc := at.Cloudtrail("")
+	svc := at.Cloudtrail(regionValue)
 	ctx := context.Background()
 
 	arnValue, err := t.Arn()
@@ -91,6 +93,7 @@ func (t *lumiAwsCloudtrailTrail) GetStatus() (interface{}, error) {
 		return nil, err
 	}
 
+	// no pagination required
 	trailstatus, err := svc.GetTrailStatusRequest(&cloudtrail.GetTrailStatusInput{
 		Name: &arnValue,
 	}).Send(ctx)
@@ -98,84 +101,15 @@ func (t *lumiAwsCloudtrailTrail) GetStatus() (interface{}, error) {
 		return nil, err
 	}
 
-	lumiAwsCloudtrailTrailStatus, err := t.Runtime.CreateResource("aws.cloudtrail.trailstatus",
-		"arn", arnValue,
-		"isLogging", toBool(trailstatus.IsLogging),
-		"latestCloudWatchLogsDeliveryError", toString(trailstatus.LatestCloudWatchLogsDeliveryError),
-		"latestCloudWatchLogsDeliveryTime", trailstatus.LatestCloudWatchLogsDeliveryTime,
-		"latestDeliveryError", toString(trailstatus.LatestDeliveryError),
-		"latestDeliveryTime", trailstatus.LatestDeliveryTime,
-		"latestDigestDeliveryError", toString(trailstatus.LatestDigestDeliveryError),
-		"latestDigestDeliveryTime", trailstatus.LatestDigestDeliveryTime,
-		"latestNotificationError", toString(trailstatus.LatestNotificationError),
-		"latestNotificationTime", trailstatus.LatestNotificationTime,
-		"startLoggingTime", trailstatus.StartLoggingTime,
-		"stopLoggingTime", trailstatus.StopLoggingTime,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return lumiAwsCloudtrailTrailStatus, nil
+	return jsonToDict(trailstatus)
 }
 
-func (t *lumiAwsCloudtrailTrailstatus) id() (string, error) {
-	return t.Arn()
-}
-
-func (t *lumiAwsCloudtrailTrail) GetMgmtEvents() (interface{}, error) {
-	at, err := awstransport(t.Runtime.Motor.Transport)
-	if err != nil {
-		return nil, err
-	}
-
-	svc := at.Cloudtrail("")
-	ctx := context.Background()
-
-	arnValue, err := t.Arn()
-	if err != nil {
-		return nil, err
-	}
-
-	trailmgmtevents, err := svc.GetEventSelectorsRequest(&cloudtrail.GetEventSelectorsInput{
-		TrailName: &arnValue,
-	}).Send(ctx)
-	if err != nil {
-		return nil, err
-	}
-	eventSelectors := []interface{}{}
-	for i, trailmgmtevent := range trailmgmtevents.EventSelectors {
-		stringState, err := cloudtrail.ReadWriteType.MarshalValue(trailmgmtevent.ReadWriteType)
-		if err != nil {
-			return nil, err
-		}
-
-		lumiAwsTrailEventSelectors, err := t.Runtime.CreateResource("aws.cloudtrail.trailmgmtevents.eventselectors",
-			"id", arnValue+"/"+strconv.Itoa(i),
-			"includeManagementEvents", toBool(trailmgmtevent.IncludeManagementEvents),
-			"readWriteType", stringState,
-		)
-		eventSelectors = append(eventSelectors, lumiAwsTrailEventSelectors)
-	}
-
-	lumiAwsCloudtrailTrailMgmtEvents, err := t.Runtime.CreateResource("aws.cloudtrail.trailmgmtevents",
-		"arn", toString(trailmgmtevents.TrailARN),
-		"eventSelectors", eventSelectors,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return lumiAwsCloudtrailTrailMgmtEvents, nil
-}
-
-func (t *lumiAwsCloudtrailTrailmgmtevents) id() (string, error) {
-	return t.Arn()
-}
-func (t *lumiAwsCloudtrailTrailmgmteventsEventselectors) id() (string, error) {
-	return t.Id()
-}
-
-func (t *lumiAwsCloudtrailTrail) GetMetricFilters() (interface{}, error) {
+func (t *lumiAwsCloudtrailTrail) GetLogGroup() (interface{}, error) {
 	arnValue, err := t.CloudWatchLogsLogGroupArn()
+	if err != nil {
+		return nil, err
+	}
+
 	if err != nil || len(arnValue) < 6 {
 		return nil, errors.Wrap(err, "unable to parse cloud watch log group arn")
 	}
@@ -192,159 +126,58 @@ func (t *lumiAwsCloudtrailTrail) GetMetricFilters() (interface{}, error) {
 	ctx := context.Background()
 
 	nextToken := aws.String("no_token_to_start_with")
-	params := &cloudwatchlogs.DescribeMetricFiltersInput{LogGroupName: &groupName}
-	metricFilters := []interface{}{}
+	params := &cloudwatchlogs.DescribeLogGroupsInput{
+		LogGroupNamePrefix: &groupName,
+	}
 	for nextToken != nil {
-		metricsResp, err := svc.DescribeMetricFiltersRequest(params).Send(ctx)
+		logGroups, err := svc.DescribeLogGroupsRequest(params).Send(ctx)
 		if err != nil {
-			return nil, errors.Wrap(err, "could not gather log metric filters")
+			return nil, errors.Wrap(err, "could not gather aws cloudwatch log groups")
 		}
-		nextToken = metricsResp.NextToken
-		if metricsResp.NextToken != nil {
+		nextToken = logGroups.NextToken
+		if logGroups.NextToken != nil {
 			params.NextToken = nextToken
 		}
-		for _, m := range metricsResp.MetricFilters {
-			// metric transformations is an array, but everything seems to imply it will only
-			// ever have 0 or 1 items in the array (e.g. CIS says "grab the metric name associated with filter")
-			// i tried to add more metric transformations to it but could not :shrug:
-			// so here we get the data if there is the expected 1 item..
-			metricName, metricValue := "", ""
-			if len(m.MetricTransformations) == 1 {
-				metricName = toString(m.MetricTransformations[0].MetricName)
-				metricValue = toString(m.MetricTransformations[0].MetricValue)
-			} else {
-				log.Warn().Msg("unexpected length found for metric values array. not including metric name in result")
-			}
-
-			lumiAwsTrailMetricFilters, err := t.Runtime.CreateResource("aws.cloudtrail.trailmetricfilters.metricfilters",
-				"id", groupName+"/"+region+"/"+toString(m.FilterName),
-				"filterName", toString(m.FilterName),
-				"filterPattern", toString(m.FilterPattern),
-				"metricName", metricName,
-				"metricValue", metricValue,
-			)
-			if err != nil {
-				return nil, err
-			}
-			metricFilters = append(metricFilters, lumiAwsTrailMetricFilters)
-		}
-	}
-
-	lumiAwsCloudtrailTrailMetricFilters, err := t.Runtime.CreateResource("aws.cloudtrail.trailmetricfilters",
-		"id", groupName+"/"+region,
-		"metrics", metricFilters,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return lumiAwsCloudtrailTrailMetricFilters, nil
-}
-
-func (t *lumiAwsCloudtrailTrailmetricfilters) id() (string, error) {
-	return t.Id()
-}
-
-func (t *lumiAwsCloudtrailTrailmetricfiltersMetricfilters) id() (string, error) {
-	return t.Id()
-}
-
-func (t *lumiAwsCloudtrailTrailmetricfiltersMetricfilters) GetAlarmsForMetric() ([]interface{}, error) {
-	metricName, err := t.MetricName()
-	if err != nil {
-		return nil, err
-	}
-	at, err := awstransport(t.Runtime.Motor.Transport)
-	if err != nil {
-		return nil, err
-	}
-	svc := at.Cloudwatch("")
-	ctx := context.Background()
-
-	namespace := "CloudTrailMetrics"
-	params := &cloudwatch.DescribeAlarmsForMetricInput{
-		MetricName: &metricName,
-		Namespace:  &namespace,
-	}
-	res := []interface{}{}
-	alarmsForMetric, err := svc.DescribeAlarmsForMetricRequest(params).Send(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not gather aws cloudwatch alarms")
-	}
-
-	for _, alarm := range alarmsForMetric.MetricAlarms {
-		actions := []interface{}{}
-		// alarm actions is a list of sns topic arns
-		for _, topicArn := range alarm.AlarmActions {
-			// to make this information valuable to the user, we call the sns list subscriptions by topic api
-			// with each sns topic arn and create an object that includes the topic arn,
-			//sub arn, and whetheror not that sub arn is valid
-			subscriptionArns, err := t.listSnsSubscriptionsByTopic(topicArn)
-			if err != nil {
-				return nil, err
-			}
-			for _, subscriptionArn := range subscriptionArns {
-				// the existence of a valid subscription arn("arn:aws:sns:<region>:<aws_account_number>:<SnsTopicName>:<SubscriptionID>"
-				// suggests the subscription is active
-				isValid := isValidSubscription(subscriptionArn)
-				lumiAwsCloudwatchAlarmAction, err := t.Runtime.CreateResource("aws.cloudtrail.alarmactions",
-					"snsTopicArn", topicArn,
-					"subscriptionArn", subscriptionArn,
-					"validSubscription", isValid,
+		for _, loggroup := range logGroups.LogGroups {
+			if toString(loggroup.Arn) == arnValue {
+				lumiLogGroup, err := t.Runtime.CreateResource("aws.cloudwatch.loggroup",
+					"arn", toString(loggroup.Arn),
+					"name", toString(loggroup.LogGroupName),
 				)
 				if err != nil {
 					return nil, err
 				}
-				actions = append(actions, lumiAwsCloudwatchAlarmAction)
+				return lumiLogGroup, nil
 			}
 		}
-		lumiAwsCloudwatchAlarm, err := t.Runtime.CreateResource("aws.cloudtrail.trailmetricfilters.metricfilters.alarms",
-			"arn", toString(alarm.AlarmArn),
-			"name", toString(alarm.AlarmName),
-			"alarmActions", actions,
-		)
-		if err != nil {
-			return nil, err
-		}
-		res = append(res, lumiAwsCloudwatchAlarm)
-
 	}
-	return res, nil
+	return nil, errors.New("unable to find matching log group")
 }
 
-func (t *lumiAwsCloudtrailTrailmetricfiltersMetricfiltersAlarms) id() (string, error) {
-	return t.Arn()
-}
-
-func (t *lumiAwsCloudtrailAlarmactions) id() (string, error) {
-	return t.SubscriptionArn()
-}
-
-func (t *lumiAwsCloudtrailTrailmetricfiltersMetricfilters) listSnsSubscriptionsByTopic(topicArn string) ([]string, error) {
-	subs := []string{}
-
+func (t *lumiAwsCloudtrailTrail) GetEventSelectors() (interface{}, error) {
+	regionValue, err := t.Region()
+	if err != nil {
+		return nil, err
+	}
 	at, err := awstransport(t.Runtime.Motor.Transport)
 	if err != nil {
 		return nil, err
 	}
-	svc := at.Sns("")
+
+	svc := at.Cloudtrail(regionValue)
 	ctx := context.Background()
 
-	subsByTopic, err := svc.ListSubscriptionsByTopicRequest(&sns.ListSubscriptionsByTopicInput{TopicArn: &topicArn}).Send(ctx)
+	arnValue, err := t.Arn()
 	if err != nil {
-		return nil, errors.Wrap(err, "could not gather sns subscriptions info")
+		return nil, err
 	}
-	for _, sub := range subsByTopic.Subscriptions {
-		subs = append(subs, toString(sub.SubscriptionArn))
-	}
-	return subs, nil
-}
 
-func isValidSubscription(subscriptionArn string) bool {
-	if len(subscriptionArn) == 0 {
-		return false
+	// no pagination required
+	trailmgmtevents, err := svc.GetEventSelectorsRequest(&cloudtrail.GetEventSelectorsInput{
+		TrailName: &arnValue,
+	}).Send(ctx)
+	if err != nil {
+		return nil, err
 	}
-	if strings.HasPrefix(subscriptionArn, "arn:aws:sns:") {
-		return true
-	}
-	return false
+	return jsonToDictSlice(trailmgmtevents.EventSelectors)
 }
