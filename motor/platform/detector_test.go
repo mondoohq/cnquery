3116850c2b975042485158ab1e99e0ee4d70da0c
Christoph Hartmann <chris@lollyrock.com>
2021-02-04 23:20:19 +0100

    🎉 detect alma linux

--- motor/platform/detector_test.go
+++ motor/platform/detector_test.go
@@ -162,6 +162,19 @@ func TestCentos8OSDetector(t *testing.T) {
 	assert.Equal(t, []string{"redhat", "linux", "unix", "os"}, di.Family)
 }
 
+func TestAlmaLinux8OSDetector(t *testing.T) {
+	detector, err := newDetector("./testdata/detect-almalinux.toml")
+	assert.Nil(t, err, "was able to create the transport")
+	di, err := detector.Platform()
+	require.NoError(t, err)
+
+	assert.Equal(t, "almalinux", di.Name, "os name should be identified")
+	assert.Equal(t, "AlmaLinux", di.Title, "os title should be identified")
+	assert.Equal(t, "8.3", di.Release, "os version should be identified")
+	assert.Equal(t, "x86_64", di.Arch, "os arch should be identified")
+	assert.Equal(t, []string{"redhat", "linux", "unix", "os"}, di.Family)
+}
+
 func TestUbuntu1204Detector(t *testing.T) {
 	detector, err := newDetector("./testdata/detect-ubuntu1204.toml")
 	assert.Nil(t, err, "was able to create the transport")
