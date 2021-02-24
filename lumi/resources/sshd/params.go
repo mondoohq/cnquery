35991b303fd6bec28e5e5835c0f97202fa3e2656
Christoph Hartmann <chris@lollyrock.com>
2021-02-24 11:50:06 +0100

    🎉 normalize sshd config params when we parse the values

--- lumi/resources/sshd/params.go
+++ lumi/resources/sshd/params.go
@@ -1,25 +1,116 @@
 package sshd
 
 import (
-	"io"
-	"io/ioutil"
 	"regexp"
+	"strings"
 )
 
-// TODO: this is demo implementationn and it does not capture
-// all edge cases like multiple keys etc
-func Params(reader io.Reader) (map[string]string, error) {
-	content, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-
+func Params(content string) (map[string]string, error) {
 	re := regexp.MustCompile("(?m:^([[:alpha:]]+)\\s+(.*))")
-	m := re.FindAllStringSubmatch(string(content), -1)
+	m := re.FindAllStringSubmatch(content, -1)
 	res := make(map[string]string)
 	for _, mm := range m {
-		res[mm[1]] = mm[2]
+		k := mm[1]
+		// handle lower case entries and use proper ssh camel case
+		if sshKey, ok := SSH_Keywords[strings.ToLower(k)]; ok {
+			k = sshKey
+		}
+		res[k] = mm[2]
 	}
-
 	return res, nil
+}
+
+var SSH_Keywords = map[string]string{
+	"acceptenv":                       "AcceptEnv",
+	"addressfamily":                   "AddressFamily",
+	"allowagentforwarding":            "AllowAgentForwarding",
+	"allowgroups":                     "AllowGroups",
+	"allowstreamlocalforwarding":      "AllowStreamLocalForwarding",
+	"allowtcpforwarding":              "AllowTcpForwarding",
+	"allowusers":                      "AllowUsers",
+	"authenticationmethods":           "AuthenticationMethods",
+	"authorizedkeyscommand":           "AuthorizedKeysCommand",
+	"authorizedkeyscommanduser":       "AuthorizedKeysCommandUser",
+	"authorizedkeysfile":              "AuthorizedKeysFile",
+	"authorizedprincipalscommand":     "AuthorizedPrincipalsCommand",
+	"authorizedprincipalscommanduser": "AuthorizedPrincipalsCommandUser",
+	"authorizedprincipalsfile":        "AuthorizedPrincipalsFile",
+	"banner":                          "Banner",
+	"casignaturealgorithms":           "CASignatureAlgorithms",
+	"challengeresponseauthentication": "ChallengeResponseAuthentication",
+	"chrootdirectory":                 "ChrootDirectory",
+	"ciphers":                         "Ciphers",
+	"clientalivecountmax":             "ClientAliveCountMax",
+	"clientaliveinterval":             "ClientAliveInterval",
+	"compression":                     "Compression",
+	"denygroups":                      "DenyGroups",
+	"denyusers":                       "DenyUsers",
+	"disableforwarding":               "DisableForwarding",
+	"exposeauthinfo":                  "ExposeAuthInfo",
+	"fingerprinthash":                 "FingerprintHash",
+	"forcecommand":                    "ForceCommand",
+	"gssapiauthentication":            "GSSAPIAuthentication",
+	"gssapicleanupcredentials":        "GSSAPICleanupCredentials",
+	"gssapistrictacceptorcheck":       "GSSAPIStrictAcceptorCheck",
+	"gatewayports":                    "GatewayPorts",
+	"hostcertificate":                 "HostCertificate",
+	"hostkey":                         "HostKey",
+	"hostkeyagent":                    "HostKeyAgent",
+	"hostkeyalgorithms":               "HostKeyAlgorithms",
+	"hostbasedacceptedkeytypes":       "HostbasedAcceptedKeyTypes",
+	"hostbasedauthentication":         "HostbasedAuthentication",
+	"hostbasedusesnamefrompacketonly": "HostbasedUsesNameFromPacketOnly",
+	"ipqos":                           "IPQoS",
+	"ignorerhosts":                    "IgnoreRhosts",
+	"ignoreuserknownhosts":            "IgnoreUserKnownHosts",
+	"include":                         "Include",
+	"kbdinteractiveauthentication":    "KbdInteractiveAuthentication",
+	"kerberosauthentication":          "KerberosAuthentication",
+	"kerberosgetafstoken":             "KerberosGetAFSToken",
+	"kerberosorlocalpasswd":           "KerberosOrLocalPasswd",
+	"kerberosticketcleanup":           "KerberosTicketCleanup",
+	"kexalgorithms":                   "KexAlgorithms",
+	"listenaddress":                   "ListenAddress",
+	"loglevel":                        "LogLevel",
+	"logingracetime":                  "LoginGraceTime",
+	"macs":                            "MACs",
+	"match":                           "Match",
+	"maxauthtries":                    "MaxAuthTries",
+	"maxsessions":                     "MaxSessions",
+	"maxstartups":                     "MaxStartups",
+	"passwordauthentication":          "PasswordAuthentication",
+	"permitemptypasswords":            "PermitEmptyPasswords",
+	"permitlisten":                    "PermitListen",
+	"permitopen":                      "PermitOpen",
+	"permitrootlogin":                 "PermitRootLogin",
+	"permittty":                       "PermitTTY",
+	"permittunnel":                    "PermitTunnel",
+	"permituserenvironment":           "PermitUserEnvironment",
+	"permituserrc":                    "PermitUserRC",
+	"pidfile":                         "PidFile",
+	"port":                            "Port",
+	"printlastlog":                    "PrintLastLog",
+	"printmotd":                       "PrintMotd",
+	"pubkeyacceptedkeytypes":          "PubkeyAcceptedKeyTypes",
+	"pubkeyauthoptions":               "PubkeyAuthOptions",
+	"pubkeyauthentication":            "PubkeyAuthentication",
+	"rdomain":                         "RDomain",
+	"rekeylimit":                      "RekeyLimit",
+	"revokedkeys":                     "RevokedKeys",
+	"securitykeyprovider":             "SecurityKeyProvider",
+	"setenv":                          "SetEnv",
+	"streamlocalbindmask":             "StreamLocalBindMask",
+	"streamlocalbindunlink":           "StreamLocalBindUnlink",
+	"strictmodes":                     "StrictModes",
+	"subsystem":                       "Subsystem",
+	"syslogfacility":                  "SyslogFacility",
+	"tcpkeepalive":                    "TCPKeepAlive",
+	"trustedusercakeys":               "TrustedUserCAKeys",
+	"usedns":                          "UseDNS",
+	"usepam":                          "UsePAM",
+	"versionaddendum":                 "VersionAddendum",
+	"x11displayoffset":                "X11DisplayOffset",
+	"x11forwarding":                   "X11Forwarding",
+	"x11uselocalhost":                 "X11UseLocalhost",
+	"xauthlocation":                   "XAuthLocation",
 }
