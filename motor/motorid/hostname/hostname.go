91c3300d0ce42489d6746710b4623f5233d0efe4
Christoph Hartmann <chris@lollyrock.com>
2021-03-01 12:40:50 +0100

    🐛 use /etc/hostname for platform identifier on linux

--- motor/motorid/hostname/hostname.go
+++ motor/motorid/hostname/hostname.go
@@ -2,6 +2,8 @@ package hostname
 
 import (
 	"errors"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/afero"
 	"io/ioutil"
 	"strings"
 
@@ -16,18 +18,35 @@ func Hostname(t transports.Transport, p *platform.Platform) (string, error) {
 		return hostname, errors.New("your platform is not supported by hostname resource")
 	}
 
-	// NOTE: hostname command works more reliable than t.RunCommand("powershell -c \"$env:computername\"")
+	// linux:
+	// we prefer the hostname over /etc/hostname since systemd is not updating the value all the time
+	//
+	// windows:
+	// hostname command works more reliable than t.RunCommand("powershell -c \"$env:computername\"")
 	// since it will return a non-zero exit code.
 	cmd, err := t.RunCommand("hostname")
-	if err != nil {
-		return hostname, err
-	}
-	data, err := ioutil.ReadAll(cmd.Stdout)
-	if err != nil {
-		return hostname, err
+	if err == nil {
+		data, err := ioutil.ReadAll(cmd.Stdout)
+		if err == nil {
+			return strings.TrimSpace(string(data)), nil
+		}
+	} else {
+		log.Warn().Err(err).Msg("could not run hostname command")
 	}
 
-	hostname = string(data)
+	// try to use /etc/hostname since it's also working on static analysis
+	if p.IsFamily(platform.FAMILY_LINUX) {
+		afs := &afero.Afero{Fs: t.FS()}
+		ok, err := afs.Exists("/etc/hostname")
+		if err == nil && ok {
+			content, err := afs.ReadFile("/etc/hostname")
+			if err == nil {
+				return strings.TrimSpace(string(content)), nil
+			}
+		} else {
+			log.Warn().Err(err).Msg("could not read /etc/hostname file")
+		}
+	}
 
-	return strings.TrimSpace(hostname), nil
+	return "", errors.New("could not detect hostname")
 }
--- motor/motorid/hostname/hostnname_test.go
+++ motor/motorid/hostname/hostnname_test.go
@@ -11,6 +11,22 @@ import (
 	"go.mondoo.io/mondoo/motor/transports/mock"
 )
 
+func TestHostnameLinuxEtcHostname(t *testing.T) {
+	trans, err := mock.NewFromToml(&transports.TransportConfig{Backend: transports.TransportBackend_CONNECTION_MOCK, Path: "./testdata/hostname_arch.toml"})
+	require.NoError(t, err)
+
+	m, err := motor.New(trans)
+	require.NoError(t, err)
+
+	p, err := m.Platform()
+	require.NoError(t, err)
+
+	hostame, err := hostname.Hostname(trans, p)
+	require.NoError(t, err)
+
+	assert.Equal(t, "9be843c4be9f", hostame)
+}
+
 func TestHostnameLinux(t *testing.T) {
 	trans, err := mock.NewFromToml(&transports.TransportConfig{Backend: transports.TransportBackend_CONNECTION_MOCK, Path: "./testdata/hostname_linux.toml"})
 	require.NoError(t, err)
