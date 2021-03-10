9538f1838bdd3c81701388a945b6ffaf0e152114
Christoph Hartmann <chris@lollyrock.com>
2021-03-10 23:37:36 +0100

    🥳 k8s cluster version and uid detection

--- motor/transports/k8s/platform.go
+++ motor/transports/k8s/platform.go
@@ -1,6 +1,29 @@
 package k8s
 
+import (
+	"errors"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+)
+
 func (t *Transport) Identifier() (string, error) {
-	// TODO: we need a cluster id to make this reliable
-	return "//platformid.api.mondoo.app/runtime/k8s", nil
+	// we use "kube-system" namespace uid as identifier for the cluster
+	result, err := t.Resources("namespaces", "kube-system")
+	if err != nil {
+		return "", err
+	}
+
+	if len(result.RootsResources) != 1 {
+		return "", errors.New("could not identify the cluster")
+	}
+
+	resource := result.RootsResources[0]
+
+	obj, err := meta.Accessor(resource)
+	if err != nil {
+		return "", err
+	}
+
+	uid := obj.GetUID()
+	return "//platformid.api.mondoo.app/runtime/k8s/uid/" + string(uid), nil
 }
