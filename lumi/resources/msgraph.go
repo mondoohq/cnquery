8d7669fb8087a9bd09c60f57f5de91f49e0cb6e1
Christoph Hartmann <chris@lollyrock.com>
2021-03-18 01:13:32 +0100

    🎉 add msgraph api organziation, user and domain

--- lumi/resources/msgraph.go
+++ lumi/resources/msgraph.go
@@ -19,6 +19,224 @@ func ms365transport(t transports.Transport) (*ms365_transport.Transport, error) 
 	return at, nil
 }
 
+func (m *lumiMsgraphBeta) id() (string, error) {
+	return "msgraph.beta", nil
+}
+
+func (m *lumiMsgraphBeta) GetSettings() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	settings, err := graphBetaClient.Settings().Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return jsonToDictSlice(settings)
+}
+
+func (m *lumiMsgraphBeta) GetOrganizations() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("Organization.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	orgs, err := graphBetaClient.Organization().Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []interface{}{}
+	for i := range orgs {
+		org := orgs[i]
+
+		assignedPlans, _ := jsonToDictSlice(org.AssignedPlans)
+		verifiedDomains, _ := jsonToDictSlice(org.VerifiedDomains)
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.organization",
+			"id", toString(org.ID),
+			"assignedPlans", assignedPlans,
+			"createdDateTime", org.CreatedDateTime,
+			"displayName", toString(org.DisplayName),
+			"verifiedDomains", verifiedDomains,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBeta) GetUsers() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("User.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	users, err := graphBetaClient.Users().Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []interface{}{}
+	for i := range users {
+		user := users[i]
+
+		settings, _ := jsonToDict(user.Settings)
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.user",
+			"id", toString(user.ID),
+			"accountEnabled", toBool(user.AccountEnabled),
+			"city", toString(user.City),
+			"companyName", toString(user.CompanyName),
+			"country", toString(user.Country),
+			"createdDateTime", user.CreatedDateTime,
+			"department", toString(user.Department),
+			"displayName", toString(user.DisplayName),
+			"employeeId", toString(user.EmployeeID),
+			"givenName", toString(user.GivenName),
+			"jobTitle", toString(user.JobTitle),
+			"mail", toString(user.Mail),
+			"mobilePhone", toString(user.MobilePhone),
+			"otherMails", strSliceToInterface(user.OtherMails),
+			"officeLocation", toString(user.OfficeLocation),
+			"postalCode", toString(user.PostalCode),
+			"state", toString(user.State),
+			"streetAddress", toString(user.StreetAddress),
+			"surname", toString(user.Surname),
+			"userPrincipalName", toString(user.UserPrincipalName),
+			"userType", toString(user.UserType),
+			"settings", settings,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBeta) GetDomains() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("Domain.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	domains, err := graphBetaClient.Domains().Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []interface{}{}
+	for i := range domains {
+		domain := domains[i]
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.domain",
+			"id", toString(domain.ID),
+			"authenticationType", toString(domain.AuthenticationType),
+			"availabilityStatus", toString(domain.AvailabilityStatus),
+			"isAdminManaged", toBool(domain.IsAdminManaged),
+			"isDefault", toBool(domain.IsDefault),
+			"isInitial", toBool(domain.IsInitial),
+			"isRoot", toBool(domain.IsRoot),
+			"isVerified", toBool(domain.IsVerified),
+			"passwordNotificationWindowInDays", toInt(domain.PasswordNotificationWindowInDays),
+			"passwordValidityPeriodInDays", toInt(domain.PasswordValidityPeriodInDays),
+			"supportedServices", sliceInterface(domain.SupportedServices),
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBetaOrganization) id() (string, error) {
+	return "msgraph.beta.organization", nil
+}
+
+func (m *lumiMsgraphBetaUser) id() (string, error) {
+	return "msgraph.beta.user", nil
+}
+
+func (m *lumiMsgraphBetaDomain) id() (string, error) {
+	return "msgraph.beta.domain", nil
+}
+
+func (m *lumiMsgraphBetaUser) GetSettings() (interface{}, error) {
+	id, err := m.Id()
+	if err != nil {
+		return nil, err
+	}
+
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("User.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	userSettings, err := graphBetaClient.Users().ID(id).Settings().Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return jsonToDict(userSettings)
+}
+
 func (m *lumiMsgraphBetaSecurity) id() (string, error) {
 	return "msgraph.beta.security", nil
 }
@@ -141,4 +359,64 @@ func (m *lumiMsgraphBetaSecurity) GetSecureScores() ([]interface{}, error) {
 
 func (s *lumiMsgraphBetaSecuritySecurityscore) id() (string, error) {
 	return s.Id()
+}
+
+func (s *lumiMsgraphBetaProfiles) id() (string, error) {
+	return "msgraph.beta.profiles", nil
+}
+
+func (m *lumiMsgraphBetaProfiles) GetAuthorizationPolicy() (interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("Policy.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	policy, err := graphBetaClient.Policies().ID("authorizationPolicy").Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	additionalDataRaw, ok := policy.AdditionalData["value"]
+	if ok {
+		additonalDataSlice, ok := additionalDataRaw.([]interface{})
+		if ok && len(additonalDataSlice) > 0 {
+			return jsonToDict(additonalDataSlice[0])
+		}
+	}
+	return nil, nil
+}
+
+func (m *lumiMsgraphBetaProfiles) GetIdentitySecurityDefaultsEnforcementPolicy() (interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("Policy.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	policy, err := graphBetaClient.Policies().ID("identitySecurityDefaultsEnforcementPolicy").Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return jsonToDict(policy.AdditionalData)
 }
