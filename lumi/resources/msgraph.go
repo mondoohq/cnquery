a8cc8ab943d016630ec2fda6ac0b5bfc531286c3
Christoph Hartmann <chris@lollyrock.com>
2021-03-18 23:46:05 +0100

    🎉 add msgraph device management resources

--- lumi/resources/msgraph.go
+++ lumi/resources/msgraph.go
@@ -467,3 +467,215 @@ func (m *lumiMsgraphBetaProfiles) GetIdentitySecurityDefaultsEnforcementPolicy()
 	}
 	return jsonToDict(policy.AdditionalData)
 }
+
+func (m *lumiMsgraphBetaRolemanagement) id() (string, error) {
+	return "msgraph.rolemanagement", nil
+}
+
+func (m *lumiMsgraphBetaRolemanagement) GetRoleDefinitions() (interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("Directory.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	roles, err := graphBetaClient.RoleManagement().Directory().RoleDefinitions().Request().Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []interface{}{}
+	for i := range roles {
+		role := roles[i]
+
+		rolePermissions, _ := jsonToDictSlice(role.RolePermissions)
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.rolemanagement.roledefinition",
+			"id", toString(role.ID),
+			"description", toString(role.Description),
+			"displayName", toString(role.DisplayName),
+			"isBuiltIn", toBool(role.IsBuiltIn),
+			"isEnabled", toBool(role.IsEnabled),
+			"rolePermissions", rolePermissions,
+			"templateId", toString(role.TemplateID),
+			"version", toString(role.Version),
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBetaRolemanagementRoledefinition) id() (string, error) {
+	return m.Id()
+}
+
+func (m *lumiMsgraphBetaRolemanagementRoledefinition) GetAssignments() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("Directory.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	roleDefinitionID, err := m.Id()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	r := graphBetaClient.RoleManagement().Directory().RoleAssignments().Request()
+	r.Filter("roleDefinitionId eq '" + roleDefinitionID + "'")
+	r.Expand("principal")
+	roleAssignments, err := r.Get(ctx)
+
+	res := []interface{}{}
+	for i := range roleAssignments {
+		roleAssignment := roleAssignments[i]
+
+		principal, _ := jsonToDict(roleAssignment.Principal)
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.rolemanagement.roleassignment",
+			"id", toString(roleAssignment.ID),
+			"roleDefinitionId", toString(roleAssignment.RoleDefinitionID),
+			"principalId", toString(roleAssignment.PrincipalID),
+			"principal", principal,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBetaRolemanagementRoleassignment) id() (string, error) {
+	return m.Id()
+}
+
+func (m *lumiMsgraphBetaDevicemanagement) id() (string, error) {
+	return "msgraph.beta.devicemanagement", nil
+}
+
+func (m *lumiMsgraphBetaDevicemanagement) GetDeviceConfigurations() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("DeviceManagementConfiguration.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	configurations, err := graphBetaClient.DeviceManagement().DeviceConfigurations().Request().Get(ctx)
+
+	res := []interface{}{}
+	for i := range configurations {
+		configuration := configurations[i]
+
+		properties, _ := jsonToDict(configuration.AdditionalData)
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.devicemanagement.deviceconfiguration",
+			"id", toString(configuration.ID),
+			"lastModifiedDateTime", configuration.LastModifiedDateTime,
+			"roleScopeTagIds", sliceInterface(configuration.RoleScopeTagIDs),
+			"supportsScopeTags", toBool(configuration.SupportsScopeTags),
+			"createdDateTime", configuration.CreatedDateTime,
+			"description", toString(configuration.Description),
+			"displayName", toString(configuration.DisplayName),
+			"version", toInt(configuration.Version),
+			"properties", properties,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBetaDevicemanagement) GetDeviceCompliancePolicies() ([]interface{}, error) {
+	mt, err := ms365transport(m.Runtime.Motor.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	missingPermissions := mt.MissingRoles("DeviceManagementConfiguration.Read.All")
+	if len(missingPermissions) > 0 {
+		return nil, errors.New("current credentials have insufficient privileges: " + strings.Join(missingPermissions, ","))
+	}
+
+	graphBetaClient, err := mt.GraphBetaClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	r := graphBetaClient.DeviceManagement().DeviceCompliancePolicies().Request()
+	r.Expand("assignments")
+	compliancePolicies, err := r.Get(ctx)
+
+	res := []interface{}{}
+	for i := range compliancePolicies {
+		compliancePolicy := compliancePolicies[i]
+
+		properties, _ := jsonToDict(compliancePolicy.AdditionalData)
+		assignments, _ := jsonToDictSlice(compliancePolicy.Assignments)
+
+		lumiResource, err := m.Runtime.CreateResource("msgraph.beta.devicemanagement.devicecompliancepolicy",
+			"id", toString(compliancePolicy.ID),
+			"createdDateTime", compliancePolicy.CreatedDateTime,
+			"description", toString(compliancePolicy.Description),
+			"displayName", toString(compliancePolicy.DisplayName),
+			"lastModifiedDateTime", compliancePolicy.LastModifiedDateTime,
+			"roleScopeTagIds", sliceInterface(compliancePolicy.RoleScopeTagIDs),
+			"version", toInt(compliancePolicy.Version),
+			"properties", properties,
+			"assignments", assignments,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, lumiResource)
+	}
+
+	return res, nil
+}
+
+func (m *lumiMsgraphBetaDevicemanagementDeviceconfiguration) id() (string, error) {
+	return m.Id()
+}
+
+func (m *lumiMsgraphBetaDevicemanagementDevicecompliancepolicy) id() (string, error) {
+	return m.Id()
+}
