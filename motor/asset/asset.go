a0988a5fb365fd48551cd1d1d8eb70c046736b5b
Christoph Hartmann <chris@lollyrock.com>
2021-04-07 18:32:56 +0200

    🧹 update and minimize gogo proto references

--- motor/asset/asset.go
+++ motor/asset/asset.go
@@ -4,7 +4,7 @@ import (
 	fmt "fmt"
 )
 
-//go:generate protoc --proto_path=$GOPATH/src:. --proto_path=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto --falcon_out=. --iam-actions_out=. --gofast_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:$GOPATH/src asset.proto
+//go:generate protoc --proto_path=$GOPATH/src:. --proto_path=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.2/gogoproto --falcon_out=. --iam-actions_out=. --gofast_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:$GOPATH/src asset.proto
 
 func (a *Asset) HumanName() string {
 	if a == nil {
--- motor/transports/transport.go
+++ motor/transports/transport.go
@@ -1,6 +1,6 @@
 package transports
 
-//go:generate protoc --proto_path=$GOPATH/src:. --proto_path=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto --falcon_out=. --iam-actions_out=. --gofast_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:$GOPATH/src transports.proto
+//go:generate protoc --proto_path=$GOPATH/src:. --falcon_out=. --iam-actions_out=. --gofast_out=$GOPATH/src transports.proto
 
 import (
 	"regexp"
