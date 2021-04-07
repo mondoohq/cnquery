7d06786e34230986c804ecdee9a731f713f3bd77
Christoph Hartmann <chris@lollyrock.com>
2021-04-07 18:32:56 +0200

    🧹 update and minimize gogo proto references

--- motor/platform/platform.go
+++ motor/platform/platform.go
@@ -1,6 +1,6 @@
 package platform
 
-//go:generate protoc --proto_path=$GOPATH/src:. --proto_path=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto --falcon_out=. --iam-actions_out=. --gofast_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:$GOPATH/src platform.proto
+//go:generate protoc --proto_path=$GOPATH/src:. --falcon_out=. --iam-actions_out=. --gofast_out=$GOPATH/src platform.proto
 
 func (p *Platform) IsFamily(family string) bool {
 	for i := range p.Family {
