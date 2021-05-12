183270110c69965acf4f76359b5731b15ec97f7a
Jay Mundrawala <jay@mondoo.io>
2021-05-12 12:47:16 -0500

    Set up golangci-lint to run locally
    This sets up golangci-lint to run locally with the gofmt linter
    set up.
    
    You can run it with the makefile:
    ```
    make linting/golangci-lint/run
    ```
    
    Running `linting/golangci-lint/run` will download the appropriate
    version of golangci-lint to your .cache directory. It will redownload
    it when we decide to update the version we use in CI, or it gets
    deleted.
    
    There is also a `linting/golangci-lint/fix` target which will attempt
    to fix things
    
    Here's an example of running it:
    ```
    ➤ make linting/golangci-lint/run
    ./.cache/golangci-lint/golangci-lint run
    vadvisor/specs/oval/uname.go:3: File is not `gofmt`-ed (gofmt)
    
    lumi/resources/processes/flag_test.go:50: File is not `gofmt`-ed (gofmt)
    	cmd string
    vadvisor/service/database.go:7: File is not `gofmt`-ed (gofmt)
    	"github.com/cockroachdb/errors"
    make: *** [Makefile:476: linting/golangci-lint/run] Error 1
    jaym@robothell:~/w/g/s/g/mondoo [jdm/golangci-lint]
    ➤ make linting/golangci-lint/fix
    ./.cache/golangci-lint/golangci-lint run --fix
    ```

--- lumi/resources/processes/flag_test.go
+++ lumi/resources/processes/flag_test.go
@@ -47,7 +47,7 @@ func TestParseShorthand(t *testing.T) {
 }
 
 type testSet struct {
-	cmd string
+	cmd   string
 	flags map[string]string
 }
 
@@ -56,7 +56,7 @@ func TestFlagParser(t *testing.T) {
 		{
 			cmd: "/usr/local/bin/kube-proxy --config=/var/lib/kube-proxy/config.conf --hostname-override=minikube",
 			flags: map[string]string{
-				"config": "/var/lib/kube-proxy/config.conf",
+				"config":            "/var/lib/kube-proxy/config.conf",
 				"hostname-override": "minikube",
 			},
 		},
@@ -71,35 +71,35 @@ func TestFlagParser(t *testing.T) {
 		{
 			cmd: "kube-apiserver --advertise-address=192.168.99.103 --allow-privileged=true --authorization-mode=Node,RBAC --client-ca-file=/var/lib/minikube/certs/ca.crt --enable-admission-plugins=NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,NodeRestriction,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota --enable-bootstrap-token-auth=true --etcd-cafile=/var/lib/minikube/certs/etcd/ca.crt --etcd-certfile=/var/lib/minikube/certs/apiserver-etcd-client.crt --etcd-keyfile=/var/lib/minikube/certs/apiserver-etcd-client.key --etcd-servers=https://127.0.0.1:2379 --insecure-port=0 --kubelet-client-certificate=/var/lib/minikube/certs/apiserver-kubelet-client.crt --kubelet-client-key=/var/lib/minikube/certs/apiserver-kubelet-client.key --kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname --proxy-client-cert-file=/var/lib/minikube/certs/front-proxy-client.crt --proxy-client-key-file=/var/lib/minikube/certs/front-proxy-client.key --requestheader-allowed-names=front-proxy-client --requestheader-client-ca-file=/var/lib/minikube/certs/front-proxy-ca.crt --requestheader-extra-headers-prefix=X-Remote-Extra- --requestheader-group-headers=X-Remote-Group --requestheader-username-headers=X-Remote-User --secure-port=8443 --service-account-issuer=https://kubernetes.default.svc.cluster.local --service-account-key-file=/var/lib/minikube/certs/sa.pub --service-account-signing-key-file=/var/lib/minikube/certs/sa.key --service-cluster-ip-range=10.96.0.0/12 --tls-cert-file=/var/lib/minikube/certs/apiserver.crt --tls-private-key-file=/var/lib/minikube/certs/apiserver.key",
 			flags: map[string]string{
-				"advertise-address": "192.168.99.103",
-				"allow-privileged": "true",
-				"authorization-mode": "Node,RBAC",
-				"client-ca-file": "/var/lib/minikube/certs/ca.crt",
-				"enable-admission-plugins": "NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,NodeRestriction,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota",
-				"enable-bootstrap-token-auth": "true",
-				"etcd-cafile": "/var/lib/minikube/certs/etcd/ca.crt",
-				"etcd-certfile": "/var/lib/minikube/certs/apiserver-etcd-client.crt",
-				"etcd-keyfile": "/var/lib/minikube/certs/apiserver-etcd-client.key",
-				"etcd-servers": "https://127.0.0.1:2379",
-				"insecure-port": "0",
-				"kubelet-client-certificate": "/var/lib/minikube/certs/apiserver-kubelet-client.crt",
-				"kubelet-client-key": "/var/lib/minikube/certs/apiserver-kubelet-client.key",
-				"kubelet-preferred-address-types": "InternalIP,ExternalIP,Hostname",
-				"proxy-client-cert-file": "/var/lib/minikube/certs/front-proxy-client.crt",
-				"proxy-client-key-file": "/var/lib/minikube/certs/front-proxy-client.key",
-				"requestheader-allowed-names": "front-proxy-client",
-				"requestheader-client-ca-file": "/var/lib/minikube/certs/front-proxy-ca.crt",
+				"advertise-address":                  "192.168.99.103",
+				"allow-privileged":                   "true",
+				"authorization-mode":                 "Node,RBAC",
+				"client-ca-file":                     "/var/lib/minikube/certs/ca.crt",
+				"enable-admission-plugins":           "NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,NodeRestriction,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota",
+				"enable-bootstrap-token-auth":        "true",
+				"etcd-cafile":                        "/var/lib/minikube/certs/etcd/ca.crt",
+				"etcd-certfile":                      "/var/lib/minikube/certs/apiserver-etcd-client.crt",
+				"etcd-keyfile":                       "/var/lib/minikube/certs/apiserver-etcd-client.key",
+				"etcd-servers":                       "https://127.0.0.1:2379",
+				"insecure-port":                      "0",
+				"kubelet-client-certificate":         "/var/lib/minikube/certs/apiserver-kubelet-client.crt",
+				"kubelet-client-key":                 "/var/lib/minikube/certs/apiserver-kubelet-client.key",
+				"kubelet-preferred-address-types":    "InternalIP,ExternalIP,Hostname",
+				"proxy-client-cert-file":             "/var/lib/minikube/certs/front-proxy-client.crt",
+				"proxy-client-key-file":              "/var/lib/minikube/certs/front-proxy-client.key",
+				"requestheader-allowed-names":        "front-proxy-client",
+				"requestheader-client-ca-file":       "/var/lib/minikube/certs/front-proxy-ca.crt",
 				"requestheader-extra-headers-prefix": "X-Remote-Extra-",
-				"requestheader-group-headers": "X-Remote-Group",
-				"requestheader-username-headers": "X-Remote-User",
-				"secure-port": "8443",
-				"service-account-issuer": "https://kubernetes.default.svc.cluster.local",
-				"service-account-key-file": "/var/lib/minikube/certs/sa.pub",
-				"service-account-signing-key-file": "/var/lib/minikube/certs/sa.key",
-				"service-cluster-ip-range": "10.96.0.0/12",
-				"tls-cert-file": "/var/lib/minikube/certs/apiserver.crt",
-				"tls-private-key-file": "/var/lib/minikube/certs/apiserver.key",
-				},
+				"requestheader-group-headers":        "X-Remote-Group",
+				"requestheader-username-headers":     "X-Remote-User",
+				"secure-port":                        "8443",
+				"service-account-issuer":             "https://kubernetes.default.svc.cluster.local",
+				"service-account-key-file":           "/var/lib/minikube/certs/sa.pub",
+				"service-account-signing-key-file":   "/var/lib/minikube/certs/sa.key",
+				"service-cluster-ip-range":           "10.96.0.0/12",
+				"tls-cert-file":                      "/var/lib/minikube/certs/apiserver.crt",
+				"tls-private-key-file":               "/var/lib/minikube/certs/apiserver.key",
+			},
 		},
 	}
 
@@ -108,6 +108,6 @@ func TestFlagParser(t *testing.T) {
 		fs := FlagSet{}
 		err := fs.ParseCommand(test.cmd)
 		require.NoError(t, err)
-		assert.Equal(t,test.flags,fs.actual, test.cmd)
+		assert.Equal(t, test.flags, fs.actual, test.cmd)
 	}
 }
