ab17cb816c0246707cad81956d2a8d312bf27bdd
Christoph Hartmann <chris@lollyrock.com>
2021-06-29 18:26:19 +0200

    🐛 fix ssh platform detection

--- motor/discovery/instance/instance.go
+++ motor/discovery/instance/instance.go
@@ -2,8 +2,9 @@ package instance
 
 import (
 	"go.mondoo.io/mondoo/motor/asset"
-	"go.mondoo.io/mondoo/motor/platform"
+	"go.mondoo.io/mondoo/motor/motorid/hostname"
 	"go.mondoo.io/mondoo/motor/transports"
+	"go.mondoo.io/mondoo/motor/transports/resolver"
 )
 
 type Resolver struct{}
@@ -20,23 +21,39 @@ func (r *Resolver) ParseConnectionURL(url string, opts ...transports.TransportCo
 	return transports.NewTransportFromUrl(url, opts...)
 }
 
-func (r *Resolver) Resolve(t *transports.TransportConfig) ([]*asset.Asset, error) {
+func (r *Resolver) Resolve(tc *transports.TransportConfig) ([]*asset.Asset, error) {
 	assetInfo := &asset.Asset{
-		// Name: in.Name,
-		// PlatformIDs: refIds,
-		// Labels: in.Labels,
-		State: asset.State_STATE_ONLINE,
+		Connections: []*transports.TransportConfig{tc},
+		State:       asset.State_STATE_ONLINE,
+	}
+
+	m, err := resolver.New(tc)
+	if err != nil {
+		return nil, err
+	}
+	defer m.Close()
+
+	// store detected platform identifier with asset
+	assetInfo.PlatformIds = m.Meta.Identifier
+
+	// determine platform information
+	p, err := m.Platform()
+	if err == nil {
+		assetInfo.Platform = p
+	}
+
+	// use hostname as asset name
+	if p != nil && assetInfo.Name == "" {
+		// retrieve hostname
+		hostname, err := hostname.Hostname(m.Transport, p)
+		if err == nil && len(hostname) > 0 {
+			assetInfo.Name = hostname
+		}
 	}
 
 	// use hostname as name if asset name was not explicitly provided
 	if assetInfo.Name == "" {
-		assetInfo.Name = t.Host
-	}
-
-	assetInfo.Connections = []*transports.TransportConfig{t}
-
-	assetInfo.Platform = &platform.Platform{
-		Kind: transports.Kind_KIND_BARE_METAL,
+		assetInfo.Name = tc.Host
 	}
 
 	return []*asset.Asset{assetInfo}, nil
