3b212f1d25197a293a8c04c73c0185e2999abb8d
Christoph Hartmann <chris@lollyrock.com>
2021-07-18 17:24:58 +0200

    remove marshal / unmarshal credential

--- motor/transports/credential.go
+++ motor/transports/credential.go
@@ -1,37 +1,10 @@
 package transports
 
 import (
-	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"os"
 )
-
-func (s *CredentialType) UnmarshalJSON(data []byte) error {
-	// check if we have a number
-
-	var code int32
-	err := json.Unmarshal(data, &code)
-	if err == nil {
-		t := CredentialType(code)
-		s = &t
-	}
-	if err != nil {
-		var name string
-		err = json.Unmarshal(data, &name)
-		code, ok := CredentialType_value[name]
-		if !ok {
-			return errors.New("unknown type value: " + name)
-		}
-		t := CredentialType(code)
-		s = &t
-	}
-	return nil
-}
-
-func (s *CredentialType) MarshalJSON() ([]byte, error) {
-	return []byte(s.String()), nil
-}
 
 func NewPrivateKeyCredential(user string, pemBytes []byte, password string) *Credential {
 	return &Credential{
