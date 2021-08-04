2028871ce76aa0ae70636054666b49c38516f612
Christoph Hartmann <chris@lollyrock.com>
2021-08-04 17:03:09 +0200

    ⭐️ macos plist file parser

--- lumi/resources/parse.go
+++ lumi/resources/parse.go
@@ -7,9 +7,12 @@ package resources
 import (
 	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"strings"
 
 	"go.mondoo.io/mondoo/lumi"
 	"go.mondoo.io/mondoo/lumi/resources/parsers"
+	"howett.net/plist"
 )
 
 func (s *lumiParse) id() (string, error) {
@@ -163,4 +166,71 @@ func (s *lumiParseJson) GetParams(content string) (map[string]interface{}, error
 	}
 
 	return res, nil
+}
+
+func (s *lumiParsePlist) init(args *lumi.Args) (*lumi.Args, ParseJson, error) {
+	if x, ok := (*args)["path"]; ok {
+		path, ok := x.(string)
+		if !ok {
+			return nil, nil, errors.New("wrong type for 'path' in parse.json initialization, it must be a string")
+		}
+
+		f, err := s.Runtime.CreateResource("file", "path", path)
+		if err != nil {
+			return nil, nil, err
+		}
+		(*args)["file"] = f
+		delete(*args, "path")
+	} else {
+		return nil, nil, errors.New("missing 'path' argument for parse.json initialization")
+	}
+
+	return args, nil, nil
+}
+
+func (s *lumiParsePlist) id() (string, error) {
+	r, err := s.File()
+	if err != nil {
+		return "", err
+	}
+
+	path, err := r.Path()
+	if err != nil {
+		return "", err
+	}
+
+	return path, nil
+}
+
+func (s *lumiParsePlist) GetFile() (File, error) {
+	// TODO: all of this is a placeholder, in case we initialize the plist resource with content instead of a file
+	f, err := s.Runtime.CreateResource("file", "path", "/")
+	if err != nil {
+		return nil, err
+	}
+	return f.(File), nil
+}
+
+func (s *lumiParsePlist) GetContent(file File) (string, error) {
+	// we cannot load the file directly since we do not know the plist format
+	cmd, err := s.Runtime.Motor.Transport.RunCommand("plutil -convert xml1 /Library/Preferences/com.apple.SoftwareUpdate.plist -o -")
+	if err != nil {
+		return "", err
+	}
+	content, err := ioutil.ReadAll(cmd.Stdout)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+
+func (s *lumiParsePlist) GetParams(content string) (map[string]interface{}, error) {
+	var data map[string]interface{}
+	decoder := plist.NewDecoder(strings.NewReader(content))
+	err := decoder.Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
