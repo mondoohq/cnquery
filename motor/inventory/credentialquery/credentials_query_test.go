833a6f2b5ae174f5bdc304367aa78dab222abf04
Christoph Hartmann <chris@lollyrock.com>
2021-08-18 00:26:40 +0200

    ⭐️ replace embedded executor with mql

--- motor/inventory/credentialquery/credentials_query_test.go
+++ motor/inventory/credentialquery/credentials_query_test.go
@@ -3,67 +3,43 @@ package credentialquery
 import (
 	"testing"
 
-	"go.mondoo.io/mondoo/types"
+	"go.mondoo.io/mondoo/motor/vault"
+
+	"go.mondoo.io/mondoo/motor/asset"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/require"
-	"go.mondoo.io/mondoo/llx"
-	"go.mondoo.io/mondoo/policy/executor"
 )
 
 func TestSecretKeySimple(t *testing.T) {
-	query := `{type: 'ssh_agent'}`
+	query := `{ type: 'ssh_agent' }`
 
-	e, err := executor.NewEmbeddedExecutor()
+	runner, err := NewCredentialQueryRunner(query)
 	require.NoError(t, err)
-
-	value, err := e.Run(query, map[string]*llx.Primitive{})
+	cred, err := runner.Run(&asset.Asset{})
 	require.NoError(t, err)
-
-	sMeta := &CredentialQueryResponse{}
-	decoder, _ := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-		Metadata: nil,
-		Result:   sMeta,
-		TagName:  "json",
-	})
-	err = decoder.Decode(value)
-	require.NoError(t, err)
-	assert.Equal(t, "ssh_agent", sMeta.Type)
+	assert.Equal(t, vault.CredentialType_ssh_agent, cred.Type)
 }
 
 func TestSecretKeyIfReturn(t *testing.T) {
-	e, err := executor.NewEmbeddedExecutor()
-	require.NoError(t, err)
-
 	query := `
-		if (props.a == 'windows' && props.labels['key'] == 'value') {
+		if (props.labels['key'] == 'value') {
 			return {type: 'password', secret_id: 'theonekey'}
 		}
-		return {type: 'password', secret_id: 'otherkey'}
+		return {type: 'private_key', secret_id: 'otherkey'}
 	`
 
-	props := map[string]*llx.Primitive{
-		"a": llx.StringPrimitive("windows"),
-		"labels": llx.MapData(map[string]interface{}{
+	runner, err := NewCredentialQueryRunner(query)
+	require.NoError(t, err)
+
+	cred, err := runner.Run(&asset.Asset{
+		Labels: map[string]string{
 			"key": "value",
-		}, types.String).Result().Data,
-	}
-
-	value, err := e.Run(query, props)
-	require.NoError(t, err)
-
-	sMeta := &CredentialQueryResponse{}
-	decoder, _ := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-		Metadata: nil,
-		Result:   sMeta,
-		TagName:  "json",
+		},
 	})
-	err = decoder.Decode(value)
 	require.NoError(t, err)
 
-	// NOTE: this is not working yet
-	assert.Equal(t, "password", sMeta.Type)
-	assert.Equal(t, "theonekey", sMeta.SecretId)
+	assert.Equal(t, vault.CredentialType_password, cred.Type)
+	assert.Equal(t, "theonekey", cred.SecretId)
 }
