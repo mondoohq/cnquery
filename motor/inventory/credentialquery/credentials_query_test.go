32031f188f59a6f2eb11179c4a47f4d11eb3fc5f
Christoph Hartmann <chris@lollyrock.com>
2021-08-18 14:13:45 +0200

    🧹 add if conditional test to credential query

--- motor/inventory/credentialquery/credentials_query_test.go
+++ motor/inventory/credentialquery/credentials_query_test.go
@@ -43,3 +43,33 @@ func TestSecretKeyIfReturn(t *testing.T) {
 	assert.Equal(t, vault.CredentialType_password, cred.Type)
 	assert.Equal(t, "theonekey", cred.SecretId)
 }
+
+func TestSecretKeyIfConditionalReturn(t *testing.T) {
+	query := `
+		if (props.labels['Name'] == 'ssh') { 
+	       return { user: 'ec2-user', type: 'private_key', secret_id: 'arn:aws:secretsmanager:us-east-2:172746783610:secret:vj/secret-lHvP9r'}
+        }
+        return { secret_id: '' }"
+	`
+
+	runner, err := NewCredentialQueryRunner(query)
+	require.NoError(t, err)
+
+	// check with provided label
+	cred, err := runner.Run(&asset.Asset{
+		Labels: map[string]string{
+			"Name": "ssh",
+		},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, vault.CredentialType_private_key, cred.Type)
+	assert.Equal(t, "arn:aws:secretsmanager:us-east-2:172746783610:secret:vj/secret-lHvP9r", cred.SecretId)
+
+	// check without a label
+	cred, err = runner.Run(&asset.Asset{
+		Labels: map[string]string{},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, vault.CredentialType_undefined, cred.Type)
+	assert.Equal(t, "", cred.SecretId)
+}
