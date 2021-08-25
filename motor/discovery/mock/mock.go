e2b8b8edf010f7662d165b5b92bde07fe833d979
Jay Mundrawala <jay@mondoo.io>
2021-08-25 09:27:44 -0500

    🧹 Make mondoo scan accept recordings
    For example, now i can do:
    ```
    mondoo scan -t 'mock://recording-20210824073514.toml?hostname=foo1' --config mondoo.json
    mondoo scan -t 'mock://recording-20210824073514.toml?hostname=foo2' --config mondoo.json
    ```

--- motor/discovery/mock/mock.go
+++ motor/discovery/mock/mock.go
@@ -3,7 +3,10 @@ package mock
 import (
 	"go.mondoo.io/mondoo/motor/asset"
 	"go.mondoo.io/mondoo/motor/discovery/common"
+	"go.mondoo.io/mondoo/motor/motorid/hostname"
+	"go.mondoo.io/mondoo/motor/platform"
 	"go.mondoo.io/mondoo/motor/transports"
+	"go.mondoo.io/mondoo/motor/transports/resolver"
 )
 
 type Resolver struct{}
@@ -16,8 +19,45 @@ func (r *Resolver) AvailableDiscoveryTargets() []string {
 	return []string{}
 }
 
-func (r *Resolver) Resolve(t *transports.TransportConfig, cfn common.CredentialFn, sfn common.QuerySecretFn) ([]*asset.Asset, error) {
-	return []*asset.Asset{{
-		Connections: []*transports.TransportConfig{t},
-	}}, nil
+func (r *Resolver) Resolve(tc *transports.TransportConfig, cfn common.CredentialFn, sfn common.QuerySecretFn) ([]*asset.Asset, error) {
+	assetInfo := &asset.Asset{
+		State:       asset.State_STATE_ONLINE,
+		Connections: []*transports.TransportConfig{tc},
+	}
+
+	// use hostname as name if asset name was not explicitly provided
+	if assetInfo.Name == "" {
+		assetInfo.Name = tc.Host
+	}
+
+	assetInfo.Connections = []*transports.TransportConfig{tc}
+
+	assetInfo.Platform = &platform.Platform{
+		Kind: transports.Kind_KIND_BARE_METAL,
+	}
+
+	m, err := resolver.NewMotorConnection(tc, cfn)
+	if err != nil {
+		return nil, err
+	}
+	defer m.Close()
+
+	// store detected platform identifier with asset
+	assetInfo.PlatformIds = m.Meta.Identifier
+
+	// determine platform information
+	p, err := m.Platform()
+	if err == nil {
+		assetInfo.Platform = p
+	}
+
+	// use hostname as asset name
+	if p != nil && assetInfo.Name == "" {
+		// retrieve hostname
+		hostname, err := hostname.Hostname(m.Transport, p)
+		if err == nil && len(hostname) > 0 {
+			assetInfo.Name = hostname
+		}
+	}
+	return []*asset.Asset{assetInfo}, nil
 }
