af06e40aadd908b3ad68de4efc6af6f07a760127
kmacgugan <kyleen@gmail.com>
2021-10-05 23:26:41 -0700

    Add resources for ip tables and socket stats
    Add queries to dil policy
    Queries commented out to give new resources time to bake
    Add test for iptables v6 to test logic around opt being available or not

---
+++ lumi/resources/iptables_test.go
@@ -0,0 +1,54 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseStat(t *testing.T) {
+	// not opt
+	t.Run("parsing conf lines", func(t *testing.T) {
+		line := []string{"Chain OUTPUT (policy DROP 227 packets, 12904 bytes)",
+			"num      pkts      bytes target     prot opt in     out     source               destination",
+			"2           0        0 ACCEPT     tcp      *      *       ::/0                 ::/0                 state NEW,ESTABLISHED"}
+		expected := []Stat{{
+			LineNumber:  2,
+			Packets:     0,
+			Bytes:       0,
+			Target:      "ACCEPT",
+			Protocol:    "tcp",
+			Opt:         "  ",
+			Input:       "*",
+			Output:      "*",
+			Source:      "::/0",
+			Destination: "::/0",
+			Options:     "state NEW,ESTABLISHED"},
+		}
+		result, err := ParseStat(line, true)
+		require.NoError(t, err)
+		require.Equal(t, expected, result)
+	})
+	//opt
+	t.Run("parsing conf lines", func(t *testing.T) {
+		line := []string{"Chain OUTPUT (policy DROP 227 packets, 12904 bytes)",
+			"num      pkts      bytes target     prot opt in     out     source               destination",
+			"2           0        0 ACCEPT     tcp    opt  *      *       ::/0                 ::/0                 state NEW,ESTABLISHED"}
+		expected := []Stat{{
+			LineNumber:  2,
+			Packets:     0,
+			Bytes:       0,
+			Target:      "ACCEPT",
+			Protocol:    "tcp",
+			Opt:         "opt",
+			Input:       "*",
+			Output:      "*",
+			Source:      "::/0",
+			Destination: "::/0",
+			Options:     "state NEW,ESTABLISHED"},
+		}
+		result, err := ParseStat(line, true)
+		require.NoError(t, err)
+		require.Equal(t, expected, result)
+	})
+}
