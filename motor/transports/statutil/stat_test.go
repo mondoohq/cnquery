413c9fb0ab41552bbfd5fd043ce7541ea0d4924a
Jay Mundrawala <jay@mondoo.io>
2021-10-13 13:11:38 -0500

    🐛 transform unix mode to golang
    The golang representation is different for everything except the lower 9
    bits. Since we're using the golang api for file mode, we should
    translate into their conventions
    
    Fix #629

--- motor/transports/statutil/stat_test.go
+++ motor/transports/statutil/stat_test.go
@@ -1,6 +1,7 @@
 package statutil
 
 import (
+	"io/fs"
 	"os"
 	"path/filepath"
 	"testing"
@@ -27,6 +28,16 @@ func TestLinuxStatCmd(t *testing.T) {
 	assert.Equal(t, false, fi.IsDir())
 	assert.Equal(t, os.FileMode(0x180), fi.Mode())
 	assert.Equal(t, time.Unix(1590420240, 0), fi.ModTime())
+	require.NoError(t, err)
+	mode := fi.Mode()
+	assert.Zero(t, mode&fs.ModeSetuid)
+
+	fi, err = statHelper.Stat("/usr/bin/su")
+	require.NoError(t, err)
+	mode = fi.Mode()
+	assert.NotZero(t, mode&fs.ModeSetuid)
+	assert.Zero(t, mode&fs.ModeSetgid)
+
 }
 
 func TestOpenbsdStatCmd(t *testing.T) {
@@ -46,4 +57,52 @@ func TestOpenbsdStatCmd(t *testing.T) {
 	assert.Equal(t, time.Unix(1592996018, 0), fi.ModTime())
 	assert.Equal(t, int64(0), fi.Sys().(*transports.FileInfo).Uid)
 	assert.Equal(t, int64(0), fi.Sys().(*transports.FileInfo).Gid)
+}
+
+func TestToFileMode(t *testing.T) {
+	t.Run("directory and setgid", func(t *testing.T) {
+		m := toFileMode(0040000 | 0002000 | 0755)
+		assert.True(t, m.IsDir())
+		assert.True(t, (m&fs.ModeSetgid) > 0)
+		assert.False(t, (m&fs.ModeSetuid) > 0)
+		assert.False(t, (m&fs.ModeSticky) > 0)
+		assert.Equal(t, fs.FileMode(0755), (m & 0777))
+	})
+
+	t.Run("directory and setuid", func(t *testing.T) {
+		m := toFileMode(0040000 | 0004000 | 0755)
+		assert.True(t, m.IsDir())
+		assert.False(t, (m&fs.ModeSetgid) > 0)
+		assert.True(t, (m&fs.ModeSetuid) > 0)
+		assert.False(t, (m&fs.ModeSticky) > 0)
+		assert.Equal(t, fs.FileMode(0755), (m & 0777))
+	})
+
+	t.Run("directory and setuid and sticky", func(t *testing.T) {
+		m := toFileMode(0040000 | 0004000 | 0001000 | 0755)
+		assert.True(t, m.IsDir())
+		assert.False(t, (m&fs.ModeSetgid) > 0)
+		assert.True(t, (m&fs.ModeSetuid) > 0)
+		assert.True(t, (m&fs.ModeSticky) > 0)
+		assert.Equal(t, fs.FileMode(0755), (m & 0777))
+	})
+
+	t.Run("file and setuid", func(t *testing.T) {
+		m := toFileMode(0170000 | 0100000 | 0004000 | 0755)
+		assert.False(t, m.IsDir())
+		assert.False(t, (m&fs.ModeSetgid) > 0)
+		assert.True(t, (m&fs.ModeSetuid) > 0)
+		assert.False(t, (m&fs.ModeSticky) > 0)
+		assert.Equal(t, fs.FileMode(0755), (m & 0777))
+	})
+
+	t.Run("file and setuid and setgid", func(t *testing.T) {
+		m := toFileMode(0170000 | 0100000 | 0004000 | 0002000 | 0755)
+		assert.False(t, m.IsDir())
+		assert.True(t, (m&fs.ModeSetgid) > 0)
+		assert.True(t, (m&fs.ModeSetuid) > 0)
+		assert.False(t, (m&fs.ModeSticky) > 0)
+		assert.Equal(t, fs.FileMode(0755), (m & 0777))
+	})
+
 }
