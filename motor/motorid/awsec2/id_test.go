5729fc00c6ae231aca4ab6c356031cd455dd0ecc
Victoria Jeffrey <vj@email.com>
2021-10-13 22:45:32 -0600

    🧹 cleanup; skip terminated and transitioning ec2 instances
    also add lambda timeout time to env on lambda function; this
    will be used at a later time for tracking long-running operations

--- motor/motorid/awsec2/id_test.go
+++ motor/motorid/awsec2/id_test.go
@@ -13,4 +13,13 @@ func TestParseInstanceId(t *testing.T) {
 	assert.Equal(t, id.Account, "185972265011")
 	assert.Equal(t, id.Region, "us-east-1")
 	assert.Equal(t, id.Id, "i-07f67838ada5879af")
+
+	path = "//platformid.api.mondoo.app/runtime/aws/ec2/v1/account/185972265011/regions/us-east-1/instances/i-07f67838ada5879af"
+	id, err = ParseMondooInstanceID(path)
+	assert.Error(t, err, "invalid mondoo aws ec2 instance id")
+
+	path = "//platformid.api.mondoo.app/runtime/aws/ec2/v1/accounts/185972265011/regions/us/instances/i-07f67838ada5879af"
+	id, err = ParseMondooInstanceID(path)
+	assert.Error(t, err, "invalid mondoo aws ec2 instance id")
+
 }
