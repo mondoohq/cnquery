2101f89f57d3b99cd4973f65f17055c06ae52299
Victoria Jeffrey <vj@email.com>
2021-10-15 23:02:02 -0600

    🧹 awsec2ebs: save instance list as we scan; trigger scan msg when timing out

--- motor/transports/awsec2ebs/setup_unit_test.go
+++ motor/transports/awsec2ebs/setup_unit_test.go
@@ -6,15 +6,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/tj/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewVolumeAttachmentLoc(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	loc1 := newVolumeAttachmentLoc()
-	rand.Seed(time.Now().UnixNano())
-	loc2 := newVolumeAttachmentLoc()
-	assert.NotEqual(t, loc1, loc2)
-	assert.Equal(t, len(loc1), 9)
-	assert.Equal(t, strings.HasPrefix(loc1, "/dev/xvd"), true)
+	require.Equal(t, len(loc1), 9)
+	require.Equal(t, strings.HasPrefix(loc1, "/dev/xvd"), true)
 }
