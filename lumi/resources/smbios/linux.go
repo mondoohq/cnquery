be7df57dc42a974e3a65c37706a80b3abdd13603
Jay Mundrawala <jay@mondoo.io>
2021-11-15 12:49:13 -0600

    🐛 Fix machine resource on linux
    There were a few things that were breaking on my machine:
    - /sys/class/dmi/id is a symlink and not a directory. The function that
      walks directory does not follow symlinks, so we were trying to read a
      directory instead of skipping over it
    - on my system, there are files in /sys/class/dmi/id/power that return
      errors if you try to read them. For example:
      ```
      [root@robothell mondoo]# cat /sys/class/dmi/id/power/autosuspend_delay_ms
      cat: /sys/class/dmi/id/power/autosuspend_delay_ms: Input/output error
      ```
    
    Fixes #736

--- lumi/resources/smbios/linux.go
+++ lumi/resources/smbios/linux.go
@@ -3,6 +3,7 @@ package smbios
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/spf13/afero"
 	"go.mondoo.io/mondoo/motor"
@@ -21,64 +22,67 @@ func (s *LinuxSmbiosManager) Info() (*SmBiosInfo, error) {
 
 	fs := s.motor.Transport.FS()
 	afs := &afero.Afero{Fs: fs}
-	root := "/sys/class/dmi/id"
+	root := "/sys/class/dmi/id/"
 	wErr := afs.Walk(root, func(path string, info os.FileInfo, fErr error) error {
 		if info.IsDir() {
 			return nil
 		}
 
-		f, err := fs.Open(path)
-		if err != nil {
-			return err
-		}
-		data, err := ioutil.ReadAll(f)
-		if err != nil {
-			return err
-		}
-		f.Close()
-		value := string(data)
-
+		var dst *string
 		switch info.Name() {
 		case "bios_date":
-			smInfo.BIOS.ReleaseDate = value
+			dst = &smInfo.BIOS.ReleaseDate
 		case "bios_vendor":
-			smInfo.BIOS.Vendor = value
+			dst = &smInfo.BIOS.Vendor
 		case "bios_version":
-			smInfo.BIOS.Version = value
+			dst = &smInfo.BIOS.Version
 		case "board_asset_tag":
-			smInfo.BaseBoardInfo.AssetTag = value
+			dst = &smInfo.BaseBoardInfo.AssetTag
 		case "board_name":
-			smInfo.BaseBoardInfo.Model = value
+			dst = &smInfo.BaseBoardInfo.Model
 		case "board_serial":
-			smInfo.BaseBoardInfo.SerialNumber = value
+			dst = &smInfo.BaseBoardInfo.SerialNumber
 		case "board_vendor":
-			smInfo.BaseBoardInfo.Vendor = value
+			dst = &smInfo.BaseBoardInfo.Vendor
 		case "board_version":
-			smInfo.BaseBoardInfo.Version = value
+			dst = &smInfo.BaseBoardInfo.Version
 		case "chassis_asset_tag":
-			smInfo.ChassisInfo.AssetTag = value
+			dst = &smInfo.ChassisInfo.AssetTag
 		case "chassis_serial":
-			smInfo.ChassisInfo.SerialNumber = value
+			dst = &smInfo.ChassisInfo.SerialNumber
 		case "chassis_type":
-			smInfo.ChassisInfo.Type = value
+			dst = &smInfo.ChassisInfo.Type
 		case "chassis_vendor":
-			smInfo.ChassisInfo.Vendor = value
+			dst = &smInfo.ChassisInfo.Vendor
 		case "chassis_version":
-			smInfo.ChassisInfo.Version = value
+			dst = &smInfo.ChassisInfo.Version
 		case "product_family":
-			smInfo.SysInfo.Familiy = value
+			dst = &smInfo.SysInfo.Familiy
 		case "product_name":
-			smInfo.SysInfo.Model = value
+			dst = &smInfo.SysInfo.Model
 		case "product_serial":
-			smInfo.SysInfo.SerialNumber = value
+			dst = &smInfo.SysInfo.SerialNumber
 		case "product_sku":
-			smInfo.SysInfo.SKU = value
+			dst = &smInfo.SysInfo.SKU
 		case "product_uuid":
-			smInfo.SysInfo.UUID = value
+			dst = &smInfo.SysInfo.UUID
 		case "product_version":
-			smInfo.SysInfo.Version = value
+			dst = &smInfo.SysInfo.Version
 		case "sys_vendor":
-			smInfo.SysInfo.Vendor = value
+			dst = &smInfo.SysInfo.Vendor
+		}
+
+		if dst != nil {
+			f, err := fs.Open(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			data, err := ioutil.ReadAll(f)
+			if err != nil {
+				return err
+			}
+			*dst = strings.TrimSpace(string(data))
 		}
 
 		return nil
