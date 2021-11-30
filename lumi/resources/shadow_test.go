3e5c5cc32dbdbc259e6fa53e0d3a213ab355110d
Dominik Richter <dominik.richter@gmail.com>
2021-11-30 10:36:53 -0800

    🐛 use numbers in shadow.entry instead of string
    Addresses bad error messages seen in #177.
    
    Closes #177

--- lumi/resources/shadow_test.go
+++ lumi/resources/shadow_test.go
@@ -1,6 +1,7 @@
 package resources_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +11,7 @@ func TestResource_Shadow(t *testing.T) {
 	t.Run("list shadow entries", func(t *testing.T) {
 		res := testQuery(t, "shadow.list")
 		assert.NotEmpty(t, res)
+		assert.Equal(t, 3, len(res[0].Data.Value.([]interface{})))
 	})
 
 	t.Run("test a specific shadow entry", func(t *testing.T) {
@@ -17,5 +19,29 @@ func TestResource_Shadow(t *testing.T) {
 		assert.NotEmpty(t, res)
 		assert.Empty(t, res[0].Result().Error)
 		assert.Equal(t, "root", res[0].Data.Value)
+	})
+
+	t.Run("test empty dates that set upper bounds", func(t *testing.T) {
+		res := testQuery(t, "shadow.list[0].maxdays")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, int64(math.MaxInt64), res[0].Data.Value)
+
+		res = testQuery(t, "shadow.list[0].inactivedays")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, int64(math.MaxInt64), res[0].Data.Value)
+	})
+
+	t.Run("test empty dates that set lower bounds", func(t *testing.T) {
+		res := testQuery(t, "shadow.list[0].mindays")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, int64(-1), res[0].Data.Value)
+
+		res = testQuery(t, "shadow.list[0].warndays")
+		assert.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, int64(-1), res[0].Data.Value)
 	})
 }
