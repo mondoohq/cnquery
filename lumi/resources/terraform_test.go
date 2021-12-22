49bdd33a970e11360e885f0cfc26d8604163852b
Christoph Hartmann <chris@lollyrock.com>
2021-12-22 12:50:01 +0100

    🧹 add terraform test case that commented out resources are not parsed

--- lumi/resources/terraform_test.go
+++ lumi/resources/terraform_test.go
@@ -30,28 +30,35 @@ func terraformTestQuery(t *testing.T, query string) []*llx.RawResult {
 func TestResource_Terraform(t *testing.T) {
 	t.Run("terraform providers", func(t *testing.T) {
 		res := terraformTestQuery(t, "terraform.providers[0].type")
-		assert.NotEmpty(t, res)
+		require.NotEmpty(t, res)
 		assert.Empty(t, res[0].Result().Error)
 		assert.Equal(t, string("provider"), res[0].Data.Value)
 	})
 
+	t.Run("terraform ignore commented out resources", func(t *testing.T) {
+		res := terraformTestQuery(t, "terraform.providers.length")
+		require.NotEmpty(t, res)
+		assert.Empty(t, res[0].Result().Error)
+		assert.Equal(t, int64(1), res[0].Data.Value)
+	})
+
 	t.Run("terraform nested blocks", func(t *testing.T) {
 		res := terraformTestQuery(t, "terraform.blocks.where( type == \"resource\" && labels.contains(\"aws_instance\"))[0].type")
-		assert.NotEmpty(t, res)
+		require.NotEmpty(t, res)
 		assert.Empty(t, res[0].Result().Error)
 		assert.Equal(t, string("resource"), res[0].Data.Value)
 	})
 
 	t.Run("terraform jsonencode blocks", func(t *testing.T) {
 		res := terraformTestQuery(t, "terraform.resources.where( nameLabel == 'aws_iam_policy' && labels[1] == 'policy' )[0].arguments['policy'][0]['Version']")
-		assert.NotEmpty(t, res)
+		require.NotEmpty(t, res)
 		assert.Empty(t, res[0].Result().Error)
 		assert.Equal(t, string("2012-10-17"), res[0].Data.Value)
 	})
 
 	t.Run("terraform providers", func(t *testing.T) {
 		res := terraformTestQuery(t, "terraform.resources.where( nameLabel  == 'google_compute_instance')[0].arguments['metadata']")
-		assert.NotEmpty(t, res)
+		require.NotEmpty(t, res)
 		assert.Empty(t, res[0].Result().Error)
 		assert.Equal(t, map[string]interface{}{"enable-oslogin": true}, res[0].Data.Value)
 	})
---
+++ lumi/resources/testdata/terraform/comment.tf
@@ -0,0 +1,5 @@
+# hcl parser should not see this
+# provider "aws" {
+#   alias  = "east"
+#   region = "us-east-1"
+# }
