fe8a753179be02765da5c37f125b17bdd9ef818d
Christoph Hartmann <chris@lollyrock.com>
2021-12-22 12:46:27 +0100

    ⭐️ support terraform objects

---
+++ lumi/resources/testdata/terraform/object_metdata.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  // metadata is a nested object and no block
+  metadata = {
+    enable-oslogin = true
+  }
+}
