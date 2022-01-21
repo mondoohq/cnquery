d36c60908032a34c6b4645971a4f7d4b7d604b5e
Christoph Hartmann <chris@lollyrock.com>
2022-01-21 12:04:59 +0100

    ⭐️ custom discovery for k8s to detect a better name

---
+++ motor/transports/k8s/k8s_test.go
@@ -0,0 +1,26 @@
+// +build debugtest
+
+package k8s
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mondoo.io/mondoo/motor/transports"
+)
+
+func TestKubernetes(t *testing.T) {
+	trans, err := New(&transports.TransportConfig{
+		Backend: transports.TransportBackend_CONNECTION_K8S,
+	})
+	require.NoError(t, err)
+
+	id, err := trans.Identifier()
+	require.NoError(t, err)
+	fmt.Println(id)
+
+	info, err := trans.ClusterInfo()
+	require.NoError(t, err)
+	fmt.Printf("%v", info)
+}
