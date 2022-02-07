f370782b7c3d422bace9825c1ec4ca42e3b72b2a
Jay Mundrawala <jay@mondoo.io>
2022-02-07 16:16:52 -0600

    🧹 Log agent identity and version
    This adds agent identity and version to the logs.
    
    Looks something like:
    ```
    INF  X-Client-Location=[] ctags={"agent":"//agents.api.mondoo.app/spaces/test-infallible-taussig-796596/serviceaccounts/24nguwC6pqBVqHpSh1dPHi9Vedw","mondoo-version":"unstable"} duration=431.434185 method=POST req-id=54c65b90-17a1-4500-9b68-b7a3709b0ccc req-size=4403 rsp-size=0 status=OK uri=/Collector/StoreScores
    ```

--- logger/context.go
+++ logger/context.go
@@ -10,6 +10,29 @@ import (
 
 const RequestIDFieldKey = "req-id"
 
+type tagsCtxKey struct{}
+
+type tags struct {
+	m map[string]string
+}
+
+func AddTag(ctx context.Context, tagName string, value string) {
+	if v, ok := ctx.Value(tagsCtxKey{}).(*tags); ok {
+		v.m[tagName] = value
+	}
+}
+
+func GetTags(ctx context.Context) map[string]string {
+	if v, ok := ctx.Value(tagsCtxKey{}).(*tags); ok {
+		return v.m
+	}
+	return nil
+}
+
+func WithTagsContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, tagsCtxKey{}, &tags{m: map[string]string{}})
+}
+
 // RequestScopedContext returns a context that contains a logger which logs the request ID
 // Given a context, a logger can be retrieved as follows
 //  ctx := RequestScopedContext(context.Background(), "req-id")
@@ -23,7 +46,7 @@ func RequestScopedContext(ctx context.Context, reqID string) context.Context {
 		reqID = "_" + uuid.New().String()
 	}
 	l := log.With().Str(RequestIDFieldKey, reqID).Logger()
-	return l.WithContext(ctx)
+	return WithTagsContext(l.WithContext(ctx))
 }
 
 // FromContext returns the logger in the context if present, otherwise the it
@@ -35,6 +58,15 @@ func FromContext(ctx context.Context) *zerolog.Logger {
 		// logger instead of the default noop logger
 		l := log.With().Str(RequestIDFieldKey, "global").Logger()
 		return &l
+	}
+	tags := GetTags(ctx)
+	if len(tags) > 0 {
+		dict := zerolog.Dict()
+		for k, v := range tags {
+			dict.Str(k, v)
+		}
+		lv := l.With().Dict("ctags", dict).Logger()
+		return &lv
 	}
 	return l
 }
