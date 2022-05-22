eec9a02a0ed22198eff204761be4943006f47c12
Christoph Hartmann <chris@lollyrock.com>
2022-05-22 16:19:22 +0200

    ⭐️ support certificate authentication for ms365 backend

--- motor/transports/ms365/client.go
+++ motor/transports/ms365/client.go
@@ -1,9 +1,11 @@
 package ms365
 
 import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/cockroachdb/errors"
 	a "github.com/microsoft/kiota-authentication-azure-go"
+	"go.mondoo.io/mondoo/motor/vault"
 )
 
 const DefaultMSGraphScope = "https://graph.microsoft.com/.default"
@@ -44,10 +46,29 @@ var (
 )
 
 func (t *Transport) Auth() (*a.AzureIdentityAuthenticationProvider, error) {
-	cred, err := azidentity.NewClientSecretCredential(t.tenantID, t.clientID, t.clientSecret, &azidentity.ClientSecretCredentialOptions{})
-	if err != nil {
-		return nil, errors.Wrap(err, "error creating credentials")
+	var credential azcore.TokenCredential
+	var err error
+
+	// we only support private key authentication for ms 365
+	switch t.cred.Type {
+	case vault.CredentialType_pkcs12:
+		certs, privateKey, err := azidentity.ParseCertificates(t.cred.Secret, []byte(t.cred.Password))
+		if err != nil {
+			return nil, errors.Wrap(err, "could not parse pfx file")
+		}
+
+		credential, err = azidentity.NewClientCertificateCredential(t.tenantID, t.clientID, certs, privateKey, &azidentity.ClientCertificateCredentialOptions{})
+		if err != nil {
+			return nil, errors.Wrap(err, "error creating credentials")
+		}
+	case vault.CredentialType_password:
+		credential, err = azidentity.NewClientSecretCredential(t.tenantID, t.clientID, string(t.cred.Secret), &azidentity.ClientSecretCredentialOptions{})
+		if err != nil {
+			return nil, errors.Wrap(err, "error creating credentials")
+		}
+	default:
+		return nil, errors.New("invalid secret configuration for ms365 transport: " + t.cred.Type.String())
 	}
 
-	return a.NewAzureIdentityAuthenticationProviderWithScopes(cred, DefaultMSGraphScopes)
+	return a.NewAzureIdentityAuthenticationProviderWithScopes(credential, DefaultMSGraphScopes)
 }
