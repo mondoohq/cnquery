bb797a2c7d600786cf1a865651cc633bd13aeeb9
Jay Mundrawala <jay@mondoo.io>
2022-07-01 11:25:05 -0500

    🐛 Handle /proc paths in docker through our catfs implementation
    The docker implemenation was doing a poor job handling directories. It
    listed all the contents of a directory on open, along with reading each
    file. The cat implemenation handles this better. It knows how to stat
    files using the stat command. Files are not read until needed

--- motor/transports/container/docker_engine/file.go
+++ motor/transports/container/docker_engine/file.go
@@ -11,16 +11,14 @@ import (
 	"path/filepath"
 	"strings"
 
-	"archive/tar"
-
 	dockertypes "github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	"github.com/rs/zerolog/log"
+	"github.com/spf13/afero"
 	"go.mondoo.io/mondoo/motor/transports"
 	"go.mondoo.io/mondoo/motor/transports/fsutil"
 )
 
-func FileOpen(dockerClient *client.Client, path string, container string, transport *Transport) (*File, error) {
+func FileOpen(dockerClient *client.Client, path string, container string, transport *Transport) (afero.File, error) {
 	f := &File{
 		path:         path,
 		dockerClient: dockerClient,
@@ -40,7 +38,7 @@ type File struct {
 }
 
 func (f *File) Open() error {
-	r, _, err := f.getFileReader(f.path)
+	r, _, err := f.getFileDockerReader(f.path)
 	if err != nil {
 		return os.ErrNotExist
 	}
@@ -62,7 +60,7 @@ func (f *File) Name() string {
 }
 
 func (f *File) Stat() (os.FileInfo, error) {
-	r, dstat, err := f.getFileReader(f.path)
+	r, dstat, err := f.getFileDockerReader(f.path)
 	if err != nil {
 		return nil, err
 	}
@@ -138,18 +136,6 @@ func (f *File) WriteString(s string) (ret int, err error) {
 	return 0, errors.New("not implemented")
 }
 
-func (f *File) getFileReader(path string) (io.ReadCloser, dockertypes.ContainerPathStat, error) {
-	// special handling for /proc file system, since you cannot copy them via
-	// the docker api
-	if strings.HasPrefix(f.path, "/proc") {
-		r, stat, err := f.getFileCatReader(f.path)
-		return r, stat, err
-	} else {
-		r, stat, err := f.getFileDockerReader(f.path)
-		return r, stat, err
-	}
-}
-
 func (f *File) getFileDockerReader(path string) (io.ReadCloser, dockertypes.ContainerPathStat, error) {
 	r, stat, err := f.dockerClient.CopyFromContainer(context.Background(), f.container, path)
 
@@ -161,74 +147,9 @@ func (f *File) getFileDockerReader(path string) (io.ReadCloser, dockertypes.Cont
 	return r, stat, err
 }
 
-// getFileCatReader is used for /proc/* on docker containers
-func (f *File) getFileCatReader(path string) (io.ReadCloser, dockertypes.ContainerPathStat, error) {
-	c, err := f.transport.RunCommand("find " + f.path + " -type f")
-	if err != nil {
-		return nil, dockertypes.ContainerPathStat{}, err
-	}
-	content, err := ioutil.ReadAll(c.Stdout)
-	if err != nil {
-		return nil, dockertypes.ContainerPathStat{}, err
-	}
-
-	// all files
-	entries := strings.Split(string(content), "\n")
-
-	// pipe content to a tar stream
-	tarReader, tarWriter := io.Pipe()
-
-	// stream content into the pipe
-	tw := tar.NewWriter(tarWriter)
-
-	go func() {
-		defer tw.Close()
-		// create a tar stream by reading all the content via cat
-		for i := range entries {
-			path := entries[i]
-			ec, err := f.transport.RunCommand("cat " + entries[i])
-			if err != nil {
-				log.Error().Str("file", path).Err(err).Msg("docker> could read file content")
-				continue
-			}
-
-			econtent, err := ioutil.ReadAll(ec.Stdout)
-			if err != nil {
-				log.Error().Str("file", path).Err(err).Msg("docker> could read file content")
-				continue
-			}
-
-			// send tar header
-			hdr := &tar.Header{
-				Name: path,
-				// Mode: int64(fileinfo.Mode()),
-				Size: int64(len(econtent)),
-			}
-
-			if err := tw.WriteHeader(hdr); err != nil {
-				log.Error().Str("file", path).Err(err).Msg("docker> could not write tar header")
-			}
-
-			_, err = tw.Write(econtent)
-			if err != nil {
-				log.Error().Str("file", path).Err(err).Msg("docker> could not write tar stream")
-			}
-		}
-	}()
-
-	return tarReader, dockertypes.ContainerPathStat{}, nil
-}
-
 // returns a TarReader stream the caller is responsible for closing the stream
 func (f *File) Tar() (io.ReadCloser, error) {
-	// special handling for /proc file system, since you cannot copy them via
-	// the docker api
-	if strings.HasPrefix(f.path, "/proc") {
-		r, _, err := f.getFileCatReader(f.path)
-		return r, err
-	}
-
-	r, _, err := f.getFileReader(f.path)
+	r, _, err := f.getFileDockerReader(f.path)
 	return r, err
 }
 
