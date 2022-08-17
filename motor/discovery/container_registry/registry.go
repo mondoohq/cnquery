a4a36b66a1054bf061ab4bcfe714484a260ae1da
Christian Zunker <christian@mondoo.com>
2022-08-17 13:49:02 +0200

    🐛 Fix platform detection for registry images
    The platform is now also detected for images pulled from a registry.
    
    Fixes #1359
    
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- motor/discovery/container_registry/registry.go
+++ motor/discovery/container_registry/registry.go
@@ -185,8 +185,6 @@ func (a *DockerRegistryImages) toAsset(ref name.Reference, creds []*vault.Creden
 		PlatformIds: []string{containerid.MondooContainerImageID(imgDigest)},
 		Name:        imageUrl,
 		Platform: &platform.Platform{
-			Name:    "docker-image",
-			Title:   "Docker Image",
 			Kind:    providers.Kind_KIND_CONTAINER_IMAGE,
 			Runtime: providers.RUNTIME_DOCKER_REGISTRY,
 		},
