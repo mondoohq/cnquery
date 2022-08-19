f913f9d147c36df9b2197291bb4565682897ea0e
Christian Zunker <christian@mondoo.com>
2022-08-19 05:55:03 +0200

    🐛 Fix platform title for non OS and non k8s resources
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- motor/platform/platform_test.go
+++ motor/platform/platform_test.go
@@ -76,34 +76,45 @@ func TestPrettyTitle(t *testing.T) {
 			Platform: &Platform{
 				Title:   "Kali GNU/Linux Rolling",
 				Version: "2019.4",
+				Family:  []string{"linux", "unix", "os"},
 			},
 			Expected: "Kali GNU/Linux Rolling",
 		},
 		{
 			Platform: &Platform{
 				Title:   "Red Hat Enterprise Linux",
-				Runtime: providers.RUNTIME_AWS,
-				Kind:    providers.Kind_KIND_API,
+				Runtime: providers.RUNTIME_AWS_EC2,
 				Version: "7",
+				Family:  []string{"linux", "unix", "os"},
 			},
-			Expected: "Red Hat Enterprise Linux, Amazon Web Services",
+			Expected: "Red Hat Enterprise Linux, AWS EC2 Instance",
 		},
 		{
 			Platform: &Platform{
 				Title:   "Red Hat Enterprise Linux",
 				Kind:    providers.Kind_KIND_API,
 				Version: "7",
+				Family:  []string{"linux", "unix"},
 			},
-			Expected: "Red Hat Enterprise Linux, API",
+			Expected: "Red Hat Enterprise Linux",
+		},
+		{
+			Platform: &Platform{
+				Title:   "Red Hat Enterprise Linux",
+				Kind:    providers.Kind_KIND_BARE_METAL,
+				Version: "7",
+				Family:  []string{"linux", "unix", "os"},
+			},
+			Expected: "Red Hat Enterprise Linux, bare metal",
 		},
 		{
 			Platform: &Platform{
 				Title:   "Red Hat Enterprise Linux 8",
-				Runtime: providers.RUNTIME_AWS,
-				Kind:    providers.Kind_KIND_API,
+				Kind:    providers.Kind_KIND_CONTAINER_IMAGE,
 				Version: "8",
+				Family:  []string{"linux", "unix", "os"},
 			},
-			Expected: "Red Hat Enterprise Linux 8, Amazon Web Services",
+			Expected: "Red Hat Enterprise Linux 8, Container Image",
 		},
 		{
 			Platform: &Platform{
@@ -113,6 +124,30 @@ func TestPrettyTitle(t *testing.T) {
 				Version: "8",
 			},
 			Expected: "Amazon Web Services 8",
+		},
+		{
+			Platform: &Platform{
+				Title:   "Test Deployment",
+				Runtime: providers.RUNTIME_KUBERNETES_CLUSTER,
+				Family:  []string{"k8s-workload", "k8s"},
+			},
+			Expected: "Test Deployment, Kubernetes Cluster",
+		},
+		{
+			Platform: &Platform{
+				Title:   "Test Deployment",
+				Runtime: providers.RUNTIME_KUBERNETES_MANIFEST,
+				Family:  []string{"k8s-workload", "k8s"},
+			},
+			Expected: "Test Deployment, Kubernetes Manifest File",
+		},
+		{
+			Platform: &Platform{
+				Title:   "k8s test cluster",
+				Runtime: providers.RUNTIME_KUBERNETES,
+				Family:  []string{"k8s"},
+			},
+			Expected: "k8s test cluster",
 		},
 	}
 
