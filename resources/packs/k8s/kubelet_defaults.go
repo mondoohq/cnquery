ebf6fa0d2c19764ff693f888e1eb83f549d93550
Christian Zunker <827818+czunker@users.noreply.github.com>
2022-09-23 09:59:40 +0200

    🧹 Fix license header (#92)
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- resources/packs/k8s/kubelet_defaults.go
+++ resources/packs/k8s/kubelet_defaults.go
@@ -1,22 +1,21 @@
-// copyright: 2019, Dominik Richter and Christoph Hartmann
-// author: Dominik Richter
-// author: Christoph Hartmann
-
-// https://github.com/kubernetes/kubernetes/blob/release-1.25/pkg/kubelet/apis/config/v1beta1/defaults.go
-package k8s
-
 /*
 Copyright 2015 The Kubernetes Authors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// https://github.com/kubernetes/kubernetes/blob/release-1.25/pkg/kubelet/apis/config/v1beta1/defaults.go
+package k8s
 
 import (
 	"time"
