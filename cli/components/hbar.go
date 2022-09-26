20beb21a4302720b723707757bed5d6fedcee1a5
Dominik Richter <dominik.richter@gmail.com>
2022-09-26 01:19:00 -0700

    ✨ simple cli hbar ported (#104)
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ cli/components/hbar.go
@@ -0,0 +1,23 @@
+package components
+
+import (
+	"math"
+	"strings"
+)
+
+// Hbar creates a horizontal bar up to len characters with a
+// length proportional to percent, in range of [0, 100]
+func Hbar(len uint32, percent float32) string {
+	if percent > 100 {
+		percent = 100
+	}
+	if percent < 0 {
+		percent = 0
+	}
+	segments := math.Round(float64(len) * (float64(percent) / 100))
+
+	if segments == 0 {
+		return ""
+	}
+	return strings.Repeat("█", int(segments)) + " "
+}
