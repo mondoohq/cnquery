11c8984046d606df7da7b8c275dae2f0d3f6a06c
Ivan Milchev <ivan@mondoo.com>
2022-10-05 20:32:33 +0200

    🧹 Improve logging of k8s namespace filter (#173)
    Instead of logging multiple messages for every namespace in the cluster
    now we log just 1 warning at the start
    
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- motor/discovery/k8s/list_jobs.go
+++ motor/discovery/k8s/list_jobs.go
@@ -44,7 +44,7 @@ func ListJobs(
 		for i := range namespaces {
 			namespace := namespaces[i]
 			if !isIncluded(namespace.Name, namespaceFilter) {
-				log.Info().Str("namespace", namespace.Name).Strs("filter", namespaceFilter).Msg("namespace not included")
+				log.Debug().Str("namespace", namespace.Name).Strs("filter", namespaceFilter).Msg("namespace not included")
 				continue
 			}
 
