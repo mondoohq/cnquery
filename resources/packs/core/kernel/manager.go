d49a9332cc4e0d9d7172c3afb74fa88fa8fa4a98
Christoph Hartmann <chris@lollyrock.com>
2022-10-06 14:21:54 +0200

    🐛 handle sysctl exec properly and introduce log messages
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- resources/packs/core/kernel/manager.go
+++ resources/packs/core/kernel/manager.go
@@ -108,14 +108,15 @@ func (s *LinuxKernelManager) Info() (KernelInfo, error) {
 func (s *LinuxKernelManager) Parameters() (map[string]string, error) {
 	if s.provider.Capabilities().HasCapability(providers.Capability_RunCommand) {
 		cmd, err := s.provider.RunCommand("/sbin/sysctl -a")
-		// means sysctl is not present, fallback to /proc/sys walking
-		if err == nil {
+		// in case of err, the command is not there and we fallback to /proc/sys walking
+		if err == nil && cmd.ExitStatus == 0 {
+			log.Debug().Msg("using sysctl to read kernel parameters")
 			return ParseSysctl(cmd.Stdout, "=")
 		}
 	}
 
+	log.Debug().Msg("using /proc/sys walking to read kernel parameters")
 	fs := s.provider.FS()
-
 	fsUtil := afero.Afero{Fs: fs}
 	kernelParameters := make(map[string]string)
 	err := fsUtil.Walk(sysctlPath, func(path string, f os.FileInfo, err error) error {
