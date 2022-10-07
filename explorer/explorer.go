73d1790c390abce211a579148c3acb933c7e4b5a
Dominik Richter <dominik.richter@gmail.com>
2022-10-07 00:18:44 -0700

    🛑 rename proto so it is less likely to conflict
    Since protos conflict on names and global services in GRPC
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -1,6 +1,6 @@
 package explorer
 
-//go:generate protoc --proto_path=../:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. explorer.proto
+//go:generate protoc --proto_path=../:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. cnquery_explorer.proto
 
 const (
 	SERVICE_NAME = "explorer.api.mondoo.com"
