8b73f5b9ffe518056f5e323ebde0b9633eaf6222
Jay Mundrawala <jay@mondoo.com>
2022-10-24 10:46:17 -0500

    7.0.3
    Signed-off-by: Jay Mundrawala <jay@mondoo.com>

--- scripts/pkg/windows/chocolatey/cnquery/tools/chocolateyInstall.ps1
+++ scripts/pkg/windows/chocolatey/cnquery/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$version  = '6.19.0'
+$version  = '7.0.3'
 $url      = "https://releases.mondoo.com/cnquery/${version}/cnquery_${version}_windows_amd64.zip"
-$checksum = 'e58b0becdd0232a2a7e90b0e53ba40105d75eb2b33412f93309beda1c7293662'
+$checksum = '886c778d8368d71de18efb60f3cb24032c475e8c3eaabdab197597e36ed1130f'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
