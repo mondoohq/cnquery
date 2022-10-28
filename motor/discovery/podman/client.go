0fd6aaaffcc33a8b43fd233ddb59971202d04183
Preslav <preslav@mondoo.com>
2022-10-28 23:00:49 +0200

    add discovery of podman connection details.

--- motor/discovery/podman/client.go
+++ motor/discovery/podman/client.go
@@ -3,7 +3,9 @@ package podman
 import (
 	"context"
 
+	"github.com/containers/common/pkg/config"
 	"github.com/containers/podman/v4/pkg/bindings"
+	"github.com/pkg/errors"
 )
 
 type podmanClient struct {
@@ -11,7 +13,15 @@ type podmanClient struct {
 }
 
 func NewPodmanDiscovery() (*podmanClient, error) {
-	conn, err := bindings.NewConnectionWithIdentity(context.Background(), "socket here", "identity", true)
+	cfg, err := config.ReadCustomConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "podman config not found")
+	}
+	url, identity, isMachine, err := cfg.ActiveDestination()
+	if err != nil {
+		return nil, err
+	}
+	conn, err := bindings.NewConnectionWithIdentity(context.Background(), url, identity, isMachine)
 	if err != nil {
 		return nil, err
 	}
