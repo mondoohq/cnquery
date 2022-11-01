7fdf5f86b6abaefa0c735e8db6ba921ea273dcfe
Christoph Hartmann <chris@lollyrock.com>
2022-11-01 15:06:26 +0100

    🐛 add missing errors import (#422)

--- resources/packs/core/processes/linuxproc_windows.go
+++ resources/packs/core/processes/linuxproc_windows.go
@@ -2,6 +2,10 @@
 
 package processes
 
+import (
+	"errors"
+)
+
 // Read out all connected sockets. This is not yet implemented on non-Unix
 // systems and needs some work to function via remote connections
 func (lpm *LinuxProcManager) procSocketInods(pid int64, procPidPath string) ([]int64, error) {
