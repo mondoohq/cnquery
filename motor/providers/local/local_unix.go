50bb02862da5f1bbbeae32e93b967f3ff6f4d650
Christian Zunker <827818+czunker@users.noreply.github.com>
2022-12-01 08:21:27 +0100

    🐛 Fix file stat when run with sudo (#517)
    Fixes #512
    
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- motor/providers/local/local_unix.go
+++ motor/providers/local/local_unix.go
@@ -6,11 +6,17 @@ package local
 import (
 	"os"
 	"syscall"
+
+	pos "go.mondoo.com/cnquery/motor/providers/os"
 )
 
 func (p *Provider) fileowner(stat os.FileInfo) (int64, int64) {
 	uid := int64(-1)
 	gid := int64(-1)
+	if stat, ok := stat.Sys().(*pos.FileInfo); ok {
+		uid = stat.Uid
+		gid = stat.Gid
+	}
 	if stat, ok := stat.Sys().(*syscall.Stat_t); ok {
 		uid = int64(stat.Uid)
 		gid = int64(stat.Gid)
