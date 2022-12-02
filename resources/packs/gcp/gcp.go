2a54f7dc740a0c0ae22cdd7cfb4de4f4c282fb51
Christoph Hartmann <chris@lollyrock.com>
2022-12-01 20:56:07 -0800

    🧹 refactor gcp pack

--- resources/packs/gcp/gcp.go
+++ resources/packs/gcp/gcp.go
@@ -1,9 +1,23 @@
 package gcp
 
-import "go.mondoo.com/cnquery/resources/packs/gcp/info"
+import (
+	"errors"
+
+	"go.mondoo.com/cnquery/motor/providers"
+	gcp_provider "go.mondoo.com/cnquery/motor/providers/google"
+	"go.mondoo.com/cnquery/resources/packs/gcp/info"
+)
 
 var Registry = info.Registry
 
 func init() {
 	Init(Registry)
+}
+
+func gcpProvider(t providers.Instance) (*gcp_provider.Provider, error) {
+	provider, ok := t.(*gcp_provider.Provider)
+	if !ok {
+		return nil, errors.New("gcp resource is not supported on this transport")
+	}
+	return provider, nil
 }
