350f8942eaee2b670db74d7e46c61e331054d962
Christoph Hartmann <chris@lollyrock.com>
2022-12-12 12:52:09 +0100

    ⭐️ expose macOS updates as `os.updates` (#587)
    addresses #588
    after #586 
    
    brings the handling for os updates to macOS
    
    ```
    cnquery> os.updates
    os.updates: [
      0: os.update name="MSU_UPDATE_21G217_patch_12.6.1"
      1: os.update name="Command Line Tools beta 3 for Xcode"
      2: os.update name="Command Line Tools for Xcode"
      3: os.update name="Safari16.1MontereyAuto"
    ]
    ```

---
+++ resources/packs/os/updates/mac_updates_test.go
@@ -0,0 +1,26 @@
+package updates
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMacUpdatesParser(t *testing.T) {
+	f, err := os.Open("./testdata/com.apple.SoftwareUpdate.plist")
+	defer f.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m, err := ParseSoftwarePlistUpdates(f)
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(m), "detected the right amount of updates")
+
+	pkg, err := findKb(m, "MSU_UPDATE_21G217_patch_12.6.1")
+	require.NoError(t, err)
+	assert.Equal(t, "MSU_UPDATE_21G217_patch_12.6.1", pkg.Name, "update detected")
+	assert.Equal(t, "macOS Monterey 12.6.1", pkg.Description, "update title detected")
+}
