769a9e3c81bc227f34f6c5cf057932cb0bed2ad7
Ivan Milchev <ivan@mondoo.com>
2022-12-12 11:17:09 +0100

    revert unintended change
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- apps/cnquery/cmd/builder/parse.go
+++ apps/cnquery/cmd/builder/parse.go
@@ -220,6 +220,7 @@ func ParseTargetAsset(cmd *cobra.Command, args []string, providerType providers.
 		}
 	case providers.ProviderType_DOCKER:
 		connection.Backend = providerType
+		connection.Host = args[0]
 	case providers.ProviderType_K8S:
 		connection.Backend = providerType
 
