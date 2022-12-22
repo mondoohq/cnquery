d1c6eefc9ea9b92d9ac0bb8e485f823a7dcc9ed4
Joel Diaz <joel@mondoo.com>
2022-12-22 10:29:30 -0500

    ⭐️ k8s-ingress with certificates (#607)

--- resources/packs/k8s/ingress.go
+++ resources/packs/k8s/ingress.go
@@ -6,6 +6,7 @@ import (
 
 	"go.mondoo.com/cnquery/resources"
 	"go.mondoo.com/cnquery/resources/packs/core"
+
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -33,6 +34,11 @@ func (k *mqlK8s) GetIngresses() ([]interface{}, error) {
 			return nil, err
 		}
 
+		tls, err := getTLS(ingress, objId, k.MotorRuntime, k.GetSecrets)
+		if err != nil {
+			return nil, err
+		}
+
 		r, err := k.MotorRuntime.CreateResource("k8s.ingress",
 			"id", objId,
 			"uid", string(obj.GetUID()),
@@ -43,6 +49,7 @@ func (k *mqlK8s) GetIngresses() ([]interface{}, error) {
 			"created", &ts.Time,
 			"manifest", manifest,
 			"rules", rules,
+			"tls", tls,
 		)
 		if err != nil {
 			return nil, err
@@ -224,4 +231,88 @@ func (k *mqlK8sIngressservicebackend) id() (string, error) {
 
 func (k *mqlK8sIngressresourceref) id() (string, error) {
 	return k.Id()
+}
+
+func (k *mqlK8sIngresstls) id() (string, error) {
+	return k.Id()
+}
+
+func getTLS(ingress *networkingv1.Ingress, objId string, motorRuntime *resources.Runtime, getSecrets func() ([]interface{}, error)) ([]interface{}, error) {
+	tlsData := []interface{}{}
+	if len(ingress.Spec.TLS) > 0 {
+		// This returns ALL Secrets found in the cluster!
+		secretsInterface, err := getSecrets()
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch Secrets referenced in Ingress: %s", err)
+		}
+
+		// Build up a map of Secrets found in the same Namespace as this Ingress resource
+		secrets := map[string]*mqlK8sSecret{}
+		for _, secInterface := range secretsInterface {
+			secret, ok := secInterface.(*mqlK8sSecret)
+			if !ok {
+				return nil, errors.New("returned list of Secrets failed type assertion")
+			}
+
+			ns, err := secret.Namespace()
+			if err != nil {
+				return nil, err
+			}
+
+			if ingress.Namespace != ns {
+				continue
+			}
+			name, err := secret.Name()
+			if err != nil {
+				return nil, err
+			}
+			secrets[name] = secret
+		}
+
+		// There is the potential for no Secret to be found or that a Secret
+		// is found (can happen when scanning static manifest files or simply an Ingress
+		// which references a non-existent Secret) but either improperly-formatted
+		// or simply not containing TLS data. In either event just keep trying to
+		// process as much as we can.
+		for i, tls := range ingress.Spec.TLS {
+			secret, ok := secrets[tls.SecretName]
+			if !ok {
+				continue
+			}
+
+			certs, err := secret.GetCertificates()
+			if err != nil {
+				return nil, errors.New("error getting certificate data from Secret")
+			}
+			if certs == nil {
+				// no TLS data in Secret referenced
+				// k8s will allow this, so we'll just follow along with this being
+				// a non-critical issue and skip processing the Secret
+				continue
+			}
+
+			certList, ok := certs.([]interface{})
+			if !ok {
+				return nil, fmt.Errorf("expected a list of Certificates")
+			}
+
+			hosts := make([]interface{}, len(tls.Hosts))
+			for j, host := range tls.Hosts {
+				hosts[j] = host
+			}
+
+			ingressTls, err := motorRuntime.CreateResource("k8s.ingresstls",
+				"id", fmt.Sprintf("%s-tls%d", objId, i),
+				"hosts", hosts,
+				"certificates", certList,
+			)
+			if err != nil {
+				return nil, fmt.Errorf("error creating k8s.ingresstls: %s", err)
+			}
+
+			tlsData = append(tlsData, ingressTls)
+		}
+	}
+
+	return tlsData, nil
 }
