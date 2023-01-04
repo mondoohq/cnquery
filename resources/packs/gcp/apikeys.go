72e4e8324e5094cb20c16acc9dd2a0f4709e0ea2
Ivan Milchev <ivan@mondoo.com>
2023-01-04 22:13:15 +0100

    implement gcp api keys restrictions
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- resources/packs/gcp/apikeys.go
+++ resources/packs/gcp/apikeys.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"go.mondoo.com/cnquery/resources"
 	"go.mondoo.com/cnquery/resources/packs/core"
 	"google.golang.org/api/apikeys/v2"
 	"google.golang.org/api/option"
@@ -32,13 +33,98 @@ func (g *mqlGcpProject) GetApiKeys() ([]interface{}, error) {
 		return nil, err
 	}
 
-	keys, err := apiKeysSvc.Projects.Locations.Keys.List(fmt.Sprintf("projects/%s/locations/-", projectId)).Do()
+	keys, err := apiKeysSvc.Projects.Locations.Keys.List(fmt.Sprintf("projects/%s/locations/global", projectId)).Do()
 	if err != nil {
 		return nil, err
 	}
 
 	mqlKeys := make([]interface{}, 0, len(keys.Keys))
 	for _, k := range keys.Keys {
+		var mqlRestrictions resources.ResourceType
+		if k.Restrictions != nil {
+			var mqlAndroidRestr resources.ResourceType
+			if k.Restrictions.AndroidKeyRestrictions != nil {
+				mqlAllowedApps := make([]resources.ResourceType, 0, len(k.Restrictions.AndroidKeyRestrictions.AllowedApplications))
+				for _, a := range k.Restrictions.AndroidKeyRestrictions.AllowedApplications {
+					mqlAllowedApp, err := g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions.androidKeyRestrictions.application",
+						"id", fmt.Sprintf("%s/androidRestrictions/allowedApplications/%s", k.Name, a.PackageName),
+						"packageName", a.PackageName,
+						"sha1Fingerprint", a.Sha1Fingerprint,
+					)
+					if err != nil {
+						return nil, err
+					}
+					mqlAllowedApps = append(mqlAllowedApps, mqlAllowedApp)
+				}
+
+				mqlAndroidRestr, err = g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions.androidKeyRestrictions",
+					"id", fmt.Sprintf("%s/androidRestrictions", k.Name),
+					"allowedApplications", mqlAllowedApps,
+				)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			mqlApiTargets := make([]interface{}, 0, len(k.Restrictions.ApiTargets))
+			for i, a := range k.Restrictions.ApiTargets {
+				mqlApiTarget, err := g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions.apiTarget",
+					"id", fmt.Sprintf("%s/apiTargets/%d", k.Name, i),
+					"methods", core.StrSliceToInterface(a.Methods),
+					"service", a.Service,
+				)
+				if err != nil {
+					return nil, err
+				}
+				mqlApiTargets = append(mqlApiTargets, mqlApiTarget)
+			}
+
+			var mqlBrowserRest resources.ResourceType
+			if k.Restrictions.BrowserKeyRestrictions != nil {
+				mqlBrowserRest, err = g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions.browserKeyRestrictions",
+					"id", fmt.Sprintf("%s/browserKeyRestrictions", k.Name),
+					"allowedReferrers", core.StrSliceToInterface(k.Restrictions.BrowserKeyRestrictions.AllowedReferrers),
+				)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			var mqlIosRestr resources.ResourceType
+			if k.Restrictions.IosKeyRestrictions != nil {
+				mqlIosRestr, err = g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions.iosKeyRestrictions",
+					"id", fmt.Sprintf("%s/iosKeyRestrictions", k.Name),
+					"allowedBundleIds", core.StrSliceToInterface(k.Restrictions.IosKeyRestrictions.AllowedBundleIds),
+				)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			var mqlServerKeyRestr resources.ResourceType
+			if k.Restrictions.ServerKeyRestrictions != nil {
+				mqlServerKeyRestr, err = g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions.serverKeyRestrictions",
+					"id", fmt.Sprintf("%s/serverKeyRestrictions", k.Name),
+					"allowedIps", core.StrSliceToInterface(k.Restrictions.ServerKeyRestrictions.AllowedIps),
+				)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			mqlRestrictions, err = g.MotorRuntime.CreateResource("gcp.project.apiKey.restrictions",
+				"parentResourcePath", k.Name,
+				"androidKeyRestrictions", mqlAndroidRestr,
+				"apiTargets", mqlApiTargets,
+				"browserKeyRestrictions", mqlBrowserRest,
+				"iosKeyRestrictions", mqlIosRestr,
+				"serverKeyRestrictions", mqlServerKeyRestr,
+			)
+			if err != nil {
+				return nil, err
+			}
+		}
+
 		mqlKey, err := g.MotorRuntime.CreateResource("gcp.project.apiKey",
 			"projectId", projectId,
 			"resourcePath", k.Name,
@@ -48,7 +134,7 @@ func (g *mqlGcpProject) GetApiKeys() ([]interface{}, error) {
 			"displayName", k.DisplayName,
 			"keyString", k.KeyString,
 			"name", parseResourceName(k.Name),
-			"restrictions", nil, // TODO
+			"restrictions", mqlRestrictions,
 			"updated", parseTime(k.UpdateTime),
 		)
 		if err != nil {
@@ -63,34 +149,34 @@ func (g *mqlGcpProjectApiKey) id() (string, error) {
 	return g.ResourcePath()
 }
 
-func (g *mqlGcpProjectApiKeyRestriction) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictions) id() (string, error) {
 	parent, err := g.ParentResourcePath()
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("%s/restriction", parent), nil
+	return fmt.Sprintf("%s/restrictions", parent), nil
 }
 
-func (g *mqlGcpProjectApiKeyRestrictionAndroidKeyRestrictions) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictionsAndroidKeyRestrictions) id() (string, error) {
 	return g.Id()
 }
 
-func (g *mqlGcpProjectApiKeyRestrictionAndroidKeyRestrictionsApplication) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictionsAndroidKeyRestrictionsApplication) id() (string, error) {
 	return g.Id()
 }
 
-func (g *mqlGcpProjectApiKeyRestrictionApiTarget) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictionsApiTarget) id() (string, error) {
 	return g.Id()
 }
 
-func (g *mqlGcpProjectApiKeyRestrictionBrowserKeyRestrictions) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictionsBrowserKeyRestrictions) id() (string, error) {
 	return g.Id()
 }
 
-func (g *mqlGcpProjectApiKeyRestrictionIosKeyRestrictions) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictionsIosKeyRestrictions) id() (string, error) {
 	return g.Id()
 }
 
-func (g *mqlGcpProjectApiKeyRestrictionServerKeyRestrictions) id() (string, error) {
+func (g *mqlGcpProjectApiKeyRestrictionsServerKeyRestrictions) id() (string, error) {
 	return g.Id()
 }
