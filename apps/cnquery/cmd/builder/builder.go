036103212e455c644985b70d3c129d1a100f93c5
Tim Smith <tsmith84@gmail.com>
2023-01-06 14:38:36 -0800

    Update apps/cnquery/cmd/builder/builder.go
    Co-authored-by: Letha <letha@mondoo.com>

--- apps/cnquery/cmd/builder/builder.go
+++ apps/cnquery/cmd/builder/builder.go
@@ -573,7 +573,7 @@ func azureProviderCmd(commonCmdFlags commonFlagsFn, preRun commonPreRunFn, runFn
 	cmd.Flags().String("tenant-id", "", "Directory (tenant) ID of the service principal")
 	cmd.Flags().String("client-id", "", "Application (client) ID of the service principal")
 	cmd.Flags().String("client-secret", "", "Secret for application")
-	cmd.Flags().String("certificate-path", "", "Path to certificate that's used for certificate-based authentication in PKCS 12 format (pfx)")
+	cmd.Flags().String("certificate-path", "", "Path (in PKCS #12/PFX format) to the authentication certificate")
 	cmd.Flags().String("certificate-secret", "", "Passphrase for certificate file")
 	cmd.Flags().String("subscription", "", "ID of the Azure subscription to scan")
 	cmd.Flags().String("subscriptions", "", "Comma-separated list of Azure subscriptions to include")
