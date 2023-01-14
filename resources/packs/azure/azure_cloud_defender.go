9610dc2c4fbe8a4e150ed78f8deedcf9cca6bc79
Preslav <preslav@mondoo.com>
2023-01-14 11:41:05 +0200

    ✨ Add azure.cloudDefender.defenderForContainers and azure.cloudDefender.defenderForServers
    Signed-off-by: Preslav <preslav@mondoo.com>

--- resources/packs/azure/azure_cloud_defender.go
+++ resources/packs/azure/azure_cloud_defender.go
@@ -9,13 +9,26 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	security "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/security/armsecurity"
+
 	"go.mondoo.com/cnquery/resources/packs/core"
+)
+
+const (
+	vaQualysPolicyDefinitionId string = "/providers/Microsoft.Authorization/policyDefinitions/13ce0167-8ca6-4048-8e6b-f996402e3c1b"
+
+	// There are two policy per component: one for ARC clusters and one for k8s clusters
+	arcClusterDefenderExtensionDefinitionId        string = "/providers/Microsoft.Authorization/policyDefinitions/708b60a6-d253-4fe0-9114-4be4c00f012c"
+	kubernetesClusterDefenderExtensionDefinitionId string = "/providers/Microsoft.Authorization/policyDefinitions/64def556-fbad-4622-930e-72d1d5589bf5"
+
+	arcClusterPolicyExtensionDefinitionId       string = "/providers/Microsoft.Authorization/policyDefinitions/0adc5395-9169-4b9b-8687-af838d69410a"
+	kubernetesClusterPolicyExtensonDefinitionId string = "/providers/Microsoft.Authorization/policyDefinitions/0adc5395-9169-4b9b-8687-af838d69410a"
 )
 
 func (a *mqlAzureCloudDefender) id() (string, error) {
@@ -44,6 +57,113 @@ func (a *mqlAzureCloudDefender) GetMonitoringAgentAutoProvision() (interface{}, 
 	}
 	autoProvision := *setting.Properties.AutoProvision
 	return autoProvision == security.AutoProvisionOn, nil
+}
+
+func (a *mqlAzureCloudDefender) GetDefenderForContainers() (interface{}, error) {
+	at, err := azureTransport(a.MotorRuntime.Motor.Provider)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	token, err := at.GetTokenCredential()
+	if err != nil {
+		return nil, err
+	}
+	rawToken, err := token.GetToken(ctx, policy.TokenRequestOptions{
+		Scopes: []string{"https://management.core.windows.net//.default"},
+	})
+	if err != nil {
+		return nil, err
+	}
+	ep := cloud.AzurePublic.Services[cloud.ResourceManager].Endpoint
+	pas, err := getPolicyAssignments(ctx, at.SubscriptionID(), ep, rawToken.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	type defenderForContainers struct {
+		DefenderDaemonSet        bool `json:"defenderDaemonSet"`
+		AzurePolicyForKubernetes bool `json:"azurePolicyForKubernetes"`
+	}
+
+	kubernetesDefender := false
+	arcDefender := false
+	kubernetesPolicyExt := false
+	arcPoilcyExt := false
+	for _, it := range pas.PolicyAssignments {
+		if it.Properties.PolicyDefinitionID == arcClusterDefenderExtensionDefinitionId &&
+			it.Properties.Scope == fmt.Sprintf("/subscriptions/%s", at.SubscriptionID()) {
+			arcDefender = true
+		}
+		if it.Properties.PolicyDefinitionID == kubernetesClusterDefenderExtensionDefinitionId &&
+			it.Properties.Scope == fmt.Sprintf("/subscriptions/%s", at.SubscriptionID()) {
+			kubernetesDefender = true
+		}
+		if it.Properties.PolicyDefinitionID == arcClusterPolicyExtensionDefinitionId &&
+			it.Properties.Scope == fmt.Sprintf("/subscriptions/%s", at.SubscriptionID()) {
+			arcPoilcyExt = true
+		}
+		if it.Properties.PolicyDefinitionID == kubernetesClusterPolicyExtensonDefinitionId &&
+			it.Properties.Scope == fmt.Sprintf("/subscriptions/%s", at.SubscriptionID()) {
+			kubernetesPolicyExt = true
+		}
+	}
+
+	def := defenderForContainers{
+		DefenderDaemonSet:        arcDefender && kubernetesDefender,
+		AzurePolicyForKubernetes: arcPoilcyExt && kubernetesPolicyExt,
+	}
+	return core.JsonToDict(def)
+}
+
+func (a *mqlAzureCloudDefender) GetDefenderForServers() (interface{}, error) {
+	at, err := azureTransport(a.MotorRuntime.Motor.Provider)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	token, err := at.GetTokenCredential()
+	if err != nil {
+		return nil, err
+	}
+	rawToken, err := token.GetToken(ctx, policy.TokenRequestOptions{
+		Scopes: []string{"https://management.core.windows.net//.default"},
+	})
+	if err != nil {
+		return nil, err
+	}
+	ep := cloud.AzurePublic.Services[cloud.ResourceManager].Endpoint
+	list, err := getPolicyAssignments(ctx, at.SubscriptionID(), ep, rawToken.Token)
+	if err != nil {
+		return nil, err
+	}
+	serverVASetings, err := getServerVulnAssessmentSettings(ctx, at.SubscriptionID(), ep, rawToken.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	type defenderForServers struct {
+		Enabled                         bool   `json:"enabled"`
+		VulnerabilityManagementToolName string `json:"vulnerabilityManagementToolName"`
+	}
+
+	resp := defenderForServers{}
+	for _, it := range list.PolicyAssignments {
+		if it.Properties.PolicyDefinitionID == vaQualysPolicyDefinitionId {
+			resp.Enabled = true
+			resp.VulnerabilityManagementToolName = "Microsoft Defender for Cloud integrated Qualys scanner"
+		}
+	}
+	for _, sett := range serverVASetings.Settings {
+		if sett.Properties.SelectedProvider == "MdeTvm" && sett.Name == "AzureServersSetting" {
+			resp.Enabled = true
+			resp.VulnerabilityManagementToolName = "Microsoft Defender vulnerability management"
+
+		}
+	}
+	return core.JsonToDict(resp)
 }
 
 func (a *mqlAzureCloudDefender) GetSecurityContacts() (interface{}, error) {
@@ -135,4 +255,124 @@ func getSecurityContacts(ctx context.Context, subscriptionId, host, token string
 	result := []security.Contact{}
 	err = json.Unmarshal(raw, &result)
 	return result, err
+}
+
+func getPolicyAssignments(ctx context.Context, subscriptionId, host, token string) (PolicyAssignments, error) {
+	urlPath := "/subscriptions/{subscriptionId}/providers/Microsoft.Authorization/policyAssignments"
+	urlPath = strings.ReplaceAll(urlPath, "{subscriptionId}", url.PathEscape(subscriptionId))
+	urlPath = runtime.JoinPaths(host, urlPath)
+	client := http.Client{}
+	req, err := http.NewRequest("GET", urlPath, nil)
+	if err != nil {
+		return PolicyAssignments{}, err
+	}
+	q := req.URL.Query()
+	q.Set("api-version", "2022-06-01")
+	req.URL.RawQuery = q.Encode()
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return PolicyAssignments{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return PolicyAssignments{}, errors.New("failed to fetch security contacts from " + urlPath + ": " + resp.Status)
+	}
+
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return PolicyAssignments{}, err
+	}
+	result := PolicyAssignments{}
+	err = json.Unmarshal(raw, &result)
+	return result, err
+}
+
+func getServerVulnAssessmentSettings(ctx context.Context, subscriptionId, host, token string) (ServerVulnerabilityAssessmentsSettingsList, error) {
+	urlPath := "/subscriptions/{subscriptionId}/providers/Microsoft.Security/serverVulnerabilityAssessmentsSettings"
+	urlPath = strings.ReplaceAll(urlPath, "{subscriptionId}", url.PathEscape(subscriptionId))
+	urlPath = runtime.JoinPaths(host, urlPath)
+	client := http.Client{}
+	req, err := http.NewRequest("GET", urlPath, nil)
+	if err != nil {
+		return ServerVulnerabilityAssessmentsSettingsList{}, err
+	}
+	q := req.URL.Query()
+	q.Set("api-version", "2022-01-01-preview")
+	req.URL.RawQuery = q.Encode()
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return ServerVulnerabilityAssessmentsSettingsList{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return ServerVulnerabilityAssessmentsSettingsList{}, errors.New("failed to fetch security contacts from " + urlPath + ": " + resp.Status)
+	}
+
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ServerVulnerabilityAssessmentsSettingsList{}, err
+	}
+	result := ServerVulnerabilityAssessmentsSettingsList{}
+	err = json.Unmarshal(raw, &result)
+	return result, err
+}
+
+type PolicyAssignment struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Location string `json:"location,omitempty"`
+	Identity struct {
+		Type        string `json:"type"`
+		PrincipalID string `json:"principalId"`
+		TenantID    string `json:"tenantId"`
+	} `json:"identity,omitempty"`
+	Properties struct {
+		DisplayName string `json:"displayName"`
+		Description string `json:"description"`
+		Metadata    struct {
+			Category string `json:"category"`
+		} `json:"metadata"`
+		PolicyDefinitionID string `json:"policyDefinitionId"`
+		Parameters         struct {
+			AllowedSkus struct {
+				Value string `json:"value"`
+			} `json:"allowedSkus"`
+		} `json:"parameters"`
+		Scope     string        `json:"scope"`
+		NotScopes []interface{} `json:"notScopes"`
+	} `json:"properties"`
+}
+type PolicyAssignments struct {
+	PolicyAssignments []PolicyAssignment `json:"value"`
+}
+
+type ServerVulnerabilityAssessmentsSettings struct {
+	Properties struct {
+		SelectedProvider string `json:"selectedProvider"`
+	} `json:"properties"`
+	SystemData struct {
+		CreatedBy          string    `json:"createdBy"`
+		CreatedByType      string    `json:"createdByType"`
+		CreatedAt          time.Time `json:"createdAt"`
+		LastModifiedBy     string    `json:"lastModifiedBy"`
+		LastModifiedByType string    `json:"lastModifiedByType"`
+		LastModifiedAt     time.Time `json:"lastModifiedAt"`
+	} `json:"systemData"`
+	Kind string `json:"kind"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+	ID   string `json:"id"`
+}
+
+type ServerVulnerabilityAssessmentsSettingsList struct {
+	Settings []ServerVulnerabilityAssessmentsSettings `json:"value"`
 }
