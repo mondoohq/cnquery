761b7735aac8b376d18f0e6f0ef7bfcd80795748
Ivan Milchev <ivan@mondoo.com>
2023-01-17 17:01:44 +0100

    add gcp logging metrics and sinks
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- resources/packs/gcp/logging.go
+++ resources/packs/gcp/logging.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"cloud.google.com/go/logging/logadmin"
 	"go.mondoo.com/cnquery/resources/packs/core"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/logging/v2"
 	"google.golang.org/api/option"
 )
@@ -109,6 +111,124 @@ func (g *mqlGcpProjectLoggingservice) GetBuckets() ([]interface{}, error) {
 		mqlBuckets = append(mqlBuckets, mqlBucket)
 	}
 	return mqlBuckets, nil
+}
+
+func (g *mqlGcpProjectLoggingservice) GetMetrics() ([]interface{}, error) {
+	provider, err := gcpProvider(g.MotorRuntime.Motor.Provider)
+	if err != nil {
+		return nil, err
+	}
+
+	projectId, err := g.ProjectId()
+	if err != nil {
+		return nil, err
+	}
+
+	creds, err := provider.Credentials(logging.CloudPlatformReadOnlyScope, logging.LoggingReadScope)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	logadminClient, err := logadmin.NewClient(ctx, projectId, option.WithCredentials(creds))
+	if err != nil {
+		return nil, err
+	}
+
+	var metrics []interface{}
+	it := logadminClient.Metrics(ctx)
+	for {
+		m, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		metric, err := g.MotorRuntime.CreateResource("gcp.project.loggingservice.metric",
+			"id", m.ID,
+			"projectId", projectId,
+			"description", m.Description,
+			"filter", m.Filter,
+		)
+		if err != nil {
+			return nil, err
+		}
+		metrics = append(metrics, metric)
+	}
+	return metrics, nil
+}
+
+func (g *mqlGcpProjectLoggingservice) GetSinks() ([]interface{}, error) {
+	provider, err := gcpProvider(g.MotorRuntime.Motor.Provider)
+	if err != nil {
+		return nil, err
+	}
+
+	projectId, err := g.ProjectId()
+	if err != nil {
+		return nil, err
+	}
+
+	creds, err := provider.Credentials(logging.CloudPlatformReadOnlyScope, logging.LoggingReadScope)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	logadminClient, err := logadmin.NewClient(ctx, projectId, option.WithCredentials(creds))
+	if err != nil {
+		return nil, err
+	}
+
+	var sinks []interface{}
+	it := logadminClient.Sinks(ctx)
+	for {
+		s, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		sink, err := g.MotorRuntime.CreateResource("gcp.project.loggingservice.sink",
+			"id", s.ID,
+			"projectId", projectId,
+			"destination", s.Destination,
+			"filter", s.Filter,
+			"writerIdentity", s.WriterIdentity,
+			"includeChildren", s.IncludeChildren,
+		)
+		if err != nil {
+			return nil, err
+		}
+		sinks = append(sinks, sink)
+	}
+	return sinks, nil
+}
+
+func (g *mqlGcpProjectLoggingserviceMetric) id() (string, error) {
+	projectId, err := g.ProjectId()
+	if err != nil {
+		return "", err
+	}
+	id, err := g.Id()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/gcp.project.loggingservice.metric/%s", projectId, id), nil
+}
+
+func (g *mqlGcpProjectLoggingserviceSink) id() (string, error) {
+	projectId, err := g.ProjectId()
+	if err != nil {
+		return "", err
+	}
+	id, err := g.Id()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/gcp.project.loggingservice.sink/%s", projectId, id), nil
 }
 
 func (g *mqlGcpProjectLoggingserviceBucket) id() (string, error) {
