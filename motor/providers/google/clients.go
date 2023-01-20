acf037ad068b999991cafd47c5ae35436b212344
Ivan Milchev <ivan@mondoo.com>
2023-01-20 11:59:51 +0100

    check credential type for gcp auth
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- motor/providers/google/clients.go
+++ motor/providers/google/clients.go
@@ -2,10 +2,12 @@ package google
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/rs/zerolog/log"
+	"go.mondoo.com/cnquery/motor/vault"
 	"golang.org/x/oauth2"
 	googleoauth "golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
@@ -20,7 +22,11 @@ func (t *Provider) Credentials(scopes ...string) (*googleoauth.Credentials, erro
 	}
 	if t.cred != nil {
 		// use service account from secret
-		return googleoauth.CredentialsFromJSONWithParams(ctx, t.cred.Secret, credParams)
+		data, err := credsServiceAccountData(t.cred)
+		if err != nil {
+			return nil, err
+		}
+		return googleoauth.CredentialsFromJSONWithParams(ctx, data, credParams)
 	}
 
 	if t.serviceAccountSubject != "" {
@@ -38,7 +44,11 @@ func (t *Provider) Client(scope ...string) (*http.Client, error) {
 
 	// use service account from secret if one is provided
 	if t.cred != nil {
-		return serviceAccountAuth(ctx, t.serviceAccountSubject, t.cred.Secret, scope...)
+		data, err := credsServiceAccountData(t.cred)
+		if err != nil {
+			return nil, err
+		}
+		return serviceAccountAuth(ctx, t.serviceAccountSubject, data, scope...)
 	}
 
 	// use service account authentication if we loaded a service account
@@ -75,4 +85,13 @@ func serviceAccountAuth(ctx context.Context, subject string, serviceAccount []by
 	}
 
 	return client, nil
+}
+
+func credsServiceAccountData(cred *vault.Credential) ([]byte, error) {
+	switch cred.Type {
+	case vault.CredentialType_json:
+		return cred.Secret, nil
+	default:
+		return nil, fmt.Errorf("unsupported credential type: %s", cred.Type)
+	}
 }
