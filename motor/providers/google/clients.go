f26bf13d318c77fdda20c8e0f148f95c3c96e278
Ivan Milchev <ivan@mondoo.com>
2023-01-20 12:52:45 +0100

    ⭐️ Allow providing GCP service account via inventory vault (#801)
    Add support for providing GCP service account via an inventory vault.
    For testing use `mondoo-dev` project and the following inventory:
    ```yaml
    apiVersion: v1
    kind: Inventory
    metadata:
      name: inventory
    spec:
      assets:
        - name: cool-stuff
          connections:
            - backend: 13
              credentials:
                - secret_id: storage/random-bucket2/foo
                  type: 1
                  secret_encoding: 3
              options:
              discover:
                targets:
                  - auto
      vault:
        name: gcp-berglas
        type: gcp-berglas
        options:
          project_id: mondoo-dev-262313
    ```
    
    Then the following command can be used: 
    ```bash
    ./cnquery scan --inventory-file inv.yaml
    ```
    
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- motor/providers/google/clients.go
+++ motor/providers/google/clients.go
@@ -2,10 +2,12 @@ package google
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/rs/zerolog/log"
+	"go.mondoo.com/cnquery/motor/vault"
 	"golang.org/x/oauth2"
 	googleoauth "golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
@@ -14,14 +16,21 @@ import (
 
 func (t *Provider) Credentials(scopes ...string) (*googleoauth.Credentials, error) {
 	ctx := context.Background()
+	credParams := googleoauth.CredentialsParams{
+		Scopes:  scopes,
+		Subject: t.serviceAccountSubject,
+	}
+	if t.cred != nil {
+		// use service account from secret
+		data, err := credsServiceAccountData(t.cred)
+		if err != nil {
+			return nil, err
+		}
+		return googleoauth.CredentialsFromJSONWithParams(ctx, data, credParams)
+	}
 
 	if t.serviceAccountSubject != "" {
 		// use custom service account provided by user
-		credParams := googleoauth.CredentialsParams{
-			Scopes:  scopes,
-			Subject: t.serviceAccountSubject,
-		}
-
 		return googleoauth.CredentialsFromJSONWithParams(ctx, t.serviceAccount, credParams)
 	}
 
@@ -32,6 +41,15 @@ func (t *Provider) Credentials(scopes ...string) (*googleoauth.Credentials, erro
 
 func (t *Provider) Client(scope ...string) (*http.Client, error) {
 	ctx := context.Background()
+
+	// use service account from secret if one is provided
+	if t.cred != nil {
+		data, err := credsServiceAccountData(t.cred)
+		if err != nil {
+			return nil, err
+		}
+		return serviceAccountAuth(ctx, t.serviceAccountSubject, data, scope...)
+	}
 
 	// use service account authentication if we loaded a service account
 	if t.serviceAccount != nil {
@@ -67,4 +85,13 @@ func serviceAccountAuth(ctx context.Context, subject string, serviceAccount []by
 	}
 
 	return client, nil
+}
+
+func credsServiceAccountData(cred *vault.Credential) ([]byte, error) {
+	switch cred.Type {
+	case vault.CredentialType_json:
+		return cred.Secret, nil
+	default:
+		return nil, fmt.Errorf("unsupported credential type: %s", cred.Type)
+	}
 }
