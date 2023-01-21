fc15328b2dd28d419139ced085d68d6decae58cc
Christoph Hartmann <chris@lollyrock.com>
2023-01-21 15:38:23 +0100

    ⭐️ gist support for github user

--- resources/packs/github/github_user.go
+++ resources/packs/github/github_user.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"context"
+	"io"
 	"strconv"
 	"strings"
 	"time"
@@ -10,6 +11,8 @@ import (
 	"github.com/google/go-github/v49/github"
 	"go.mondoo.com/cnquery/resources"
 	"go.mondoo.com/cnquery/resources/packs/core"
+	"go.mondoo.com/cnquery/stringx"
+	"go.mondoo.com/ranger-rpc"
 )
 
 func (g *mqlGithubUser) id() (string, error) {
@@ -161,4 +164,142 @@ func (g *mqlGithubUser) GetRepositories() ([]interface{}, error) {
 	}
 
 	return res, nil
+}
+
+func (g *mqlGithubGist) id() (string, error) {
+	id, err := g.Id()
+	if err != nil {
+		return "", err
+	}
+	return "github.gist/" + id, nil
+}
+
+func (g *mqlGithubUser) GetGists() ([]interface{}, error) {
+	gt, err := githubProvider(g.MotorRuntime.Motor.Provider)
+	if err != nil {
+		return nil, err
+	}
+
+	userLogin, err := g.Login()
+	if err != nil {
+		return nil, err
+	}
+
+	listOpts := &github.GistListOptions{
+		ListOptions: github.ListOptions{PerPage: paginationPerPage},
+	}
+
+	var allGists []*github.Gist
+	for {
+		gists, resp, err := gt.Client().Gists.List(context.Background(), userLogin, listOpts)
+		if err != nil {
+			if strings.Contains(err.Error(), "404") {
+				return nil, nil
+			}
+			return nil, err
+		}
+		allGists = append(allGists, gists...)
+		if resp.NextPage == 0 {
+			break
+		}
+		listOpts.Page = resp.NextPage
+	}
+
+	res := []interface{}{}
+	for i := range allGists {
+		gist := allGists[i]
+
+		files := []interface{}{}
+		for k := range gist.Files {
+			f := gist.Files[k]
+
+			gistFile, err := g.MotorRuntime.CreateResource("github.gistfile",
+				"gistId", core.ToString(gist.ID),
+				"filename", f.GetFilename(),
+				"type", f.GetType(),
+				"language", f.GetLanguage(),
+				"rawUrl", f.GetRawURL(),
+				"size", int64(f.GetSize()),
+			)
+			if err != nil {
+				return nil, err
+			}
+			files = append(files, gistFile)
+		}
+
+		r, err := g.MotorRuntime.CreateResource("github.gist",
+			"id", core.ToString(gist.ID),
+			"description", core.ToString(gist.Description),
+			"createdAt", gist.CreatedAt,
+			"updatedAt", gist.UpdatedAt,
+			"public", core.ToBool(gist.Public),
+			"owner", g,
+			"files", files,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, r)
+	}
+
+	return res, nil
+}
+
+func (g *mqlGithubGistfile) id() (string, error) {
+	id, err := g.GistId()
+	if err != nil {
+		return "", err
+	}
+	filename, err := g.Filename()
+	if err != nil {
+		return "", err
+	}
+	return "github.gistfile/" + id + "/" + filename, nil
+}
+
+var supportedGistContentTypes = []string{
+	"text/plain",
+	"text/markdown",
+	"text/html",
+	"text/x-yaml",
+	"application/json",
+	"application/javascript",
+	"application/x-ruby",
+	"application/x-perl",
+	"application/x-php",
+	"application/x-python",
+	"application/x-java",
+	"application/x-csharp",
+	"application/x-clojure",
+	"application/x-sh",
+	"application/x-powershell",
+	"application/x-msdos-program",
+}
+
+func (g *mqlGithubGistfile) GetContent() (string, error) {
+	rawUrl, err := g.RawUrl()
+	if err != nil {
+		return "", err
+	}
+
+	filetyp, err := g.Type()
+	if err != nil {
+		return "", err
+	}
+
+	// supported content types
+	if !stringx.Contains(supportedGistContentTypes, filetyp) {
+		return "", errors.New("unsupported content type: " + filetyp)
+	}
+
+	resp, err := ranger.DefaultHttpClient().Get(rawUrl)
+	if err != nil {
+		return "", err
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
 }
