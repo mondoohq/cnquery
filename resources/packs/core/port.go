63ea97d0311f4dfc0899c29c1b07ffdcc5e4fc4b
Joel Diaz <joel@mondoo.com>
2023-02-06 14:35:07 -0500

    little-to-big endian ipv6 addresses
    When processing a line in /proc/net/tcp6 that looks like:
    
      1: 00000000000000000000000001000000:0277 0000000000000...
    
    The decoded result is a process listening on ::100:0 on port 631.
    
    $ sudo cnquery run local -c "ports.listening[11]"
    → discover related assets for 1 asset(s)
    → resolved assets resolved-assets=1
    ports.listening[11]: port port=631 protocol="tcp" address="::100:0"
    process.executable="cupsd"
    
    In reality, this process (cups) is listening on ::1 on port 631.
    
    ::100:0 is clearly not the same as ::1.
    
    When on little endian, make sure we translate the ipv6 hex address into
    network endian (ie big endian) so that things are properly decoded:
    
    $ sudo ./cnquery run local -c "ports.listening[11]"
    → discover related assets for 1 asset(s)
    → resolved assets resolved-assets=1
    ports.listening[11]: port port=631 protocol="tcp" address="[::1]"
    process.executable="cupsd"
    
    In big-endian land, the ipv6 addresses need no byte-swapping (from
    CentOS 7 on ppc64 arch not ppc64le):
    
    [root@localhost ~]# cat /proc/net/tcp6
      sl  local_address                         remote_address
    st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
       0: 00000000000000000000000000000000:0050
          00000000000000000000000000000000:0000 0A 00000000:00000000
    				       00:00000000 00000000     0
    					  0 23158 1 c0000000f8cb1100 100
    					    0 0 10 0
       1: 00000000000000000000000000000000:0016
          00000000000000000000000000000000:0000 0A 00000000:00000000
    				       00:00000000 00000000     0
    					  0 18110 1 c000000029920000 100
    					    0 0 10 0
       2: 00000000000000000000000000000001:0019  # <- THIS ONE
          00000000000000000000000000000000:0000 0A 00000000:00000000
    				       00:00000000 00000000     0
    					  0 18749 1 c0000000f8cb0880 100
    					    0 0 10 0
    [root@localhost ~]# uname -a
    Linux localhost.localdomain 3.10.0-1160.el7.ppc64 #1 SMP Wed Sep 30
    02:20:51 UTC 2020 ppc64 ppc64 ppc64 GNU/Linux
    [root@localhost ~]# lscpu | grep Endian
    Byte Order:            Big Endian

--- resources/packs/core/port.go
+++ resources/packs/core/port.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"bufio"
+	"encoding/binary"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -11,6 +12,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unsafe"
 
 	"go.mondoo.com/cnquery/resources"
 	"go.mondoo.com/cnquery/resources/packs/core/lsof"
@@ -140,7 +142,8 @@ func hex2ipv4(s string) (string, error) {
 }
 
 func hex2ipv6(s string) (string, error) {
-	ipBytes, err := hex.DecodeString(s)
+	networkEndian := ipv6EndianTranslation(s)
+	ipBytes, err := hex.DecodeString(networkEndian)
 	if err != nil {
 		return "", err
 	}
@@ -155,6 +158,49 @@ func hex2ipv6(s string) (string, error) {
 	} else {
 		return "", err
 	}
+}
+
+func ipv6EndianTranslation(s string) string {
+	var nativeEndianness binary.ByteOrder
+
+	buf := [2]byte{}
+	*(*uint16)(unsafe.Pointer(&buf[0])) = uint16(0xABCD)
+
+	switch buf {
+	case [2]byte{0xCD, 0xAB}:
+		nativeEndianness = binary.LittleEndian
+	case [2]byte{0xAB, 0xCD}:
+		nativeEndianness = binary.BigEndian
+	default:
+		panic("neither little nor big endian detected...")
+	}
+
+	if nativeEndianness == binary.BigEndian {
+		return s
+	}
+
+	if len(s) != 32 {
+		// not an IPv6 address in hex format
+		return ""
+	}
+
+	// read 8 bytes at a time and little-to-big byte swap
+	// Ex: fe80:0000:0000:0000:5578:afa9:4caf:27a1 becomes
+	//     0000:80fe:0000:0000:a9af:7855:a127:af4c
+	swappedBytes := make([]byte, len(s))
+	for i := 0; i < len(s); i += 8 {
+		swappedBytes[i] = s[i+6]
+		swappedBytes[i+1] = s[i+7]
+		swappedBytes[i+2] = s[i+4]
+		swappedBytes[i+3] = s[i+5]
+
+		swappedBytes[i+4] = s[i+2]
+		swappedBytes[i+5] = s[i+3]
+		swappedBytes[i+6] = s[i+0]
+		swappedBytes[i+7] = s[i+1]
+	}
+
+	return string(swappedBytes)
 }
 
 func (p *mqlPorts) users() (map[int64]User, error) {
