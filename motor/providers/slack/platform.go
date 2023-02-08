788a6b609d05c52f8cb5b918a19e14df9ca2c763
Christoph Hartmann <chris@lollyrock.com>
2023-02-08 09:27:41 +0100

    🧹 improve asset platform information for slack

--- motor/providers/slack/platform.go
+++ motor/providers/slack/platform.go
@@ -4,10 +4,11 @@ import "go.mondoo.com/cnquery/motor/platform"
 
 func (p *Provider) PlatformInfo() (*platform.Platform, error) {
 	return &platform.Platform{
-		Name:    "slack",
-		Title:   "Slack",
+		Name:    "slack-team",
+		Title:   "Slack Team",
 		Runtime: p.Runtime(),
 		Kind:    p.Kind(),
+		Family:  []string{"slack"},
 	}, nil
 }
 
