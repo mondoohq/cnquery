83d9302a1dade5126c4f3c6932cc56ad2eeea28c
Preslav Gerchev <preslav@mondoo.com>
2023-02-09 18:13:52 +0100

    Small text improvements to azure provider cmd. (#916)
    - Remove the `pfx` word from the error we return if the certificate
    cannot be parsed, we do not know what's the format of the file from
    where the data came in
    - Add .pem as a supported format for the `--certificate-path` arg of
    `azure shell`

--- apps/cnquery/cmd/builder/builder.go
+++ apps/cnquery/cmd/builder/builder.go
@@ -582,7 +582,7 @@ func azureProviderCmd(commonCmdFlags commonFlagsFn, preRun commonPreRunFn, runFn
 	cmd.Flags().String("tenant-id", "", "Directory (tenant) ID of the service principal")
 	cmd.Flags().String("client-id", "", "Application (client) ID of the service principal")
 	cmd.Flags().String("client-secret", "", "Secret for application")
-	cmd.Flags().String("certificate-path", "", "Path (in PKCS #12/PFX format) to the authentication certificate")
+	cmd.Flags().String("certificate-path", "", "Path (in PKCS #12/PFX or PEM format) to the authentication certificate")
 	cmd.Flags().String("certificate-secret", "", "Passphrase for the authentication certificate file")
 	cmd.Flags().String("subscription", "", "ID of the Azure subscription to scan")
 	cmd.Flags().String("subscriptions", "", "Comma-separated list of Azure subscriptions to include")
