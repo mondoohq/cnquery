1e02cfaeb841b0c87b5032b6e697f51f1c5bda24
Dominik Richter <dominik.richter@gmail.com>
2023-03-03 11:53:44 -0800

    🐛 validate impact value + 🧹 restructure
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ explorer/impact_test.go
@@ -0,0 +1,130 @@
+package explorer
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParsing(t *testing.T) {
+	tests := []struct {
+		title string
+		data  string
+		res   *Impact
+	}{
+		{
+			"simple number",
+			"30",
+			&Impact{
+				Value: &ImpactValue{Value: 30},
+			},
+		},
+		{
+			"complex definition",
+			`{"weight": 20, "value": 40}`,
+			&Impact{
+				Weight: 20,
+				Value:  &ImpactValue{Value: 40},
+			},
+		},
+	}
+
+	for i := range tests {
+		cur := tests[i]
+		t.Run(cur.title, func(t *testing.T) {
+			var res *Impact
+			err := json.Unmarshal([]byte(cur.data), &res)
+			require.NoError(t, err)
+			assert.Equal(t, cur.res, res)
+		})
+	}
+
+	errTests := []struct {
+		title string
+		data  string
+		err   string
+	}{
+		{
+			"invalid low impact",
+			"-1",
+			"impact must be between 0 and 100",
+		},
+		{
+			"invalid high impact",
+			"101",
+			"impact must be between 0 and 100",
+		},
+		{
+			"invalid low impact in complex struct",
+			`{"value": -1}`,
+			"impact must be between 0 and 100",
+		},
+		{
+			"invalid high impact in complex struct",
+			`{"value": 101, "weight": 90}`,
+			"impact must be between 0 and 100",
+		},
+	}
+
+	for i := range errTests {
+		cur := errTests[i]
+		t.Run(cur.title, func(t *testing.T) {
+			var res *Impact
+			err := json.Unmarshal([]byte(cur.data), &res)
+			assert.EqualError(t, err, cur.err)
+		})
+	}
+}
+
+func TestMerging(t *testing.T) {
+	tests := []struct {
+		title string
+		base  *Impact
+		main  *Impact
+		res   *Impact
+	}{
+		{
+			"nil base",
+			nil,
+			&Impact{Value: &ImpactValue{Value: 20}},
+			&Impact{Value: &ImpactValue{Value: 20}},
+		},
+		{
+			"empty base",
+			&Impact{},
+			&Impact{Value: &ImpactValue{Value: 20}},
+			&Impact{Value: &ImpactValue{Value: 20}},
+		},
+		{
+			"empty main",
+			&Impact{Value: &ImpactValue{Value: 20}},
+			&Impact{},
+			&Impact{Value: &ImpactValue{Value: 20}},
+		},
+		{
+			"inherit value",
+			&Impact{Value: &ImpactValue{Value: 20}},
+			&Impact{Weight: 10, Scoring: Impact_AVERAGE},
+			&Impact{
+				Value:  &ImpactValue{Value: 20},
+				Weight: 10, Scoring: Impact_AVERAGE,
+			},
+		},
+		{
+			"inherit scoring (explicit)",
+			&Impact{Scoring: Impact_IGNORE},
+			&Impact{Scoring: Impact_SCORING_UNSPECIFIED, Value: &ImpactValue{Value: 78}},
+			&Impact{Scoring: Impact_IGNORE, Value: &ImpactValue{Value: 78}},
+		},
+	}
+
+	for i := range tests {
+		cur := tests[i]
+		t.Run(cur.title, func(t *testing.T) {
+			cur.main.AddBase(cur.base)
+			assert.Equal(t, cur.res, cur.main)
+		})
+	}
+}
