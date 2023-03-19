e1241865c5420e7c15a637657f1c08b4c01dab49
Dominik Richter <dominik.richter@gmail.com>
2023-03-19 00:50:25 -0700

    🔨 filters.summarize (#1080)
    For an easier access to all filters in the struct
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- explorer/filters_test.go
+++ explorer/filters_test.go
@@ -23,3 +23,21 @@ func TestNewFilters(t *testing.T) {
 		}, f.Items)
 	})
 }
+
+func TestSummarize(t *testing.T) {
+	t.Run("with NewFilters initialization", func(t *testing.T) {
+		f := NewFilters("true", "asset.platform != ''")
+		assert.Equal(t, "asset.platform != '', true", f.Summarize())
+	})
+
+	t.Run("with mixed filters", func(t *testing.T) {
+		f := &Filters{
+			Items: map[string]*Mquery{
+				"one": {Mql: "asset.name"},
+				"two": {Title: "filter 2"},
+			},
+		}
+
+		assert.Equal(t, "asset.name, filter 2", f.Summarize())
+	})
+}
