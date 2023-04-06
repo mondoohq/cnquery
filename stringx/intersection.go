633aa2a2a50456ffcc9b1edeeba03e099a2f5025
Preslav <preslav@mondoo.com>
2023-04-06 17:17:55 +0200

    🧹 Add basic, string slice intersection fn.

---
+++ stringx/intersection.go
@@ -0,0 +1,17 @@
+package stringx
+
+func Intersection(a, b []string) []string {
+	entriesMap := map[string]struct{}{}
+	res := []string{}
+
+	for i := range a {
+		entriesMap[a[i]] = struct{}{}
+	}
+
+	for i := range b {
+		if _, ok := entriesMap[b[i]]; ok {
+			res = append(res, b[i])
+		}
+	}
+	return res
+}
