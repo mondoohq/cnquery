281359a85d8c0f4737ed52b5de36e7808b711627
Marius Kimmina <mar.kimmina@gmail.com>
2023-06-04 13:07:20 +0200

    clean up
    Signed-off-by: Marius Kimmina <mar.kimmina@gmail.com>

--- cli/reporter/reporter.go
+++ cli/reporter/reporter.go
@@ -55,10 +55,6 @@ func AllFormats() string {
 }
 
 type Reporter struct {
-	// Pager set to true will use a pager for the output. Only relevant for all
-	// non-json/yaml/junit/csv reports (for now)
-	//UsePager    bool
-	//Pager       string
 	Format      Format
 	Printer     *printer.Printer
 	Colors      *colors.Theme
