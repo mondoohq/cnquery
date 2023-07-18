a791d4f9f55600607aee5f9cc9e100280cbbf51c
Christoph Hartmann <chris@lollyrock.com>
2023-07-18 12:38:15 +0200

    ⭐️ native windows registry resource (#1359)
    This PR adds native support for Windows registry keys when cnquery is
    executed locally. Specifically cnspec is going to profit from this
    change with a 3.8 times faster execution for CIS Windows 2022 benchmark.
    
    Besides the performance improvements, this PR also adds support for
    multi-line string and binary data support for registry keys.
    
    We deprecate `registrykey.properties` 
    
    ```
    cnspec> registrykey(path: 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters').properties
    registrykey.properties: {
      EnableAuthenticateUserSharing: "0"
      Guid: ""
      NullSessionPipes: ""
      ServiceDll: "%SystemRoot%\\system32\\srvsvc.dll"
      ServiceDllUnloadOnStop: "1"
      autodisconnect: "15"
      enableforcedlogoff: "1"
      enablesecuritysignature: "0"
      requiresecuritysignature: "0"
      restrictnullsessaccess: "1"
    }
    ```
    
    in favor of the new `registrykey.items`. This returns the full property
    MQL resources and also allows inspection of type:
    
    ```
    cnspec> registrykey(path: 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters').items { * }
    registrykey.items: [
      0: {
        value: "0"
        type: "dword"
        name: "EnableAuthenticateUserSharing"
        path: "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\LanManServer\\Parameters"
        data: 0
        exists: true
      }
      1: {
        value: ""
        type: "multistring"
        name: "NullSessionPipes"
        path: "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\LanManServer\\Parameters"
        data: [
          0: ""
        ]
        exists: true
      }
      2: {
        value: "%SystemRoot%\\system32\\srvsvc.dll"
        type: "expandstring"
        name: "ServiceDll"
        path: "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\LanManServer\\Parameters"
        data: "%SystemRoot%\\system32\\srvsvc.dll"
        exists: true
      }
      3: {
        value: "1"
        type: "dword"
        name: "ServiceDllUnloadOnStop"
        path: "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\LanManServer\\Parameters"
        data: 1
        exists: true
      }
    ...
    ```
    
    The resource `registrykey.property` introduces a new `data` field that
    returns `dict` type. This allows the support for more different data
    types. Therefore we deprecate the use of `value` which was only a string
    representation.
    
    ```
    registrykey.property( path: 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters', name: 'autodisconnect' ) { * }
    registrykey.property: {
      data: 15
      name: "autodisconnect"
      path: "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\LanManServer\\Parameters"
      exists: true
      type: "dword"
      value: "15"
    }
    ```
    
    ## Performance Testing
    
    A CIS Windows 2022 Level 1 Member Benchmarks with latest 8.18.0 requires
    1 minute and 48 seconds.
    
    ```
    Days              : 0
    Hours             : 0
    Minutes           : 1
    Seconds           : 48
    Milliseconds      : 69
    Ticks             : 1080693311
    TotalDays         : 0.00125080244328704
    TotalHours        : 0.0300192586388889
    TotalMinutes      : 1.80115551833333
    TotalSeconds      : 108.0693311
    TotalMilliseconds : 108069.3311
    ```
    
    The same benchmark with native support is complete in 28 seconds now:
    
    ```
    Days              : 0
    Hours             : 0
    Minutes           : 0
    Seconds           : 28
    Milliseconds      : 638
    Ticks             : 286380873
    TotalDays         : 0.00033145934375
    TotalHours        : 0.00795502425
    TotalMinutes      : 0.477301455
    TotalSeconds      : 28.6380873
    TotalMilliseconds : 28638.0873
    ```
    
    ---------
    
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- resources/packs/os/registrykey.go
+++ resources/packs/os/registrykey.go
@@ -2,12 +2,16 @@ package os
 
 import (
 	"errors"
+	"runtime"
 	"strings"
 
 	"github.com/rs/zerolog/log"
+	"go.mondoo.com/cnquery/motor/providers/local"
 	"go.mondoo.com/cnquery/motor/providers/os/powershell"
 	"go.mondoo.com/cnquery/resources"
 	"go.mondoo.com/cnquery/resources/packs/os/windows"
+	"go.mondoo.com/ranger-rpc/codes"
+	"go.mondoo.com/ranger-rpc/status"
 )
 
 func (k *mqlRegistrykey) id() (string, error) {
@@ -18,6 +22,22 @@ func (k *mqlRegistrykey) GetExists() (bool, error) {
 	path, err := k.Path()
 	if err != nil {
 		return false, err
+	}
+
+	// if we are running locally on windows, we can use native api
+	_, ok := k.MotorRuntime.Motor.Provider.(*local.Provider)
+	if ok && runtime.GOOS == "windows" {
+		items, err := windows.GetNativeRegistryKeyItems(path)
+		if err == nil && len(items) > 0 {
+			return true, nil
+		}
+		std, ok := status.FromError(err)
+		if ok && std.Code() == codes.NotFound {
+			return false, nil
+		}
+		if err != nil {
+			return false, err
+		}
 	}
 
 	script := powershell.Encode(windows.GetRegistryKeyItemScript(path))
@@ -44,13 +64,20 @@ func (k *mqlRegistrykey) GetExists() (bool, error) {
 	return true, nil
 }
 
-func (k *mqlRegistrykey) GetProperties() (map[string]interface{}, error) {
+// GetEntries returns a list of registry key property resources
+func (k *mqlRegistrykey) getEntries() ([]windows.RegistryKeyItem, error) {
 	path, err := k.Path()
 	if err != nil {
 		return nil, err
 	}
 
-	res := map[string]interface{}{}
+	// if we are running locally on windows, we can use native api
+	_, ok := k.MotorRuntime.Motor.Provider.(*local.Provider)
+	if ok && runtime.GOOS == "windows" {
+		return windows.GetNativeRegistryKeyItems(path)
+	}
+
+	// parse the output of the powershell script
 	script := powershell.Encode(windows.GetRegistryKeyItemScript(path))
 	mqlCmd, err := k.MotorRuntime.CreateResource("command", "command", script)
 	if err != nil {
@@ -67,19 +94,61 @@ func (k *mqlRegistrykey) GetProperties() (map[string]interface{}, error) {
 
 	stdout, err := cmd.Stdout()
 	if err != nil {
-		return res, err
+		return nil, err
 	}
-	entries, err := windows.ParseRegistryKeyItems(strings.NewReader(stdout))
+
+	return windows.ParsePowershellRegistryKeyItems(strings.NewReader(stdout))
+}
+
+// Deprecated: GetProperties returns the properties of a registry key
+// This function is deprecated and will be removed in a future release
+func (k *mqlRegistrykey) GetProperties() (map[string]interface{}, error) {
+	res := map[string]interface{}{}
+
+	entries, err := k.getEntries()
 	if err != nil {
 		return nil, err
 	}
 
 	for i := range entries {
 		rkey := entries[i]
-		res[rkey.Key] = rkey.GetValue()
+		res[rkey.Key] = rkey.String()
 	}
 
 	return res, nil
+}
+
+// GetItems returns a list of registry key property resources
+func (k *mqlRegistrykey) GetItems() ([]interface{}, error) {
+	entries, err := k.getEntries()
+	if err != nil {
+		return nil, err
+	}
+
+	path, err := k.Path()
+	if err != nil {
+		return nil, err
+	}
+
+	// create MQL mount entry resources for each mount
+	items := make([]interface{}, len(entries))
+	for i, entry := range entries {
+		mqlRegistryPropertyEntry, err := k.MotorRuntime.CreateResource("registrykey.property",
+			"path", path,
+			"name", entry.Key,
+			"value", entry.String(),
+			"type", entry.Kind(),
+			"data", entry.GetRawValue(),
+			"exists", true,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		items[i] = mqlRegistryPropertyEntry.(RegistrykeyProperty)
+	}
+
+	return items, nil
 }
 
 func (k *mqlRegistrykey) GetChildren() ([]interface{}, error) {
@@ -90,27 +159,38 @@ func (k *mqlRegistrykey) GetChildren() ([]interface{}, error) {
 		return nil, err
 	}
 
-	script := powershell.Encode(windows.GetRegistryKeyChildItemsScript(path))
-	mqlCmd, err := k.MotorRuntime.CreateResource("command", "command", script)
-	if err != nil {
-		return res, err
-	}
-	cmd := mqlCmd.(Command)
-	exitcode, err := cmd.Exitcode()
-	if err != nil {
-		return nil, err
-	}
-	if exitcode != 0 {
-		return nil, errors.New("could not retrieve registry key")
-	}
+	var children []windows.RegistryKeyChild
+	// if we are running locally on windows, we can use native api
+	_, ok := k.MotorRuntime.Motor.Provider.(*local.Provider)
+	if ok && runtime.GOOS == "windows" {
+		children, err = windows.GetNativeRegistryKeyChildren(path)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// parse powershell script
+		script := powershell.Encode(windows.GetRegistryKeyChildItemsScript(path))
+		mqlCmd, err := k.MotorRuntime.CreateResource("command", "command", script)
+		if err != nil {
+			return res, err
+		}
+		cmd := mqlCmd.(Command)
+		exitcode, err := cmd.Exitcode()
+		if err != nil {
+			return nil, err
+		}
+		if exitcode != 0 {
+			return nil, errors.New("could not retrieve registry key")
+		}
 
-	stdout, err := cmd.Stdout()
-	if err != nil {
-		return res, err
-	}
-	children, err := windows.ParseRegistryKeyChildren(strings.NewReader(stdout))
-	if err != nil {
-		return nil, err
+		stdout, err := cmd.Stdout()
+		if err != nil {
+			return res, err
+		}
+		children, err = windows.ParsePowershellRegistryKeyChildren(strings.NewReader(stdout))
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	for i := range children {
@@ -156,6 +236,12 @@ func (p *mqlRegistrykeyProperty) init(args *resources.Args) (*resources.Args, Re
 		return args, nil, nil
 	}
 
+	// if the data is set, we do not need to fetch the data first
+	dataRaw := (*args)["data"]
+	if dataRaw != nil {
+		return args, nil, nil
+	}
+
 	// create resource here, but do not use it yet
 	obj, err := p.MotorRuntime.CreateResource("registrykey", "path", path)
 	if err != nil {
@@ -175,17 +261,21 @@ func (p *mqlRegistrykeyProperty) init(args *resources.Args) (*resources.Args, Re
 
 	// path exists
 	if exists {
-		properties, err := registryKey.Properties()
+		items, err := registryKey.Items()
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// search for property
-		for k := range properties {
-			if strings.EqualFold(k, name) {
-				(*args)["exists"] = true
-				(*args)["value"] = properties[k].(string)
-				break
+		for i := range items {
+			property := items[i].(RegistrykeyProperty)
+			itemName, err := property.Name()
+			if err != nil {
+				return nil, nil, err
+			}
+
+			// property exists, return it
+			if strings.EqualFold(itemName, name) {
+				return nil, property, nil
 			}
 		}
 	}
@@ -197,7 +287,17 @@ func (p *mqlRegistrykeyProperty) GetExists() (bool, error) {
 	return false, errors.New("could not determine if the property exists")
 }
 
-func (p *mqlRegistrykeyProperty) GetValue() (string, error) {
+func (p *mqlRegistrykeyProperty) GetType() (string, error) {
+	// NOTE: if we reach here the value has not been set in init, therefore we return an error
+	return "", errors.New("requested property does not exist")
+}
+
+func (p *mqlRegistrykeyProperty) GetData() (interface{}, error) {
+	// NOTE: if we reach here the value has not been set in init, therefore we return an error
+	return "", errors.New("requested property does not exist")
+}
+
+func (p *mqlRegistrykeyProperty) GetValue() (interface{}, error) {
 	// NOTE: if we reach here the value has not been set in init, therefore we return an error
 	return "", errors.New("requested property does not exist")
 }
--- resources/packs/os/windows/registrykey.go
+++ resources/packs/os/windows/registrykey.go
@@ -1,65 +1,10 @@
 package windows
 
 import (
-	"encoding/binary"
-	"encoding/json"
-	"fmt"
-	"io"
-	"math"
-	"strconv"
-
-	"github.com/rs/zerolog/log"
+	"go.mondoo.com/cnquery/resources/packs/core"
 )
 
-const getRegistryKeyItemScript = `
-$path = '%s'
-$reg = Get-Item ('Registry::' + $path)
-if ($reg -eq $null) {
-  Write-Error "Could not find registry key"
-  exit 1
-}
-$properties = @()
-$reg.Property | ForEach-Object {
-    $fetchKeyValue = $_
-    if ("(default)".Equals($_)) { $fetchKeyValue = '' }
-    $entry = New-Object psobject -Property @{
-      "key" = $_
-      "value" = New-Object psobject -Property @{
-        "data" =  $(Get-ItemProperty ('Registry::' + $path)).$_;
-        "kind"  = $reg.GetValueKind($fetchKeyValue);
-      }
-    }
-    $properties += $entry
-}
-ConvertTo-Json -Compress $properties
-`
-
-func GetRegistryKeyItemScript(path string) string {
-	return fmt.Sprintf(getRegistryKeyItemScript, path)
-}
-
-const getRegistryKeyChildItemsScript = `
-$path = '%s'
-$children = Get-ChildItem -Path ('Registry::' + $path) -rec -ea SilentlyContinue
-
-$properties = @()
-$children | ForEach-Object {
-  $entry = New-Object psobject -Property @{
-    "name" = $_.PSChildName
-    "path" = $_.Name
-    "properties" = $_.Property
-    "children" = $_.SubKeyCount
-  }
-  $properties += $entry
-}
-ConvertTo-Json -compress $properties
-`
-
-func GetRegistryKeyChildItemsScript(path string) string {
-	return fmt.Sprintf(getRegistryKeyChildItemsScript, path)
-}
-
-// derrived from "golang.org/x/sys/windows/registry"
+// derived from "golang.org/x/sys/windows/registry"
 // see https://github.com/golang/sys/blob/master/windows/registry/value.go#L17-L31
 const (
 	NONE                       = 0
@@ -81,106 +26,81 @@ type RegistryKeyItem struct {
 	Value RegistryKeyValue
 }
 
-type RegistryKeyValue struct {
-	Kind   int
-	Binary []byte
-	Number int64
-	String string
-}
-
-type keyKindRaw struct {
-	Kind int
-	Data interface{}
-}
-
-func (k *RegistryKeyValue) UnmarshalJSON(b []byte) error {
-	var raw keyKindRaw
-
-	// try to unmarshal the type
-	err := json.Unmarshal(b, &raw)
-	if err != nil {
-		return err
-	}
-	k.Kind = raw.Kind
-
-	if raw.Data == nil {
-		return nil
-	}
-
-	// see https://docs.microsoft.com/en-us/powershell/scripting/samples/working-with-registry-entries?view=powershell-7
-	switch raw.Kind {
+func (k RegistryKeyItem) Kind() string {
+	switch k.Value.Kind {
 	case NONE:
-		// ignore
-	case SZ: // Any string value
-		k.String = raw.Data.(string)
-	case EXPAND_SZ: // A string that can contain environment variables that are dynamically expanded
-		k.String = raw.Data.(string)
-	case BINARY: // Binary data
-		k.Binary = []byte(raw.Data.(string))
-	case DWORD: // A number that is a valid UInt32
-		data := raw.Data.(float64)
-		number := int64(data)
-		// string fallback
-		k.Number = number
-		k.String = strconv.FormatInt(number, 10)
+		return "bone"
+	case SZ:
+		return "string"
+	case EXPAND_SZ:
+		return "expandstring"
+	case BINARY:
+		return "binary"
+	case DWORD:
+		return "dword"
 	case DWORD_BIG_ENDIAN:
-		log.Warn().Msg("DWORD_BIG_ENDIAN for registry key is not supported")
+		return "dword"
 	case LINK:
-		log.Warn().Msg("LINK for registry key is not supported")
-	case MULTI_SZ: // A multiline string
-		k.String = raw.Data.(string)
+		return "link"
+	case MULTI_SZ:
+		return "multistring"
 	case RESOURCE_LIST:
-		log.Warn().Msg("RESOURCE_LIST for registry key is not supported")
+		return "<unsupported>"
 	case FULL_RESOURCE_DESCRIPTOR:
-		log.Warn().Msg("FULL_RESOURCE_DESCRIPTOR for registry key is not supported")
+		return "<unsupported>"
 	case RESOURCE_REQUIREMENTS_LIST:
-		log.Warn().Msg("RESOURCE_REQUIREMENTS_LIST for registry key is not supported")
-	case QWORD: // 8 bytes of binary data
-		f := raw.Data.(float64)
-		buf := make([]byte, 8)
-		binary.LittleEndian.PutUint64(buf[:], math.Float64bits(f))
-		k.Binary = buf
+		return "<unsupported>"
+	case QWORD:
+		return "qword"
+	}
+	return "<unsupported>"
+}
+
+func (k RegistryKeyItem) GetRawValue() interface{} {
+	switch k.Value.Kind {
+	case NONE:
+		return nil
+	case SZ:
+		return k.Value.String
+	case EXPAND_SZ:
+		return k.Value.String
+	case BINARY:
+		return k.Value.Binary
+	case DWORD:
+		return k.Value.Number
+	case DWORD_BIG_ENDIAN:
+		return nil
+	case LINK:
+		return nil
+	case MULTI_SZ:
+		return core.StrSliceToInterface(k.Value.MultiString)
+	case RESOURCE_LIST:
+		return nil
+	case FULL_RESOURCE_DESCRIPTOR:
+		return nil
+	case RESOURCE_REQUIREMENTS_LIST:
+		return nil
+	case QWORD:
+		return k.Value.Number
 	}
 	return nil
 }
 
-func (k RegistryKeyItem) GetValue() string {
-	return k.Value.String
+// String returns a string representation of the registry key value
+func (k RegistryKeyItem) String() string {
+	return k.Value.String // conversion to string is handled in UnmarshalJSON
+}
+
+type RegistryKeyValue struct {
+	Kind        int
+	Binary      []byte
+	Number      int64
+	String      string
+	MultiString []string
 }
 
 type RegistryKeyChild struct {
 	Name       string
 	Path       string
 	Properties []string
-	Children   int
-}
-
-func ParseRegistryKeyItems(r io.Reader) ([]RegistryKeyItem, error) {
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	var items []RegistryKeyItem
-	err = json.Unmarshal(data, &items)
-	if err != nil {
-		return nil, err
-	}
-
-	return items, nil
-}
-
-func ParseRegistryKeyChildren(r io.Reader) ([]RegistryKeyChild, error) {
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	var children []RegistryKeyChild
-	err = json.Unmarshal(data, &children)
-	if err != nil {
-		return nil, err
-	}
-
-	return children, nil
 }
