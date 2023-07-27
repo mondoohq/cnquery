b6d7893aabdfb836725fb47d59a7f82d12622bb4
Dominik Richter <dominik.richter@gmail.com>
2023-07-27 01:33:16 -0700

    ✨ migrate ssh privatekeys (#1392)
    After https://github.com/mondoohq/cnquery/pull/1391
    
    Migrate sshkeys and privatekey resource
    
    Also deprecate path. We want to connect to full resources, path is a
    great reference
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ providers/os/resources/privatekey.go
@@ -0,0 +1,38 @@
+package resources
+
+import (
+	"errors"
+
+	"go.mondoo.com/cnquery/llx"
+	"go.mondoo.com/cnquery/providers-sdk/v1/plugin"
+)
+
+func initPrivatekey(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[string]*llx.RawData, plugin.Resource, error) {
+	if x, ok := args["path"]; ok {
+		path, ok := x.Value.(string)
+		if !ok {
+			return nil, nil, errors.New("wrong type for 'path' in privatekey initialization, it must be a string")
+		}
+
+		f, err := CreateResource(runtime, "file", map[string]*llx.RawData{
+			"path": llx.StringData(path),
+		})
+		if err != nil {
+			return nil, nil, err
+		}
+		args["file"] = llx.ResourceData(f, "file")
+	}
+
+	return args, nil, nil
+}
+
+func (r *mqlPrivatekey) id() (string, error) {
+	// TODO: use path or hash depending on initialization
+
+	file := r.GetFile()
+	if file.Error != nil {
+		return "", file.Error
+	}
+
+	return "privatekey:" + file.Data.Path.Data, nil
+}
