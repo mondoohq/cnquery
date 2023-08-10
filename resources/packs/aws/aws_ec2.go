d182bc2f7a766495e2892b07c36ad68c01b731bf
Christian Zunker <christian@mondoo.com>
2023-08-10 08:37:18 +0200

    🐛 Use bool value and not pointer for entry.Egress
    Fixes #1463
    
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- resources/packs/aws/aws_ec2.go
+++ resources/packs/aws/aws_ec2.go
@@ -168,7 +168,7 @@ func (s *mqlAwsEc2Networkacl) GetEntries() ([]interface{}, error) {
 	res := []interface{}{}
 	for _, entry := range networkacls.NetworkAcls[0].Entries {
 		args := []interface{}{
-			"egress", *entry.Egress,
+			"egress", core.ToBool(entry.Egress),
 			"ruleAction", string(entry.RuleAction),
 			"id", id + "-" + strconv.Itoa(core.ToIntFrom32(entry.RuleNumber)),
 		}
