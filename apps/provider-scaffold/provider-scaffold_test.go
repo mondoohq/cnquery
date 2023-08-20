2a880f55b96785d8b522f0532712c4a6822879d0
Christoph Hartmann <chris@lollyrock.com>
2023-08-20 12:56:01 +0200

    ⭐️ provider scaffolding

---
+++ apps/provider-scaffold/provider-scaffold_test.go
@@ -0,0 +1,23 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package main
+
+import (
+	"testing"
+)
+
+func TestGenerator(t *testing.T) {
+	cfg := config{
+		Path:                "../../cnquery/providers/oci",
+		ProviderID:          "oci",
+		ProviderName:        "Oracle Cloud Infrastructure",
+		GoPackage:           "go.mondoo.com/cnquery/providers/oci",
+		CamelcaseProviderID: "Oci",
+	}
+
+	err := generateProvider(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
