64412ec91f3af3e632a4cbfdf79f83319449cfc8
Christoph Hartmann <chris@lollyrock.com>
2023-08-22 17:40:01 +0000

    ⭐️ ipmi provider (#1555)
    Start IPMI Service
    
    ```
    docker run -d -p 623:623/udp vaporio/ipmi-simulator 
    ```
    
    Now use cnquery to connect to IPMI service:
    
    ```
    cnquery shell ipmi ADMIN@0.0.0.0:623 --password ADMIN
    ```
    
    ```coffeescript
    cnquery> ipmi.chassis { * }
    ipmi.chassis: {
      status: {
        chassisIntrusion: false
        coolingFanFault: false
        driveFault: false
        frontPanelLockout: false
        lastPowerEvent: {
          ac-failed: false
          command: false
          fault: false
          interlock: false
          overload: false
        }
        mainPowerFault: false
        powerControlFault: false
        powerInterlock: false
        powerOverload: false
        powerRestorePolicy: "always-off"
        systemPower: true
      }
      systemBootOptions: {
        bootFlags: {
          applyToNextBootOnly: true
          biosFlagsValid: false
          biosMuxControlOverride: "recommended"
          biosSharedModeOverride: false
          biosVerbosity: "default"
          bootDeviceSelector: "disk"
          cmosClear: false
          consoleRedirection: "bios"
          forceProgressEventTrap: false
          legacyBootType: true
          lockKeyboard: false
          lockOutPowerButton: false
          lockOutResetButton: false
          lockOutSleepButton: false
          screenBlank: false
          userPasswordBypass: false
        }
        parameterValidUnlocked: true
        parameterVersion: 1.000000
      }
    }
    cnquery> ipmi.deviceID
    ipmi.deviceID: {
      additionalDeviceSupport: {
        bridge: false
        chassisDevice: true
        fruInventoryDevice: true
        ipmbEventGenerator: false
        ipmbEventReciever: true
        sdrRepositoryDevice: true
        selDevice: true
        sensorDevice: true
      }
      deviceAvailable: true
      deviceID: 0.000000
      deviceRevision: 3.000000
      firmwareRevision: "9.08"
      ipmiVersion: 2.000000
      manufacturerID: 4753.000000
      manufacturerName: "Unknown"
      productID: 3842.000000
      productName: "Unknown"
      providesDeviceSDRs: false
    }
    cnquery> ipmi.guid
    ipmi.guid: "A123456789AB"
    ```

---
+++ providers/ipmi/config/config.go
@@ -0,0 +1,38 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package config
+
+import "go.mondoo.com/cnquery/providers-sdk/v1/plugin"
+
+var Config = plugin.Provider{
+	Name:    "ipmi",
+	ID:      "go.mondoo.com/cnquery/providers/ipmi",
+	Version: "9.0.0",
+	Connectors: []plugin.Connector{
+		{
+			Name:      "ipmi",
+			Use:       "ipmi",
+			Short:     "Ipmi",
+			Discovery: []string{},
+			Flags: []plugin.Flag{
+				{
+					Long:        "ask-pass",
+					Type:        plugin.FlagType_Bool,
+					Default:     "false",
+					Desc:        "Prompt for connection password.",
+					ConfigEntry: "-",
+				},
+				{
+					Long:        "password",
+					Short:       "p",
+					Type:        plugin.FlagType_String,
+					Default:     "",
+					Desc:        "Set the connection password for SSH.",
+					Option:      plugin.FlagOption_Password,
+					ConfigEntry: "-",
+				},
+			},
+		},
+	},
+}
