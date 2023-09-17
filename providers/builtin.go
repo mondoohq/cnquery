8ffaa6e20c5e2e434c3efc5418d6298a3683a11a
Dominik Richter <dominik.richter@gmail.com>
2023-09-16 20:59:25 -0700

    🐛 fix incorrect os-connection + 🧹 add os builtin
    - the os provider does not support the mock connection
    - add back the builtin comments for the os provider, since we still use it (until it gets wired into its own go.mod)
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- providers/builtin.go
+++ providers/builtin.go
@@ -15,10 +15,15 @@ import (
 
 	coreconf "go.mondoo.com/cnquery/providers/core/config"
 	core "go.mondoo.com/cnquery/providers/core/provider"
+	// osconf "go.mondoo.com/cnquery/providers/os/config"
+	// os "go.mondoo.com/cnquery/providers/os/provider"
 )
 
 //go:embed core/resources/core.resources.json
 var coreInfo []byte
+
+// //go:embed os/resources/os.resources.json
+// var osInfo []byte
 
 var builtinProviders = map[string]*builtinProvider{
 	coreconf.Config.ID: {
@@ -31,4 +36,14 @@ var builtinProviders = map[string]*builtinProvider{
 		},
 		Config: &coreconf.Config,
 	},
+	// osconf.Config.ID: {
+	// 	Runtime: &RunningProvider{
+	// 		Name:     osconf.Config.Name,
+	// 		ID:       osconf.Config.ID,
+	// 		Plugin:   os.Init(),
+	// 		Schema:   MustLoadSchema("os", osInfo),
+	// 		isClosed: false,
+	// 	},
+	// 	Config: &osconf.Config,
+	// },
 }
