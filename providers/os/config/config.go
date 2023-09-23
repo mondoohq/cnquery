d250145b39d909151d0bef9bfb03d56147b20010
Dominik Richter <dominik.richter@gmail.com>
2023-09-22 23:57:44 -0700

    🎉 os-9.0.1 (#1859)
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/providers/os",
-	Version: "9.0.0",
+	Version: "9.0.1",
 	ConnectionTypes: []string{
 		provider.LocalConnectionType,
 		provider.SshConnectionType,
