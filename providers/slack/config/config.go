abb813b2489ecbd635e3443d8b52a83940b238ef
Mondoo <hello@mondoo.com>
2023-09-25 00:44:17 -0700

    🎉 os-9.0.2, slack-9.0.1, terraform-9.0.1, vcd-9.0.1, vsphere-9.0.1
    This release was created by cnquery's provider versioning bot.
    You can find me under: `providers-sdk/v1/util/version`.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- providers/slack/config/config.go
+++ providers/slack/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "slack",
 	ID:              "go.mondoo.com/cnquery/providers/slack",
-	Version:         "9.0.0",
+	Version:         "9.0.1",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
