49928809b6f2c3002e3b96325a35cedc170741d1
Christoph Hartmann <chris@lollyrock.com>
2023-09-27 23:43:13 +0200

    🧹 fix panic during login with wrong token

--- apps/cnquery/cmd/status.go
+++ apps/cnquery/cmd/status.go
@@ -179,7 +179,7 @@ func (s Status) RenderCliStatus() {
 
 	if s.Client.Registered && s.Client.PingPongError == nil {
 		log.Info().Msg(theme.DefaultTheme.Success("client authenticated successfully"))
-	} else {
+	} else if s.Client.PingPongError == nil {
 		log.Error().Err(s.Client.PingPongError).
 			Msgf("The Mondoo Platform credentials provided at %s didn't successfully authenticate with Mondoo Platform. Please re-authenticate with Mondoo Platform. To learn how, read https://mondoo.com/docs/cnspec/cnspec-adv-install/registration.",
 				viper.ConfigFileUsed())
