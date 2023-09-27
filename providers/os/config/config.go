c2c4003e085d00a5cf8649c3820e1ce493c72fbd
Dominik Richter <dominik.richter@gmail.com>
2023-09-27 02:56:27 -0700

    🎉 gcp-9.0.2, k8s-9.0.2, os-9.0.4, terraform-9.0.3 (#1948)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/providers/os",
-	Version: "9.0.3",
+	Version: "9.0.4",
 	ConnectionTypes: []string{
 		provider.LocalConnectionType,
 		provider.SshConnectionType,
