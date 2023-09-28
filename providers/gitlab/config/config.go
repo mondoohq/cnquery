799697fe05846df7406a862b61bbd1c039691245
Dominik Richter <dominik.richter@gmail.com>
2023-09-28 16:38:33 -0700

    ⭐ gitlab provider with discovery (#1974)
    Add discovery options to GitLab provider.
    
    Example:
    
    ```bash
    cnquery run gitlab --group mondoolabs --discover projects
    ```
    
    We also support `--discover groups` if you want to add the affected
    group to the set as well.

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -9,16 +9,25 @@ import (
 )
 
 var Config = plugin.Provider{
-	Name:            "gitlab",
-	ID:              "go.mondoo.com/cnquery/providers/gitlab",
-	Version:         "9.0.1",
-	ConnectionTypes: []string{provider.ConnectionType},
+	Name:    "gitlab",
+	ID:      "go.mondoo.com/cnquery/providers/gitlab",
+	Version: "9.0.1",
+	ConnectionTypes: []string{
+		provider.ConnectionType,
+		provider.GitlabGroupConnection,
+		provider.GitlabProjectConnection,
+	},
 	Connectors: []plugin.Connector{
 		{
-			Name:      "gitlab",
-			Use:       "gitlab",
-			Short:     "GitLab",
-			Discovery: []string{},
+			Name:  "gitlab",
+			Use:   "gitlab",
+			Short: "GitLab",
+			Discovery: []string{
+				provider.DiscoveryAuto,
+				provider.DiscoveryGroup,
+				provider.DiscoveryProject,
+				provider.DiscoveryTerraform,
+			},
 			Flags: []plugin.Flag{
 				{
 					Long:    "token",
@@ -29,8 +38,15 @@ var Config = plugin.Provider{
 				{
 					Long:    "group",
 					Type:    plugin.FlagType_String,
+					Option:  plugin.FlagOption_Required,
 					Default: "",
 					Desc:    "a GitLab group to scan",
+				},
+				{
+					Long:    "project",
+					Type:    plugin.FlagType_String,
+					Default: "",
+					Desc:    "a GitLab project to scan",
 				},
 			},
 		},
