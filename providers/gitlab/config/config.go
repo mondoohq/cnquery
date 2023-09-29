57e186b1894b5d46e21f53556eb443f13e7cae97
Mondoo <hello@mondoo.com>
2023-09-29 15:31:16 +0200

    🎉 aws-9.0.3, github-9.0.2, gitlab-9.0.2, k8s-9.0.4, okta-9.0.3, os-9.0.5, terraform-9.0.4
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/providers/gitlab",
-	Version: "9.0.1",
+	Version: "9.0.2",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
