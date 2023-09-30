bc979377a9980f6a7b8e3cb61ae6889d8eece0c8
Christoph Hartmann <chris@lollyrock.com>
2023-09-30 17:52:35 +0200

    🧹 update test package

--- _motor/providers/awsec2ebs/types_test.go
+++ _motor/providers/awsec2ebs/types_test.go
@@ -6,13 +6,13 @@ package awsec2ebs
 import (
 	"testing"
 
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseInstanceId(t *testing.T) {
 	path := "account/185972265011/region/us-east-1/instance/i-07f67838ada5879af"
 	id, err := ParseInstanceId(path)
-	assert.NilError(t, err)
+	assert.NotNil(t, err)
 	assert.Equal(t, id.Account, "185972265011")
 	assert.Equal(t, id.Region, "us-east-1")
 	assert.Equal(t, id.Id, "i-07f67838ada5879af")
--- providers/os/id/awsecs/awsecs_test.go
+++ providers/os/id/awsecs/awsecs_test.go
@@ -6,16 +6,16 @@ package awsecs
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mondoo.com/cnquery/providers/os/connection/mock"
 	"go.mondoo.com/cnquery/providers/os/detector"
-	"gotest.tools/assert"
 )
 
 func TestParseECSContainerId(t *testing.T) {
 	path := "//platformid.api.mondoo.app/runtime/aws/ecs/v1/accounts/185972265011/regions/us-east-1/container/vjtest/f088b38d61ac45d6a946b5aebbe7197a/314e35e0-2d0a-4408-b37e-16063461d73a"
 	id, err := ParseMondooECSContainerId(path)
-	assert.NilError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, id.Account, "185972265011")
 	assert.Equal(t, id.Region, "us-east-1")
 	assert.Equal(t, id.Id, "vjtest/f088b38d61ac45d6a946b5aebbe7197a/314e35e0-2d0a-4408-b37e-16063461d73a")
