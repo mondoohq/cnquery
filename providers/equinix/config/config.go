9962297167f343838922548d0d2b1115eaa0e6b6
Christoph Hartmann <chris@lollyrock.com>
2023-10-01 15:03:51 +0200

    🐛 fix equinix cli

--- providers/equinix/config/config.go
+++ providers/equinix/config/config.go
@@ -15,16 +15,27 @@ var Config = plugin.Provider{
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
-			Name:      "equinix",
-			Use:       "equinix",
-			Short:     "Equinix Metal",
+			Name:  "equinix",
+			Use:   "equinix [org <org id>] [project <project-id>] [--token <token>]",
+			Short: "Equinix Metal",
+			Long: `equinix is designed for querying resources within a specified 
+project or organization on the Equinix Metal platform.
+
+Available Commands:
+  org <org id>              Specifies the organization to interact with, using the organization identifier.
+  project <project-id>      Specifies the project to interact with, using the project identifier.
+
+If the PACKET_AUTH_TOKEN environment variable is set, the token flag is not required.
+`,
+			MinArgs:   2,
+			MaxArgs:   2,
 			Discovery: []string{},
 			Flags: []plugin.Flag{
 				{
 					Long:    "token",
 					Type:    plugin.FlagType_String,
 					Default: "",
-					Desc:    "Slack API token",
+					Desc:    " Specifies the Equinix API token for authenticating the user",
 				},
 			},
 		},
