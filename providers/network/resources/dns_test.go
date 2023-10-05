07e18ea7e81a2ecb03fbe0587928a95b0451b995
Christian Zunker <christian@mondoo.com>
2023-10-05 07:21:28 +0200

    🐛 Fix dns.fqdn when connection via IP
    Fixes #1958
    
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- providers/network/resources/dns_test.go
+++ providers/network/resources/dns_test.go
@@ -7,6 +7,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnquery/llx"
+	"go.mondoo.com/cnquery/providers-sdk/v1/inventory"
+	"go.mondoo.com/cnquery/providers-sdk/v1/plugin"
+	"go.mondoo.com/cnquery/providers/network/connection"
+	"go.mondoo.com/cnquery/providers/network/resources"
 )
 
 func TestResource_DNS(t *testing.T) {
@@ -19,4 +25,45 @@ func TestResource_DomainName(t *testing.T) {
 	assert.NotEmpty(t, res)
 	res = x.TestQuery(t, "domainName(\"mondoo.com\").tld")
 	assert.Equal(t, "com", string(res[0].Result().Data.Value))
+}
+
+func TestResource_DnsFqdn(t *testing.T) {
+	testCases := []struct {
+		hostName   string
+		expectedId string
+	}{
+		{
+			hostName:   "127.0.0.1",
+			expectedId: "dns/",
+		},
+		{
+			hostName:   "3.127.139.132",
+			expectedId: "dns/",
+		},
+		{
+			hostName:   "www.mondoo.com",
+			expectedId: "dns/www.mondoo.com",
+		},
+		{
+			hostName:   "ec2-3-127-139-132.eu-central-1.compute.amazonaws.com",
+			expectedId: "dns/ec2-3-127-139-132.eu-central-1.compute.amazonaws.com",
+		},
+	}
+
+	runtime := &plugin.Runtime{}
+
+	for _, tc := range testCases {
+		conf := &inventory.Config{
+			Host: tc.hostName,
+		}
+		runtime.Connection = connection.NewHostConnection(1, nil, conf)
+
+		dns, err := resources.NewResource(
+			runtime,
+			"dns",
+			map[string]*llx.RawData{},
+		)
+		require.NoError(t, err)
+		require.Equal(t, tc.expectedId, dns.MqlID())
+	}
 }
