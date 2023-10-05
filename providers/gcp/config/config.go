25fc42b7f688772137a84429766354fdd35442f4
Mondoo <hello@mondoo.com>
2023-10-06 00:01:25 +0200

    🎉 gcp-9.0.8, os-9.0.11, terraform-9.0.10
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "9.0.7",
+	Version: "9.0.8",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
