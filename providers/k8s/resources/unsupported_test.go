55a1c658dae8bef3d5ea3059228a3a4c63e7ce09
Christian Zunker <christian@mondoo.com>
2023-10-09 15:10:03 +0200

    Add test
    Signed-off-by: Christian Zunker <christian@mondoo.com>

---
+++ providers/k8s/resources/unsupported_test.go
@@ -0,0 +1,35 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnquery/v9/providers-sdk/v1/inventory"
+	"go.mondoo.com/cnquery/v9/providers/k8s/connection/manifest"
+	"go.mondoo.com/cnquery/v9/providers/k8s/connection/shared"
+	sharedres "go.mondoo.com/cnquery/v9/providers/k8s/connection/shared/resources"
+)
+
+func TestManifestFile_OutdatedApi(t *testing.T) {
+	manifestFile := "./testdata/nginx-deployment.yaml"
+
+	conn, err := manifest.NewConnection(0, &inventory.Asset{
+		Connections: []*inventory.Config{
+			{
+				Options: map[string]string{
+					shared.OPTION_NAMESPACE: "default",
+				},
+			},
+		},
+	}, manifest.WithManifestFile(manifestFile))
+	require.NoError(t, err)
+	require.NotNil(t, conn)
+
+	manifestConn := conn.(*manifest.Connection)
+	deployment := manifestConn.ManifestParser.Objects[0]
+
+	parsed, err := sharedres.GetPodSpec(deployment)
+	require.Error(t, err)
+	require.Nil(t, parsed)
+	require.Equal(t, "object Deployment with version apps/v1beta1 is not supported", err.Error())
+}
