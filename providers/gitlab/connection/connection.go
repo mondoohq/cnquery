b91bb56682f7308d99a9371ed84419e3f268c9cd
Victoria Jeffrey <vj@mondoo.com>
2023-10-08 18:37:37 -0600

    WIP fix gitlab discovery for enterprise gitlab

--- providers/gitlab/connection/connection.go
+++ providers/gitlab/connection/connection.go
@@ -7,6 +7,8 @@ import (
 	"errors"
 	"net/url"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 	"github.com/xanzy/go-gitlab"
@@ -22,8 +24,10 @@ type GitLabConnection struct {
 	project     *gitlab.Project
 	projectID   string // only used for initial setup, use project.ID afterwards!
 	groupName   string
+	groupID     string
 	projectName string
 	client      *gitlab.Client
+	url         string
 }
 
 func NewGitLabConnection(id uint32, asset *inventory.Asset, conf *inventory.Config) (*GitLabConnection, error) {
@@ -67,8 +71,10 @@ func NewGitLabConnection(id uint32, asset *inventory.Asset, conf *inventory.Conf
 		id:          id,
 		asset:       asset,
 		groupName:   conf.Options["group"],
+		groupID:     conf.Options["group-id"],
 		projectName: conf.Options["project"],
 		projectID:   conf.Options["project-id"],
+		url:         conf.Options["url"],
 		client:      client,
 	}
 
@@ -87,6 +93,18 @@ func (c *GitLabConnection) Asset() *inventory.Asset {
 	return c.asset
 }
 
+func (c *GitLabConnection) GroupName() string {
+	return c.groupName
+}
+
+func (c *GitLabConnection) GroupID() int {
+	i, err := strconv.Atoi(c.groupID)
+	if err == nil {
+		return i
+	}
+	return 0
+}
+
 func (c *GitLabConnection) Client() *gitlab.Client {
 	return c.client
 }
@@ -99,9 +117,28 @@ func (c *GitLabConnection) Group() (*gitlab.Group, error) {
 		return nil, errors.New("cannot look up gitlab group, no group name defined")
 	}
 
+	// if group name has a slash, we know its a subgroup
+	if names := strings.Split(c.groupName, "/"); len(names) > 1 {
+		return c.findSubgroup(names[0], names[1])
+	}
+
 	var err error
 	c.group, _, err = c.Client().Groups.GetGroup(c.groupName, nil)
 	return c.group, err
+}
+
+func (c *GitLabConnection) findSubgroup(parentId string, name string) (*gitlab.Group, error) {
+	log.Debug().Msgf("find subgroup for %s %s", parentId, name)
+	groups, err := DiscoverSubAndDescendantGroupsForGroup(c, parentId)
+	if err != nil {
+		return nil, err
+	}
+	for i := range groups {
+		if name == groups[i].Name {
+			return groups[i], nil
+		}
+	}
+	return nil, errors.New("not found")
 }
 
 func (c *GitLabConnection) IsGroup() bool {
@@ -144,4 +181,61 @@ func (c *GitLabConnection) Project() (*gitlab.Project, error) {
 	}
 	c.project, _, err = c.Client().Projects.GetProject(pid, nil)
 	return c.project, err
+}
+
+func DiscoverSubAndDescendantGroupsForGroup(conn *GitLabConnection, rootGroup string) ([]*gitlab.Group, error) {
+	var list []*gitlab.Group
+	// discover subgroups
+	subgroups, err := groupSubgroups(conn, rootGroup)
+	if err != nil {
+		log.Debug().Err(err).Msgf("cannot discover subgroups for %v", rootGroup)
+	} else {
+		list = append(list, subgroups...)
+	}
+	// discover descendant groups
+	descgroups, err := groupDescendantGroups(conn, rootGroup)
+	if err != nil {
+		log.Debug().Err(err).Msgf("cannot discover descendant groups for %v", rootGroup)
+	} else {
+		list = append(list, descgroups...)
+	}
+	return list, nil
+}
+
+func groupDescendantGroups(conn *GitLabConnection, gid interface{}) ([]*gitlab.Group, error) {
+	log.Debug().Msgf("calling list descendant groups with %v", gid)
+	perPage := 50
+	page := 1
+	total := 50
+	groups := []*gitlab.Group{}
+	for page*perPage <= total {
+		grps, resp, err := conn.Client().Groups.ListDescendantGroups(gid, &gitlab.ListDescendantGroupsOptions{ListOptions: gitlab.ListOptions{Page: page, PerPage: perPage}})
+		if err != nil {
+			return nil, err
+		}
+		groups = append(groups, grps...)
+		total = resp.TotalItems
+		page += 1
+	}
+
+	return groups, nil
+}
+
+func groupSubgroups(conn *GitLabConnection, gid interface{}) ([]*gitlab.Group, error) {
+	log.Debug().Msgf("calling list subgroups with %v", gid)
+	perPage := 50
+	page := 1
+	total := 50
+	groups := []*gitlab.Group{}
+	for page*perPage <= total {
+		grps, resp, err := conn.Client().Groups.ListSubGroups(gid, &gitlab.ListSubGroupsOptions{ListOptions: gitlab.ListOptions{Page: page, PerPage: perPage}})
+		if err != nil {
+			return nil, err
+		}
+		groups = append(groups, grps...)
+		total = resp.TotalItems
+		page += 1
+	}
+
+	return groups, nil
 }
