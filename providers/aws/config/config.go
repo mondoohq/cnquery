031e4220de406cf584ff4e4677e45e65ccb5e9ab
Christoph Hartmann <chris@lollyrock.com>
2023-10-17 12:25:15 +0200

    🎉 arista-9.0.7, atlassian-9.0.1, aws-9.0.16, azure-9.0.11, core-9.0.3, equinix-9.0.6, gcp-9.0.10, github-9.0.9, gitlab-9.0.9, google-workspace-9.0.7, ipmi-9.0.6, k8s-9.0.13, ms365-9.0.10, network-9.0.5, oci-9.0.9, okta-9.0.8, opcua-9.0.7, os-9.0.13, slack-9.0.7, terraform-9.0.15, vcd-9.0.8, vsphere-9.0.7 (#2270)

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "9.0.15",
+	Version:         "9.0.16",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "azure",
 	ID:              "go.mondoo.com/cnquery/v9/providers/azure",
-	Version:         "9.0.10",
+	Version:         "9.0.11",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
