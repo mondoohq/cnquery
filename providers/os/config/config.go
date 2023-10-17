ddb878f6275efdfdd6f29507d6c33047fb492987
Mondoo <hello@mondoo.com>
2023-10-17 12:03:17 +0200

    🎉 arista-9.0.7, atlassian-9.0.1, aws-9.0.16, azure-9.0.11, core-9.0.3, equinix-9.0.6, gcp-9.0.10, github-9.0.9, gitlab-9.0.9, google-workspace-9.0.7, ipmi-9.0.6, k8s-9.0.13, ms365-9.0.10, network-9.0.5, oci-9.0.9, okta-9.0.8, opcua-9.0.7, os-9.0.13, slack-9.0.7, terraform-9.0.15, vcd-9.0.8, vsphere-9.0.7
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "9.0.12",
+	Version: "9.0.13",
 	ConnectionTypes: []string{
 		provider.LocalConnectionType,
 		provider.SshConnectionType,
