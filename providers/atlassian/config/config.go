8a4b04367bbaab26b141d5e347d8b4453e9ed7fa
Marius Kimmina <mar.kimmina@gmail.com>
2023-10-17 10:43:35 +0200

    🧹 add help to atlassian provider
    Signed-off-by: Marius Kimmina <mar.kimmina@gmail.com>

--- providers/atlassian/config/config.go
+++ providers/atlassian/config/config.go
@@ -21,9 +21,27 @@ var Config = plugin.Provider{
 	},
 	Connectors: []plugin.Connector{
 		{
-			Name:      "atlassian",
-			Use:       "atlassian",
-			Short:     "Atlassian",
+			Name:  "atlassian",
+			Use:   "atlassian",
+			Short: "Atlassian",
+			Long: `atlassian is designed for querying resources within Atlassian Cloud.
+
+The provider offers 4 subcommands:
+1. Admin
+"cnquery shell atlassian admin" requires either the env variable "ATLASSIAN_ADMIN_TOKEN" or the --admin-token flag to be set
+
+2. Jira
+"cnquery shell atlassian jira" requires either the env variables "ATLASSIAN_USER", "ATLASSIAN_HOST" and "ATLASSIAN_USER_TOKEN" or 
+the flags --user, --host, --user-token to be set
+
+3. Confluence
+"cnquery shell atlassian confluence" requires either the env variables "ATLASSIAN_USER", "ATLASSIAN_HOST" and "ATLASSIAN_USER_TOKEN" or 
+the flags --user, --host, --user-token to be set
+
+4. SCIM
+"cnquery shell atlassian scim DIRECTORYID" requires a directory-id and either the env variable "ATLASSIAN_SCIM_TOKEN" or the --scim-token flag to be set
+You receiv both the token and the directory-id from atlassian when you setup an identity provider.
+`,
 			MaxArgs:   2,
 			Discovery: []string{},
 			Flags: []plugin.Flag{
