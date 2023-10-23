da6c5d2d235baef16755a1844a95cb1b71fa5d5b
Christoph Hartmann <chris@lollyrock.com>
2023-10-23 15:03:01 +0200

    🎉 aws-9.0.19, azure-9.0.15, gcp-9.0.13, github-9.0.12, gitlab-9.0.12, google-workspace-9.0.9, k8s-9.0.15, oci-9.0.11, opcua-9.0.9, os-9.0.16, terraform-9.0.17 (#2342)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/opcua/config/config.go
+++ providers/opcua/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "opcua",
 	ID:              "go.mondoo.com/cnquery/v9/providers/opcua",
-	Version:         "9.0.8",
+	Version:         "9.0.9",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "9.0.15",
+	Version: "9.0.16",
 	ConnectionTypes: []string{
 		provider.LocalConnectionType,
 		provider.SshConnectionType,
