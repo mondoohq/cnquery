39b90883e5b5590b7293a4e1e89d8e4872c6a3d6
Marius Kimmina <mar.kimmina@gmail.com>
2023-10-25 18:41:04 +0200

    🐛 fix --asset-name flag

--- explorer/scan/local_scanner.go
+++ explorer/scan/local_scanner.go
@@ -272,6 +272,14 @@ func (s *LocalScanner) distributeJob(job *Job, ctx context.Context, upstream *up
 		return nil, false, nil
 	}
 
+	// if there is exactly one asset, assure that the --asset-name is used
+	// TODO: make it so that the --asset-name is set for the root asset only even if multiple assets are there
+	// This is a temporary fix that only works if there is only one asset
+	if len(assets) == 1 && assetList[0].Name != "" && assetList[0].Name != assets[0].asset.Name {
+		log.Debug().Str("asset", assets[0].asset.Name).Msg("Overriding asset name with --asset-name flag")
+		assets[0].asset.Name = assetList[0].Name
+	}
+
 	justAssets := []*inventory.Asset{}
 	for _, asset := range assets {
 		asset.asset.KindString = asset.asset.GetPlatform().Kind
