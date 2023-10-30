9341783f25921eb6b1a95d6f1b5f3d546f7784f9
Christoph Hartmann <chris@lollyrock.com>
2023-10-30 08:53:53 +0100

    ⭐️ implement vuln report for vsphere (#2363)

---
+++ providers/vsphere/resources/asset_advisories.go
@@ -0,0 +1,43 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package resources
+
+import (
+	"context"
+	"go.mondoo.com/cnquery/v9/providers-sdk/v1/resources"
+	"go.mondoo.com/cnquery/v9/providers-sdk/v1/upstream/mvd"
+	"go.mondoo.com/cnquery/v9/providers-sdk/v1/util/convert"
+	"go.mondoo.com/cnquery/v9/providers/vsphere/connection"
+)
+
+// fetches the vulnerability report and returns the full report
+func (p *mqlAsset) vulnerabilityReport() (interface{}, error) {
+	runtime := p.MqlRuntime
+
+	mcc := runtime.Upstream
+	if mcc == nil || mcc.ApiEndpoint == "" {
+		return nil, resources.MissingUpstreamError{}
+	}
+
+	// get new mvd client
+	scannerClient, err := mvd.NewAdvisoryScannerClient(mcc.ApiEndpoint, mcc.HttpClient, mcc.Plugins...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn := runtime.Connection.(*connection.VsphereConnection)
+	apiPackages := []*mvd.Package{}
+
+	scanjob := &mvd.AnalyseAssetRequest{
+		Platform: mvd.NewMvdPlatform(conn.Asset().Platform),
+		Packages: apiPackages,
+	}
+
+	report, err := scannerClient.AnalyseAsset(context.Background(), scanjob)
+	if err != nil {
+		return nil, err
+	}
+
+	return convert.JsonToDict(report)
+}
