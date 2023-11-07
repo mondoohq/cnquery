15282c58097f2fa69aae257f788a4002fd181a2d
Marius Kimmina <38843153+mariuskimmina@users.noreply.github.com>
2023-11-07 16:47:01 +0100

    🎉 aws-9.1.3, gcp-9.1.2, google-workspace-9.1.2, k8s-9.1.2, ms365-9.1.2, oci-9.1.2, os-9.1.3, vsphere-9.1.2 (#2519)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "9.1.2",
+	Version: "9.1.3",
 	ConnectionTypes: []string{
 		provider.LocalConnectionType,
 		provider.SshConnectionType,
