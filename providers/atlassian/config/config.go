1e9dbf0254fce89927ab7120fc16a16cb4892ffb
Marius Kimmina <38843153+mariuskimmina@users.noreply.github.com>
2023-11-17 18:54:25 +0100

    🎉 arista-9.1.3, atlassian-9.1.3, aws-9.1.7, azure-9.1.5, equinix-9.1.4, gcp-9.1.5, github-9.1.4, gitlab-9.1.4, google-workspace-9.1.5, ipmi-9.1.4, k8s-9.1.5, ms365-9.2.1, oci-9.1.5, okta-9.1.4, opcua-9.1.3, os-9.1.6, slack-9.1.3, terraform-9.1.3, vcd-9.1.3, vsphere-9.1.4 (#2600)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/atlassian/config/config.go
+++ providers/atlassian/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "atlassian",
 	ID:      "go.mondoo.com/cnquery/providers/atlassian",
-	Version: "9.1.2",
+	Version: "9.1.3",
 	ConnectionTypes: []string{
 		provider.DefaultConnectionType,
 		"jira",
--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "azure",
 	ID:              "go.mondoo.com/cnquery/v9/providers/azure",
-	Version:         "9.1.4",
+	Version:         "9.1.5",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
