a38c2a9759e217cc20bd9fdb1430c1989374bb56
Christoph Hartmann <chris@lollyrock.com>
2023-11-27 23:50:40 +0100

    🎉 arista-9.1.5, atlassian-9.1.5, aws-9.1.9, azure-9.1.7, core-9.1.5, equinix-9.1.6, gcp-9.1.7, github-9.1.6, gitlab-9.1.6, google-workspace-9.1.7, ipmi-9.1.6, k8s-9.1.7, ms365-9.2.3, oci-9.1.7, okta-9.1.6, opcua-9.1.5, os-9.1.8, slack-9.1.5, terraform-9.1.5, vcd-9.1.5, vsphere-9.1.6 (#2659)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/vsphere/config/config.go
+++ providers/vsphere/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vsphere",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vsphere",
-	Version:         "9.1.5",
+	Version:         "9.1.6",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
