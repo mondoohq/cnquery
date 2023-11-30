5495e3f1eed1d14b395b4ecc32620f538192ff62
Christian Zunker <827818+czunker@users.noreply.github.com>
2023-11-30 17:40:54 +0100

    🎉 arista-9.1.7, atlassian-9.1.7, aws-9.1.11, azure-9.1.9, equinix-9.1.8, gcp-9.1.9, github-9.1.8, gitlab-9.1.8, google-workspace-9.1.9, ipmi-9.1.8, k8s-9.1.9, ms365-9.2.5, oci-9.1.9, okta-9.1.8, opcua-9.1.7, os-9.1.10, slack-9.1.7, terraform-9.1.7, vcd-9.1.7, vsphere-9.1.8 (#2716)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "9.1.10",
+	Version:         "9.1.11",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
