aba76d7a0d607127537ec9105eb68b859924a2c5
Preslav Gerchev <preslav@mondoo.com>
2023-12-01 15:10:12 +0200

    🎉 arista-9.1.8, atlassian-9.1.8, aws-9.1.12, azure-9.1.10, equinix-9.1.9, gcp-9.1.10, github-9.1.9, gitlab-9.1.9, google-workspace-9.1.10, ipmi-9.1.9, k8s-9.1.10, ms365-9.2.6, oci-9.1.10, okta-9.1.9, opcua-9.1.8, os-9.1.11, slack-9.1.8, terraform-9.1.8, vcd-9.1.8, vsphere-9.1.9 (#2722)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/opcua/config/config.go
+++ providers/opcua/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "opcua",
 	ID:              "go.mondoo.com/cnquery/v9/providers/opcua",
-	Version:         "9.1.7",
+	Version:         "9.1.8",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/vcd/config/config.go
+++ providers/vcd/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vcd",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vcd",
-	Version:         "9.1.7",
+	Version:         "9.1.8",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
