49262f02ffdbb29c35260985cee717e89bc4306a
Marius Kimmina <38843153+mariuskimmina@users.noreply.github.com>
2023-12-05 18:08:21 +0100

    🎉 arista-9.1.9, atlassian-9.1.9, aws-9.1.13, azure-9.1.11, equinix-9.1.10, gcp-9.1.11, github-9.1.10, gitlab-9.1.10, google-workspace-9.1.11, ipmi-9.1.10, k8s-9.1.12, ms365-9.2.7, network-9.1.6, oci-9.1.11, okta-9.1.10, opcua-9.1.9, os-9.1.12, slack-9.1.9, terraform-9.1.9, vcd-9.1.9, vsphere-9.1.10 (#2747)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "9.1.10",
+	Version: "9.1.11",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
