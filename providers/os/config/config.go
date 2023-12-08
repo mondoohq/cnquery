8d38b314597647b66b7636ba47013bf2be1ff6b7
Mondoo <hello@mondoo.com>
2023-12-08 16:48:17 +0100

    🎉 network-9.1.8, os-9.1.14
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "9.1.13",
+	Version: "9.1.14",
 	ConnectionTypes: []string{
 		provider.LocalConnectionType,
 		provider.SshConnectionType,
