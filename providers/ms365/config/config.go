a4d3470fe350f2923cfd9c0a4c8c24a479c3832a
Christoph Hartmann <chris@lollyrock.com>
2023-12-11 16:40:24 +0100

    🎉 arista-9.1.10, atlassian-9.1.10, aws-9.1.15, azure-9.1.12, core-9.1.6, equinix-9.1.11, gcp-9.1.13, github-9.1.12, gitlab-9.1.11, google-workspace-9.1.13, ipmi-9.1.11, k8s-9.1.13, ms365-9.2.8, oci-9.1.12, okta-9.1.11, opcua-9.1.10, os-9.1.15, slack-9.1.10, terraform-9.1.10, vcd-9.1.10, vsphere-9.1.11 (#2800)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/ms365/config/config.go
+++ providers/ms365/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ms365",
 	ID:              "go.mondoo.com/cnquery/v9/providers/ms365",
-	Version:         "9.2.7",
+	Version:         "9.2.8",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
