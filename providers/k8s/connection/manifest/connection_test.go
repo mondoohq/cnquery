caa15c4a7ec86d508776d0f9fba1955e330aa5cf
Christian Zunker <christian@mondoo.com>
2023-12-14 08:37:46 +0100

    🐛 Fix platform IDs for operator manifests
    Fixes #2818
    
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- providers/k8s/connection/manifest/connection_test.go
+++ providers/k8s/connection/manifest/connection_test.go
@@ -103,3 +103,140 @@ func TestManifestDiscovery(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, inv.Spec.Assets, 1)
 }
+
+func TestOperatorManifest(t *testing.T) {
+	path := "./testdata/mondoo-operator-manifests.yaml"
+
+	runtime := K8s()
+	rootAsset := &inventory.Asset{
+		Connections: []*inventory.Config{{
+			Type: "k8s",
+			Options: map[string]string{
+				shared.OPTION_MANIFEST: path,
+			},
+			Discover: &inventory.Discovery{
+				Targets: []string{"auto"},
+			},
+		}},
+	}
+	conn, err := manifest.NewConnection(0, rootAsset, manifest.WithManifestFile(path))
+	require.NoError(t, err)
+
+	err = runtime.Connect(&plugin.ConnectReq{
+		Asset: rootAsset,
+	})
+	require.NoError(t, err)
+
+	pluginRuntime := &plugin.Runtime{
+		Connection:     conn,
+		HasRecording:   false,
+		CreateResource: resources.CreateResource,
+	}
+	inv, err := resources.Discover(pluginRuntime)
+	require.NoError(t, err)
+	require.Len(t, inv.Spec.Assets, 2)
+
+	require.Len(t, inv.Spec.Assets[1].PlatformIds, 1)
+
+	for i := range inv.Spec.Assets {
+		asset := inv.Spec.Assets[i]
+		err = runtime.Connect(&plugin.ConnectReq{
+			Asset: asset,
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, asset.PlatformIds[0])
+	}
+	require.NotEqual(t, inv.Spec.Assets[0].PlatformIds[0], inv.Spec.Assets[1].PlatformIds[0])
+	require.Equal(t, "//platformid.api.mondoo.app/runtime/k8s/uid/7b0dacb1266786d90e70e4c924064ef619eff6b1ccb4b0769f408510570fbbd2", inv.Spec.Assets[0].PlatformIds[0])
+	require.Equal(t, "//platformid.api.mondoo.app/runtime/k8s/uid/7b0dacb1266786d90e70e4c924064ef619eff6b1ccb4b0769f408510570fbbd2/namespace/mondoo-operator/deployments/name/mondoo-operator-controller-manager", inv.Spec.Assets[1].PlatformIds[0])
+}
+
+func TestManifestNoObjects(t *testing.T) {
+	path := "./testdata/no-discovered-objects.yaml"
+
+	runtime := K8s()
+	rootAsset := &inventory.Asset{
+		Connections: []*inventory.Config{{
+			Type: "k8s",
+			Options: map[string]string{
+				shared.OPTION_MANIFEST: path,
+			},
+			Discover: &inventory.Discovery{
+				Targets: []string{"auto"},
+			},
+		}},
+	}
+	conn, err := manifest.NewConnection(0, rootAsset, manifest.WithManifestFile(path))
+	require.NoError(t, err)
+
+	err = runtime.Connect(&plugin.ConnectReq{
+		Asset: rootAsset,
+	})
+	require.NoError(t, err)
+
+	pluginRuntime := &plugin.Runtime{
+		Connection:     conn,
+		HasRecording:   false,
+		CreateResource: resources.CreateResource,
+	}
+	inv, err := resources.Discover(pluginRuntime)
+	require.NoError(t, err)
+	require.Len(t, inv.Spec.Assets, 1)
+
+	require.Len(t, inv.Spec.Assets[0].PlatformIds, 1)
+
+	for i := range inv.Spec.Assets {
+		asset := inv.Spec.Assets[i]
+		err = runtime.Connect(&plugin.ConnectReq{
+			Asset: asset,
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, asset.PlatformIds[0])
+	}
+	require.NotEmpty(t, inv.Spec.Assets[0].PlatformIds[0])
+}
+
+func TestManifestDir(t *testing.T) {
+	path := "./testdata/"
+
+	runtime := K8s()
+	rootAsset := &inventory.Asset{
+		Connections: []*inventory.Config{{
+			Type: "k8s",
+			Options: map[string]string{
+				shared.OPTION_MANIFEST: path,
+			},
+			Discover: &inventory.Discovery{
+				Targets: []string{"auto"},
+			},
+		}},
+	}
+	conn, err := manifest.NewConnection(0, rootAsset, manifest.WithManifestFile(path))
+	require.NoError(t, err)
+
+	err = runtime.Connect(&plugin.ConnectReq{
+		Asset: rootAsset,
+	})
+	require.NoError(t, err)
+
+	pluginRuntime := &plugin.Runtime{
+		Connection:     conn,
+		HasRecording:   false,
+		CreateResource: resources.CreateResource,
+	}
+	inv, err := resources.Discover(pluginRuntime)
+	require.NoError(t, err)
+	require.Len(t, inv.Spec.Assets, 3)
+
+	for i := range inv.Spec.Assets {
+		asset := inv.Spec.Assets[i]
+		err = runtime.Connect(&plugin.ConnectReq{
+			Asset: asset,
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, asset.PlatformIds[0])
+	}
+	require.NotEmpty(t, inv.Spec.Assets[0].PlatformIds[0])
+	// we have the operator deployment twice
+	require.Equal(t, inv.Spec.Assets[1].PlatformIds[0], inv.Spec.Assets[2].PlatformIds[0])
+}
