6f118adb12cf995e07f273b52271fd663d101dd6
Christoph Hartmann <chris@lollyrock.com>
2023-12-16 22:37:39 +0100

    🐛 set default sudo executable

---
+++ providers/os/connection/ssh_test.go
@@ -0,0 +1,24 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package connection
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mondoo.com/cnquery/v9/providers-sdk/v1/inventory"
+)
+
+func TestSSHDefaultSettings(t *testing.T) {
+	conn := &SshConnection{
+		conf: &inventory.Config{
+			Sudo: &inventory.Sudo{
+				Active: true,
+			},
+		},
+	}
+	conn.setDefaultSettings()
+	assert.Equal(t, int32(22), conn.conf.Port)
+	assert.Equal(t, "sudo", conn.conf.Sudo.Executable)
+}
