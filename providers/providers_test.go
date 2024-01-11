96a1e58ac1403cea9319b1835060e59df4e835d3
Preslav <preslav@mondoo.com>
2024-01-11 19:57:28 +0200

    Add a test for provider shutdown.
    Signed-off-by: Preslav <preslav@mondoo.com>

---
+++ providers/providers_test.go
@@ -0,0 +1,53 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package providers
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnquery/v9/providers-sdk/v1/plugin"
+)
+
+type testPlugin struct {
+	plugin.Service
+}
+
+func (t *testPlugin) Connect(req *plugin.ConnectReq, callback plugin.ProviderCallback) (*plugin.ConnectRes, error) {
+	return nil, nil
+}
+
+func (t *testPlugin) MockConnect(req *plugin.ConnectReq, callback plugin.ProviderCallback) (*plugin.ConnectRes, error) {
+	return nil, nil
+}
+
+func (t *testPlugin) ParseCLI(req *plugin.ParseCLIReq) (*plugin.ParseCLIRes, error) {
+	return nil, nil
+}
+
+func (t *testPlugin) Shutdown(req *plugin.ShutdownReq) (*plugin.ShutdownRes, error) {
+	// sleep more than the heartbeat interval to ensure that even if shutting down
+	// the provider can still respond to heartbeats
+	time.Sleep(10 * time.Second)
+	return &plugin.ShutdownRes{}, nil
+}
+
+func (t *testPlugin) GetData(req *plugin.DataReq) (*plugin.DataRes, error) {
+	return nil, nil
+}
+
+func (t *testPlugin) StoreData(req *plugin.StoreReq) (*plugin.StoreRes, error) {
+	return nil, nil
+}
+
+func TestProviderShutdown(t *testing.T) {
+	s := &RunningProvider{
+		Plugin: &testPlugin{},
+	}
+	err := s.heartbeat()
+	require.NoError(t, err)
+	err = s.Shutdown()
+	require.NoError(t, err)
+}
