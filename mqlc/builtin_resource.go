a2ab5b0c395c3264396412c188a4c1a4929969b0
Dominik Richter <dominik.richter@gmail.com>
2024-01-12 09:07:31 -0800

    ⭐ support named arguments in functions (#2964) (#2967)
    * ⭐ support named arguments in functions
    
    This is an exciting new v10 feature that allows users to set named arguments in functions.
    
    This means that you can do things like this:
    
    ```coffee
    users.all(user:
      groups.contains(group:
        user.uid == group.gid
      )
    )
    ```
    
    This handles all cases where you felt you had to assign `_` to a
    variable. This makes it easier in all cases where you could only use one
    expression (like with `all`, `one`, etc).
    
    You can still rely on using `_` if no variable name is defined. This
    remains the default.
    
    
    
    * 🐛 support named functions in dicts
    
    
    
    ---------
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- mqlc/builtin_resource.go
+++ mqlc/builtin_resource.go
@@ -134,22 +134,22 @@ func compileResourceWhere(c *compiler, typ types.Type, ref uint64, id string, ca
 	if call == nil {
 		return types.Nil, errors.New("missing filter argument for calling '" + id + "'")
 	}
-	if len(call.Function) > 1 {
-		return types.Nil, errors.New("too many arguments when calling '" + id + "', only 1 is supported")
-	}
-
 	// if the where function is called without arguments, we don't have to do anything
 	// so we just return the caller type as no additional step in the compiler is necessary
 	if len(call.Function) == 0 {
 		return typ, nil
 	}
-
-	arg := call.Function[0]
-	if arg.Name != "" {
-		return types.Nil, errors.New("called '" + id + "' function with a named parameter, which is not supported")
+	if len(call.Function) > 1 {
+		return types.Nil, errors.New("too many arguments when calling '" + id + "', only 1 is supported")
 	}
 
-	refs, err := c.blockExpressions([]*parser.Expression{arg.Value}, types.Array(types.Type(resource.ListType)), ref)
+	arg := call.Function[0]
+	bindingName := "_"
+	if arg.Name != "" {
+		bindingName = arg.Name
+	}
+
+	refs, err := c.blockExpressions([]*parser.Expression{arg.Value}, types.Array(types.Type(resource.ListType)), ref, bindingName)
 	if err != nil {
 		return types.Nil, err
 	}
@@ -209,11 +209,12 @@ func compileResourceMap(c *compiler, typ types.Type, ref uint64, id string, call
 	}
 
 	arg := call.Function[0]
+	bindingName := "_"
 	if arg.Name != "" {
-		return types.Nil, errors.New("called '" + id + "' function with a named parameter, which is not supported")
+		bindingName = arg.Name
 	}
 
-	refs, err := c.blockExpressions([]*parser.Expression{arg.Value}, types.Array(types.Type(resource.ListType)), ref)
+	refs, err := c.blockExpressions([]*parser.Expression{arg.Value}, types.Array(types.Type(resource.ListType)), ref, bindingName)
 	if err != nil {
 		return types.Nil, err
 	}
--- providers-sdk/v1/testutils/mockprovider/resources/all.go
+++ providers-sdk/v1/testutils/mockprovider/resources/all.go
@@ -51,7 +51,7 @@ func (c *mqlMgroup) id() (string, error) {
 
 func (c *mqlMuser) dict() (any, error) {
 	return map[string]any{
-		"a1": []any{int64(1), int64(2), int64(3)},
-		"s1": "hello world",
+		"listInt": []any{int64(1), int64(2), int64(3)},
+		"string":  "hello world",
 	}, nil
 }
