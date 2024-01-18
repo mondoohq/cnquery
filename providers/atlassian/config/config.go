ab0770963a4c1537845403f4552fed7ad1dd76d3
Mondoo <hello@mondoo.com>
2024-01-18 02:13:15 -0800

    🎉 arista-9.1.16, atlassian-9.1.16, aws-9.1.26, azure-9.2.2, core-9.1.9, equinix-9.1.17, gcp-9.1.19, github-9.1.20, gitlab-9.1.17, google-workspace-9.1.20, ipmi-9.1.17, k8s-9.1.23, ms365-9.2.15, network-9.1.9, oci-9.1.18, okta-9.1.17, opcua-9.1.16, os-9.1.21, slack-9.1.18, terraform-9.1.16, vcd-9.1.16, vsphere-9.1.19
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/atlassian/config/config.go
+++ providers/atlassian/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "atlassian",
 	ID:      "go.mondoo.com/cnquery/v9/providers/atlassian",
-	Version: "9.1.15",
+	Version: "9.1.16",
 	ConnectionTypes: []string{
 		provider.DefaultConnectionType,
 		"jira",
