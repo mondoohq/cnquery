1df06430d74d9c1b46a37bf6e9e5c26b491bf24f
Preslav <preslav@mondoo.com>
2024-01-22 13:21:28 +0200

    Update network test.
    Signed-off-by: Preslav <preslav@mondoo.com>

--- providers/azure/resources/network_test.go
+++ providers/azure/resources/network_test.go
@@ -10,11 +10,13 @@ import (
 )
 
 func TestParseAzurePortRange(t *testing.T) {
-	entry := "80,1024-65535"
+	entry := "*,80,1024-65535"
 	ranges := parseAzureSecurityRulePortRange(entry)
-	assert.Equal(t, 2, len(ranges))
-	assert.Equal(t, "80", ranges[0].FromPort)
-	assert.Equal(t, "80", ranges[0].ToPort)
-	assert.Equal(t, "1024", ranges[1].FromPort)
-	assert.Equal(t, "65535", ranges[1].ToPort)
+	assert.Equal(t, 3, len(ranges))
+	assert.Equal(t, "*", ranges[0].FromPort)
+	assert.Equal(t, "*", ranges[0].ToPort)
+	assert.Equal(t, "80", ranges[1].FromPort)
+	assert.Equal(t, "80", ranges[1].ToPort)
+	assert.Equal(t, "1024", ranges[2].FromPort)
+	assert.Equal(t, "65535", ranges[2].ToPort)
 }
