9a4150a96f6a6beab3c68692d56aac310fcaaf89
Dominik Richter <dominik.richter@gmail.com>
2024-01-23 23:14:39 -0800

    🐛 fix crash on empty array.flat with missing type info (#3106)
    This can unfortunately still happen, where the type info is missing and
    we essentially get the array indication in the type field, but nothing
    else. The subsequent for loop then crashes it.
    
    Add testing for this use-case and fix the crash by forcing the any-type
    into it.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ llx/builtin_array_test.go
@@ -0,0 +1,23 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package llx
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnquery/v10/types"
+)
+
+func TestArrayFlat(t *testing.T) {
+	t.Run("empty array with missing type info", func(t *testing.T) {
+		res, ref, err := arrayFlat(nil, &RawData{
+			Type:  types.ArrayLike,
+			Value: []any{},
+		}, nil, 0)
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), ref)
+		require.Equal(t, ArrayData(nil, types.Any), res)
+	})
+}
