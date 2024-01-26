c360d7e2457c951517a6f72edb595ba8a08566f9
Christoph Hartmann <chris@lollyrock.com>
2024-01-26 18:33:44 +0100

    🎉 azure-10.0.2, network-9.1.10, slack-10.0.2 (#3133)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "azure",
 	ID:      "go.mondoo.com/cnquery/v9/providers/azure",
-	Version: "10.0.1",
+	Version: "10.0.2",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(azureinstancesnapshot.SnapshotConnectionType),
