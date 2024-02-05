28721f7b394131257e5db59c0f5f2fc821d44413
Dominik Richter <dominik.richter@gmail.com>
2024-02-04 20:43:59 -0800

    🟢 fix ssh params test
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- providers/os/resources/sshd/params_test.go
+++ providers/os/resources/sshd/params_test.go
@@ -23,9 +23,9 @@ func TestSSHParser(t *testing.T) {
 	assert.NotNil(t, sshParams, "params are not nil")
 
 	// check result for multiple host-keys
-	assert.Equal(t, "/etc/ssh/ssh_host_rsa_key,/etc/ssh/ssh_host_ecdsa_key,/etc/ssh/ssh_host_ed25519_key", sshParams["HostKey"])
-	assert.Equal(t, "yes", sshParams["X11Forwarding"])
-	assert.Equal(t, "60", sshParams["LoginGraceTime"])
+	assert.Equal(t, "/etc/ssh/ssh_host_rsa_key,/etc/ssh/ssh_host_ecdsa_key,/etc/ssh/ssh_host_ed25519_key", sshParams[0].Params["HostKey"])
+	assert.Equal(t, "yes", sshParams[0].Params["X11Forwarding"])
+	assert.Equal(t, "60", sshParams[0].Params["LoginGraceTime"])
 }
 
 func TestSSHParseCaseInsensitive(t *testing.T) {
@@ -39,7 +39,7 @@ func TestSSHParseCaseInsensitive(t *testing.T) {
 
 	assert.NotNil(t, sshParams, "params are not nil")
 
-	assert.Equal(t, "22", sshParams["Port"])
-	assert.Equal(t, "any", sshParams["AddressFamily"])
-	assert.Equal(t, "0.0.0.0", sshParams["ListenAddress"])
+	assert.Equal(t, "22", sshParams[0].Params["Port"])
+	assert.Equal(t, "any", sshParams[0].Params["AddressFamily"])
+	assert.Equal(t, "0.0.0.0", sshParams[0].Params["ListenAddress"])
 }
