a0419abb8774b777a11313a4a6fab8fb07cc421b
Mondoo <hello@mondoo.com>
2024-02-13 18:27:52 +0100

    🎉 os-10.1.1
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "10.1.0",
+	Version: "10.1.1",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
