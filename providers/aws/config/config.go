a486946b2b23a77cfeaf093ccc1a71f04b671d2e
Mondoo <hello@mondoo.com>
2024-02-14 11:23:58 +0200

    🎉 aws-10.1.2
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "10.1.1",
+	Version:         "10.1.2",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
