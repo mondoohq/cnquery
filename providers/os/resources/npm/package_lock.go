729f1495ad26002e9aad71602951dd912bc17280
Christoph Hartmann <chris@lollyrock.com>
2024-02-15 21:46:46 +0100

    ⭐️ support multiple versions of package lock file

--- providers/os/resources/npm/package_lock.go
+++ providers/os/resources/npm/package_lock.go
@@ -10,22 +10,64 @@ import (
 	"go.mondoo.com/cnquery/v10/providers-sdk/v1/upstream/mvd"
 )
 
-type PackageJsonLockEntry struct {
-	Version string `json:"version"`
-	Dev     bool   `json:"dev"`
+// packageLock is the struct to represent the package.lock file
+// see https://docs.npmjs.com/cli/v10/configuring-npm/package-lock-json
+type packageLock struct {
+	Name            string `json:"name"`
+	Version         string `json:"version"`
+	LockfileVersion int    `json:"lockfileVersion"`
+	Requires        bool   `json:"requires"`
+	// Packages maps package locations to an object containing the information about that package,
+	// root project is typically listed with a key of ""
+	Packages map[string]packageLockPackage `json:"packages"`
+	// Dependencies contains legacy data for supporting versions of npm that use lockfileVersion: 1 or lower.
+	// We can ignore that for lockfileVersion: 2+
+	Dependencies map[string]packageLockDependency `jsonn:"dependencies"`
 }
 
-// PackageJsonLock is the struct to represent the package.lock file
-type PackageJsonLock struct {
-	Name         string                          `json:"name"`
-	Version      string                          `json:"version"`
-	Dependencies map[string]PackageJsonLockEntry `jsonn:"dependencies"`
+type packageLockDependency struct {
+	Version   string `json:"version"`
+	Resolved  string `json:"resolved"`
+	Integrity string `json:"integrity"`
+	Dev       bool   `json:"dev"`
 }
 
+type packageLockPackage struct {
+	Name      string             `json:"name"`
+	Version   string             `json:"version"`
+	Resolved  string             `json:"resolved"`
+	Integrity string             `json:"integrity"`
+	License   packageLockLicense `json:"license"`
+}
+
+type packageLockLicense []string
+
+// UnmarshalJSON is a custom unmarshaler for the packageLockLicense type. It allows to handle the license field
+// which could be either a string or an array.
+func (l *packageLockLicense) UnmarshalJSON(data []byte) (err error) {
+
+	var slice []string
+	if err := json.Unmarshal(data, &slice); err == nil {
+		*l = slice
+		return nil
+	}
+
+	var single string
+	if err = json.Unmarshal(data, &single); err == nil {
+		*l = []string{single}
+		return nil
+	}
+
+	// if it's neither a string nor an array, ignore it
+	return nil
+}
+
+// PackageLockParser is the parser for the package.lock file npm format.
+// see https://docs.npmjs.com/cli/v10/configuring-npm/package-lock-json
 type PackageLockParser struct{}
 
 func (p *PackageLockParser) Parse(r io.Reader) ([]*mvd.Package, error) {
-	var packageJsonLock PackageJsonLock
+	var packageJsonLock packageLock
 	err := json.NewDecoder(r).Decode(&packageJsonLock)
 	if err != nil {
 		return nil, err
