266df6108fcd5ca8f5ba008414556e309059461a
Mondoo <hello@mondoo.com>
2024-02-21 11:29:21 -0700

    🎉 arista-10.0.2, atlassian-10.0.2, aws-10.2.4, azure-10.2.3, equinix-10.2.3, gcp-10.2.3, google-workspace-10.2.3, ipmi-10.2.3, k8s-10.2.3, ms365-10.2.4, oci-10.2.3, okta-10.2.3, opcua-10.2.3, slack-10.2.3, terraform-10.2.3, vcd-10.2.3, vsphere-10.2.3
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/arista/config/config.go
+++ providers/arista/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "arista",
 	ID:              "go.mondoo.com/cnquery/v9/providers/arista",
-	Version:         "10.0.1",
+	Version:         "10.0.2",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/ms365/config/config.go
+++ providers/ms365/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ms365",
 	ID:              "go.mondoo.com/cnquery/v9/providers/ms365",
-	Version:         "10.2.3",
+	Version:         "10.2.4",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
