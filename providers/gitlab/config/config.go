348c1f05fb3e8dade68a6b179eabbf1bea932404
Ivan Milchev <ivan@mondoo.com>
2024-02-22 19:13:43 +0100

    🎉 arista-10.0.2, atlassian-10.0.2, aws-10.2.5, azure-10.2.3, equinix-10.2.3, gcp-10.2.3, github-10.2.5, gitlab-10.2.6, google-workspace-10.2.3, ipmi-10.2.3, k8s-10.2.3, ms365-10.2.4, oci-10.2.3, okta-10.2.3, opcua-10.2.3, slack-10.2.3, terraform-10.2.3, vcd-10.2.3, vsphere-10.2.3 (#3411)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gitlab",
-	Version: "10.2.5",
+	Version: "10.2.6",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
