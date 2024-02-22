611197101dea43ec4ae0d70d276bf8ee0b699516
Hossein Rouhani <h_rouhani@hotmail.com>
2024-02-22 15:02:37 +0100

    converted resourceType into []interface{} slice
    Signed-off-by: Hossein Rouhani <h_rouhani@hotmail.com>

--- providers/aws/resources/aws_config.go
+++ providers/aws/resources/aws_config.go
@@ -73,13 +73,17 @@ func (a *mqlAwsConfig) getRecorders(conn *connection.AwsConnection) []*jobpool.J
 					recording = val.recording
 					lastStatus = val.lastStatus
 				}
+				resourceTypesInterface := make([]interface{}, len(r.RecordingGroup.ResourceTypes))
+				for i, resourceType := range r.RecordingGroup.ResourceTypes {
+					resourceTypesInterface[i] = resourceType
+				}
 				mqlRecorder, err := CreateResource(a.MqlRuntime, "aws.config.recorder",
 					map[string]*llx.RawData{
 						"name":                       llx.StringDataPtr(r.Name),
 						"roleArn":                    llx.StringDataPtr(r.RoleARN),
 						"allSupported":               llx.BoolData(r.RecordingGroup.AllSupported),
 						"includeGlobalResourceTypes": llx.BoolData(r.RecordingGroup.IncludeGlobalResourceTypes),
-						"resourceTypes":              llx.ArrayData(r.RecordingGroup.ResourceTypes, types.Any),
+						"resourceTypes":              llx.ArrayData(resourceTypesInterface, types.Any),
 						"recording":                  llx.BoolData(recording),
 						"region":                     llx.StringData(regionVal),
 						"lastStatus":                 llx.StringData(lastStatus),
