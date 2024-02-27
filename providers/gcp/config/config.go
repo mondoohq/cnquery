852717131fa761296ef01d2fc0c32e7b2b3a7f54
Ivan Milchev <ivan@mondoo.com>
2024-02-27 14:36:10 +0100

    🎉 arista-10.0.3, atlassian-10.0.3, aws-10.2.6, azure-10.2.4, equinix-10.2.4, gcp-10.2.4, github-10.2.6, gitlab-10.2.7, google-workspace-10.2.4, ipmi-10.2.4, k8s-10.2.4, ms365-10.2.5, oci-10.2.4, okta-10.2.4, opcua-10.2.4, os-10.2.3, slack-10.2.4, terraform-10.2.4, vcd-10.2.4, vsphere-10.2.5 (#3440)

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "10.2.3",
+	Version: "10.2.4",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
