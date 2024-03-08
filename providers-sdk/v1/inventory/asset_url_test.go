fe83bb02eb8341baa2dac7528c5bebc0a429608c
Dominik Richter <dominik.richter@gmail.com>
2024-03-08 12:57:45 -0800

    ⭐ introduce asset URLs (#3529)
    * ⭐ introduce asset URLs
    
    From the code docs:
    
    Assets are generally structured in a giant graph. However, we often find
    it difficult to reason with arbitrary graphs. As humans, we tend to
    group assets into hierarchical tree structures, that make it easy for us
    to put them into a box and reason about them.
    
    For example: A techology-centric view of the world would group a VM
    in a cloud environment like this:
      /aws/accountX/ec2/instances/linux/debian/8.0
    
    Every entry in this path structure follows a strict schema. Thus "aws" above
    is the chosen path value for the key "technology". As you can see, some
    keys lead to predefined (limited) values (technology can be aws, azure, os,
    k8s, etc), while other keys can have (almost) arbitrary values (eg account).
    
    Providers create this schema and may extend this schema. Providers cannot
    create conflicting entries in this schema.
    
    Assets can belong to multiple URLs at the same time, which allows us to
    look at it from different perspectives.
    
    URLs enable fast lookup, but do not restrict in terms of the search.
    This support looking at e.g. linux instances on all kinds of environments
    and runtimes.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    * 🟢 opinionated linter...
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    * ✨ add method to build asset url chain from path
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    ---------
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ providers-sdk/v1/inventory/asset_url_test.go
@@ -0,0 +1,96 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package inventory
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnquery/v10/utils/sortx"
+)
+
+func genTestSchema(t *testing.T) *AssetUrlSchema {
+	root, err := newAssetUrlSchema("technology")
+	require.NoError(t, err)
+
+	err = root.Add(&AssetUrlBranch{
+		PathSegments: []string{"technology=aws"},
+		Key:          "account",
+		Values: map[string]*AssetUrlBranch{
+			"*": {
+				Key: "service",
+				Values: map[string]*AssetUrlBranch{
+					"ec2": {
+						References: []string{"technology=os"},
+					},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	err = root.Add(&AssetUrlBranch{
+		PathSegments: []string{"technology=os"},
+		Key:          "family",
+		Values: map[string]*AssetUrlBranch{
+			"windows": {
+				Key: "platform",
+				Values: map[string]*AssetUrlBranch{
+					"windows server": nil,
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	err = root.Add(&AssetUrlBranch{
+		PathSegments: []string{"technology=os", "family=windows", "platform=windows server"},
+		Key:          "version",
+		Values: map[string]*AssetUrlBranch{
+			"2019": nil,
+			"2022": nil,
+		},
+	})
+	require.NoError(t, err)
+
+	return root
+}
+
+func TestAddSubtree(t *testing.T) {
+	root := genTestSchema(t)
+
+	t.Run("refresh cache and access keys", func(t *testing.T) {
+		err := root.RefreshCache()
+		require.NoError(t, err)
+
+		keys := sortx.Keys(root.keys)
+		assert.Equal(t, []string{
+			"account", "family", "platform", "service", "technology", "version",
+		}, keys)
+	})
+
+	t.Run("build a query with referenced tree positions", func(t *testing.T) {
+		err := root.RefreshCache()
+		require.NoError(t, err)
+
+		queries := root.BuildQueries([]KV{
+			{Key: "platform", Value: "windows server"},
+		})
+		assert.Equal(t, []AssetUrlChain{
+			{
+				KV{"technology", "aws"},
+				KV{"account", "*"},
+				KV{"service", "ec2"},
+				KV{"family", "windows"},
+				KV{"platform", "windows server"},
+			},
+			{
+				KV{"technology", "os"},
+				KV{"family", "windows"},
+				KV{"platform", "windows server"},
+			},
+		}, queries)
+	})
+}
