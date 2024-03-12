9fa0fc86ad1cd51aa33717c3dea66900a2985421
Ivan Milchev <ivan@mondoo.com>
2024-03-12 12:42:44 +0100

    🎉 arista-10.1.2, atlassian-10.1.2, aws-10.3.2, azure-10.3.2, equinix-10.3.2, gcp-10.3.3, github-10.3.2, gitlab-10.3.2, google-workspace-10.3.2, ipmi-10.3.2, k8s-10.3.2, ms365-10.3.2, oci-10.3.2, okta-10.3.2, opcua-10.3.2, slack-10.3.2, terraform-10.3.2, vcd-10.3.2, vsphere-10.3.2 (#3548)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "10.3.2",
+	Version: "10.3.3",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
--- providers/github/config/config.go
+++ providers/github/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "github",
 	ID:              "go.mondoo.com/cnquery/v9/providers/github",
-	Version:         "10.3.1",
+	Version:         "10.3.2",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
