4910da22f64a82825181cae6d5486ef7f372dedc
Mondoo Tools <tools@mondoo.com>
2024-03-15 08:54:27 +0000

    ✨ Update providers 20240315

--- providers/oci/config/config.go
+++ providers/oci/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "oci",
 	ID:              "go.mondoo.com/cnquery/v9/providers/oci",
-	Version:         "10.3.2",
+	Version:         "10.3.3",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/vsphere/config/config.go
+++ providers/vsphere/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vsphere",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vsphere",
-	Version:         "10.3.2",
+	Version:         "10.3.3",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
