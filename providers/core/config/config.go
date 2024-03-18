1d2a7a33a872ee893d00f3bcca1afb4276a9d323
Dominik Richter <dominik.richter@gmail.com>
2024-03-18 14:28:27 -0700

    ✨ add asset url trees for "other" platforms to core (#3595)
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- providers/core/config/config.go
+++ providers/core/config/config.go
@@ -3,11 +3,24 @@
 
 package config
 
-import "go.mondoo.com/cnquery/v10/providers-sdk/v1/plugin"
+import (
+	"go.mondoo.com/cnquery/v10/providers-sdk/v1/inventory"
+	"go.mondoo.com/cnquery/v10/providers-sdk/v1/plugin"
+)
 
 var Config = plugin.Provider{
 	Name:       "core",
 	ID:         "go.mondoo.com/cnquery/v9/providers/core",
 	Version:    "10.3.1",
 	Connectors: []plugin.Connector{},
+	AssetUrlTrees: []*inventory.AssetUrlBranch{
+		{
+			PathSegments: []string{"technology=other"},
+			Key:          "platform",
+			Title:        "Other Platform",
+			Values: map[string]*inventory.AssetUrlBranch{
+				"*": nil,
+			},
+		},
+	},
 }
