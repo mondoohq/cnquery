ca32278530afbdf79354f32bfc1736c9934ae0d3
Victoria Jeffrey <vj@mondoo.com>
2024-03-17 21:53:25 -0600

    🐛 ensure ec2 instances get a name when scanned

--- providers/os/id/awsec2/metadata_cmd_test.go
+++ providers/os/id/awsec2/metadata_cmd_test.go
@@ -38,7 +38,7 @@ func TestEC2RoleProviderInstanceIdentityUnixNoName(t *testing.T) {
 	ident, err := metadata.Identify()
 
 	assert.Nil(t, err)
-	assert.Equal(t, "", ident.InstanceName)
+	assert.Equal(t, "i-1234567890abcdef0", ident.InstanceName)
 	assert.Equal(t, "//platformid.api.mondoo.app/runtime/aws/ec2/v1/accounts/123456789012/regions/us-west-2/instances/i-1234567890abcdef0", ident.InstanceID)
 	assert.Equal(t, "//platformid.api.mondoo.app/runtime/aws/accounts/123456789012", ident.AccountID)
 }
@@ -68,7 +68,7 @@ func TestEC2RoleProviderInstanceIdentityWindowsNoName(t *testing.T) {
 	ident, err := metadata.Identify()
 
 	assert.Nil(t, err)
-	assert.Equal(t, "", ident.InstanceName)
+	assert.Equal(t, "i-1234567890abcdef0", ident.InstanceName)
 	assert.Equal(t, "//platformid.api.mondoo.app/runtime/aws/ec2/v1/accounts/123456789012/regions/us-east-1/instances/i-1234567890abcdef0", ident.InstanceID)
 	assert.Equal(t, "//platformid.api.mondoo.app/runtime/aws/accounts/123456789012", ident.AccountID)
 }
