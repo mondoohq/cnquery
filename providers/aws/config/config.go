1ebc09afe75754c0b4b1240a7d659222d4fc799a
Christoph Hartmann <chris@lollyrock.com>
2024-04-09 00:38:53 +0200

    🎉 aws-10.5.1 (#3728)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "10.5.0",
+	Version:         "10.5.1",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
