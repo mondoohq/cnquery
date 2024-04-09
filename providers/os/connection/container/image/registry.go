37fb5db0407ddb16a1cb627e8e46295c4d839508
Preslav <preslav@mondoo.com>
2024-04-09 23:06:17 +0300

    Add defaults for GetImageDescriptor and LoadImage.

--- providers/os/connection/container/image/registry.go
+++ providers/os/connection/container/image/registry.go
@@ -7,13 +7,20 @@ import (
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"go.mondoo.com/cnquery/v10/providers/os/connection/container/auth"
 )
 
 func GetImageDescriptor(ref name.Reference, opts ...remote.Option) (*remote.Descriptor, error) {
+	if len(opts) == 0 {
+		opts = auth.DefaultOpts(ref.Name(), false)
+	}
 	return remote.Get(ref, opts...)
 }
 
 func LoadImageFromRegistry(ref name.Reference, opts ...remote.Option) (v1.Image, error) {
+	if len(opts) == 0 {
+		opts = auth.DefaultOpts(ref.Name(), false)
+	}
 	img, err := remote.Image(ref, opts...)
 	if err != nil {
 		return nil, err
