570943cb8339c938329d23ea2629a07ba489c015
Mondoo <hello@mondoo.com>
2024-04-16 10:02:59 +0200

    🎉 k8s-10.5.2
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/k8s/config/config.go
+++ providers/k8s/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "k8s",
 	ID:              "go.mondoo.com/cnquery/v9/providers/k8s",
-	Version:         "10.5.1",
+	Version:         "10.5.2",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
