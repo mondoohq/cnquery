875343c68aa2f9a2e3303d1772f236fdd43beab0
Mondoo Tools <tools@mondoo.com>
2024-04-17 09:33:29 +0000

    ✨ Update providers 20240417

--- providers/ipmi/config/config.go
+++ providers/ipmi/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ipmi",
 	ID:              "go.mondoo.com/cnquery/v9/providers/ipmi",
-	Version:         "10.5.0",
+	Version:         "10.5.1",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
