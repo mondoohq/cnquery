bf6b44e6384fcbb473b4bf766d5e99e3287236ff
vjeffrey <vj@mondoo.com>
2024-04-29 11:19:17 -0600

    🎉 arista-11.0.3, atlassian-11.0.4, aws-11.0.4, azure-11.0.4, core-11.0.2, equinix-11.0.3, gcp-11.0.6, github-11.0.3, gitlab-11.0.3, google-workspace-11.0.3, ipmi-11.0.3, k8s-11.0.3, ms365-11.0.3, oci-11.0.3, okta-11.0.3, opcua-11.0.3, os-11.0.3, slack-11.0.5, terraform-11.0.3, vcd-11.0.3, vsphere-11.0.3 (#3878)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "11.0.5",
+	Version: "11.0.6",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
