7333f39761f1bc0f3b73e3d9ca239d56ac41a2f4
Ivan Milchev <ivan@mondoo.com>
2024-05-02 20:10:02 +0300

    🎉 github-11.1.2 (#3914)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/github/config/config.go
+++ providers/github/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "github",
 	ID:              "go.mondoo.com/cnquery/v9/providers/github",
-	Version:         "11.1.1",
+	Version:         "11.1.2",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
