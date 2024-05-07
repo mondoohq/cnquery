a5ec494b498e123c21923b6c43c03c9125af7c89
Salim Afiune Maya <afiune@mondoo.com>
2024-05-07 15:06:43 -0700

    :test_tube: add test cases for dockerfile resource
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- providers/os/resources/docker_file_test.go
+++ providers/os/resources/docker_file_test.go
@@ -12,7 +12,56 @@ import (
 )
 
 func TestParseDockerfile(t *testing.T) {
-	dockerfile := `
+	cases := []struct {
+		purpose           string
+		subjectDockerFile string
+
+		expectedLabels          map[string]interface{}
+		expectedEnv             map[string]interface{}
+		expectedFromImage       string
+		expectedFromTag         string
+		expectedUser            plugin.TValue[*mqlDockerFileUser]
+		expectedCmd             plugin.TValue[*mqlDockerFileRun]
+		expectedEntrypoint      plugin.TValue[*mqlDockerFileRun]
+		expectedRunStruct       []plugin.TValue[*mqlDockerFileRun]
+		expectedCopyStruct      []plugin.TValue[*mqlDockerFileCopy]
+		expectedAddStruct       []plugin.TValue[*mqlDockerFileAdd]
+		expectedExposeStructArr []plugin.TValue[*mqlDockerFileExpose]
+	}{
+		{
+			purpose: "minimal instructions with CMD",
+			subjectDockerFile: `
+FROM alpine
+CMD ["/bin/sh", "-c", "echo 'Hola'"]
+`,
+			expectedLabels:    map[string]interface{}{},
+			expectedEnv:       map[string]interface{}{},
+			expectedFromImage: "alpine",
+			expectedCmd: plugin.TValue[*mqlDockerFileRun]{
+				Data: &mqlDockerFileRun{
+					Script: plugin.TValue[string]{Data: "/bin/sh\n-c\necho 'Hola'"},
+				},
+			},
+		},
+		{
+			purpose: "without CMD but with ENTRYPOINT",
+			subjectDockerFile: `
+FROM debian:stable
+ENTRYPOINT ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
+`,
+			expectedLabels:    map[string]interface{}{},
+			expectedEnv:       map[string]interface{}{},
+			expectedFromImage: "debian",
+			expectedFromTag:   "stable",
+			expectedEntrypoint: plugin.TValue[*mqlDockerFileRun]{
+				Data: &mqlDockerFileRun{
+					Script: plugin.TValue[string]{Data: "/usr/sbin/apache2ctl\n-D\nFOREGROUND"},
+				},
+			},
+		},
+		{
+			purpose: "with all instructions",
+			subjectDockerFile: `
 FROM alpine:3.14
 ENV foo=bar
 LABEL a=b
@@ -25,61 +74,135 @@ EXPOSE 80/udp
 EXPOSE 8080
 COPY /foo /bar
 ADD /foo-add /bar-add
-`
-
-	r := &plugin.Runtime{Resources: &syncx.Map[plugin.Resource]{}}
-
-	file := &mqlFile{
-		Content:    plugin.TValue[string]{Data: dockerfile, State: plugin.StateIsSet},
-		Path:       plugin.TValue[string]{Data: "Dockerfile", State: plugin.StateIsSet},
-		MqlRuntime: r,
+`,
+			expectedLabels: map[string]interface{}{
+				"a": "b",
+				"c": "d",
+			},
+			expectedEnv: map[string]interface{}{
+				"foo": "bar",
+			},
+			expectedFromImage: "alpine",
+			expectedFromTag:   "3.14",
+			expectedUser: plugin.TValue[*mqlDockerFileUser]{
+				Data: &mqlDockerFileUser{
+					User:  plugin.TValue[string]{Data: "1001"},
+					Group: plugin.TValue[string]{Data: "1001"},
+				},
+			},
+			expectedEntrypoint: plugin.TValue[*mqlDockerFileRun]{
+				Data: &mqlDockerFileRun{
+					Script: plugin.TValue[string]{Data: "sh"},
+				},
+			},
+			expectedCmd: plugin.TValue[*mqlDockerFileRun]{
+				Data: &mqlDockerFileRun{
+					Script: plugin.TValue[string]{Data: "curl\nhttp://example.com"},
+				},
+			},
+			expectedCopyStruct: []plugin.TValue[*mqlDockerFileCopy]{
+				{Data: &mqlDockerFileCopy{
+					Src: plugin.TValue[[]interface{}]{
+						Data: []interface{}{"/foo"}},
+					Dst: plugin.TValue[string]{
+						Data: "/bar"},
+				}},
+			},
+			expectedRunStruct: []plugin.TValue[*mqlDockerFileRun]{
+				{Data: &mqlDockerFileRun{
+					Script: plugin.TValue[string]{
+						Data: "apk add --no-cache curl"},
+				}},
+			},
+			expectedAddStruct: []plugin.TValue[*mqlDockerFileAdd]{
+				{Data: &mqlDockerFileAdd{
+					Src: plugin.TValue[[]interface{}]{
+						Data: []interface{}{"/foo-add"}},
+					Dst: plugin.TValue[string]{
+						Data: "/bar-add"},
+				}},
+			},
+			expectedExposeStructArr: []plugin.TValue[*mqlDockerFileExpose]{
+				{Data: &mqlDockerFileExpose{
+					Port:     plugin.TValue[int64]{Data: int64(80)},
+					Protocol: plugin.TValue[string]{Data: "udp"},
+				}},
+				{Data: &mqlDockerFileExpose{
+					Port:     plugin.TValue[int64]{Data: int64(8080)},
+					Protocol: plugin.TValue[string]{Data: "tcp"}, // this is the default
+				}},
+			},
+		},
 	}
-	dockerFile := mqlDockerFile{
-		File:       plugin.TValue[*mqlFile]{Data: file, State: plugin.StateIsSet},
-		MqlRuntime: r,
+
+	for _, kase := range cases {
+		t.Run(kase.purpose, func(t *testing.T) {
+			r := &plugin.Runtime{Resources: &syncx.Map[plugin.Resource]{}}
+
+			file := &mqlFile{
+				Content:    plugin.TValue[string]{Data: kase.subjectDockerFile, State: plugin.StateIsSet},
+				Path:       plugin.TValue[string]{Data: "Dockerfile", State: plugin.StateIsSet},
+				MqlRuntime: r,
+			}
+			dockerFile := mqlDockerFile{
+				File:       plugin.TValue[*mqlFile]{Data: file, State: plugin.StateIsSet},
+				MqlRuntime: r,
+			}
+
+			require.NoError(t, dockerFile.parse(file))
+
+			actualMqlDockerFileStage := dockerFile.Stages.Data[0].(*mqlDockerFileStage)
+
+			require.Equal(t, kase.expectedLabels, actualMqlDockerFileStage.Labels.Data)
+			require.Equal(t, kase.expectedEnv, actualMqlDockerFileStage.Env.Data)
+			require.Equal(t, kase.expectedFromImage, actualMqlDockerFileStage.From.Data.Image.Data)
+			require.Equal(t, kase.expectedFromTag, actualMqlDockerFileStage.From.Data.Tag.Data)
+
+			if kase.expectedCmd.Data == nil {
+				require.Nil(t, actualMqlDockerFileStage.Cmd.Data)
+			} else {
+				require.Equal(t, kase.expectedCmd.Data.Script.Data, actualMqlDockerFileStage.Cmd.Data.Script.Data)
+			}
+
+			if kase.expectedUser.Data == nil {
+				require.Nil(t, actualMqlDockerFileStage.User.Data)
+			} else {
+				require.Equal(t, kase.expectedUser.Data.User.Data, actualMqlDockerFileStage.User.Data.User.Data)
+				require.Equal(t, kase.expectedUser.Data.Group.Data, actualMqlDockerFileStage.User.Data.Group.Data)
+			}
+
+			if kase.expectedEntrypoint.Data == nil {
+				require.Nil(t, actualMqlDockerFileStage.Entrypoint.Data)
+			} else {
+				require.Equal(t, kase.expectedEntrypoint.Data.Script.Data, actualMqlDockerFileStage.Entrypoint.Data.Script.Data)
+			}
+
+			require.Equal(t, len(kase.expectedCopyStruct), len(actualMqlDockerFileStage.Copy.Data))
+			for i, cpy := range actualMqlDockerFileStage.Copy.Data {
+				actualCopy := cpy.(*mqlDockerFileCopy)
+				require.Equal(t, kase.expectedCopyStruct[i].Data.Src.Data, actualCopy.Src.Data)
+				require.Equal(t, kase.expectedCopyStruct[i].Data.Dst.Data, actualCopy.Dst.Data)
+			}
+
+			require.Equal(t, len(kase.expectedRunStruct), len(actualMqlDockerFileStage.Run.Data))
+			for i, run := range actualMqlDockerFileStage.Run.Data {
+				actualRun := run.(*mqlDockerFileRun)
+				require.Equal(t, kase.expectedRunStruct[i].Data.Script.Data, actualRun.Script.Data)
+			}
+
+			require.Equal(t, len(kase.expectedAddStruct), len(actualMqlDockerFileStage.Add.Data))
+			for i, cpy := range actualMqlDockerFileStage.Add.Data {
+				actualAdd := cpy.(*mqlDockerFileAdd)
+				require.Equal(t, kase.expectedAddStruct[i].Data.Src.Data, actualAdd.Src.Data)
+				require.Equal(t, kase.expectedAddStruct[i].Data.Dst.Data, actualAdd.Dst.Data)
+			}
+
+			require.Equal(t, len(kase.expectedExposeStructArr), len(actualMqlDockerFileStage.Expose.Data))
+			for i, expose := range actualMqlDockerFileStage.Expose.Data {
+				actualExpose := expose.(*mqlDockerFileExpose)
+				require.Equal(t, kase.expectedExposeStructArr[i].Data.Port.Data, actualExpose.Port.Data)
+				require.Equal(t, kase.expectedExposeStructArr[i].Data.Protocol.Data, actualExpose.Protocol.Data)
+			}
+		})
 	}
-	err := dockerFile.parse(file)
-
-	require.NoError(t, err)
-
-	s := dockerFile.Stages.Data[0].(*mqlDockerFileStage)
-	expectedLabels := map[string]interface{}{
-		"a": "b",
-		"c": "d",
-	}
-	expectedEnv := map[string]interface{}{
-		"foo": "bar",
-	}
-	require.Equal(t, "alpine", s.From.Data.Image.Data)
-	require.Equal(t, "3.14", s.From.Data.Tag.Data)
-	require.Equal(t, expectedLabels, s.Labels.Data)
-	require.Equal(t, expectedEnv, s.Env.Data)
-
-	copy := s.Copy.Data[0].(*mqlDockerFileCopy)
-	require.Equal(t, []interface{}{"/foo"}, copy.Src.Data)
-	require.Equal(t, "/bar", copy.Dst.Data)
-
-	run := s.Run.Data[0].(*mqlDockerFileRun)
-	require.Equal(t, "apk add --no-cache curl", run.Script.Data)
-
-	require.Equal(t, "curl\nhttp://example.com", s.Cmd.Data.Script.Data)
-	require.Equal(t, "sh", s.Entrypoint.Data.Script.Data)
-
-	require.Equal(t, "1001", s.User.Data.User.Data)
-	require.Equal(t, "1001", s.User.Data.Group.Data)
-
-	exposes := []*mqlDockerFileExpose{
-		s.Expose.Data[0].(*mqlDockerFileExpose),
-		s.Expose.Data[1].(*mqlDockerFileExpose),
-	}
-
-	require.Equal(t, int64(80), exposes[0].Port.Data)
-	require.Equal(t, "udp", exposes[0].Protocol.Data)
-	require.Equal(t, int64(8080), exposes[1].Port.Data)
-	// verify default protocol if not specified
-	require.Equal(t, "tcp", exposes[1].Protocol.Data)
-
-	add := s.Add.Data[0].(*mqlDockerFileAdd)
-	require.Equal(t, []interface{}{"/foo-add"}, add.Src.Data)
-	require.Equal(t, "/bar-add", add.Dst.Data)
 }
