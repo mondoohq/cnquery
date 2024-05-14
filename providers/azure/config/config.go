3acd620195e48e68af81bb6625532ab3564b00c1
Mondoo Tools <tools@mondoo.com>
2024-05-14 20:29:11 +0000

    ✨ Update providers 20240514

--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "azure",
 	ID:      "go.mondoo.com/cnquery/v9/providers/azure",
-	Version: "11.0.8",
+	Version: "11.0.9",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(azureinstancesnapshot.SnapshotConnectionType),
--- providers/google-workspace/config/config.go
+++ providers/google-workspace/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "google-workspace",
 	ID:              "go.mondoo.com/cnquery/v9/providers/google-workspace",
-	Version:         "11.0.7",
+	Version:         "11.0.8",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
