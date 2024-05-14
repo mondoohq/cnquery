mondoohq/cnquery

commit 901df6c5f85558a6e55cb9fe377d4f7da128a2d8

🧹 Reduce the number of times we try to get platform ident (#3660)
* 🧹 Reduce the number of times we try to get platform ident

 We were getting platform identity information multiple times. We would
 connect, then see the asset's platform not set, and redo it.

* fix test

Signed-off-by: Ivan Milchev <ivan@mondoo.com>

---------

Signed-off-by: Ivan Milchev <ivan@mondoo.com>
Co-authored-by: Ivan Milchev <ivan@mondoo.com>

providers-sdk/v1/sysinfo/sysinfo.go
@@ -4,15 +4,12 @@
 package sysinfo
 
 import (
-	"errors"
-
 	"github.com/rs/zerolog/log"
 
 	"go.mondoo.com/cnquery/v11"
 	"go.mondoo.com/cnquery/v11/cli/execruntime"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/inventory"
 	"go.mondoo.com/cnquery/v11/providers/os/connection/local"
-	"go.mondoo.com/cnquery/v11/providers/os/detector"
 	"go.mondoo.com/cnquery/v11/providers/os/id"
 	"go.mondoo.com/cnquery/v11/providers/os/id/hostname"
 	"go.mondoo.com/cnquery/v11/providers/os/resources/networkinterface"
@@ -47,18 +44,14 @@ func Get() (*SystemInfo, error) {
 		Type: "local",
 	}, &asset)
 
-	fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+	fingerprint, platform, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 	if err == nil {
 		if len(fingerprint.PlatformIDs) > 0 {
 			sysInfo.PlatformId = fingerprint.PlatformIDs[0]
 		}
 	}
 
-	var ok bool
-	sysInfo.Platform, ok = detector.DetectOS(conn)
-	if !ok {
-		return nil, errors.New("failed to detect the OS")
-	}
+	sysInfo.Platform = platform
 
 	sysInfo.Hostname, _ = hostname.Hostname(conn, sysInfo.Platform)
 

providers/os/id/platform.go
@@ -35,12 +35,12 @@ type PlatformInfo struct {
 	RelatedPlatformIDs []string
 }
 
-func IdentifyPlatform(conn shared.Connection, p *inventory.Platform, idDetectors []string) (*PlatformFingerprint, error) {
+func IdentifyPlatform(conn shared.Connection, p *inventory.Platform, idDetectors []string) (*PlatformFingerprint, *inventory.Platform, error) {
 	var ok bool
 	if p == nil {
 		p, ok = detector.DetectOS(conn)
 		if !ok {
-			return nil, errors.New("cannot detect os")
+			return nil, nil, errors.New("cannot detect os")
 		}
 	}
 
@@ -99,7 +99,7 @@ func IdentifyPlatform(conn shared.Connection, p *inventory.Platform, idDetectors
 
 	// if we found zero platform ids something went wrong
 	if len(platformIds) == 0 {
-		return nil, errors.New("could not determine a platform identifier")
+		return nil, nil, errors.New("could not determine a platform identifier")
 	}
 
 	fingerprint.PlatformIDs = platformIds
@@ -111,7 +111,7 @@ func IdentifyPlatform(conn shared.Connection, p *inventory.Platform, idDetectors
 	}
 
 	log.Debug().Interface("id-detector", idDetectors).Strs("platform-ids", platformIds).Msg("detected platform ids")
-	return &fingerprint, nil
+	return &fingerprint, p, nil
 }
 
 func GatherNameForPlatformId(id string) string {

providers/os/provider/detector.go
@@ -11,6 +11,7 @@ import (
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/inventory"
 	"go.mondoo.com/cnquery/v11/providers/os/connection/shared"
 	"go.mondoo.com/cnquery/v11/providers/os/detector"
+	"go.mondoo.com/cnquery/v11/providers/os/id"
 	"go.mondoo.com/cnquery/v11/providers/os/id/aws"
 	"go.mondoo.com/cnquery/v11/providers/os/id/azure"
 	"go.mondoo.com/cnquery/v11/providers/os/id/gcp"
@@ -130,4 +131,26 @@ func relatedIds2assets(ids []string) []*inventory.Asset {
 		res[i] = &inventory.Asset{Id: ids[i]}
 	}
 	return res
+}
+
+func appendRelatedAssetsFromFingerprint(f *id.PlatformFingerprint, a *inventory.Asset) {
+	if f == nil || len(f.RelatedAssets) == 0 {
+		return
+	}
+	included := make(map[string]struct{}, len(a.RelatedAssets))
+	for i := range a.RelatedAssets {
+		included[a.RelatedAssets[i].Id] = struct{}{}
+	}
+	for _, ra := range f.RelatedAssets {
+		shouldAdd := true
+		for _, pId := range ra.PlatformIDs {
+			if _, ok := included[pId]; ok {
+				shouldAdd = false
+				break
+			}
+		}
+		if shouldAdd {
+			a.RelatedAssets = append(a.RelatedAssets, &inventory.Asset{Id: ra.PlatformIDs[0]})
+		}
+	}
 }

providers/os/provider/provider.go
@@ -312,11 +312,13 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 		case shared.Type_Local.String():
 			conn = local.NewConnection(connId, conf, asset)
 
-			fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+			fingerprint, p, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 			if err == nil {
 				asset.Name = fingerprint.Name
 				asset.PlatformIds = fingerprint.PlatformIDs
 				asset.IdDetector = fingerprint.ActiveIdDetectors
+				asset.Platform = p
+				appendRelatedAssetsFromFingerprint(fingerprint, asset)
 			}
 
 		case shared.Type_SSH.String():
@@ -325,13 +327,15 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 				return nil, err
 			}
 
-			fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+			fingerprint, p, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 			if err == nil {
 				if conn.Asset().Connections[0].Runtime != "vagrant" {
 					asset.Name = fingerprint.Name
 				}
 				asset.PlatformIds = fingerprint.PlatformIDs
 				asset.IdDetector = fingerprint.ActiveIdDetectors
+				asset.Platform = p
+				appendRelatedAssetsFromFingerprint(fingerprint, asset)
 			}
 
 		case shared.Type_Winrm.String():
@@ -340,11 +344,13 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 				return nil, err
 			}
 
-			fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+			fingerprint, p, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 			if err == nil {
 				asset.Name = fingerprint.Name
 				asset.PlatformIds = fingerprint.PlatformIDs
 				asset.IdDetector = fingerprint.ActiveIdDetectors
+				asset.Platform = p
+				appendRelatedAssetsFromFingerprint(fingerprint, asset)
 			}
 
 		case shared.Type_Tar.String():
@@ -353,11 +359,13 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 				return nil, err
 			}
 
-			fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+			fingerprint, p, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 			if err == nil {
 				asset.Name = fingerprint.Name
 				asset.PlatformIds = fingerprint.PlatformIDs
 				asset.IdDetector = fingerprint.ActiveIdDetectors
+				asset.Platform = p
+				appendRelatedAssetsFromFingerprint(fingerprint, asset)
 			}
 
 		case shared.Type_DockerSnapshot.String():
@@ -366,11 +374,13 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 				return nil, err
 			}
 
-			fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+			fingerprint, p, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 			if err == nil {
 				asset.Name = fingerprint.Name
 				asset.PlatformIds = fingerprint.PlatformIDs
 				asset.IdDetector = fingerprint.ActiveIdDetectors
+				asset.Platform = p
+				appendRelatedAssetsFromFingerprint(fingerprint, asset)
 			}
 
 		case shared.Type_Vagrant.String():
@@ -408,11 +418,12 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 			// This is a workaround to set Google COS platform IDs when scanned from inside k8s
 			pID, err := conn.(*fs.FileSystemConnection).Identifier()
 			if err != nil {
-				fingerprint, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+				fingerprint, p, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
 				if err == nil {
 					asset.Name = fingerprint.Name
 					asset.PlatformIds = fingerprint.PlatformIDs
 					asset.IdDetector = fingerprint.ActiveIdDetectors
+					asset.Platform = p
 				}
 			} else {
 				// In this case asset.Name should already be set via the inventory

providers/os/provider/provider_test.go
@@ -39,10 +39,8 @@ func TestLocalConnectionIdDetectors(t *testing.T) {
 	require.Contains(t, connectResp.Asset.IdDetector, ids.IdDetector_Hostname)
 	require.Contains(t, connectResp.Asset.IdDetector, ids.IdDetector_CloudDetect)
 	require.NotContains(t, connectResp.Asset.IdDetector, ids.IdDetector_SshHostkey)
-	// here we have the hostname twice, as platformid and stand alone
-	// This get's cleaned up later in the code
-	// FIXME: this should only be 1
-	require.Len(t, connectResp.Asset.PlatformIds, 2)
+
+	require.Len(t, connectResp.Asset.PlatformIds, 1)
 
 	shutdownconnectResp, err := srv.Shutdown(&plugin.ShutdownReq{})
 	require.NoError(t, err)
@@ -78,7 +76,8 @@ func TestLocalConnectionIdDetectors_DelayedDiscovery(t *testing.T) {
 		Asset: &inventory.Asset{
 			Connections: []*inventory.Config{
 				{
-					Type:           "local",
+					Type:           "docker-image",
+					Host:           "alpine:3.19",
 					DelayDiscovery: true,
 				},
 			},
@@ -87,11 +86,8 @@ func TestLocalConnectionIdDetectors_DelayedDiscovery(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, connectResp)
 
-	require.Len(t, connectResp.Asset.IdDetector, 2)
-	require.Contains(t, connectResp.Asset.IdDetector, ids.IdDetector_Hostname)
-	require.Contains(t, connectResp.Asset.IdDetector, ids.IdDetector_CloudDetect)
-	require.NotContains(t, connectResp.Asset.IdDetector, ids.IdDetector_SshHostkey)
-	require.Len(t, connectResp.Asset.PlatformIds, 1)
+	require.Len(t, connectResp.Asset.IdDetector, 0)
+	require.Len(t, connectResp.Asset.PlatformIds, 2)
 	require.Nil(t, connectResp.Asset.Platform)
 
 	// Disable delayed discovery and reconnect
@@ -102,11 +98,6 @@ func TestLocalConnectionIdDetectors_DelayedDiscovery(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, connectResp)
 
-	require.Len(t, connectResp.Asset.IdDetector, 2)
-	require.Contains(t, connectResp.Asset.IdDetector, ids.IdDetector_Hostname)
-	require.Contains(t, connectResp.Asset.IdDetector, ids.IdDetector_CloudDetect)
-	require.NotContains(t, connectResp.Asset.IdDetector, ids.IdDetector_SshHostkey)
-	// Now the platformIDs are cleaned up
 	require.Len(t, connectResp.Asset.PlatformIds, 2)
 	// Verify the platform is set
 	require.NotNil(t, connectResp.Asset.Platform)

commit 60d360c5240c0b2e0abc07ea9e21d5e93b978aaf

🐛 Return error if no platform information
`SystemInfo.Get` used to return an error if it couldn't get the
platform information.
This regressed in https://github.com/mondoohq/cnquery/pull/3660 where
nil platform info stopped returning an error

providers-sdk/v1/sysinfo/sysinfo.go
@@ -4,6 +4,8 @@
 package sysinfo
 
 import (
+	"errors"
+
 	"github.com/rs/zerolog/log"
 
 	"go.mondoo.com/cnquery/v11"
@@ -44,11 +46,15 @@ func Get() (*SystemInfo, error) {
 		Type: "local",
 	}, &asset)
 
-	fingerprint, platform, err := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
-	if err == nil {
+	fingerprint, platform, _ := id.IdentifyPlatform(conn, asset.Platform, asset.IdDetector)
+	if fingerprint != nil {
 		if len(fingerprint.PlatformIDs) > 0 {
 			sysInfo.PlatformId = fingerprint.PlatformIDs[0]
 		}
+	}
+
+	if platform == nil {
+		return nil, errors.New("failed to detect the OS")
 	}
 
 	sysInfo.Platform = platform

providers/os/id/platform.go
@@ -99,7 +99,7 @@ func IdentifyPlatform(conn shared.Connection, p *inventory.Platform, idDetectors
 
 	// if we found zero platform ids something went wrong
 	if len(platformIds) == 0 {
-		return nil, nil, errors.New("could not determine a platform identifier")
+		return nil, p, errors.New("could not determine a platform identifier")
 	}
 
 	fingerprint.PlatformIDs = platformIds