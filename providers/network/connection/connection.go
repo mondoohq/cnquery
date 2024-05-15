74f560c200a003ab708970a556e9c415011ac8a7
Ivan Milchev <ivan@mondoo.com>
2024-05-15 11:58:25 +0300

    🐛 properly handle insecure flag for host scans (#3996)
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- providers/network/connection/connection.go
+++ providers/network/connection/connection.go
@@ -4,21 +4,47 @@
 package connection
 
 import (
+	"crypto/tls"
+	"net"
+	"net/http"
+	"time"
+
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/inventory"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
 )
 
 type HostConnection struct {
 	plugin.Connection
-	Conf  *inventory.Config
-	asset *inventory.Asset
+	Conf       *inventory.Config
+	asset      *inventory.Asset
+	httpClient *http.Client
 }
 
 func NewHostConnection(id uint32, asset *inventory.Asset, conf *inventory.Config) *HostConnection {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	if conf.Insecure {
+		transport.TLSClientConfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	}
+
 	return &HostConnection{
 		Connection: plugin.NewConnection(id, asset),
 		Conf:       conf,
 		asset:      asset,
+		httpClient: &http.Client{Transport: transport},
 	}
 }
 
@@ -35,4 +61,8 @@ func (p *HostConnection) FQDN() string {
 		return ""
 	}
 	return p.Conf.Host
+}
+
+func (p *HostConnection) Client() *http.Client {
+	return p.httpClient
 }
