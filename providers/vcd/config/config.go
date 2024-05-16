77b39c05c6b66fc2f85498fde31ef56e07a423e2
Mondoo Tools <tools@mondoo.com>
2024-05-16 11:15:44 +0000

    ✨ Update providers 20240516

--- providers/vcd/config/config.go
+++ providers/vcd/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vcd",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vcd",
-	Version:         "11.0.7",
+	Version:         "11.0.8",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
