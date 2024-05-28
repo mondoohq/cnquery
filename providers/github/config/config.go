d0a6fb7fe265b2cfee4c5c298b67479932d7c604
Mondoo Tools <tools@mondoo.com>
2024-05-28 12:44:23 +0000

    ✨ Update providers 20240528

--- providers/github/config/config.go
+++ providers/github/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "github",
 	ID:              "go.mondoo.com/cnquery/v9/providers/github",
-	Version:         "11.2.1",
+	Version:         "11.2.2",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
