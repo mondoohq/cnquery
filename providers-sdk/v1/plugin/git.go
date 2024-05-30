5a886cd19e3a33fcd2d4e0e7300a4211ce3cf92d
Ivan Milchev <ivan@mondoo.com>
2024-05-30 10:54:41 +0200

    adjust git temp dir name
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- providers-sdk/v1/plugin/git.go
+++ providers-sdk/v1/plugin/git.go
@@ -66,7 +66,7 @@ func NewGitClone(asset *inventory.Asset) (string, func(), error) {
 }
 
 func gitClone(gitUrl string) (string, func(), error) {
-	cloneDir, err := os.MkdirTemp(os.TempDir(), "gitClone")
+	cloneDir, err := os.MkdirTemp(os.TempDir(), "cnquery-git-clone")
 	if err != nil {
 		return "", nil, errors.Wrap(err, "failed to create temporary dir for git processing")
 	}
