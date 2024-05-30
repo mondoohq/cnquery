655f3b1b73a1d279e17dd8954a4c00b4f2e4f23d
Mondoo <hello@mondoo.com>
2024-05-30 16:41:27 +0200

    🎉 gitlab-11.1.0, github-11.3.0
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gitlab",
-	Version: "11.0.11",
+	Version: "11.1.0",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
