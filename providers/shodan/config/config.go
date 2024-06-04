71337af1a155433186e26578295a5f12df593b09
Mondoo Tools <tools@mondoo.com>
2024-06-04 18:38:27 +0000

    ✨ Update providers 20240604

--- providers/shodan/config/config.go
+++ providers/shodan/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "shodan",
 	ID:              "go.mondoo.com/cnquery/v11/providers/shodan",
-	Version:         "11.0.1",
+	Version:         "11.0.2",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
