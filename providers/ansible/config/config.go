9ff0f401c888a6e0d9aecc3d86fa669d9aa94633
Christian Zunker <christian@mondoo.com>
2024-06-04 14:25:05 +0200

    🧹 Align ansible provider version with other providers
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- providers/ansible/config/config.go
+++ providers/ansible/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ansible",
 	ID:              "go.mondoo.com/cnquery/v11/providers/ansible",
-	Version:         "10.0.1",
+	Version:         "11.0.0",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
