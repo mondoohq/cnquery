61ad0a34ff77e8c6736817f71ec1bb8fd7310801
Mondoo Tools <tools@mondoo.com>
2024-06-05 04:32:19 +0000

    ✨ Update providers 20240605

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "11.2.7",
+	Version: "11.2.8",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
