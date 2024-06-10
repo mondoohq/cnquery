1e1e3f2b1f584e7ab9b9b7355911263917ed40af
Ivan Milchev <ivan@mondoo.com>
2024-06-10 14:19:29 +0300

    disable debug messages from snowflake provider
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- providers/snowflake/main.go
+++ providers/snowflake/main.go
@@ -4,8 +4,11 @@
 package main
 
 import (
+	"io"
+	"log"
 	"os"
 
+	"github.com/snowflakedb/gosnowflake"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
 	"go.mondoo.com/cnquery/v11/providers/snowflake/provider"
 )
@@ -15,5 +18,8 @@ func main() {
 	// This is required for the snowflake api that is used by the provider
 	os.Setenv("SF_TF_NO_INSTRUMENTED_SQL", "1")
 	os.Setenv("SF_TF_GOSNOWFLAKE_LOG_LEVEL", "error")
+	// The following line changes the log level to debug
+	_ = gosnowflake.GetLogger().SetLogLevel("error")
+	log.SetOutput(io.Discard)
 	plugin.Start(os.Args, provider.Init())
 }
