fa6b36eca04881f0205d3493d492aa3dfcd04c23
Christian Zunker <christian@mondoo.com>
2024-06-14 13:18:24 +0200

    Handle 'invalid argument' error
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- providers/os/fs/find_files.go
+++ providers/os/fs/find_files.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"regexp"
 	"strings"
+
+	"golang.org/x/sys/unix"
 )
 
 func FindFiles(iofs fs.FS, from string, r *regexp.Regexp, typ string, perm *uint32) ([]string, error) {
@@ -34,7 +36,7 @@ func handleFsError(err error) error {
 	if err != nil {
 		// Check for denied permissions and non-existent files. This can sometimes happen, especially for procfs
 		// We don't want to error out in such cases. We can safely skip over the file.
-		if errors.Is(err, os.ErrPermission) || errors.Is(err, os.ErrNotExist) || errors.Is(err, os.ErrInvalid) {
+		if errors.Is(err, os.ErrPermission) || errors.Is(err, os.ErrNotExist) || errors.Is(err, os.ErrInvalid) || errors.Is(err, unix.EINVAL) {
 			return nil
 		}
 		return err
