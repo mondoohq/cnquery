034cb0d77278823644eb513953e15b270fe78a26
Ivan Milchev <ivan@mondoo.com>
2024-06-14 15:44:01 +0300

    🐛 fix os provider build (#4242)
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

---
+++ providers/os/fs/find_files_unix.go
@@ -0,0 +1,27 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+//go:build linux || darwin || netbsd || openbsd || freebsd
+// +build linux darwin netbsd openbsd freebsd
+
+package fs
+
+import (
+	"errors"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+// handleFsError checks if the error is a permission denied or non-existent file error and returns nil in such cases.
+func handleFsError(err error) error {
+	if err != nil {
+		// Check for denied permissions and non-existent files. This can sometimes happen, especially for procfs
+		// We don't want to error out in such cases. We can safely skip over the file.
+		if errors.Is(err, os.ErrPermission) || errors.Is(err, os.ErrNotExist) || errors.Is(err, os.ErrInvalid) || errors.Is(err, unix.EINVAL) {
+			return nil
+		}
+		return err
+	}
+	return nil
+}
