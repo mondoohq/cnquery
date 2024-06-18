11ec995e29a2fa8436b397f80ee0d3abda89cc0b
Mondoo Tools <tools@mondoo.com>
2024-06-18 13:04:00 +0000

    ✨ Update providers 20240618

--- providers/arista/config/config.go
+++ providers/arista/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "arista",
 	ID:              "go.mondoo.com/cnquery/v9/providers/arista",
-	Version:         "11.0.15",
+	Version:         "11.0.16",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
