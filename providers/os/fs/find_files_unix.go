84e41a9c6f33d0dc324397d7e7a36ee8e43df535
Preslav <preslav@mondoo.com>
2024-06-18 15:21:58 +0300

    🐛 Fix find files panic for FS connections.
    Signed-off-by: Preslav <preslav@mondoo.com>

--- providers/os/fs/find_files_unix.go
+++ providers/os/fs/find_files_unix.go
@@ -13,15 +13,16 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// handleFsError checks if the error is a permission denied or non-existent file error and returns nil in such cases.
-func handleFsError(err error) error {
+// handleFsError checks if the error is a permission denied or non-existent file error and returns nil in such cases. the bool
+// indicates if the file should be skipped
+func handleFsError(err error) (bool, error) {
 	if err != nil {
 		// Check for denied permissions and non-existent files. This can sometimes happen, especially for procfs
 		// We don't want to error out in such cases. We can safely skip over the file.
 		if errors.Is(err, os.ErrPermission) || errors.Is(err, os.ErrNotExist) || errors.Is(err, os.ErrInvalid) || errors.Is(err, unix.EINVAL) {
-			return nil
+			return true, nil
 		}
-		return err
+		return true, err
 	}
-	return nil
+	return false, nil
 }
