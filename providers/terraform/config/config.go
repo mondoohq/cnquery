0034e8e89985deecf31f71ad5cc3816cd8fab877
Mondoo Tools <tools@mondoo.com>
2024-07-09 17:43:15 +0000

    ✨ Update providers 20240709

--- providers/terraform/config/config.go
+++ providers/terraform/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "terraform",
 	ID:      "go.mondoo.com/cnquery/v9/providers/terraform",
-	Version: "11.0.19",
+	Version: "11.0.20",
 	ConnectionTypes: []string{
 		provider.StateConnectionType,
 		provider.PlanConnectionType,
