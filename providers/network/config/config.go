34e7c4daa92329b5a6f0682bcd3c44da9058cf3f
Mondoo Tools <tools@mondoo.com>
2024-07-09 17:52:16 +0000

    ✨ Update providers 20240709

--- providers/network/config/config.go
+++ providers/network/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "network",
 	ID:              "go.mondoo.com/cnquery/v9/providers/network",
-	Version:         "11.0.14",
+	Version:         "11.0.15",
 	ConnectionTypes: []string{provider.HostConnectionType},
 	CrossProviderTypes: []string{
 		"go.mondoo.com/cnquery/providers/os",
--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "11.0.19",
+	Version:         "11.0.20",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
