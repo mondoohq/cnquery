0aed43eac4feb2a2eeda2d66e95c8478a69853de
Preslav <preslav@mondoo.com>
2024-07-15 13:40:12 +0300

    🧹 Fix ms365 build.
    Signed-off-by: Preslav <preslav@mondoo.com>

--- providers/ms365/resources/rolemanagement.go
+++ providers/ms365/resources/rolemanagement.go
@@ -28,7 +28,7 @@ func (a *mqlMicrosoftRolemanagement) roleDefinitions() ([]interface{}, error) {
 		return nil, err
 	}
 	ctx := context.Background()
-	resp, err := graphClient.RoleManagement().Directory().RoleDefinitions().Get(ctx, &rolemanagement.DirectoryRoledefinitionsRoleDefinitionsRequestBuilderGetRequestConfiguration{})
+	resp, err := graphClient.RoleManagement().Directory().RoleDefinitions().Get(ctx, &rolemanagement.DirectoryRoleDefinitionsRequestBuilderGetRequestConfiguration{})
 	if err != nil {
 		return nil, transformError(err)
 	}
@@ -68,8 +68,8 @@ func (a *mqlMicrosoftRolemanagementRoledefinition) assignments() ([]interface{},
 	}
 	roleDefinitionId := a.Id.Data
 	filter := "roleDefinitionId eq '" + roleDefinitionId + "'"
-	requestConfig := &rolemanagement.DirectoryRoleassignmentsRoleAssignmentsRequestBuilderGetRequestConfiguration{
-		QueryParameters: &rolemanagement.DirectoryRoleassignmentsRoleAssignmentsRequestBuilderGetQueryParameters{
+	requestConfig := &rolemanagement.DirectoryRoleAssignmentsRequestBuilderGetRequestConfiguration{
+		QueryParameters: &rolemanagement.DirectoryRoleAssignmentsRequestBuilderGetQueryParameters{
 			Filter: &filter,
 			Expand: []string{"principal"},
 		},
