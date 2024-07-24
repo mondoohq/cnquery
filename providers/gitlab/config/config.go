041461ad1c6da019c010208b8b74f0dd053da58e
vjeffrey <vj@mondoo.com>
2024-07-24 14:10:27 -0600

    🎉 gitlab-11.1.12 (#4402)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gitlab",
-	Version: "11.1.11",
+	Version: "11.1.12",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
