5a74b5d5bc24dac64d4b94701671cda398d1d2e6
Mondoo Tools <tools@mondoo.com>
2024-07-25 18:31:29 +0000

    ✨ Update providers 20240725

--- providers/atlassian/config/config.go
+++ providers/atlassian/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "atlassian",
 	ID:      "go.mondoo.com/cnquery/v9/providers/atlassian",
-	Version: "11.0.23",
+	Version: "11.0.24",
 	ConnectionTypes: []string{
 		provider.DefaultConnectionType,
 		"jira",
--- providers/terraform/config/config.go
+++ providers/terraform/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "terraform",
 	ID:      "go.mondoo.com/cnquery/v9/providers/terraform",
-	Version: "11.0.22",
+	Version: "11.0.23",
 	ConnectionTypes: []string{
 		provider.StateConnectionType,
 		provider.PlanConnectionType,
