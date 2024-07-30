682d9f0a14f6ef22220039effb2bf0efc072f627
Mondoo <hello@mondoo.com>
2024-07-30 18:41:11 +0300

    🎉 google-workspace-11.1.0
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/google-workspace/config/config.go
+++ providers/google-workspace/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "google-workspace",
 	ID:              "go.mondoo.com/cnquery/v9/providers/google-workspace",
-	Version:         "11.0.23",
+	Version:         "11.1.0",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
