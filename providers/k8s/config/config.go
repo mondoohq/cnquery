58f7b9adb5826591fb6f5a1e2f31a0a3dd176a2d
Mondoo Tools <tools@mondoo.com>
2024-07-30 22:43:51 +0000

    ✨ Update providers 20240730

--- providers/k8s/config/config.go
+++ providers/k8s/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "k8s",
 	ID:              "go.mondoo.com/cnquery/v9/providers/k8s",
-	Version:         "11.1.17",
+	Version:         "11.1.18",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/vcd/config/config.go
+++ providers/vcd/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vcd",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vcd",
-	Version:         "11.0.23",
+	Version:         "11.0.24",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
