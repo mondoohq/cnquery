0e16f48239dea48a6e7758d84e1ed8712f8f99cb
Mondoo Tools <tools@mondoo.com>
2024-07-30 12:07:42 +0000

    ✨ Update providers 20240730

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "11.1.19",
+	Version:         "11.1.20",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
--- providers/network/config/config.go
+++ providers/network/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "network",
 	ID:              "go.mondoo.com/cnquery/v9/providers/network",
-	Version:         "11.0.17",
+	Version:         "11.0.18",
 	ConnectionTypes: []string{provider.HostConnectionType},
 	CrossProviderTypes: []string{
 		"go.mondoo.com/cnquery/providers/os",
