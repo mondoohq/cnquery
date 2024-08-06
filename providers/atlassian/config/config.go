d95848e57fdf5704255322fb0eda0e2b9f9ddb9a
Mondoo Tools <tools@mondoo.com>
2024-08-06 16:34:15 +0000

    ✨ Update providers 20240806

--- providers/atlassian/config/config.go
+++ providers/atlassian/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "atlassian",
 	ID:      "go.mondoo.com/cnquery/v9/providers/atlassian",
-	Version: "11.0.25",
+	Version: "11.0.26",
 	ConnectionTypes: []string{
 		provider.DefaultConnectionType,
 		"jira",
