bcb367ef04e591a6b90e7a953edfc9204a5744fe
Mondoo Tools <tools@mondoo.com>
2024-08-08 16:40:57 +0000

    ✨ Update providers 20240808

--- providers/equinix/config/config.go
+++ providers/equinix/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "equinix",
 	ID:              "go.mondoo.com/cnquery/v9/providers/equinix",
-	Version:         "11.0.24",
+	Version:         "11.0.25",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "11.0.24",
+	Version:         "11.0.25",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
