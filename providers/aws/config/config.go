b41f613a13cbf11a6980a4f7271dbea8fc77e67e
Christoph Hartmann <chris@lollyrock.com>
2024-08-12 21:25:14 +0200

    🎉 aws-11.3.0 (#4543)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "11.2.2",
+	Version:         "11.3.0",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
