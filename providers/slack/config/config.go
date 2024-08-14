777c523f11c0b44316a2e103c870981a35fcea4f
Jay Mundrawala <jay@mondoo.com>
2024-08-14 14:45:29 -0500

    add slack url segments

--- providers/slack/config/config.go
+++ providers/slack/config/config.go
@@ -4,6 +4,7 @@
 package config
 
 import (
+	"go.mondoo.com/cnquery/v11/providers-sdk/v1/inventory"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
 	"go.mondoo.com/cnquery/v11/providers/slack/provider"
 )
@@ -34,6 +35,16 @@ var Config = plugin.Provider{
 					Default: "",
 					Desc:    "Team ID (required for org token)",
 				},
+			},
+		},
+	},
+	AssetUrlTrees: []*inventory.AssetUrlBranch{
+		{
+			PathSegments: []string{"technology=saas", "provider=slack"},
+			Key:          "kind",
+			Title:        "Kind",
+			Values: map[string]*inventory.AssetUrlBranch{
+				"team": nil,
 			},
 		},
 	},
