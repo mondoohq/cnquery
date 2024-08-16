5367de59f8a7a2f8fa374513d87e469aa3bd924a
Christoph Hartmann <chris@lollyrock.com>
2024-08-16 19:10:22 +0200

    🧹 improve azure policy assignments resource

--- providers/azure/resources/policy.go
+++ providers/azure/resources/policy.go
@@ -7,13 +7,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-
 	"go.mondoo.com/cnquery/v11/llx"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
 	"go.mondoo.com/cnquery/v11/providers/azure/connection"
-
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 )
 
 func initAzureSubscriptionPolicy(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[string]*llx.RawData, plugin.Resource, error) {
@@ -30,30 +26,17 @@ func initAzureSubscriptionPolicy(runtime *plugin.Runtime, args map[string]*llx.R
 	return args, nil, nil
 }
 
-func (a *mqlAzureSubscriptionPolicyAssignment) id() (string, error) {
-	// Ensure that all the parts of the ID are available
-	if a.Scope.Data == "" || a.Name.Data == "" {
-		return "", errors.New("missing required fields to generate id")
-	}
-
-	return fmt.Sprintf("azure.subscription.policy/%s/%s", a.Scope.Data, a.Name.Data), nil
-}
-
-func (a *mqlAzureSubscriptionPolicy) policyAssignments() ([]interface{}, error) {
+func (a *mqlAzureSubscriptionPolicy) assignments() ([]interface{}, error) {
 	conn := a.MqlRuntime.Connection.(*connection.AzureConnection)
 	ctx := context.Background()
-	token := conn.Token()
 	subId := a.SubscriptionId.Data
 
-	rawToken, err := token.GetToken(ctx, policy.TokenRequestOptions{
-		Scopes: []string{"https://management.core.windows.net//.default"},
-	})
+	armConn, err := getArmSecurityConnection(ctx, conn, subId)
 	if err != nil {
 		return nil, err
 	}
 
-	ep := cloud.AzurePublic.Services[cloud.ResourceManager].Endpoint
-	pas, err := getPolicyAssignments(ctx, subId, ep, rawToken.Token)
+	pas, err := getPolicyAssignments(ctx, armConn)
 	if err != nil {
 		return nil, err
 	}
@@ -61,9 +44,12 @@ func (a *mqlAzureSubscriptionPolicy) policyAssignments() ([]interface{}, error) 
 	res := []interface{}{}
 	for _, assignment := range pas.PolicyAssignments {
 		assignmentData := map[string]*llx.RawData{
-			"name":  llx.StringData(assignment.Properties.DisplayName),
-			"scope": llx.StringData(assignment.Properties.Scope),
-			"type":  llx.StringData(assignment.Type),
+			"__id":            llx.StringData(fmt.Sprintf("azure.subscription.policy/%s/%s", assignment.Properties.Scope, assignment.Properties.DisplayName)),
+			"id":              llx.StringData(assignment.Properties.PolicyDefinitionID),
+			"name":            llx.StringData(assignment.Properties.DisplayName),
+			"scope":           llx.StringData(assignment.Properties.Scope),
+			"description":     llx.StringData(assignment.Properties.Description),
+			"enforcementMode": llx.StringData(assignment.Properties.EnforcementMode),
 		}
 
 		mqlAssignment, err := CreateResource(a.MqlRuntime, "azure.subscription.policy.assignment", assignmentData)
