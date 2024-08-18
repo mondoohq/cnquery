420ce0b7194ae046104b9072f0e4a66d6c258ee8
Christoph Hartmann <chris@lollyrock.com>
2024-08-18 14:41:54 +0200

    ⭐️ improve microsoft.tenant resource

--- providers/ms365/resources/microsoft.go
+++ providers/ms365/resources/microsoft.go
@@ -3,13 +3,6 @@
 
 package resources
 
-import (
-	"context"
-
-	"github.com/microsoftgraph/msgraph-sdk-go/organization"
-	"go.mondoo.com/cnquery/v11/providers/ms365/connection"
-)
-
 type mqlMicrosoftInternal struct {
 	// index users by id
 	idxUsersById map[string]*mqlMicrosoftUser
@@ -37,27 +30,4 @@ func (a *mqlMicrosoft) userById(id string) (*mqlMicrosoftUser, bool) {
 
 	res, ok := a.idxUsersById[id]
 	return res, ok
-}
-
-func (a *mqlMicrosoft) tenantDomainName() (string, error) {
-	conn := a.MqlRuntime.Connection.(*connection.Ms365Connection)
-	graphClient, err := conn.GraphClient()
-	if err != nil {
-		return "", err
-	}
-	ctx := context.Background()
-	resp, err := graphClient.Organization().Get(ctx, &organization.OrganizationRequestBuilderGetRequestConfiguration{})
-	if err != nil {
-		return "", transformError(err)
-	}
-	tenantDomainName := ""
-	for _, org := range resp.GetValue() {
-		for _, d := range org.GetVerifiedDomains() {
-			if *d.GetIsInitial() {
-				tenantDomainName = *d.GetName()
-			}
-		}
-	}
-
-	return tenantDomainName, nil
 }
