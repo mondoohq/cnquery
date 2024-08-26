d85d1c117d4a809ec47f3e531797ed197edcaf36
Mikita Iwanowski <info@slnt-opp.xyz>
2024-08-26 13:53:58 +0200

    ✨ Authored and Commited dates to :octocat: commits (#4607)
    * ✨ Authored and Commited date to :octocat: commits
    
    * fix: typo
    --- commited
    +++ committed

--- providers/github/resources/github_repo.go
+++ providers/github/resources/github_repo.go
@@ -645,7 +645,7 @@ func newMqlGithubCommit(runtime *plugin.Runtime, rc *github.RepositoryCommit, ow
 	conn := runtime.Connection.(*connection.GithubConnection)
 
 	// if the github author is nil, we have to load the commit again
-	if rc.Author == nil {
+	if rc.Author == nil || rc.Commit == nil {
 		rc, _, err = conn.Client().Repositories.GetCommit(conn.Context(), owner, repo, rc.GetSHA(), nil)
 		if err != nil {
 			return nil, err
@@ -685,14 +685,16 @@ func newMqlGithubCommit(runtime *plugin.Runtime, rc *github.RepositoryCommit, ow
 	}
 
 	return CreateResource(runtime, "github.commit", map[string]*llx.RawData{
-		"url":        llx.StringData(rc.GetURL()),
-		"sha":        llx.StringData(sha),
-		"author":     llx.AnyData(githubAuthor),
-		"committer":  llx.AnyData(githubCommitter),
-		"owner":      llx.StringData(owner),
-		"repository": llx.StringData(repo),
-		"commit":     llx.AnyData(mqlGitCommit),
-		"stats":      llx.MapData(stats, types.Any),
+		"url":           llx.StringData(rc.GetURL()),
+		"sha":           llx.StringData(sha),
+		"author":        llx.AnyData(githubAuthor),
+		"committer":     llx.AnyData(githubCommitter),
+		"owner":         llx.StringData(owner),
+		"repository":    llx.StringData(repo),
+		"commit":        llx.AnyData(mqlGitCommit),
+		"stats":         llx.MapData(stats, types.Any),
+		"authoredDate":  llx.TimeData(rc.Commit.Author.Date.Time),
+		"committedDate": llx.TimeData(rc.Commit.Committer.Date.Time),
 	})
 }
 
