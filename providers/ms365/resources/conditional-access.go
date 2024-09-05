c818dba08dfaf20fd4a45ac73dec85edf1bf9689
Hossein Rouhani <h_rouhani@hotmail.com>
2024-09-05 13:55:00 +0200

    adding the support for multiple NamedLocation
    Signed-off-by: Hossein Rouhani <h_rouhani@hotmail.com>

--- providers/ms365/resources/conditional-access.go
+++ providers/ms365/resources/conditional-access.go
@@ -11,31 +11,36 @@ import (
 	"go.mondoo.com/cnquery/v11/providers/ms365/connection"
 )
 
-func (a *mqlMicrosoftConditionalAccess) namedLocations() (string, error) {
+func (a *mqlMicrosoftConditionalAccess) namedLocations() ([]interface{}, error) {
 	conn := a.MqlRuntime.Connection.(*connection.Ms365Connection)
 	graphClient, err := conn.GraphClient()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// Make a request to get named locations
 	ctx := context.Background()
 	namedLocations, err := graphClient.Identity().ConditionalAccess().NamedLocations().Get(ctx, nil)
 	if err != nil {
-		return "", transformError(err)
+		return nil, transformError(err)
 	}
 
-	// Check if any of the named locations exist and return the first one
+	// Collect all named location names
+	var locationNames []interface{} // Changed to interface{} to match the expected return type
 	for _, location := range namedLocations.GetValue() {
 		// Use type assertion to check for IP named locations
 		if ipLocation, ok := location.(*models.IpNamedLocation); ok {
 			displayName := ipLocation.GetDisplayName()
 			if displayName != nil {
-				return *displayName, nil
+				locationNames = append(locationNames, *displayName)
 			}
 		}
 	}
 
-	log.Println("No named locations are defined.")
-	return "", nil
+	if len(locationNames) == 0 {
+		log.Println("No named locations are defined.")
+		return nil, nil
+	}
+
+	return locationNames, nil
 }
