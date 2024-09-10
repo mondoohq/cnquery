234804558d09b587474598727a1285748c23e1dc
Mondoo Tools <tools@mondoo.com>
2024-09-10 12:26:42 +0000

    ✨ Update providers 20240910

--- providers/equinix/config/config.go
+++ providers/equinix/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "equinix",
 	ID:              "go.mondoo.com/cnquery/v9/providers/equinix",
-	Version:         "11.0.28",
+	Version:         "11.0.29",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
