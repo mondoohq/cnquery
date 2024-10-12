177c619e3fac7b9f286c0ad7b57783c084cf64d2
Mikita Iwanowski <info@slnt-opp.xyz>
2024-10-12 18:13:21 +0200

    init

---
+++ providers/cloudflare/connection/connection.go
@@ -0,0 +1,57 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+package connection
+
+import (
+	"errors"
+	"os"
+
+	"github.com/cloudflare/cloudflare-go"
+	"go.mondoo.com/cnquery/v11/providers-sdk/v1/inventory"
+	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
+)
+
+const (
+	OPTION_API_TOKEN = "api-token"
+)
+
+type CloudflareConnection struct {
+	plugin.Connection
+	Conf  *inventory.Config
+	asset *inventory.Asset
+
+	Cf *cloudflare.API
+}
+
+func NewCloudflareConnection(id uint32, asset *inventory.Asset, conf *inventory.Config) (*CloudflareConnection, error) {
+	conn := &CloudflareConnection{
+		Connection: plugin.NewConnection(id, asset),
+		Conf:       conf,
+		asset:      asset,
+	}
+
+	// initialize your connection here
+	token := conf.Options[OPTION_API_TOKEN]
+	if token == "" {
+		token = os.Getenv("CLOUDFLARE_TOKEN")
+		if token == "" {
+			return nil, errors.New("API Token is required")
+		}
+	}
+
+	api, err := cloudflare.NewWithAPIToken(token)
+	if err != nil {
+		return nil, err
+	}
+	conn.Cf = api
+
+	return conn, nil
+}
+
+func (c *CloudflareConnection) Name() string {
+	return "cloudflare"
+}
+
+func (c *CloudflareConnection) Asset() *inventory.Asset {
+	return c.asset
+}
---
+++ providers/cloudflare/main.go
@@ -0,0 +1,14 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+package main
+
+import (
+	"os"
+
+	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
+	"go.mondoo.com/cnquery/v11/providers/cloudflare/provider"
+)
+
+func main() {
+	plugin.Start(os.Args, provider.Init())
+}
