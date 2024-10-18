7620b2eb8fd70f217f09bf1c236a2fca63b19f3c
Letha <letha@mondoo.com>
2024-10-18 15:07:43 -0700

    Add long-form help for slack provider (#4766)

--- providers/slack/config/config.go
+++ providers/slack/config/config.go
@@ -16,9 +16,15 @@ var Config = plugin.Provider{
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
-			Name:      "slack",
-			Use:       "slack",
-			Short:     "a Slack team",
+			Name:  "slack",
+			Use:   "slack",
+			Short: "a Slack team",
+			Long: `Use the slack provider to query resources in a Slack team.
+
+Examples:
+  cnquery shell slack --token <api-token>
+  cnspec scan slack --team-id <team-id> --token <api-token>
+`,
 			MinArgs:   0,
 			MaxArgs:   0,
 			Discovery: []string{},
@@ -27,13 +33,13 @@ var Config = plugin.Provider{
 					Long:    "token",
 					Type:    plugin.FlagType_String,
 					Default: "",
-					Desc:    "Slack API token",
+					Desc:    "Slack API token (To learn more, read https://mondoo.com/docs/cnspec/saas/slack/)",
 				},
 				{
 					Long:    "team-id",
 					Type:    plugin.FlagType_String,
 					Default: "",
-					Desc:    "Team ID (required for org token)",
+					Desc:    "Team ID (required if the token is an org token)",
 				},
 			},
 		},
