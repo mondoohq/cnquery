6bb21b7481a69accd252eba5dd22b498a312c901
Mondoo <hello@mondoo.com>
2024-10-31 14:26:50 +0100

    🎉 Update providers 20241030
    ansible-11.0.24, arista-11.0.37, atlassian-11.0.40, aws-11.5.2, azure-11.3.10, cloudformation-11.0.29, core-11.0.16, equinix-11.0.37, gcp-11.0.43, github-11.4.21, gitlab-11.1.31, google-workspace-11.1.15, ipmi-11.0.37, k8s-11.1.31, mondoo-11.1.15, network-11.0.32, oci-11.0.37, okta-11.0.37, opcua-11.0.37, os-11.3.2, shodan-11.0.26, slack-11.0.39, snowflake-11.0.23, terraform-11.0.37, vcd-11.0.37, vsphere-11.0.37
    
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "11.0.36",
+	Version:         "11.0.37",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
