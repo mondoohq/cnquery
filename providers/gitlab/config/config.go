530f2b8b357ade6c3f18d53e565998fbf2fc1915
Mondoo Tools <tools@mondoo.com>
2024-11-05 08:01:34 +0000

    ✨ Update providers 20241105

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gitlab",
-	Version: "11.1.31",
+	Version: "11.1.32",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
--- providers/shodan/config/config.go
+++ providers/shodan/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "shodan",
 	ID:              "go.mondoo.com/cnquery/v11/providers/shodan",
-	Version:         "11.0.26",
+	Version:         "11.0.27",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
