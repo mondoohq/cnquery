9e1b2eb0c17e2045617c9350bccf1c102e4c672b
Tom Kretschmann <tk+github.com@the-tkit.com>
2024-11-20 09:24:45 +0100

    add curl timeout for cloud metadata lookup on unix systems

--- providers/os/id/azcompute/azcompute.go
+++ providers/os/id/azcompute/azcompute.go
@@ -57,7 +57,7 @@ func (m *commandInstanceMetadata) Identify() (Identity, error) {
 	var instanceDocument string
 	switch {
 	case m.platform.IsFamily(inventory.FAMILY_UNIX):
-		cmd, err := m.conn.RunCommand("curl --noproxy '*' -H Metadata:true " + identityUrl)
+		cmd, err := m.conn.RunCommand("curl --max-time 1 --noproxy '*' -H Metadata:true " + identityUrl)
 		if err != nil {
 			return Identity{}, err
 		}
