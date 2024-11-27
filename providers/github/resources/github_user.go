32a43ddeb4a54ad2126ab82729fb83c2e71ba7d5
Tim Smith <tsmith84@gmail.com>
2024-11-27 17:44:21 +0100

    Pull in the latest version of GitHub package
    Tested pulling a repo data and it seems to work as expected
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- providers/github/resources/github_user.go
+++ providers/github/resources/github_user.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/cockroachdb/errors"
-	"github.com/google/go-github/v62/github"
+	"github.com/google/go-github/v67/github"
 	"go.mondoo.com/cnquery/v11/llx"
 	"go.mondoo.com/cnquery/v11/logger"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
