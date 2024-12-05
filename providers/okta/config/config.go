140803e1cde240f0459d5bacf1278df8d6d821ed
Mondoo Tools <tools@mondoo.com>
2024-12-05 00:05:48 +0000

    ✨ Update providers 20241205

--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "11.0.44",
+	Version:         "11.0.45",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
