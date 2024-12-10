389b89f371b341d332724f3a4aa77913b400acf2
Mondoo Tools <tools@mondoo.com>
2024-12-10 11:22:53 +0000

    ✨ Update providers 20241210

--- providers/core/config/config.go
+++ providers/core/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:       "core",
 	ID:         "go.mondoo.com/cnquery/v9/providers/core",
-	Version:    "11.0.23",
+	Version:    "11.0.24",
 	Connectors: []plugin.Connector{},
 	AssetUrlTrees: []*inventory.AssetUrlBranch{
 		{
