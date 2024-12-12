ee1123c76d527a22ef5d4a154ce0f4d82dbff14b
Ivan Milchev <ivan@mondoo.com>
2024-12-12 16:23:04 +0200

    🎉 os-11.3.13 (#4975)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "11.3.12",
+	Version: "11.3.13",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
