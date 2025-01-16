41b6963b4dbb8fc82734c7b0cfbb5210aca2065b
vjeffrey <vj@mondoo.com>
2025-01-16 16:34:29 -0700

    🎉 gcp-11.0.60 (#5099)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "11.0.59",
+	Version: "11.0.60",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
