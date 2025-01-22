545d737bfc18c4b06eeb08bbf957f7220affbff0
Salim Afiune Maya <afiune@mondoo.com>
2025-01-22 21:46:20 +0100

    🎉 terraform-11.1.0
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- providers/terraform/config/config.go
+++ providers/terraform/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "terraform",
 	ID:      "go.mondoo.com/cnquery/v9/providers/terraform",
-	Version: "11.0.53",
+	Version: "11.1.0",
 	ConnectionTypes: []string{
 		provider.StateConnectionType,
 		provider.PlanConnectionType,
