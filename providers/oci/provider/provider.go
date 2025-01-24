mondoohq/cnquery

commit ebde260387459c051c3f6448ecd048a90d9192d2

⭐ Add missing technology segment urls (#4557)
* add a common path for saas

* update github url segments

* add gitlab url segments

* add atlassian url segments

* add common url path for iac

* add ansible url segments

* add cloudformation url segments

* add okta url segments

* add terraform url segments

* add slack url segments

* add oci url segments

* add network as common technology

* allow no key

* add network host url segments

* add network ipmi url segments

* add shodan url segments

* add arista url segments

* add dockerfile url segments

* update k8s-manifest url segments

* add ms365 url segments

* fix ansible url

providers/oci/provider/provider.go
@@ -181,11 +181,12 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.OciConnection)
 	}
 
 	asset.Platform = &inventory.Platform{
-		Name:    "oci",
-		Title:   "Oracle Cloud Infrastructure",
-		Runtime: "oci",
-		Kind:    "api",
-		Family:  []string{"oci"},
+		Name:                  "oci",
+		Title:                 "Oracle Cloud Infrastructure",
+		Runtime:               "oci",
+		Kind:                  "api",
+		Family:                []string{"oci"},
+		TechnologyUrlSegments: []string{"saas", "oci", "tenancy"},
 	}
 
 	platformID := "//platformid.api.mondoo.app/runtime/oci/" + conn.TenantID()

providers/ansible/provider/provider.go
@@ -131,10 +131,11 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.AnsibleConnect
 	asset.Name = conn.Conf.Host
 
 	asset.Platform = &inventory.Platform{
-		Name:   "ansible-playbook",
-		Family: []string{"ansible"},
-		Kind:   "api",
-		Title:  "Ansible Playbook",
+		Name:                  "ansible-playbook",
+		Family:                []string{"ansible"},
+		Kind:                  "api",
+		Title:                 "Ansible Playbook",
+		TechnologyUrlSegments: []string{"iac", "ansible", "playbook"},
 	}
 
 	projectPath, ok := asset.Connections[0].Options["path"]

providers/arista/provider/provider.go
@@ -168,13 +168,14 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.AristaConnecti
 	}
 
 	asset.Platform = &inventory.Platform{
-		Name:    "arista-eos",
-		Version: version,
-		Arch:    arch,
-		Family:  []string{"arista"},
-		Kind:    "api",
-		Title:   "Arista EOS",
-		Runtime: "arista",
+		Name:                  "arista-eos",
+		Version:               version,
+		Arch:                  arch,
+		Family:                []string{"arista"},
+		Kind:                  "api",
+		Title:                 "Arista EOS",
+		Runtime:               "arista",
+		TechnologyUrlSegments: []string{"network", "arista"},
 	}
 
 	eosClient := eos.NewEos(conn.Client())

providers/cloudformation/provider/provider.go
@@ -131,11 +131,12 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.Cloudformation
 	asset.Name = conn.Conf.Host
 
 	asset.Platform = &inventory.Platform{
-		Name:    "cloudformation",
-		Family:  []string{"cloudformation"},
-		Runtime: "cloudformation",
-		Kind:    "api",
-		Title:   "AWS CloudFormation",
+		Name:                  "cloudformation",
+		Family:                []string{"cloudformation"},
+		Runtime:               "cloudformation",
+		Kind:                  "api",
+		Title:                 "AWS CloudFormation",
+		TechnologyUrlSegments: []string{"iac", "cloudformation", "template"},
 	}
 
 	projectPath, ok := asset.Connections[0].Options["path"]

providers/gitlab/provider/provider.go
@@ -175,22 +175,27 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 	return runtime.Connection.(*connection.GitLabConnection), nil
 }
 
-var (
-	projectPlatform = &inventory.Platform{
-		Name:    "gitlab-project",
-		Title:   "GitLab Project",
-		Family:  []string{"gitlab"},
-		Kind:    "api",
-		Runtime: "gitlab",
+func projectPlatform() *inventory.Platform {
+	return &inventory.Platform{
+		Name:                  "gitlab-project",
+		Title:                 "GitLab Project",
+		Family:                []string{"gitlab"},
+		Kind:                  "api",
+		Runtime:               "gitlab",
+		TechnologyUrlSegments: []string{"saas", "gitlab", "project"},
 	}
-	groupPlatform = &inventory.Platform{
-		Name:    "gitlab-group",
-		Title:   "GitLab Group",
-		Family:  []string{"gitlab"},
-		Kind:    "api",
-		Runtime: "gitlab",
+}
+
+func groupPlatform() *inventory.Platform {
+	return &inventory.Platform{
+		Name:                  "gitlab-group",
+		Title:                 "GitLab Group",
+		Family:                []string{"gitlab"},
+		Kind:                  "api",
+		Runtime:               "gitlab",
+		TechnologyUrlSegments: []string{"saas", "gitlab", "group"},
 	}
-)
+}
 
 func newGitLabGroupID(groupID int) string {
 	return "//platformid.api.mondoo.app/runtime/gitlab/group/" + strconv.Itoa(groupID)
@@ -234,7 +239,7 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.GitLabConnecti
 }
 
 func (s *Service) detectAsProject(asset *inventory.Asset, groupID int, groupFullPath string, project *gitlab.Project) {
-	asset.Platform = projectPlatform
+	asset.Platform = projectPlatform()
 	asset.Name = "GitLab Project " + project.Name
 	asset.PlatformIds = []string{
 		newGitLabProjectID(groupID, project.ID),
@@ -243,7 +248,7 @@ func (s *Service) detectAsProject(asset *inventory.Asset, groupID int, groupFull
 }
 
 func (s *Service) detectAsGroup(asset *inventory.Asset, group *gitlab.Group) error {
-	asset.Platform = groupPlatform
+	asset.Platform = groupPlatform()
 	asset.Name = "GitLab Group " + group.Name
 	asset.PlatformIds = []string{
 		newGitLabGroupID(group.ID),

providers/ipmi/provider/provider.go
@@ -155,9 +155,10 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.IpmiConnection
 	asset.Name = conn.Conf.Host
 
 	asset.Platform = &inventory.Platform{
-		Name:  "ipmi",
-		Title: "IPMI",
-		Kind:  "api",
+		Name:                  "ipmi",
+		Title:                 "IPMI",
+		Kind:                  "api",
+		TechnologyUrlSegments: []string{"network", "ipmi"},
 	}
 
 	// TODO: consider using the ipmi vendor id and product id

providers/ms365/provider/provider.go
@@ -150,11 +150,12 @@ func (s *Service) connect(req *plugin.ConnectReq, callback plugin.ProviderCallba
 
 func (s *Service) detect(asset *inventory.Asset, conn *connection.Ms365Connection) error {
 	asset.Platform = &inventory.Platform{
-		Name:    "ms365",
-		Runtime: "ms365",
-		Family:  []string{""},
-		Kind:    "api",
-		Title:   "Microsoft Azure",
+		Name:                  "ms365",
+		Runtime:               "ms365",
+		Family:                []string{""},
+		Kind:                  "api",
+		Title:                 "Microsoft Azure",
+		TechnologyUrlSegments: []string{"saas", "ms365"},
 	}
 
 	return nil
@@ -175,10 +176,11 @@ func (s *Service) discover(conn *connection.Ms365Connection, conf *inventory.Con
 		PlatformIds: []string{identifier},
 		Name:        "Microsoft 365 tenant " + conn.TenantId(),
 		Platform: &inventory.Platform{
-			Name:    "microsoft365",
-			Title:   "Microsoft 365",
-			Runtime: "ms-graph",
-			Kind:    "api",
+			Name:                  "microsoft365",
+			Title:                 "Microsoft 365",
+			Runtime:               "ms-graph",
+			Kind:                  "api",
+			TechnologyUrlSegments: []string{"saas", "ms365"},
 		},
 		Connections: []*inventory.Config{conf.Clone()}, // pass-in the current config
 		Labels: map[string]string{

providers/network/provider/provider.go
@@ -200,10 +200,11 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.HostConnection
 		asset.Name = conn.Conf.Host
 	}
 	asset.Platform = &inventory.Platform{
-		Name:   "host",
-		Family: []string{"network"},
-		Kind:   "network",
-		Title:  "Network Host",
+		Name:                  "host",
+		Family:                []string{"network"},
+		Kind:                  "network",
+		Title:                 "Network Host",
+		TechnologyUrlSegments: []string{"network", "host"},
 	}
 
 	asset.Fqdn = conn.FQDN()

commit 7fccdc1fbf55f9e2be8b94aaf399a271d8026f29

🐛 Fix oci asset url
Was broken in https://github.com/mondoohq/cnquery/pull/4557

providers/oci/provider/provider.go
@@ -186,7 +186,7 @@ func (s *Service) detect(asset *inventory.Asset, conn *connection.OciConnection)
 		Runtime:               "oci",
 		Kind:                  "api",
 		Family:                []string{"oci"},
-		TechnologyUrlSegments: []string{"saas", "oci", "tenancy"},
+		TechnologyUrlSegments: []string{"oci", "tenancy"},
 	}
 
 	platformID := "//platformid.api.mondoo.app/runtime/oci/" + conn.TenantID()