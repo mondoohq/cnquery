3d1ce25a97880bba0d5098054c3bbc02928d66f0
Salim Afiune Maya <afiune@mondoo.com>
2025-01-31 08:33:33 +0100

    ✨ backup way to describe aws regions
    This is a workaround for cases where the DescribeRegions API is blocked.
    We use a well known regional table provided by AWS to list all possible
    regions.
    
    https://api.regional-table.region-services.aws.a2z.com/index.json
    
    NOTE: if we need to validate that we have access to that region or, that
    the region is enabled, we can improve this function to do STS identity
    calls for all regions.
    
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- providers/aws/connection/connection_test.go
+++ providers/aws/connection/connection_test.go
@@ -73,3 +73,43 @@ func TestParseOptsToFilters(t *testing.T) {
 		require.Equal(t, expected, actual)
 	})
 }
+
+func TestGetRegionsFromRegionalTable(t *testing.T) {
+	t.Run("Successful region extraction and deduplication", func(t *testing.T) {
+		regions, err := getRegionsFromRegionalTable()
+		require.NoError(t, err)
+		fewExpectedRegions := []string{
+			"ap-east-1",
+			"ap-northeast-1",
+			"ap-south-1",
+			"ap-southeast-1",
+			"ca-central-1",
+			"ca-west-1",
+			"cn-north-1",
+			"cn-northwest-1",
+			"eu-central-1",
+			"eu-central-2",
+			"eu-north-1",
+			"eu-south-1",
+			"eu-south-2",
+			"eu-west-1",
+			"eu-west-2",
+			"eu-west-3",
+			"il-central-1",
+			"me-central-1",
+			"me-south-1",
+			"mx-central-1",
+			"sa-east-1",
+			"us-east-1",
+			"us-east-2",
+			"us-gov-east-1",
+			"us-gov-west-1",
+			"us-west-1",
+			"us-west-2",
+		}
+		for _, expectedRegion := range fewExpectedRegions {
+			require.Contains(t, regions, expectedRegion)
+
+		}
+	})
+}
