395ad48a7e75d05824c4f4f3fbfb1cbef08e1b89
Mondoo Tools <tools@mondoo.com>
2025-02-11 12:53:46 +0000

    ✨ Update providers 20250211

--- providers/cloudformation/config/config.go
+++ providers/cloudformation/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "cloudformation",
 	ID:              "go.mondoo.com/cnquery/v11/providers/cloudformation",
-	Version:         "11.0.48",
+	Version:         "11.0.49",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
