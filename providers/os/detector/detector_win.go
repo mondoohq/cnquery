0ccb014238dd3bad9ab6859aae8671ca037372df
Jay Mundrawala <jay@mondoo.com>
2025-02-18 13:09:37 -0600

    🐛 Detect windows 11 name
    Windows 11 returns `Windows 10` for `ProductName` in
    `HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion`. We use that as the
    platform title. This corrects the platform title if its a build that
    falls into with windows 11 build numbers

--- providers/os/detector/detector_win.go
+++ providers/os/detector/detector_win.go
@@ -5,6 +5,7 @@ package detector
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/inventory"
@@ -26,24 +27,7 @@ func runtimeWindowsDetector(pf *inventory.Platform, conn shared.Connection) (boo
 	//  try to get build + ubr number (win 10+, 2019+)
 	current, err := win.GetWindowsOSBuild(conn)
 	if err == nil && current.UBR > 0 {
-		pf.Name = "windows"
-		pf.Title = current.ProductName
-		pf.Version = current.CurrentBuild
-		pf.Build = strconv.Itoa(current.UBR)
-		pf.Arch = current.Architecture
-
-		var productType string
-		switch current.ProductType {
-		case "WinNT":
-			productType = "1" // Workstation
-		case "ServerNT":
-			productType = "3" // Server
-		case "LanmanNT":
-			productType = "2" // Domain Controller
-		}
-
-		pf.Labels["windows.mondoo.com/product-type"] = productType
-		pf.Labels["windows.mondoo.com/display-version"] = current.DisplayVersion
+		platformFromWinCurrentVersion(pf, current)
 		return true, nil
 	}
 
@@ -65,7 +49,35 @@ func runtimeWindowsDetector(pf *inventory.Platform, conn shared.Connection) (boo
 	pf.Arch = data.OSArchitecture
 	pf.Labels["windows.mondoo.com/product-type"] = data.ProductType
 
+	correctForWindows11(pf)
 	return true, nil
+}
+
+func platformFromWinCurrentVersion(pf *inventory.Platform, current *win.WindowsCurrentVersion) {
+	pf.Name = "windows"
+	pf.Title = current.ProductName
+	pf.Version = current.CurrentBuild
+	pf.Build = strconv.Itoa(current.UBR)
+	pf.Arch = current.Architecture
+
+	var productType string
+	switch current.ProductType {
+	case "WinNT":
+		productType = "1" // Workstation
+	case "ServerNT":
+		productType = "3" // Server
+	case "LanmanNT":
+		productType = "2" // Domain Controller
+	}
+
+	if pf.Labels == nil {
+		pf.Labels = map[string]string{}
+	}
+
+	pf.Labels["windows.mondoo.com/product-type"] = productType
+	pf.Labels["windows.mondoo.com/display-version"] = current.DisplayVersion
+
+	correctForWindows11(pf)
 }
 
 func staticWindowsDetector(pf *inventory.Platform, conn shared.Connection) (bool, error) {
@@ -111,5 +123,23 @@ func staticWindowsDetector(pf *inventory.Platform, conn shared.Connection) (bool
 			pf.Version = currentBuildNumber.Value.String
 		}
 	}
+
+	correctForWindows11(pf)
+
 	return true, nil
+}
+
+// correctForWindows11 replaces the windows 10 title with windows 11 if the build number is greater than 22000
+// See https://techcommunity.microsoft.com/discussions/windows-management/windows-10-21h2-and-windows-11-21h2-both-show-up-as-2009-release/2994441
+func correctForWindows11(pf *inventory.Platform) {
+	if strings.Contains(pf.Title, "10") {
+		buildNumber, err := strconv.Atoi(pf.Version)
+		if err != nil {
+			return
+		}
+		if buildNumber >= 22000 {
+			// replace 10 with 11
+			pf.Title = strings.Replace(pf.Title, "10", "11", 1)
+		}
+	}
 }
