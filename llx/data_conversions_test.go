b87f9db2f7e58b1a3b23b4d30a27314bc1ae7e12
Dominik Richter <dominik.richter@gmail.com>
2025-03-07 13:42:54 -0800

    🧹 split up test for data conversion
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- llx/data_conversions_test.go
+++ llx/data_conversions_test.go
@@ -21,29 +21,29 @@ func TestVersion_Conversions(t *testing.T) {
 		require.NoError(t, rd.Error, "no error converting version to raw data")
 		require.Equal(t, "1.2.3", rd.Value, "version to raw data is the same")
 	})
+}
 
-	t.Run("raw and result conversions", func(t *testing.T) {
-		tests := []struct {
-			raw *llx.RawData
-		}{
-			{raw: llx.VersionData("1.2.3")},
-			{raw: llx.IPData(llx.ParseIP("192.168.0.1/27"))},
-		}
-		for i := range tests {
-			cur := tests[i]
-			t.Run(cur.raw.String(), func(t *testing.T) {
-				require.NotContains(t, cur.raw.String(), llx.UNKNOWN_VALUE, fmt.Sprintf("implement String() for %#v", cur.raw))
+func TestResultRawConversions(t *testing.T) {
+	tests := []struct {
+		raw *llx.RawData
+	}{
+		{raw: llx.VersionData("1.2.3")},
+		{raw: llx.IPData(llx.ParseIP("192.168.0.1/27"))},
+	}
+	for i := range tests {
+		cur := tests[i]
+		t.Run(cur.raw.String(), func(t *testing.T) {
+			require.NotContains(t, cur.raw.String(), llx.UNKNOWN_VALUE, fmt.Sprintf("implement String() for %#v", cur.raw))
 
-				res := cur.raw.Result()
-				require.NotNil(t, res)
-				raw := res.RawData()
-				require.NotNil(t, raw)
-				assert.Equal(t, cur.raw.Type, raw.Type)
-				assert.Equal(t, cur.raw.Value, raw.Value)
-				res2 := raw.Result()
-				require.NotNil(t, res2)
-				assert.Equal(t, res, res2)
-			})
-		}
-	})
+			res := cur.raw.Result()
+			require.NotNil(t, res)
+			raw := res.RawData()
+			require.NotNil(t, raw)
+			assert.Equal(t, cur.raw.Type, raw.Type)
+			assert.Equal(t, cur.raw.Value, raw.Value)
+			res2 := raw.Result()
+			require.NotNil(t, res2)
+			assert.Equal(t, res, res2)
+		})
+	}
 }
