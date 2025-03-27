5810ba8b0397146a86d5811858d49c5a89ca5eae
Salim Afiune Maya <afiune@mondoo.com>
2025-03-27 08:43:57 -0700

    🎉 os-11.3.41 (#5385)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "11.3.40",
+	Version: "11.3.41",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
