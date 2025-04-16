b18d6d2655ea942555df6e7bd7a6f5b247041440
Salim Afiune Maya <afiune@mondoo.com>
2025-04-15 23:49:21 -0700

    ✨ cache aws connection and upstream client
    This is an attempt to reduce the load of the AWS provider. We are going
    to cache the upstream client which uses ranger under the hood and
    creates goroutines to fulfill the requests. This is not a problem for a
    few runtimes, but when we do thousands of them, we really don't need to
    create that many clients.
    
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

---
+++ providers/aws/connection/shared/shared.go
@@ -0,0 +1,17 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package shared
+
+import (
+	"go.mondoo.com/cnquery/v11/providers/os/connection/shared"
+)
+
+type Connection interface {
+	shared.Connection
+
+	// Used to avoid verifying a client with the same options more than once
+	Verify() (accountID string, err error)
+	Hash() uint64
+	SetAccountId(string)
+}
