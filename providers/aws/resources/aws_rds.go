90dde7c613ca12d91cda86abbb272974bf7c8328
Tim Smith <tsmith84@gmail.com>
2025-04-16 22:17:02 -0700

    Remove deprecated aws.rds dbclusters and dbinstances
    We replaced this with just instances and clusters
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- providers/aws/resources/aws_rds.go
+++ providers/aws/resources/aws_rds.go
@@ -30,11 +30,6 @@ func (a *mqlAwsRds) id() (string, error) {
 	return "aws.rds", nil
 }
 
-// Deprecated: use instances() instead
-func (a *mqlAwsRds) dbInstances() ([]interface{}, error) {
-	return a.instances()
-}
-
 // instances returns all RDS instances
 func (a *mqlAwsRds) instances() ([]interface{}, error) {
 	conn := a.MqlRuntime.Connection.(*connection.AwsConnection)
@@ -230,7 +225,7 @@ func (a *mqlAwsRds) getDbInstances(conn *connection.AwsConnection) []*jobpool.Jo
 	return tasks
 }
 
-// pendingMaintenanceActions returns all pending maintaince actions for all RDS instances
+// pendingMaintenanceActions returns all pending maintenance actions for all RDS instances
 func (a *mqlAwsRds) allPendingMaintenanceActions() ([]interface{}, error) {
 	conn := a.MqlRuntime.Connection.(*connection.AwsConnection)
 	res := []interface{}{}
@@ -493,7 +488,7 @@ func initAwsRdsDbcluster(runtime *plugin.Runtime, args map[string]*llx.RawData) 
 	}
 
 	rds := obj.(*mqlAwsRds)
-	rawResources := rds.GetDbClusters()
+	rawResources := rds.GetClusters()
 
 	arnVal := args["arn"].Value.(string)
 	for _, rawResource := range rawResources.Data {
@@ -528,7 +523,7 @@ func initAwsRdsDbinstance(runtime *plugin.Runtime, args map[string]*llx.RawData)
 	}
 
 	rds := obj.(*mqlAwsRds)
-	rawResources := rds.GetDbInstances()
+	rawResources := rds.GetInstances()
 
 	arnVal := args["arn"].Value.(string)
 	for _, rawResource := range rawResources.Data {
@@ -660,11 +655,6 @@ func rdsTagsToMap(tags []rds_types.Tag) map[string]interface{} {
 	}
 
 	return tagsMap
-}
-
-// Deprecated: use clusters() instead
-func (a *mqlAwsRds) dbClusters() ([]interface{}, error) {
-	return a.clusters()
 }
 
 // clusters returns all RDS clusters
