336b40a861f22d3b4d7407825678fea01579f059
Tim Smith <tsmith84@gmail.com>
2025-04-17 19:24:40 -0700

    Remove deprecated displayName in microsoft.application and microsoft.tenant
    Use name field instead
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- providers/ms365/resources/tenant.go
+++ providers/ms365/resources/tenant.go
@@ -5,6 +5,7 @@ package resources
 
 import (
 	"context"
+
 	"github.com/microsoftgraph/msgraph-sdk-go/directory"
 	"github.com/microsoftgraph/msgraph-sdk-go/models"
 	"github.com/microsoftgraph/msgraph-sdk-go/organization"
@@ -102,7 +103,6 @@ func newMicrosoftTenant(runtime *plugin.Runtime, org models.Organizationable) (*
 			"assignedPlans":         llx.DictData(assignedPlans),
 			"createdDateTime":       llx.TimeDataPtr(org.GetCreatedDateTime()), // deprecated
 			"name":                  llx.StringDataPtr(org.GetDisplayName()),
-			"displayName":           llx.StringDataPtr(org.GetDisplayName()), // deprecated
 			"verifiedDomains":       llx.DictData(verifiedDomains),
 			"onPremisesSyncEnabled": llx.BoolDataPtr(org.GetOnPremisesSyncEnabled()),
 			"createdAt":             llx.TimeDataPtr(org.GetCreatedDateTime()),
