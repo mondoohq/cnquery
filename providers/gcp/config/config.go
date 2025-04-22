15a82d8d3f6073da7e57fb273c2f010d2c376637
Mondoo Tools <tools@mondoo.com>
2025-04-22 14:42:15 +0000

    ✨ Update providers 20250422

--- providers/gcp/config/config.go
+++ providers/gcp/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gcp",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gcp",
-	Version: "11.0.83",
+	Version: "11.0.84",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(gcpinstancesnapshot.SnapshotConnectionType),
