dcf8fe5e768fe90e84eba91579b0370b16145af5
Salim Afiune Maya <afiune@mondoo.com>
2025-04-22 13:37:27 -0700

    📈 Add prometheus and grafana metrics
    When debugging `cnquery`, you can monitor and profile memory and CPU usage
    using [Prometheus](https://prometheus.io/) and [Grafana](https://grafana.com/).
    The setup provides visibility into application performance metrics and allows
    us to diagnose bottlenecks, memory leaks, and high CPU usage.
    
    **How it works?**
    
    * Prometheus: Scrapes and stores time series metrics from your application.
    * Grafana: Visualizes the metrics and allows creating dashboards and alerts.
    * `cnquery` in `DEBUG` mode: Exposes basic metrics
    
    **Setup**
    
    1. Install `prometheus` from https://prometheus.io/download/ (macOS: `brew install prometheus`)
    1. Start both, Prometheus and Grafana with `make metrics/start`
    1. (First time only) Create a Grafana Dashboard
        a. Open Grafana at http://localhost:3000
        b. Add Prometheus as a data source (URL: `http://host.docker.internal:9009`)
        c. Use an existing Go profiling dashbord from [Grafana](https://grafana.com/grafana/dashboards/) dashboards e.g. [10826](https://grafana.com/grafana/dashboards/10826-go-metrics/)
    1. Run `cnquery` with `DEBUG=1` e.g. `DEBUG=1 cnquery scan local`
    
    You should start seeing data in Grafana!
    
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- apps/cnquery/cnquery.go
+++ apps/cnquery/cnquery.go
@@ -6,10 +6,12 @@ package main
 import (
 	"go.mondoo.com/cnquery/v11"
 	"go.mondoo.com/cnquery/v11/apps/cnquery/cmd"
+	"go.mondoo.com/cnquery/v11/metrics"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/upstream/health"
 )
 
 func main() {
 	defer health.ReportPanic("cnquery", cnquery.Version, cnquery.Build)
+	go metrics.Start()
 	cmd.Execute()
 }
