e0412cc2377cf774b24e0fd9abf191236caccb43
Mondoo Tools <tools@mondoo.com>
2025-05-06 19:48:13 +0000

    ✨ Update providers 20250506

--- providers/snowflake/config/config.go
+++ providers/snowflake/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "snowflake",
 	ID:              "go.mondoo.com/cnquery/v11/providers/snowflake",
-	Version:         "11.0.62",
+	Version:         "11.0.63",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
