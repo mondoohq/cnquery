73a4e3cc719f5f1f51e708a6fb3c9ae23c12cd66
Tim Smith <tsmith84@gmail.com>
2025-05-09 22:14:59 -0700

    Add a resource for parsing journald
    Replaces a query like this:
    
    ```
    ["/etc/systemd/journald.conf"].where(file(_).exists) {
      file(_) {
        parse.ini("/etc/systemd/journald.conf").sections["Journal"]["ForwardToSyslog"].downcase == "yes"
      }
    }
    ```
    
    with this
    
    ```
    journald.config.params.ForwardToSyslog == "yes"
    ```
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- providers/os/resources/os.lr.go
+++ providers/os/resources/os.lr.go
@@ -238,6 +238,10 @@ func init() {
 			Init: initAuditdConfig,
 			Create: createAuditdConfig,
 		},
+		"journald.config": {
+			Init: initJournaldConfig,
+			Create: createJournaldConfig,
+		},
 		"service": {
 			Init: initService,
 			Create: createService,
@@ -1364,6 +1368,12 @@ var getDataFields = map[string]func(r plugin.Resource) *plugin.DataRes{
 	},
 	"auditd.config.params": func(r plugin.Resource) *plugin.DataRes {
 		return (r.(*mqlAuditdConfig).GetParams()).ToDataRes(types.Map(types.String, types.String))
+	},
+	"journald.config.file": func(r plugin.Resource) *plugin.DataRes {
+		return (r.(*mqlJournaldConfig).GetFile()).ToDataRes(types.Resource("file"))
+	},
+	"journald.config.params": func(r plugin.Resource) *plugin.DataRes {
+		return (r.(*mqlJournaldConfig).GetParams()).ToDataRes(types.Map(types.String, types.String))
 	},
 	"service.name": func(r plugin.Resource) *plugin.DataRes {
 		return (r.(*mqlService).GetName()).ToDataRes(types.String)
@@ -3766,6 +3776,18 @@ var setDataFields = map[string]func(r plugin.Resource, v *llx.RawData) bool {
 	},
 	"auditd.config.params": func(r plugin.Resource, v *llx.RawData) (ok bool) {
 		r.(*mqlAuditdConfig).Params, ok = plugin.RawToTValue[map[string]interface{}](v.Value, v.Error)
+		return
+	},
+	"journald.config.__id": func(r plugin.Resource, v *llx.RawData) (ok bool) {
+			r.(*mqlJournaldConfig).__id, ok = v.Value.(string)
+			return
+		},
+	"journald.config.file": func(r plugin.Resource, v *llx.RawData) (ok bool) {
+		r.(*mqlJournaldConfig).File, ok = plugin.RawToTValue[*mqlFile](v.Value, v.Error)
+		return
+	},
+	"journald.config.params": func(r plugin.Resource, v *llx.RawData) (ok bool) {
+		r.(*mqlJournaldConfig).Params, ok = plugin.RawToTValue[map[string]interface{}](v.Value, v.Error)
 		return
 	},
 	"service.__id": func(r plugin.Resource, v *llx.RawData) (ok bool) {
@@ -10136,6 +10158,79 @@ func (c *mqlAuditdConfig) GetFile() *plugin.TValue[*mqlFile] {
 }
 
 func (c *mqlAuditdConfig) GetParams() *plugin.TValue[map[string]interface{}] {
+	return plugin.GetOrCompute[map[string]interface{}](&c.Params, func() (map[string]interface{}, error) {
+		vargFile := c.GetFile()
+		if vargFile.Error != nil {
+			return nil, vargFile.Error
+		}
+
+		return c.params(vargFile.Data)
+	})
+}
+
+// mqlJournaldConfig for the journald.config resource
+type mqlJournaldConfig struct {
+	MqlRuntime *plugin.Runtime
+	__id string
+	mqlJournaldConfigInternal
+	File plugin.TValue[*mqlFile]
+	Params plugin.TValue[map[string]interface{}]
+}
+
+// createJournaldConfig creates a new instance of this resource
+func createJournaldConfig(runtime *plugin.Runtime, args map[string]*llx.RawData) (plugin.Resource, error) {
+	res := &mqlJournaldConfig{
+		MqlRuntime: runtime,
+	}
+
+	err := SetAllData(res, args)
+	if err != nil {
+		return res, err
+	}
+
+	if res.__id == "" {
+	res.__id, err = res.id()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if runtime.HasRecording {
+		args, err = runtime.ResourceFromRecording("journald.config", res.__id)
+		if err != nil || args == nil {
+			return res, err
+		}
+		return res, SetAllData(res, args)
+	}
+
+	return res, nil
+}
+
+func (c *mqlJournaldConfig) MqlName() string {
+	return "journald.config"
+}
+
+func (c *mqlJournaldConfig) MqlID() string {
+	return c.__id
+}
+
+func (c *mqlJournaldConfig) GetFile() *plugin.TValue[*mqlFile] {
+	return plugin.GetOrCompute[*mqlFile](&c.File, func() (*mqlFile, error) {
+		if c.MqlRuntime.HasRecording {
+			d, err := c.MqlRuntime.FieldResourceFromRecording("journald.config", c.__id, "file")
+			if err != nil {
+				return nil, err
+			}
+			if d != nil {
+				return d.Value.(*mqlFile), nil
+			}
+		}
+
+		return c.file()
+	})
+}
+
+func (c *mqlJournaldConfig) GetParams() *plugin.TValue[map[string]interface{}] {
 	return plugin.GetOrCompute[map[string]interface{}](&c.Params, func() (map[string]interface{}, error) {
 		vargFile := c.GetFile()
 		if vargFile.Error != nil {
