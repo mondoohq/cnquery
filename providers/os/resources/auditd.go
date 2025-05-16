0350c06c0123ba5c2640708d26c89723ae9a65d0
Dominik Richter <dominik.richter@gmail.com>
2025-05-16 03:09:05 -0700

    ⭐ auditd.rules resource
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- providers/os/resources/auditd.go
+++ providers/os/resources/auditd.go
@@ -10,10 +10,13 @@ import (
 	"slices"
 	"strings"
 	"sync"
+	"unicode"
 
+	"go.mondoo.com/cnquery/v11/checksums"
 	"go.mondoo.com/cnquery/v11/llx"
 	"go.mondoo.com/cnquery/v11/providers-sdk/v1/plugin"
 	"go.mondoo.com/cnquery/v11/providers/os/resources/parsers"
+	"go.mondoo.com/cnquery/v11/types"
 	"go.mondoo.com/cnquery/v11/utils/multierr"
 )
 
@@ -134,4 +137,259 @@ var auditdDowncaseKeywords = []string{
 	"use_libwrap",
 	"enable_krb5",
 	"overflow_action",
+}
+
+type mqlAuditdRulesInternal struct {
+	lock      sync.Mutex
+	loaded    bool
+	loadError error
+}
+
+const defaultAuditdRules = "/etc/audit/rules.d"
+
+func (s *mqlAuditdRules) id() (string, error) {
+	return s.Path.Data, nil
+}
+
+func (s *mqlAuditdRules) path() (string, error) {
+	return defaultAuditdRules, nil
+}
+
+func (s *mqlAuditdRules) load(path string) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if s.loaded {
+		return s.loadError
+	}
+
+	if path == "" {
+		return errors.New("the path must be non-empty to parse auditd rules")
+	}
+
+	files, err := getSortedPathFiles(s.MqlRuntime, path)
+	if err != nil {
+		s.Controls = plugin.TValue[[]any]{State: plugin.StateIsSet, Error: err}
+		s.Files = plugin.TValue[[]any]{State: plugin.StateIsSet, Error: err}
+		s.Syscalls = plugin.TValue[[]any]{State: plugin.StateIsSet, Error: err}
+		return err
+	}
+
+	var errors multierr.Errors
+	for i := range files {
+		file := files[i].(*mqlFile)
+
+		bn := file.GetBasename()
+		if !strings.HasSuffix(bn.Data, ".rules") {
+			continue
+		}
+
+		content := file.GetContent()
+		if content.Error != nil {
+			return content.Error
+		}
+
+		s.parse(content.Data, &errors)
+	}
+
+	s.loadError = errors.Deduplicate()
+	s.loaded = true
+	return s.loadError
+}
+
+func parseKeyVal(line string) (string, string, int) {
+	runes := []rune(line)
+	i := 0
+
+	// invalid prefix
+	if line[i] != '-' {
+		for ; i < len(runes); i++ {
+			if unicode.IsSpace(runes[i]) {
+				break
+			}
+		}
+		for ; i < len(runes); i++ {
+			if !unicode.IsSpace(runes[i]) {
+				break
+			}
+		}
+		return "", "", i
+	}
+
+	if len(line) < 2 {
+		return "", "", len(line)
+	}
+	if line[1] == '-' {
+		i = 2
+	} else {
+		i = 1
+	}
+
+	for ; i < len(runes); i++ {
+		if unicode.IsSpace(runes[i]) {
+			break
+		}
+	}
+	if i == len(runes) {
+		return line, "", i
+	}
+	keyend := i
+
+	for ; i < len(runes); i++ {
+		if !unicode.IsSpace(runes[i]) {
+			break
+		}
+	}
+	valstart := i
+	for ; i < len(runes); i++ {
+		if unicode.IsSpace(runes[i]) {
+			break
+		}
+	}
+	valend := i
+
+	for ; i < len(runes); i++ {
+		if !unicode.IsSpace(runes[i]) {
+			break
+		}
+	}
+
+	return line[:keyend], line[valstart:valend], i
+}
+
+func (s *mqlAuditdRules) parse(content string, errors *multierr.Errors) {
+	s.Syscalls.State = plugin.StateIsSet
+	s.Files.State = plugin.StateIsSet
+	s.Controls.State = plugin.StateIsSet
+
+	lines := strings.Split(content, "\n")
+	for _, rawline := range lines {
+		line := strings.TrimSpace(rawline)
+		if line == "" || line[0] == '#' {
+			continue
+		}
+
+		resourceName := "auditd.rule.control"
+		args := map[string]*llx.RawData{}
+		fields := []any{}
+		syscalls := []any{}
+		other := [][2]string{}
+
+		for line != "" {
+			k, v, idx := parseKeyVal(line)
+			line = line[idx:]
+
+			switch k {
+			case "-a":
+				resourceName = "auditd.rule.syscall"
+				arr := strings.SplitN(v, ",", 2)
+				args["action"] = llx.StringData(arr[0])
+				args["list"] = llx.StringData(arr[1])
+
+			case "-F":
+				fields = append(fields, v)
+
+			case "-w":
+				resourceName = "auditd.rule.file"
+				args["path"] = llx.StringData(v)
+
+			case "-k":
+				args["keyname"] = llx.StringData(v)
+
+			case "-p":
+				args["permissions"] = llx.StringData(v)
+
+			case "-S":
+				syscalls = append(syscalls, v)
+
+			default:
+				other = append(other, [2]string{k, v})
+			}
+		}
+
+		switch resourceName {
+		case "auditd.rule.file":
+			if _, ok := args["keyname"]; !ok {
+				args["keyname"] = llx.StringData("")
+			}
+
+			r, err := CreateResource(s.MqlRuntime, resourceName, args)
+			if err != nil {
+				errors.Add(err)
+				continue
+			}
+			s.Files.Data = append(s.Files.Data, r)
+
+		case "auditd.rule.syscall":
+			args["syscalls"] = llx.ArrayData(syscalls, types.String)
+			args["fieldEntries"] = llx.ArrayData(fields, types.String)
+			if _, ok := args["keyname"]; !ok {
+				args["keyname"] = llx.StringData("")
+			}
+
+			r, err := CreateResource(s.MqlRuntime, resourceName, args)
+			if err != nil {
+				errors.Add(err)
+				continue
+			}
+			s.Syscalls.Data = append(s.Syscalls.Data, r)
+
+		default:
+			for io := range other {
+				r, err := CreateResource(s.MqlRuntime, resourceName, map[string]*llx.RawData{
+					"flag":  llx.StringData(other[io][0]),
+					"value": llx.StringData(other[io][1]),
+				})
+				if err != nil {
+					errors.Add(err)
+					continue
+				}
+				s.Controls.Data = append(s.Controls.Data, r)
+			}
+		}
+	}
+}
+
+func (s *mqlAuditdRules) controls(path string) ([]any, error) {
+	return nil, s.load(path)
+}
+
+func (s *mqlAuditdRules) files(path string) ([]any, error) {
+	return nil, s.load(path)
+}
+
+func (s *mqlAuditdRules) syscalls(path string) ([]any, error) {
+	return nil, s.load(path)
+}
+
+func (s *mqlAuditdRuleFile) id() (string, error) {
+	var f checksums.Fast
+	return f.
+		Add(s.Path.Data).
+		Add(s.Permissions.Data).
+		Add(s.Keyname.Data).
+		String(), nil
+}
+
+func (s *mqlAuditdRuleControl) id() (string, error) {
+	var f checksums.Fast
+	return f.
+		Add(s.Flag.Data).
+		Add(s.Value.Data).
+		String(), nil
+}
+
+func (s *mqlAuditdRuleSyscall) id() (string, error) {
+	var f checksums.Fast
+	f = f.
+		Add(s.Action.Data).
+		Add(s.List.Data).
+		Add(s.Keyname.Data)
+	for i := range s.Syscalls.Data {
+		f = f.Add(s.Syscalls.Data[i].(string))
+	}
+	for i := range s.FieldEntries.Data {
+		f = f.Add(s.FieldEntries.Data[i].(string))
+	}
+
+	return f.String(), nil
 }
