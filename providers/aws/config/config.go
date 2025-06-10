ed5b157f158db96a8c1169badccf53870eb02ac8
Mondoo Tools <tools@mondoo.com>
2025-06-10 08:59:22 +0000

    ✨ Update providers 20250610

--- providers/aws/config/config.go
+++ providers/aws/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "aws",
 	ID:              "go.mondoo.com/cnquery/v9/providers/aws",
-	Version:         "11.5.51",
+	Version:         "11.5.52",
 	ConnectionTypes: []string{provider.DefaultConnectionType, string(awsec2ebsconn.EBSConnectionType)},
 	Connectors: []plugin.Connector{
 		{
--- providers/slack/config/config.go
+++ providers/slack/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "slack",
 	ID:              "go.mondoo.com/cnquery/v9/providers/slack",
-	Version:         "11.0.86",
+	Version:         "11.0.87",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
