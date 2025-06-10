c7d2606f9ae657a0f1d5329b85b3f1be78667073
Mondoo Tools <tools@mondoo.com>
2025-06-10 13:29:34 +0000

    ✨ Update providers 20250610

--- providers/tailscale/config/config.go
+++ providers/tailscale/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "tailscale",
 	ID:              "go.mondoo.com/cnquery/v11/providers/tailscale",
-	Version:         "11.0.26",
+	Version:         "11.0.27",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
