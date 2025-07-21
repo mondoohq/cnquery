b27b999913cb1c6d9b4542824b6b858432f1132c
Mondoo Tools <tools@mondoo.com>
2025-07-21 14:57:47 +0000

    ✨ Update providers 20250721

--- providers/opcua/config/config.go
+++ providers/opcua/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "opcua",
 	ID:              "go.mondoo.com/cnquery/v9/providers/opcua",
-	Version:         "11.0.92",
+	Version:         "11.0.93",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
