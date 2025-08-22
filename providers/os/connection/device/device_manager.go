806ed5a1a74f2c34bfaa960686f2df2749b84faa
Preslav Gerchev <preslav@mondoo.com>
2025-08-22 14:51:58 +0300

    Update providers/os/connection/device/device_manager.go
    Co-authored-by: Mikita Iwanowski <info@slnt-opp.xyz>

--- providers/os/connection/device/device_manager.go
+++ providers/os/connection/device/device_manager.go
@@ -12,7 +12,7 @@ type DeviceManager interface {
 	Name() string
 	// IdentifyMountTargets returns a list of partitions that match the given options and can be mounted
 	IdentifyMountTargets(opts map[string]string) ([]*snapshot.Partition, error)
-	// Mounts the partition and returns the directories it was mounted to
+	// Mounts partitions and returns the directories they were mounted to
 	Mount(partitions []*snapshot.Partition) ([]*snapshot.MountedPartition, error)
 	// UnmountAndClose unmounts the partitions from the specified dirs and closes the device manager
 	UnmountAndClose()
