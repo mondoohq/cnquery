2e8ce881b06893c9f83128b1155e92db311cefb2
Casey Clayton <claytoncasey01@gmail.com>
2025-08-25 10:57:15 -0400

    🎉 vsphere-11.0.101 (#5865)
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/vsphere/config/config.go
+++ providers/vsphere/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vsphere",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vsphere",
-	Version:         "11.0.100",
+	Version:         "11.0.101",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
