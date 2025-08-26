f6a4fb1bbf36a01e2d5022eb1bf739a4d8b6eb8d
Mondoo Tools <tools@mondoo.com>
2025-08-26 22:07:48 +0000

    ✨ Update providers 20250826

--- providers/ms365/config/config.go
+++ providers/ms365/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ms365",
 	ID:              "go.mondoo.com/cnquery/v9/providers/ms365",
-	Version:         "11.1.55",
+	Version:         "11.1.56",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
