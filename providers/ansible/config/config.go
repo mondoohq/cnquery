d35155e4c32f45f432827842a1a84c9ef5b4e2a1
Mondoo Tools <tools@mondoo.com>
2025-09-01 17:54:15 +0000

    ✨ Update providers 20250901

--- providers/ansible/config/config.go
+++ providers/ansible/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ansible",
 	ID:              "go.mondoo.com/cnquery/v12/providers/ansible",
-	Version:         "11.0.89",
+	Version:         "11.0.90",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
