bbada67d74abd8f3293b59c9775a899e13ec5eb0
Dominik Richter <dominik.richter@gmail.com>
2025-09-01 21:04:06 -0400

    ✨ extract property handler for bundles/packs + update examples (#5900)

--- explorer/resolved_pack.go
+++ explorer/resolved_pack.go
@@ -3,6 +3,8 @@
 
 package explorer
 
+// HasFeature checks if the resolved policy indicates if certain features
+// should be supported on the execution and collection of it
 func (r *ResolvedPack) HasFeature(feature ServerFeature) bool {
 	for _, f := range r.Features {
 		if f == feature {
