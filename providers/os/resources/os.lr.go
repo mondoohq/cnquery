81fd236db5f463a27fc3a4b70ea2522ec423366b
Tim Smith <tsmith84@gmail.com>
2025-10-02 11:04:12 -0700

    Remove sshd.config.content
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- providers/os/resources/os.lr.go
+++ providers/os/resources/os.lr.go
@@ -1471,9 +1471,6 @@ var getDataFields = map[string]func(r plugin.Resource) *plugin.DataRes{
 	"sshd.config.files": func(r plugin.Resource) *plugin.DataRes {
 		return (r.(*mqlSshdConfig).GetFiles()).ToDataRes(types.Array(types.Resource("file")))
 	},
-	"sshd.config.content": func(r plugin.Resource) *plugin.DataRes {
-		return (r.(*mqlSshdConfig).GetContent()).ToDataRes(types.String)
-	},
 	"sshd.config.params": func(r plugin.Resource) *plugin.DataRes {
 		return (r.(*mqlSshdConfig).GetParams()).ToDataRes(types.Map(types.String, types.String))
 	},
@@ -3875,10 +3872,6 @@ var setDataFields = map[string]func(r plugin.Resource, v *llx.RawData) bool{
 	},
 	"sshd.config.files": func(r plugin.Resource, v *llx.RawData) (ok bool) {
 		r.(*mqlSshdConfig).Files, ok = plugin.RawToTValue[[]any](v.Value, v.Error)
-		return
-	},
-	"sshd.config.content": func(r plugin.Resource, v *llx.RawData) (ok bool) {
-		r.(*mqlSshdConfig).Content, ok = plugin.RawToTValue[string](v.Value, v.Error)
 		return
 	},
 	"sshd.config.params": func(r plugin.Resource, v *llx.RawData) (ok bool) {
@@ -9991,7 +9984,6 @@ type mqlSshdConfig struct {
 	mqlSshdConfigInternal
 	File            plugin.TValue[*mqlFile]
 	Files           plugin.TValue[[]any]
-	Content         plugin.TValue[string]
 	Params          plugin.TValue[map[string]any]
 	Blocks          plugin.TValue[[]any]
 	Ciphers         plugin.TValue[[]any]
@@ -10072,17 +10064,6 @@ func (c *mqlSshdConfig) GetFiles() *plugin.TValue[[]any] {
 		}
 
 		return c.files(vargFile.Data)
-	})
-}
-
-func (c *mqlSshdConfig) GetContent() *plugin.TValue[string] {
-	return plugin.GetOrCompute[string](&c.Content, func() (string, error) {
-		vargFile := c.GetFile()
-		if vargFile.Error != nil {
-			return "", vargFile.Error
-		}
-
-		return c.content(vargFile.Data)
 	})
 }
 
