0d6093e68be69e85b17630aa753019881a0ede9d
Tim Smith <tsmith84@gmail.com>
2025-10-02 22:50:39 -0700

    Detect packages on Mageia Linux
    RPM based distro. We already checked the right path. We just needed to
    know to look for RPMs
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- providers/os/resources/packages/rpm_packages.go
+++ providers/os/resources/packages/rpm_packages.go
@@ -237,7 +237,7 @@ func (rpm *RpmPkgManager) staticList() ([]Package, error) {
 		"/usr/lib/sysimage/rpm/Packages",     // used on opensuse container
 		"/usr/lib/sysimage/rpm/Packages.db",  // used on SLES bci-base container
 		"/usr/lib/sysimage/rpm/rpmdb.sqlite", // used on fedora 36+ and photon4
-		"/var/lib/rpm/rpmdb.sqlite",          // used on fedora 33-35
+		"/var/lib/rpm/rpmdb.sqlite",          // used on fedora 33-35 and mageia
 		"/var/lib/rpm/Packages",              // used on fedora 32
 		"/var/lib/rpm/Packages.db",           // used on openeuler
 	}
