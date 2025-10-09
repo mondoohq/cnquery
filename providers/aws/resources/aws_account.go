b60a09fb0515beaaff3fa781035d8990a2fc5af9
Manuel Weber <112621871+mm-weber@users.noreply.github.com>
2025-10-09 18:35:11 +0200

    :sparkles: extend aws.account. fields (#5987)
    * extend aws.account. fields
    
    Signed-off-by: Manuel Weber <manuel@mondoo.com>
    
    * new mqlr compiler res
    
    * use constants from *lr.go
    
    ---------
    
    Signed-off-by: Manuel Weber <manuel@mondoo.com>
    Co-authored-by: Dimitar ganev <dimitar@mondoo.com>

--- providers/aws/resources/aws_account.go
+++ providers/aws/resources/aws_account.go
@@ -7,6 +7,9 @@ import (
 	"context"
 	"errors"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/account"
+	"github.com/aws/aws-sdk-go-v2/service/account/types"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/aws/aws-sdk-go-v2/service/organizations"
 	"go.mondoo.com/cnquery/v12/llx"
@@ -41,7 +44,7 @@ func (a *mqlAwsAccount) organization() (*mqlAwsOrganization, error) {
 	if err != nil {
 		return nil, err
 	}
-	res, err := CreateResource(a.MqlRuntime, "aws.organization",
+	res, err := CreateResource(a.MqlRuntime, ResourceAwsOrganization,
 		map[string]*llx.RawData{
 			"arn":                llx.StringDataPtr(org.Organization.Arn),
 			"featureSet":         llx.StringData(string(org.Organization.FeatureSet)),
@@ -62,7 +65,7 @@ func (a *mqlAwsOrganization) accounts() ([]any, error) {
 	accounts := []any{}
 	for i := range orgAccounts.Accounts {
 		account := orgAccounts.Accounts[i]
-		res, err := CreateResource(a.MqlRuntime, "aws.account",
+		res, err := CreateResource(a.MqlRuntime, ResourceAwsAccount,
 			map[string]*llx.RawData{
 				"id": llx.StringDataPtr(account.Id),
 			})
@@ -102,6 +105,171 @@ func (c *mqlAwsAccount) tags() (map[string]any, error) {
 	return tags, nil
 }
 
+func (a *mqlAwsAccount) contactInformation() (map[string]any, error) {
+	conn := a.MqlRuntime.Connection.(*connection.AwsConnection)
+	client := conn.Account("") // no region for account service, use configured region
+
+	resp, err := client.GetContactInformation(context.TODO(), &account.GetContactInformationInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert the contact information to map[string]any
+	result := make(map[string]any)
+	if resp.ContactInformation != nil {
+		if resp.ContactInformation.AddressLine1 != nil {
+			result["addressLine1"] = *resp.ContactInformation.AddressLine1
+		}
+		if resp.ContactInformation.AddressLine2 != nil {
+			result["addressLine2"] = *resp.ContactInformation.AddressLine2
+		}
+		if resp.ContactInformation.AddressLine3 != nil {
+			result["addressLine3"] = *resp.ContactInformation.AddressLine3
+		}
+		if resp.ContactInformation.City != nil {
+			result["city"] = *resp.ContactInformation.City
+		}
+		if resp.ContactInformation.CompanyName != nil {
+			result["companyName"] = *resp.ContactInformation.CompanyName
+		}
+		if resp.ContactInformation.CountryCode != nil {
+			result["countryCode"] = *resp.ContactInformation.CountryCode
+		}
+		if resp.ContactInformation.DistrictOrCounty != nil {
+			result["districtOrCounty"] = *resp.ContactInformation.DistrictOrCounty
+		}
+		if resp.ContactInformation.FullName != nil {
+			result["fullName"] = *resp.ContactInformation.FullName
+		}
+		if resp.ContactInformation.PhoneNumber != nil {
+			result["phoneNumber"] = *resp.ContactInformation.PhoneNumber
+		}
+		if resp.ContactInformation.PostalCode != nil {
+			result["postalCode"] = *resp.ContactInformation.PostalCode
+		}
+		if resp.ContactInformation.StateOrRegion != nil {
+			result["stateOrRegion"] = *resp.ContactInformation.StateOrRegion
+		}
+		if resp.ContactInformation.WebsiteUrl != nil {
+			result["websiteUrl"] = *resp.ContactInformation.WebsiteUrl
+		}
+	}
+	return result, nil
+}
+
+func (a *mqlAwsAccount) alternateContacts() ([]any, error) {
+	conn := a.MqlRuntime.Connection.(*connection.AwsConnection)
+	client := conn.Account("") // no region for account service, use configured region
+
+	contactTypes := []types.AlternateContactType{
+		types.AlternateContactTypeBilling,
+		types.AlternateContactTypeOperations,
+		types.AlternateContactTypeSecurity,
+	}
+
+	var contacts []any
+
+	for _, cType := range contactTypes {
+		resp, err := client.GetAlternateContact(context.TODO(), &account.GetAlternateContactInput{
+			AlternateContactType: cType,
+		})
+
+		if err != nil {
+			// Check if contact not configured (ResourceNotFoundException)
+			var notFoundErr *types.ResourceNotFoundException
+			if errors.As(err, &notFoundErr) {
+				// Contact not configured - create resource with exists=false
+				contact, resErr := CreateResource(a.MqlRuntime, ResourceAwsAccountAlternateContact,
+					map[string]*llx.RawData{
+						"accountId":    llx.StringData(a.Id.Data),
+						"contactType":  llx.StringData(string(cType)),
+						"emailAddress": llx.StringData(""),
+						"name":         llx.StringData(""),
+						"phoneNumber":  llx.StringData(""),
+						"title":        llx.StringData(""),
+						"exists":       llx.BoolData(false),
+					})
+				if resErr != nil {
+					return nil, resErr
+				}
+				contacts = append(contacts, contact)
+				continue
+			}
+			// Other error - return it
+			return nil, err
+		}
+
+		// Contact is configured - create resource with data
+		contact, err := CreateResource(a.MqlRuntime, ResourceAwsAccountAlternateContact,
+			map[string]*llx.RawData{
+				"accountId":    llx.StringData(a.Id.Data),
+				"contactType":  llx.StringData(string(cType)),
+				"emailAddress": llx.StringData(aws.ToString(resp.AlternateContact.EmailAddress)),
+				"name":         llx.StringData(aws.ToString(resp.AlternateContact.Name)),
+				"phoneNumber":  llx.StringData(aws.ToString(resp.AlternateContact.PhoneNumber)),
+				"title":        llx.StringData(aws.ToString(resp.AlternateContact.Title)),
+				"exists":       llx.BoolData(true),
+			})
+		if err != nil {
+			return nil, err
+		}
+		contacts = append(contacts, contact)
+	}
+
+	return contacts, nil
+}
+
+func (a *mqlAwsAccount) securityContact() (*mqlAwsAccountAlternateContact, error) {
+	contacts, err := a.alternateContacts()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range contacts {
+		contact := c.(*mqlAwsAccountAlternateContact)
+		if contact.ContactType.Data == string(types.AlternateContactTypeSecurity) {
+			return contact, nil
+		}
+	}
+
+	// Should not happen as alternateContacts() always returns all three types
+	return nil, errors.New("security contact not found")
+}
+
+func (a *mqlAwsAccount) billingContact() (*mqlAwsAccountAlternateContact, error) {
+	contacts, err := a.alternateContacts()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range contacts {
+		contact := c.(*mqlAwsAccountAlternateContact)
+		if contact.ContactType.Data == string(types.AlternateContactTypeBilling) {
+			return contact, nil
+		}
+	}
+
+	// Should not happen as alternateContacts() always returns all three types
+	return nil, errors.New("billing contact not found")
+}
+
+func (a *mqlAwsAccount) operationsContact() (*mqlAwsAccountAlternateContact, error) {
+	contacts, err := a.alternateContacts()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range contacts {
+		contact := c.(*mqlAwsAccountAlternateContact)
+		if contact.ContactType.Data == string(types.AlternateContactTypeOperations) {
+			return contact, nil
+		}
+	}
+
+	// Should not happen as alternateContacts() always returns all three types
+	return nil, errors.New("operations contact not found")
+}
+
 func initAwsAccount(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[string]*llx.RawData, plugin.Resource, error) {
 	if len(args) >= 2 {
 		return args, nil, nil
@@ -115,7 +283,7 @@ func initAwsAccount(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[
 		return args, nil, errors.New("no account id specified")
 	}
 	id := args["id"].Value.(string)
-	res, err := CreateResource(runtime, "aws.account",
+	res, err := CreateResource(runtime, ResourceAwsAccount,
 		map[string]*llx.RawData{
 			"id": llx.StringData(id),
 		})
@@ -123,4 +291,12 @@ func initAwsAccount(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[
 		return nil, nil, err
 	}
 	return args, res, nil
+}
+
+func (a *mqlAwsAccountAlternateContact) id() (string, error) {
+	return a.AccountId.Data + "/" + a.ContactType.Data, nil
+}
+
+func initAwsAccountAlternateContact(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[string]*llx.RawData, plugin.Resource, error) {
+	return args, nil, nil
 }
