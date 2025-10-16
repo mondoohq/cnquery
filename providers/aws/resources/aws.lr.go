c9b90eb331c61c7bd2a3c42b1b31576bbc915999
Máté Bajusz <mate@mondoo.com>
2025-10-16 17:51:44 +0200

    fix snapshotWindow field

--- providers/aws/resources/aws.lr.go
+++ providers/aws/resources/aws.lr.go
@@ -3948,7 +3948,7 @@ var getDataFields = map[string]func(r plugin.Resource) *plugin.DataRes{
 		return (r.(*mqlAwsElasticacheCluster).GetSnapshotRetentionLimit()).ToDataRes(types.Int)
 	},
 	"aws.elasticache.cluster.snapshotWindow": func(r plugin.Resource) *plugin.DataRes {
-		return (r.(*mqlAwsElasticacheCluster).GetSnapshotWindow()).ToDataRes(types.Time)
+		return (r.(*mqlAwsElasticacheCluster).GetSnapshotWindow()).ToDataRes(types.String)
 	},
 	"aws.elasticache.cluster.transitEncryptionEnabled": func(r plugin.Resource) *plugin.DataRes {
 		return (r.(*mqlAwsElasticacheCluster).GetTransitEncryptionEnabled()).ToDataRes(types.Bool)
@@ -9909,7 +9909,7 @@ var setDataFields = map[string]func(r plugin.Resource, v *llx.RawData) bool{
 		return
 	},
 	"aws.elasticache.cluster.snapshotWindow": func(r plugin.Resource, v *llx.RawData) (ok bool) {
-		r.(*mqlAwsElasticacheCluster).SnapshotWindow, ok = plugin.RawToTValue[*time.Time](v.Value, v.Error)
+		r.(*mqlAwsElasticacheCluster).SnapshotWindow, ok = plugin.RawToTValue[string](v.Value, v.Error)
 		return
 	},
 	"aws.elasticache.cluster.transitEncryptionEnabled": func(r plugin.Resource, v *llx.RawData) (ok bool) {
@@ -24502,7 +24502,7 @@ type mqlAwsElasticacheCluster struct {
 	Region                    plugin.TValue[string]
 	SecurityGroups            plugin.TValue[[]any]
 	SnapshotRetentionLimit    plugin.TValue[int64]
-	SnapshotWindow            plugin.TValue[*time.Time]
+	SnapshotWindow            plugin.TValue[string]
 	TransitEncryptionEnabled  plugin.TValue[bool]
 	TransitEncryptionMode     plugin.TValue[string]
 }
@@ -24651,7 +24651,7 @@ func (c *mqlAwsElasticacheCluster) GetSnapshotRetentionLimit() *plugin.TValue[in
 	return &c.SnapshotRetentionLimit
 }
 
-func (c *mqlAwsElasticacheCluster) GetSnapshotWindow() *plugin.TValue[*time.Time] {
+func (c *mqlAwsElasticacheCluster) GetSnapshotWindow() *plugin.TValue[string] {
 	return &c.SnapshotWindow
 }
 
