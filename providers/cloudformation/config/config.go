e26f2e51bf998cb48cfe9198b0218918fde95faa
Mondoo Tools <tools@mondoo.com>
2025-10-21 09:58:13 +0000

    ✨ Update providers 20251021

--- providers/cloudformation/config/config.go
+++ providers/cloudformation/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "cloudformation",
 	ID:              "go.mondoo.com/cnquery/v12/providers/cloudformation",
-	Version:         "11.0.103",
+	Version:         "11.0.104",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/ms365/config/config.go
+++ providers/ms365/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ms365",
 	ID:              "go.mondoo.com/cnquery/v9/providers/ms365",
-	Version:         "11.1.66",
+	Version:         "11.1.67",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
