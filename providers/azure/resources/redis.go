b2d8f38326c4b7b2029d6baae78030442254bbd3
Máté Bajusz <mate@mondoo.com>
2025-10-22 12:04:07 +0200

    refactor + tests

--- providers/azure/resources/redis.go
+++ providers/azure/resources/redis.go
@@ -54,49 +54,16 @@ func (a *mqlAzureSubscriptionCacheService) redis() ([]any, error) {
 			if cache == nil {
 				continue
 			}
-			properties, err := convert.JsonToDict(cache)
-			if err != nil {
-				return nil, err
-			}
 
-			sku, err := convert.JsonToDict(cache.Properties.SKU)
+			rawData, err := createRedisInstanceRawData(cache)
 			if err != nil {
 				return nil, err
-			}
-			// publicNetworkAccess is an enum with possible values: Enabled, Disabled
-			var publicNetworkAccess *string
-			if cache.Properties.PublicNetworkAccess != nil {
-				val := string(*cache.Properties.PublicNetworkAccess)
-				publicNetworkAccess = &val
-			}
-			// provisioningState is an enum with possible values: Creating, Deleting, Failed, Succeeded, Updating
-			var provisioningState *string
-			if cache.Properties.ProvisioningState != nil {
-				val := string(*cache.Properties.ProvisioningState)
-				provisioningState = &val
 			}
 
 			cacheData, err := CreateResource(
 				a.MqlRuntime,
 				"azure.subscription.cacheService.redisInstance",
-				map[string]*llx.RawData{
-					"id":                  llx.StringDataPtr(cache.ID),
-					"name":                llx.StringDataPtr(cache.Name),
-					"type":                llx.StringDataPtr(cache.Type),
-					"location":            llx.StringDataPtr(cache.Location),
-					"properties":          llx.DictData(properties),
-					"hostName":            llx.StringDataPtr(cache.Properties.HostName),
-					"enableNonSslPort":    llx.BoolDataPtr(cache.Properties.EnableNonSSLPort),
-					"publicNetworkAccess": llx.StringDataPtr(publicNetworkAccess),
-					"port":                llx.IntDataPtr(cache.Properties.Port),
-					"sslPort":             llx.IntDataPtr(cache.Properties.SSLPort),
-					"provisioningState":   llx.StringDataPtr(provisioningState),
-					"redisVersion":        llx.StringDataPtr(cache.Properties.RedisVersion),
-					"replicasPerMaster":   llx.IntDataPtr(cache.Properties.ReplicasPerMaster),
-					"replicasPerPrimary":  llx.IntDataPtr(cache.Properties.ReplicasPerPrimary),
-					"sku":                 llx.DictData(sku),
-					"tags":                llx.MapData(convert.PtrMapStrToInterface(cache.Tags), types.String),
-				},
+				rawData,
 			)
 			if err != nil {
 				return nil, err
@@ -106,4 +73,47 @@ func (a *mqlAzureSubscriptionCacheService) redis() ([]any, error) {
 	}
 
 	return caches, nil
+}
+
+func createRedisInstanceRawData(cache *armredis.ResourceInfo) (map[string]*llx.RawData, error) {
+	properties, err := convert.JsonToDict(cache)
+	if err != nil {
+		return nil, err
+	}
+
+	sku, err := convert.JsonToDict(cache.Properties.SKU)
+	if err != nil {
+		return nil, err
+	}
+	// publicNetworkAccess is an enum with possible values: Enabled, Disabled
+	var publicNetworkAccess *string
+	if cache.Properties.PublicNetworkAccess != nil {
+		val := string(*cache.Properties.PublicNetworkAccess)
+		publicNetworkAccess = &val
+	}
+	// provisioningState is an enum with possible values: Creating, Deleting, Failed, Succeeded, Updating
+	var provisioningState *string
+	if cache.Properties.ProvisioningState != nil {
+		val := string(*cache.Properties.ProvisioningState)
+		provisioningState = &val
+	}
+
+	return map[string]*llx.RawData{
+		"id":                  llx.StringDataPtr(cache.ID),
+		"name":                llx.StringDataPtr(cache.Name),
+		"type":                llx.StringDataPtr(cache.Type),
+		"location":            llx.StringDataPtr(cache.Location),
+		"properties":          llx.DictData(properties),
+		"hostName":            llx.StringDataPtr(cache.Properties.HostName),
+		"enableNonSslPort":    llx.BoolDataPtr(cache.Properties.EnableNonSSLPort),
+		"publicNetworkAccess": llx.StringDataPtr(publicNetworkAccess),
+		"port":                llx.IntDataPtr(cache.Properties.Port),
+		"sslPort":             llx.IntDataPtr(cache.Properties.SSLPort),
+		"provisioningState":   llx.StringDataPtr(provisioningState),
+		"redisVersion":        llx.StringDataPtr(cache.Properties.RedisVersion),
+		"replicasPerMaster":   llx.IntDataPtr(cache.Properties.ReplicasPerMaster),
+		"replicasPerPrimary":  llx.IntDataPtr(cache.Properties.ReplicasPerPrimary),
+		"sku":                 llx.DictData(sku),
+		"tags":                llx.MapData(convert.PtrMapStrToInterface(cache.Tags), types.String),
+	}, nil
 }
--- providers/azure/resources/redis_test.go
+++ providers/azure/resources/redis_test.go
@@ -4,180 +4,75 @@
 package resources
 
 import (
-	"context"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redis/armredis/v2"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
-// mockAzureConnection implements the AzureConnection interface for testing
-type mockAzureConnection struct {
-	subscriptionID string
-	token          string
-}
-
-func (m *mockAzureConnection) ID() uint32 {
-	return 1
-}
-
-func (m *mockAzureConnection) Name() string {
-	return "mock-azure-connection"
-}
-
-func (m *mockAzureConnection) SubId() string {
-	return m.subscriptionID
-}
-
-func (m *mockAzureConnection) Token() string {
-	return m.token
-}
-
-func (m *mockAzureConnection) ClientOptions() interface{} {
-	return nil
-}
-
-func (m *mockAzureConnection) ParentID() uint32 {
-	return 0
-}
-
-// MockRedisClientFactory mocks the Azure Redis client factory
-type mockRedisClientFactory struct {
-	mock.Mock
-}
-
-func (m *mockRedisClientFactory) NewClient() *armredis.Client {
-	args := m.Called()
-	return args.Get(0).(*armredis.Client)
-}
-
-// MockRedisClient mocks the Azure Redis client
-type mockRedisClient struct {
-	mock.Mock
-}
-
-func (m *mockRedisClient) NewListBySubscriptionPager(options *armredis.ClientListBySubscriptionOptions) *runtime.Pager[armredis.ClientListBySubscriptionResponse] {
-	args := m.Called(options)
-	return args.Get(0).(*runtime.Pager[armredis.ClientListBySubscriptionResponse])
-}
-
-// mockPager mocks the Azure pager
-type mockPager struct {
-	mock.Mock
-	pages []armredis.ClientListBySubscriptionResponse
-}
-
-func (m *mockPager) More() bool {
-	args := m.Called()
-	return args.Bool(0)
-}
-
-func (m *mockPager) NextPage(ctx context.Context) (armredis.ClientListBySubscriptionResponse, error) {
-	args := m.Called(ctx)
-	return args.Get(0).(armredis.ClientListBySubscriptionResponse), args.Error(1)
-}
-
-// Helper functions to create pointers
-func stringPtr(s string) *string {
-	return &s
-}
-
-func publicNetworkAccessPtr(s armredis.PublicNetworkAccess) *armredis.PublicNetworkAccess {
-	return &s
-}
-
-func provisioningStatePtr(s armredis.ProvisioningState) *armredis.ProvisioningState {
-	return &s
-}
-
-func skuNamePtr(s armredis.SKUName) *armredis.SKUName {
-	return &s
-}
-
-func skuFamilyPtr(s armredis.SKUFamily) *armredis.SKUFamily {
-	return &s
-}
-
-func boolPtr(b bool) *bool {
-	return &b
-}
-
-func int32Ptr(i int32) *int32 {
-	return &i
+// Helper function to create pointers using generics
+func ptr[T any](v T) *T {
+	return &v
 }
 
 func TestAzureSubscriptionCacheServiceRedis(t *testing.T) {
 	// Create mock data
 	mockRedisCache := &armredis.ResourceInfo{
-		ID:       stringPtr("/subscriptions/test-subscription/resourceGroups/test-rg/providers/Microsoft.Cache/redis/test-cache"),
-		Name:     stringPtr("test-cache"),
-		Type:     stringPtr("Microsoft.Cache/redis"),
-		Location: stringPtr("eastus"),
+		ID:       ptr("/subscriptions/test-subscription/resourceGroups/test-rg/providers/Microsoft.Cache/redis/test-cache"),
+		Name:     ptr("test-cache"),
+		Type:     ptr("Microsoft.Cache/redis"),
+		Location: ptr("eastus"),
 		Properties: &armredis.Properties{
-			HostName:            stringPtr("test-cache.redis.cache.windows.net"),
-			EnableNonSSLPort:    boolPtr(true),
-			PublicNetworkAccess: publicNetworkAccessPtr(armredis.PublicNetworkAccessEnabled),
-			Port:                int32Ptr(6379),
-			SSLPort:             int32Ptr(6380),
-			ProvisioningState:   provisioningStatePtr(armredis.ProvisioningStateSucceeded),
-			RedisVersion:        stringPtr("6.0"),
-			ReplicasPerMaster:   int32Ptr(1),
-			ReplicasPerPrimary:  int32Ptr(1),
+			HostName:            ptr("test-cache.redis.cache.windows.net"),
+			EnableNonSSLPort:    ptr(true),
+			PublicNetworkAccess: ptr(armredis.PublicNetworkAccessEnabled),
+			Port:                ptr(int32(6379)),
+			SSLPort:             ptr(int32(6380)),
+			ProvisioningState:   ptr(armredis.ProvisioningStateSucceeded),
+			RedisVersion:        ptr("6.0"),
+			ReplicasPerMaster:   ptr(int32(1)),
+			ReplicasPerPrimary:  ptr(int32(1)),
 			SKU: &armredis.SKU{
-				Name:     skuNamePtr(armredis.SKUNameStandard),
-				Capacity: int32Ptr(1),
-				Family:   skuFamilyPtr(armredis.SKUFamilyC),
+				Name:     ptr(armredis.SKUNameStandard),
+				Capacity: ptr(int32(1)),
+				Family:   ptr(armredis.SKUFamilyC),
 			},
 		},
 		Tags: map[string]*string{
-			"Environment": stringPtr("Test"),
-			"Project":     stringPtr("Redis-Test"),
+			"Environment": ptr("Test"),
+			"Project":     ptr("Redis-Test"),
 		},
 	}
-
-	// Create mock response
-	mockResponse := armredis.ClientListBySubscriptionResponse{
-		ListResult: armredis.ListResult{
-			Value: []*armredis.ResourceInfo{mockRedisCache},
-		},
-	}
-
-	// Create mock pager
-	mockPager := &mockPager{}
-	mockPager.On("More").Return(true).Once()
-	mockPager.On("More").Return(false).Once()
-	mockPager.On("NextPage", mock.Anything).Return(mockResponse, nil).Once()
-
-	// Create mock client
-	mockClient := &mockRedisClient{}
-	mockClient.On("NewListBySubscriptionPager", mock.Anything).Return(mockPager)
-
-	// Create mock client factory
-	mockFactory := &mockRedisClientFactory{}
-	mockFactory.On("NewClient").Return(mockClient)
-
-	// Test the data conversion logic
 
 	t.Run("TestRedisDataConversion", func(t *testing.T) {
-		// Test that the mock data has the expected structure
-		assert.Equal(t, "test-cache", *mockRedisCache.Name)
-		assert.Equal(t, "eastus", *mockRedisCache.Location)
-		assert.Equal(t, "test-cache.redis.cache.windows.net", *mockRedisCache.Properties.HostName)
-		assert.Equal(t, true, *mockRedisCache.Properties.EnableNonSSLPort)
-		assert.Equal(t, "Enabled", string(*mockRedisCache.Properties.PublicNetworkAccess))
-		assert.Equal(t, int32(6379), *mockRedisCache.Properties.Port)
-		assert.Equal(t, int32(6380), *mockRedisCache.Properties.SSLPort)
-		assert.Equal(t, "Succeeded", string(*mockRedisCache.Properties.ProvisioningState))
-		assert.Equal(t, "6.0", *mockRedisCache.Properties.RedisVersion)
-		assert.Equal(t, int32(1), *mockRedisCache.Properties.ReplicasPerMaster)
-		assert.Equal(t, int32(1), *mockRedisCache.Properties.ReplicasPerPrimary)
-		assert.Equal(t, "Standard", string(*mockRedisCache.Properties.SKU.Name))
-		assert.Equal(t, int32(1), *mockRedisCache.Properties.SKU.Capacity)
-		assert.Equal(t, "C", string(*mockRedisCache.Properties.SKU.Family))
-		assert.Equal(t, "Test", *mockRedisCache.Tags["Environment"])
-		assert.Equal(t, "Redis-Test", *mockRedisCache.Tags["Project"])
+		// Test the actual conversion function from redis.go
+		rawData, err := createRedisInstanceRawData(mockRedisCache)
+		assert.NoError(t, err)
+		assert.NotNil(t, rawData)
+
+		// Verify the converted data matches our mock input
+		assert.Equal(t, "/subscriptions/test-subscription/resourceGroups/test-rg/providers/Microsoft.Cache/redis/test-cache", rawData["id"].Value)
+		assert.Equal(t, "test-cache", rawData["name"].Value)
+		assert.Equal(t, "Microsoft.Cache/redis", rawData["type"].Value)
+		assert.Equal(t, "eastus", rawData["location"].Value)
+		assert.Equal(t, "test-cache.redis.cache.windows.net", rawData["hostName"].Value)
+		assert.Equal(t, true, rawData["enableNonSslPort"].Value)
+		assert.Equal(t, "Enabled", rawData["publicNetworkAccess"].Value)
+		assert.Equal(t, int64(6379), rawData["port"].Value)
+		assert.Equal(t, int64(6380), rawData["sslPort"].Value)
+		assert.Equal(t, "Succeeded", rawData["provisioningState"].Value)
+		assert.Equal(t, "6.0", rawData["redisVersion"].Value)
+		assert.Equal(t, int64(1), rawData["replicasPerMaster"].Value)
+		assert.Equal(t, int64(1), rawData["replicasPerPrimary"].Value)
+
+		// Verify properties dict is not nil
+		assert.NotNil(t, rawData["properties"].Value)
+
+		// Verify SKU dict is not nil
+		assert.NotNil(t, rawData["sku"].Value)
+
+		// Verify tags are converted properly
+		assert.NotNil(t, rawData["tags"].Value)
 	})
 
 	t.Run("TestEnumConversions", func(t *testing.T) {
