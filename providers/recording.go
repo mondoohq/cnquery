ee5c554290d4205cf6b5aa90ed0c40e5503b963a
Preslav <preslav@mondoo.com>
2025-10-24 22:51:10 +0300

    wip: static provider.
    Signed-off-by: Preslav <preslav@mondoo.com>

---
+++ providers/recording.go
@@ -0,0 +1,191 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package providers
+
+import (
+	"github.com/cockroachdb/errors"
+	"go.mondoo.com/cnquery/v12/llx"
+	"go.mondoo.com/cnquery/v12/providers-sdk/v1/inventory"
+	"go.mondoo.com/cnquery/v12/providers-sdk/v1/plugin"
+	"go.mondoo.com/cnquery/v12/providers-sdk/v1/recording"
+	"go.mondoo.com/ranger-rpc/codes"
+	"go.mondoo.com/ranger-rpc/status"
+)
+
+var (
+	_                         plugin.ProviderPlugin = &recordingProvider{}
+	_                         plugin.StaticProvider = &recordingProvider{}
+	recordingProviderInstance                       = Provider{
+		Provider: &plugin.Provider{
+			Name:    "recording",
+			ID:      "go.mondoo.com/cnquery/v12/providers/recording",
+			Version: "12.0.0",
+			Connectors: []plugin.Connector{
+				{
+					Name:    "recording",
+					MinArgs: 0,
+					MaxArgs: 1,
+					Use:     "recording [flags]",
+					Short:   "read recording file on disk",
+					Flags: []plugin.Flag{
+						{
+							Long: "path",
+							Type: plugin.FlagType_String,
+						},
+					},
+				},
+			},
+		},
+	}
+)
+
+func (*recordingProvider) StaticName() string {
+	return "recording"
+}
+
+type RecordingProviderOpt func(*recordingProvider)
+
+// allows to set a custom recording implementation programmatically
+func WithRecording(rec llx.Recording) RecordingProviderOpt {
+	return func(rp *recordingProvider) {
+		rp.recording = rec
+	}
+}
+
+func NewRecordingProvider(opts ...RecordingProviderOpt) *recordingProvider {
+	rp := &recordingProvider{}
+	for _, o := range opts {
+		o(rp)
+	}
+	return rp
+}
+
+type recordingProvider struct {
+	initialized bool
+	recording   llx.Recording
+	runtime     *plugin.Runtime
+}
+
+func (s *recordingProvider) Heartbeat(req *plugin.HeartbeatReq) (*plugin.HeartbeatRes, error) {
+	return nil, nil
+}
+
+func (s *recordingProvider) ParseCLI(req *plugin.ParseCLIReq) (*plugin.ParseCLIRes, error) {
+	filePath := ""
+
+	pathFlag := req.Flags["path"]
+	if pathFlag != nil && pathFlag.RawData().Value.(string) != "" {
+		filePath = pathFlag.RawData().Value.(string)
+	}
+
+	asset := &inventory.Asset{
+		Connections: []*inventory.Config{
+			{
+				Type: "recording",
+				Path: filePath,
+			},
+		},
+	}
+
+	res := &plugin.ParseCLIRes{
+		Asset: asset,
+	}
+	return res, nil
+}
+
+func (s *recordingProvider) Connect(req *plugin.ConnectReq, callback plugin.ProviderCallback) (*plugin.ConnectRes, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "missing config")
+	}
+	inv, err := s.detect(req.GetAsset())
+	if err != nil {
+		return nil, err
+	}
+
+	path := req.Asset.Connections[0].Path
+	rec, err := recording.LoadRecordingFile(path)
+	if err != nil {
+		return nil, err
+	}
+	s.recording = rec
+	res := &plugin.ConnectRes{
+		Asset:     req.GetAsset(),
+		Inventory: inv,
+	}
+
+	return res, nil
+}
+
+func (s *recordingProvider) detect(asset *inventory.Asset) (*inventory.Inventory, error) {
+	if asset.GetPlatform() != nil {
+		return nil, nil
+	}
+
+	path := asset.Connections[0].Path
+	rec, err := recording.LoadRecordingFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	assets := []*inventory.Asset{}
+	for _, a := range rec.Assets {
+		invAsset := a.Asset.ToInventory()
+		invAsset.Connections = []*inventory.Config{
+			{
+				Type: "recording",
+				Path: path,
+			},
+		}
+		assets = append(assets, invAsset)
+	}
+
+	inv := inventory.New(inventory.WithAssets(assets...))
+	return inv, nil
+}
+
+func (s *recordingProvider) MockConnect(req *plugin.ConnectReq, callback plugin.ProviderCallback) (*plugin.ConnectRes, error) {
+	return nil, errors.New("the recording provider does not support the mock connect call, this is an internal error")
+}
+
+func (s *recordingProvider) Disconnect(req *plugin.DisconnectReq) (*plugin.DisconnectRes, error) {
+	return nil, nil
+}
+
+func (s *recordingProvider) Shutdown(req *plugin.ShutdownReq) (*plugin.ShutdownRes, error) {
+	return nil, nil
+}
+
+func (s *recordingProvider) GetData(req *plugin.DataReq) (*plugin.DataRes, error) {
+	data, ok := s.recording.GetData(0, req.GetResource(), req.GetResourceId(), req.GetField())
+	if !ok {
+		return nil, errors.New("resource '" + req.Resource + "' (id: " + req.ResourceId + ") doesn't exist")
+	}
+
+	res := data.Result().Data
+	return &plugin.DataRes{Data: res}, nil
+}
+
+func (s *recordingProvider) StoreData(req *plugin.StoreReq) (*plugin.StoreRes, error) {
+	for _, r := range req.GetResources() {
+		for _, f := range r.GetFields() {
+			addDataReq := llx.AddDataReq{
+				ConnectionID:      1,
+				Resource:          r.GetName(),
+				ResourceID:        r.GetId(),
+				RequestResourceId: r.GetId(),
+				Field:             f.CodeId,
+				Data:              f.GetData().RawData(),
+			}
+			s.recording.AddData(addDataReq)
+		}
+	}
+	return &plugin.StoreRes{}, nil
+}
+
+func (s *recordingProvider) Init(running *RunningProvider) {
+	if s.initialized {
+		return
+	}
+	s.initialized = true
+}
