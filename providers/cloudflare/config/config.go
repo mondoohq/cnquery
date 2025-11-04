499ba59d246de7df77b1efacbc60ba9278620cde
Mondoo Tools <tools@mondoo.com>
2025-11-04 11:54:29 +0000

    ✨ Update providers 20251104

--- providers/cloudflare/config/config.go
+++ providers/cloudflare/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "cloudflare",
 	ID:              "go.mondoo.com/cnquery/v12/providers/cloudflare",
-	Version:         "11.0.72",
+	Version:         "11.0.73",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/vsphere/config/config.go
+++ providers/vsphere/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vsphere",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vsphere",
-	Version:         "11.0.114",
+	Version:         "11.0.115",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
