8b83fdad70f199a412722a0a88a2ed26f84a7718
Máté Bajusz <mate@mondoo.com>
2025-11-13 10:06:31 +0100

    wip

--- providers/ipinfo/resources/ipinfo.go
+++ providers/ipinfo/resources/ipinfo.go
@@ -4,36 +4,65 @@
 package resources
 
 import (
-	"errors"
 	"net"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/cockroachdb/errors"
 	"github.com/ipinfo/go/v2/ipinfo"
 	"go.mondoo.com/cnquery/v12/llx"
 	"go.mondoo.com/cnquery/v12/providers-sdk/v1/plugin"
 	"go.mondoo.com/cnquery/v12/providers/ipinfo/connection"
 )
 
+// createHTTPClientForInterface creates an HTTP client bound to a specific network interface
+// by binding to the local IP address of that interface
+func createHTTPClientForInterface(interfaceIP net.IP) (*http.Client, error) {
+	// Resolve the local TCP address with dynamic port (0)
+	var localAddr net.Addr
+	var err error
+	
+	if interfaceIP.To4() != nil {
+		// IPv4
+		localAddr, err = net.ResolveTCPAddr("tcp4", interfaceIP.String()+":0")
+	} else {
+		// IPv6
+		localAddr, err = net.ResolveTCPAddr("tcp6", "["+interfaceIP.String()+"]:0")
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to resolve local address")
+	}
+
+	// Create a custom dialer that binds to the specific interface
+	dialer := &net.Dialer{
+		LocalAddr: localAddr,
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+
+	// Create a transport with the custom dialer
+	transport := &http.Transport{
+		DialContext:       dialer.DialContext,
+		DisableKeepAlives: false,
+	}
+
+	// Create an HTTP client with the custom transport
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   30 * time.Second,
+	}
+
+	return client, nil
+}
+
 func initIpinfo(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[string]*llx.RawData, plugin.Resource, error) {
-	// Get ipinfo client from connection
-	conn := runtime.Connection.(*connection.IpinfoConnection)
-	client := conn.Client()
-	if client == nil {
-		// Initialize client if not already set
-		token := os.Getenv("IPINFO_TOKEN")
-		ipinfoClient := ipinfo.NewClient(nil, nil, token)
-		conn.SetClient(ipinfoClient)
-		client = ipinfoClient
-	}
-
-	ipinfoClient, ok := client.(*ipinfo.Client)
-	if !ok {
-		return nil, nil, errors.New("failed to get ipinfo client")
-	}
-
+	token := os.Getenv("IPINFO_TOKEN")
+	
 	var queryIP net.IP
 	var inputIP *llx.RawData
 	var isLocalIP bool
+	var httpClient *http.Client
 
 	// Check if an IP was provided as input
 	if ip, ok := args["ip"]; ok {
@@ -48,11 +77,19 @@ func initIpinfo(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[stri
 		queryIP = ipVal.IP
 
 		// Check if this is a local/private IP (bogon)
-		// Local IPs won't have meaningful data from ipinfo.io
-		// So we'll query for YOUR public IP instead
 		if queryIP.IsLoopback() || queryIP.IsLinkLocalUnicast() || queryIP.IsPrivate() {
 			isLocalIP = true
-			queryIP = nil // Query for YOUR public IP instead
+			// For local IPs, bind the HTTP request to this interface
+			// This allows us to get the public IP for this specific interface
+			var err error
+			httpClient, err = createHTTPClientForInterface(queryIP)
+			if err != nil {
+				return nil, nil, errors.Wrap(err, "failed to create HTTP client for interface")
+			}
+			queryIP = nil // Query for YOUR public IP from this interface
+		} else {
+			// Public IP provided - query info about that specific IP
+			// No need to bind to interface for public IP queries
 		}
 	} else {
 		// No IP provided - get YOUR public IP (the one visible from internet)
@@ -60,8 +97,31 @@ func initIpinfo(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[stri
 		queryIP = nil
 	}
 
+	// Create ipinfo client with custom HTTP client if we bound to an interface
+	// Otherwise use default client from connection or create new one
+	var ipinfoClient *ipinfo.Client
+	conn := runtime.Connection.(*connection.IpinfoConnection)
+	
+	if httpClient != nil {
+		// Use the interface-bound HTTP client
+		ipinfoClient = ipinfo.NewClient(httpClient, nil, token)
+	} else {
+		// Use default client (from connection or create new)
+		client := conn.Client()
+		if client == nil {
+			ipinfoClient = ipinfo.NewClient(nil, nil, token)
+			conn.SetClient(ipinfoClient)
+		} else {
+			var ok bool
+			ipinfoClient, ok = client.(*ipinfo.Client)
+			if !ok {
+				return nil, nil, errors.New("failed to get ipinfo client")
+			}
+		}
+	}
+
 	// Call ipinfo.io API
-	// If queryIP is nil, it returns YOUR public IP
+	// If queryIP is nil, it returns YOUR public IP (from the bound interface if specified)
 	// If queryIP is provided, it returns info about that specific IP
 	info, err := ipinfoClient.GetIPInfo(queryIP)
 	if err != nil {
@@ -69,12 +129,12 @@ func initIpinfo(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[stri
 	}
 
 	// Set the IP field:
-	// - If input was a local IP, return YOUR public IP (the one visible from internet)
+	// - If input was a local IP, return the public IP for that interface
 	// - If input was a public IP, return that public IP
 	// - If no input, return YOUR public IP
 	if isLocalIP && info.IP != nil {
-		// Local IP was provided, but we queried for public IP
-		// Return the public IP that ipinfo.io sees
+		// Local IP was provided, we queried for public IP from that interface
+		// Return the public IP that ipinfo.io sees from that interface
 		args["ip"] = llx.IPData(llx.ParseIP(info.IP.String()))
 	} else if inputIP != nil {
 		// Public IP was provided, keep it
@@ -85,7 +145,7 @@ func initIpinfo(runtime *plugin.Runtime, args map[string]*llx.RawData) (map[stri
 	}
 
 	// Set the hostname from API response
-	// For local/private IPs, this will be empty (bogon IPs)
+	// For local/private IPs, this will be the hostname for the public IP of that interface
 	args["hostname"] = llx.StringData(info.Hostname)
 
 	return args, nil, nil
