7331477a817753f99e89d00ba82a48403af28ddc
Mondoo <hello@mondoo.com>
2025-11-19 11:11:57 +0100

    🎉 ms365-11.1.76
    This release was created by cnquery's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.

--- providers/ms365/config/config.go
+++ providers/ms365/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ms365",
 	ID:              "go.mondoo.com/cnquery/v9/providers/ms365",
-	Version:         "11.1.75",
+	Version:         "11.1.76",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
