2f795a3ecd48074bbd72575fa442bd7b2cbd53a0
Máté Bajusz <mate@mondoo.com>
2025-11-20 13:14:19 +0100

    fixes

--- providers/ipinfo/config/config.go
+++ providers/ipinfo/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ipinfo",
 	ID:              "go.mondoo.com/cnquery/v12/providers/ipinfo",
-	Version:         "12.0.0",
+	Version:         "12.10.0",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
