49e6280dbc2b1661848af3195901cb615275f848
Mondoo Tools <tools@mondoo.com>
2025-11-25 12:51:17 +0000

    ✨ Update providers 20251125

--- providers/ansible/config/config.go
+++ providers/ansible/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ansible",
 	ID:              "go.mondoo.com/cnquery/v12/providers/ansible",
-	Version:         "11.0.106",
+	Version:         "11.0.107",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/nmap/config/config.go
+++ providers/nmap/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "nmap",
 	ID:              "go.mondoo.com/cnquery/v12/providers/nmap",
-	Version:         "11.0.74",
+	Version:         "11.0.75",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
