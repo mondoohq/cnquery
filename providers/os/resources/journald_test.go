b36425a904456965e331369d204007d242c84e07
Máté Bajusz <mate@mondoo.com>
2025-12-01 13:45:27 +0100

    fix tests

--- providers/os/resources/journald_test.go
+++ providers/os/resources/journald_test.go
@@ -13,7 +13,7 @@ import (
 func TestResource_JournaldConfig(t *testing.T) {
 	x.TestSimpleErrors(t, []testutils.SimpleTest{
 		{
-			Code:        "journald.config('nopath').params",
+			Code:        "journald.config('nopath').sections",
 			ResultIndex: 0,
 			Expectation: "file 'nopath' not found",
 		},
@@ -25,21 +25,27 @@ func TestResource_JournaldConfig(t *testing.T) {
 		assert.NoError(t, res[0].Data.Error)
 	})
 
+	t.Run("journald sections", func(t *testing.T) {
+		res := x.TestQuery(t, "journald.config.sections")
+		assert.NotEmpty(t, res)
+		assert.NoError(t, res[0].Data.Error)
+	})
+
 	t.Run("journald params", func(t *testing.T) {
-		res := x.TestQuery(t, "journald.config.params")
+		res := x.TestQuery(t, "journald.config.sections[0].params")
 		assert.NotEmpty(t, res)
 		assert.NoError(t, res[0].Data.Error)
 	})
 
 	t.Run("journald is downcasing relevant params", func(t *testing.T) {
-		res := x.TestQuery(t, "journald.config.params.Compress")
+		res := x.TestQuery(t, "journald.config.sections.where(name == 'Journal')[0].params.where(name == 'Compress')[0].value")
 		assert.NotEmpty(t, res)
 		assert.NoError(t, res[0].Data.Error)
 		assert.Equal(t, "yes", res[0].Data.Value)
 	})
 
 	t.Run("journald is NOT downcasing other params", func(t *testing.T) {
-		res := x.TestQuery(t, "journald.config.params.Storage")
+		res := x.TestQuery(t, "journald.config.sections.where(name == 'Journal')[0].params.where(name == 'Storage')[0].value")
 		assert.NotEmpty(t, res)
 		assert.NoError(t, res[0].Data.Error)
 		assert.Equal(t, "persistent", res[0].Data.Value)
