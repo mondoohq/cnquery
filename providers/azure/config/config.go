57acb340fde04178c818936224bd97b3769811fd
Mondoo Tools <tools@mondoo.com>
2025-12-17 09:27:25 +0000

    ✨ Update providers 20251217

--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "azure",
 	ID:      "go.mondoo.com/cnquery/v9/providers/azure",
-	Version: "11.4.17",
+	Version: "11.4.18",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(azureinstancesnapshot.SnapshotConnectionType),
--- providers/tailscale/config/config.go
+++ providers/tailscale/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "tailscale",
 	ID:              "go.mondoo.com/cnquery/v12/providers/tailscale",
-	Version:         "11.0.66",
+	Version:         "11.0.67",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
