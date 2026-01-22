e677bb9ed5da4ee6b81e8d3fd75fbdd954fa7648
Mondoo Tools <tools@mondoo.com>
2026-01-22 06:29:10 +0000

    ✨ Update providers 20260122

--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "11.0.130",
+	Version:         "11.0.131",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/terraform/config/config.go
+++ providers/terraform/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "terraform",
 	ID:      "go.mondoo.com/cnquery/v9/providers/terraform",
-	Version: "11.1.77",
+	Version: "11.1.78",
 	ConnectionTypes: []string{
 		provider.StateConnectionType,
 		provider.PlanConnectionType,
