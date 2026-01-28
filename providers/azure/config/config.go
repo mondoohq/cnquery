64deb7fe3d43e03472fc34a9364366f2d433e2cc
Mondoo Tools <tools@mondoo.com>
2026-01-28 01:05:24 +0000

    ✨ Update providers 20260128

--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "azure",
 	ID:      "go.mondoo.com/cnquery/v9/providers/azure",
-	Version: "11.4.26",
+	Version: "11.4.27",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(azureinstancesnapshot.SnapshotConnectionType),
--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gitlab",
-	Version: "11.1.127",
+	Version: "11.1.128",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
