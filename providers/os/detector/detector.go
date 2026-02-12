56b9c4f711319e82bf5f2d7147af80a7ed9a66a1
Ivan Milchev <ivan@mondoo.com>
2026-02-12 14:29:10 +0200

    🧹 make sure windows is detected as os family (#6617)
    * 🧹 make sure windows is detected as os family
    
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>
    
    * do not run unix detection on windows local
    
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>
    
    ---------
    
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- providers/os/detector/detector.go
+++ providers/os/detector/detector.go
@@ -4,20 +4,12 @@
 package detector
 
 import (
-	"runtime"
-
 	"go.mondoo.com/cnquery/v12/providers-sdk/v1/inventory"
 	"go.mondoo.com/cnquery/v12/providers/os/connection/shared"
 )
 
 func DetectOS(conn shared.Connection) (*inventory.Platform, bool) {
-	var res *inventory.Platform
-	var ok bool
-	if conn.Type() == shared.Type_Local && runtime.GOOS == "windows" {
-		res, ok = WindowsFamily.Resolve(conn)
-	} else {
-		res, ok = OperatingSystems.Resolve(conn)
-	}
+	res, ok := OperatingSystems.Resolve(conn)
 
 	addTechnologyUrl(res)
 	return res, ok
