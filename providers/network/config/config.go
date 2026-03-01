f2fac5b101a31028ea2e3421c6accad564a020ef
Mondoo Tools <tools@mondoo.com>
2026-03-01 14:58:51 +0000

    ✨ Update providers 20260301

--- providers/network/config/config.go
+++ providers/network/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "network",
 	ID:              "go.mondoo.com/cnquery/v9/providers/network",
-	Version:         "11.0.120",
+	Version:         "11.0.121",
 	ConnectionTypes: []string{provider.HostConnectionType},
 	CrossProviderTypes: []string{
 		"go.mondoo.com/mql/providers/os",
