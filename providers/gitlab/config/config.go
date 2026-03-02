7ef7cacacfa17f06c551f361151fb817912e44f1
Tim Smith <tsmith84@gmail.com>
2026-03-02 06:47:36 -0800

    🎉 gitlab-11.2.0 (#6750)
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/gitlab/config/config.go
+++ providers/gitlab/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "gitlab",
 	ID:      "go.mondoo.com/cnquery/v9/providers/gitlab",
-	Version: "11.1.140",
+	Version: "11.2.0",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		provider.GitlabGroupConnection,
