2547e24c74e17cedf40902bb077504e43fb04bc4
Dimitar Ganev <me@dganev.com>
2026-03-16 11:19:12 +0200

    chore(okta): bump provider version to 13.1.0

--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "13.0.0",
+	Version:         "13.1.0",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
