ff5d16a1319ec3c1c3e2fb6e2171bfce96822208
Mondoo Tools <tools@mondoo.com>
2026-03-17 12:02:25 +0000

    🎉 ansible-13.0.1, arista-13.1.1, atlassian-13.0.1, aws-13.2.2, cloudflare-13.0.1, cloudformation-13.0.1, equinix-13.0.1, github-13.0.2, gitlab-13.0.1, google-workspace-13.0.2, ipinfo-13.0.1, ipmi-13.0.1, k8s-13.0.2, mondoo-13.0.1, ms365-13.0.1, nmap-13.0.1, oci-13.0.1, opcua-13.0.1, os-13.2.3, shodan-13.0.1, slack-13.0.1, snowflake-13.0.1, tailscale-13.0.1, terraform-13.0.1, vcd-13.0.1, vsphere-13.0.1

--- providers/cloudflare/config/config.go
+++ providers/cloudflare/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "cloudflare",
 	ID:              "go.mondoo.com/mql/v13/providers/cloudflare",
-	Version:         "13.0.0",
+	Version:         "13.0.1",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/vsphere/config/config.go
+++ providers/vsphere/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "vsphere",
 	ID:              "go.mondoo.com/cnquery/v9/providers/vsphere",
-	Version:         "13.0.0",
+	Version:         "13.0.1",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
