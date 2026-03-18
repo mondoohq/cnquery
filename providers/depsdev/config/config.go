3e64c07e01c02187ed0a4f3879ac3ba97f90e95d
Mondoo <hello@mondoo.com>
2026-03-18 08:38:07 -0700

    🎉 depsdev-13.0.2, os-13.2.5, network-13.0.2
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### depsdev (13.0.2)
    - 🧹 fix depsdev provider go.mod formatting (#6975)
    
    ### os (13.2.5)
    - ✨ Implement filesystem caching for container images to prevent OOM issues (#6974)
    
    ### network (13.0.2)
    - :star: Add PQC hybrid KEX detection and certificate introspection to network provider (#6953)

--- providers/depsdev/config/config.go
+++ providers/depsdev/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "depsdev",
 	ID:              "go.mondoo.com/mql/v13/providers/depsdev",
-	Version:         "13.0.1",
+	Version:         "13.0.2",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
