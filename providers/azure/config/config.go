f645352365c835456cc7009def037bd68be95f70
Mondoo Tools <tools@mondoo.com>
2026-03-19 16:41:14 +0000

    🎉 azure-13.1.4, aws-13.2.6

--- providers/azure/config/config.go
+++ providers/azure/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "azure",
 	ID:      "go.mondoo.com/cnquery/v9/providers/azure",
-	Version: "13.1.3",
+	Version: "13.1.4",
 	ConnectionTypes: []string{
 		provider.ConnectionType,
 		string(azureinstancesnapshot.SnapshotConnectionType),
