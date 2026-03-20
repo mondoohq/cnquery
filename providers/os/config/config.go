55a957eb0c83ecb785a4b8369306c4c9e573b4f1
Mondoo Tools <tools@mondoo.com>
2026-03-20 17:03:14 +0000

    🎉 azure-13.1.6, os-13.2.8

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "13.2.7",
+	Version: "13.2.8",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
