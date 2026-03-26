ca71b0e7b4580f196a24217e67f2b7c2c31aa5ea
Mondoo <hello@mondoo.com>
2026-03-26 08:03:24 -0700

    🎉 os-13.3.0
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### os (13.3.0)
    - 🧹 gofmt
    - 🐛 Support Wolfi apk database path (/usr/lib/apk/db/installed)
    - 🧹 Remove Wolfi OpenRC service manager mapping
    - ✨ Add Wolfi OS detection support
    - fix: return false instead of error for null comparison operands (#7036)

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "13.2.9",
+	Version: "13.3.0",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
