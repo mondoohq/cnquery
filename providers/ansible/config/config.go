5ccd7e1fae99eb6a6d87bd3f97d33e42dddd1721
Mondoo Tools <tools@mondoo.com>
2026-03-31 08:52:01 +0000

    🎉 ansible-13.0.3, arista-13.1.5, atlassian-13.0.3, aws-13.6.1, azure-13.3.1, cloudflare-13.0.3, cloudformation-13.0.3, depsdev-13.0.4, equinix-13.0.3, gcp-13.3.1, github-13.0.4, gitlab-13.0.4, google-workspace-13.0.4, ipinfo-13.0.3, ipmi-13.0.3, k8s-13.0.6, mondoo-13.0.3, ms365-13.0.3, network-13.0.3, nmap-13.0.3, oci-13.0.3, okta-13.1.2, opcua-13.0.3, shodan-13.0.3, slack-13.0.3, snowflake-13.0.3, tailscale-13.0.3, terraform-13.0.3, vcd-13.0.3, vsphere-13.0.3

--- providers/ansible/config/config.go
+++ providers/ansible/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "ansible",
 	ID:              "go.mondoo.com/mql/v13/providers/ansible",
-	Version:         "13.0.2",
+	Version:         "13.0.3",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/atlassian/config/config.go
+++ providers/atlassian/config/config.go
@@ -14,7 +14,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "atlassian",
 	ID:      "go.mondoo.com/cnquery/v9/providers/atlassian",
-	Version: "13.0.2",
+	Version: "13.0.3",
 	ConnectionTypes: []string{
 		provider.DefaultConnectionType,
 		"jira",
