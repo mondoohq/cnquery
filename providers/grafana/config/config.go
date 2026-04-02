c916740a62c4ae03f3f66309aa3747abca2d7a70
Mondoo Tools <tools@mondoo.com>
2026-04-02 09:10:02 +0000

    🎉 ansible-13.0.4, arista-13.1.6, atlassian-13.0.4, aws-13.6.2, azure-13.3.2, cloudflare-13.0.4, cloudformation-13.0.4, depsdev-13.0.5, equinix-13.0.4, gcp-13.3.2, github-13.0.5, gitlab-13.0.5, google-workspace-13.0.5, grafana-13.0.1, ipinfo-13.0.4, ipmi-13.0.4, mondoo-13.0.4, ms365-13.0.4, network-13.0.4, nmap-13.0.4, oci-13.0.4, okta-13.1.3, opcua-13.0.4, os-13.6.2, shodan-13.0.4, slack-13.0.4, snowflake-13.0.4, tailscale-13.0.4, terraform-13.0.4, vcd-13.0.4, vsphere-13.0.4

--- providers/grafana/config/config.go
+++ providers/grafana/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "grafana",
 	ID:              "go.mondoo.com/mql/v13/providers/grafana",
-	Version:         "13.0.0",
+	Version:         "13.0.1",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
--- providers/okta/config/config.go
+++ providers/okta/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "okta",
 	ID:              "go.mondoo.com/cnquery/v9/providers/okta",
-	Version:         "13.1.2",
+	Version:         "13.1.3",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
