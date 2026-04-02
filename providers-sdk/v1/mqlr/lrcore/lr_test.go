5cb0da38346b08d2f4057ceacbfac317e74fa678
Ivan Milchev <ivan@mondoo.com>
2026-04-02 11:13:10 +0300

    fix test
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- providers-sdk/v1/mqlr/lrcore/lr_test.go
+++ providers-sdk/v1/mqlr/lrcore/lr_test.go
@@ -303,7 +303,7 @@ func TestParseLR(t *testing.T) {
 				t.Fatal("failed to go-convert " + lrPath + ":" + err.Error())
 			}
 			assert.NotEmpty(t, godata)
-			assert.Equal(t, godata[:68], "// Copyright Mondoo, Inc. 2024, 2026\n// SPDX-License-Identifier: BUSL-1.1\n\n")
+			assert.Equal(t, "// Copyright Mondoo, Inc. 2024, 2026\n// SPDX-License-Identifier: BUSL-1.1\n\n", godata[:75])
 
 			schema, err := Schema(res)
 			if err != nil {
