79e538dbde86d0369981534c333ee8d52d4949b2
Mondoo Tools <tools@mondoo.com>
2026-05-07 17:17:18 +0000

    🎉 activedirectory-13.0.7, arista-13.2.4, atlassian-13.2.3, aws-13.21.3, azure-13.9.3, cloudflare-13.3.3, datadog-13.0.3, digitalocean-13.1.4, equinix-13.0.10, gcp-13.13.3, github-13.2.3, gitlab-13.1.4, hetzner-13.0.4, k8s-13.0.13, ms365-13.0.10, network-13.0.6, okta-13.2.3, os-13.16.2, proxmox-0.1.6, slack-13.1.5, tailscale-13.1.4, terraform-13.0.10, vcd-13.0.10, vsphere-13.1.1

--- providers/proxmox/config/config.go
+++ providers/proxmox/config/config.go
@@ -11,7 +11,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "proxmox",
 	ID:              "go.mondoo.com/mql/v13/providers/proxmox",
-	Version:         "0.1.5",
+	Version:         "0.1.6",
 	ConnectionTypes: []string{"proxmox"},
 	Connectors: []plugin.Connector{
 		{
