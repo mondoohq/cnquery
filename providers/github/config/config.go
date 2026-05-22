8961c6b27d1f9878944454699136d2c2e36f9201
Mondoo <hello@mondoo.com>
2026-05-22 16:22:25 -0700

    🎉 github-13.4.0
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### github (13.4.0)
    - ✨ github: add github.repository.pages for GitHub Pages site config (#7804)
    - ✨ github: add github.organization.copilot for Copilot Business config (#7803)
    - 🧹 stricter rules for titles+description on resources/fields (#7763)
    - 🧹 Update deps for mql and providers 20260521 (#7760)

--- providers/github/config/config.go
+++ providers/github/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "github",
 	ID:              "go.mondoo.com/cnquery/v9/providers/github",
-	Version:         "13.3.2",
+	Version:         "13.4.0",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
