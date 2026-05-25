a53cc75677ce25a0cbeaf4584bc0fbdaa5475f7e
Tim Smith <tsmith84@gmail.com>
2026-05-25 10:04:58 -0700

    🎉 aws-13.27.0, azure-13.13.0, gcp-13.17.0, os-13.17.0
    Signed-off-by: Tim Smith <tsmith84@gmail.com>
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "13.16.9",
+	Version: "13.17.0",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
