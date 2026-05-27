c4a32fc7d2668948e76d32ca5c192a3cedbe2ddb
Mondoo Tools <tools@mondoo.com>
2026-05-27 04:18:04 +0000

    🎉 aws-13.28.1, azure-13.14.1, gcp-13.18.1, google-workspace-13.0.15, huggingface-13.0.2, ms365-13.1.1

--- providers/google-workspace/config/config.go
+++ providers/google-workspace/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "google-workspace",
 	ID:              "go.mondoo.com/cnquery/v9/providers/google-workspace",
-	Version:         "13.0.14",
+	Version:         "13.0.15",
 	ConnectionTypes: []string{provider.ConnectionType},
 	Connectors: []plugin.Connector{
 		{
