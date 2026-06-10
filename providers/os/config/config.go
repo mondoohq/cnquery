47954919c86fd9239c16cb227506b8bced43b805
Mondoo <hello@mondoo.com>
2026-06-10 11:17:28 -0700

    🎉 os-13.27.0
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### os (13.27.0)
    - ✨ os/pam: add pam.conf.exists to check whether PAM config is present (#8322)

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "13.26.2",
+	Version: "13.27.0",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
