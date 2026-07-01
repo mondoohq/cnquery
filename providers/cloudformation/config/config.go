ecda13df6b22936f49599e5bc3c819d14db306c2
Mondoo <hello@mondoo.com>
2026-07-01 16:21:01 -0700

    🎉 os-13.31.0, cloudformation-13.2.0, bicep-13.4.0
    This release was created by mql's provider versioning bot.
    
    You can find me under: providers-sdk/v1/util/version.

--- providers/cloudformation/config/config.go
+++ providers/cloudformation/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "cloudformation",
 	ID:              "go.mondoo.com/mql/v13/providers/cloudformation",
-	Version:         "13.1.7",
+	Version:         "13.2.0",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Platforms:       provider.Platforms,
 	Connectors: []plugin.Connector{
