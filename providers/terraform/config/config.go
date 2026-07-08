262f992a1ed086896bb660746dd2e4c35d07c58b
Mondoo <hello@mondoo.com>
2026-07-07 17:03:21 -0700

    🎉 terraform-13.3.3
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### terraform (13.3.3)
    - 🏇 terraform.resources intermittenly returns empty (#9000)
    - 🧹 Update deps for mql and providers 20260707 (#8998)

--- providers/terraform/config/config.go
+++ providers/terraform/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:      "terraform",
 	ID:        "go.mondoo.com/cnquery/v9/providers/terraform",
-	Version:   "13.3.2",
+	Version:   "13.3.3",
 	Platforms: provider.Platforms,
 	ConnectionTypes: []string{
 		provider.StateConnectionType,
