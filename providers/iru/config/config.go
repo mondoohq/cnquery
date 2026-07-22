90009139acfb79a8326317549bf5ce98e33552f2
Mondoo <hello@mondoo.com>
2026-07-22 12:58:44 -0700

    🎉 iru-13.0.1
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### iru (13.0.1)
    - 🐛 iru: match the live Kandji API + add client tests (#9381)

--- providers/iru/config/config.go
+++ providers/iru/config/config.go
@@ -12,7 +12,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "iru",
 	ID:              "go.mondoo.com/mql/providers/iru",
-	Version:         "13.0.0",
+	Version:         "13.0.1",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Connectors: []plugin.Connector{
 		{
