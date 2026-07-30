a46b4ad2808064984ee2f56c78d84f9fb39231e0
Tim Smith <tsmith84@gmail.com>
2026-07-29 23:40:51 -0700

    🎉 network-13.3.0 (#9517)
    This release was created by mql's provider versioning bot.
    
    You can find me under: `providers-sdk/v1/util/version`.
    
    ### network (13.3.0)
    - 🐛 fix(network): add dns.reverseRecords and stop empty fqdn resolving the root zone (#9445)
    
    Co-authored-by: Mondoo <hello@mondoo.com>

--- providers/network/config/config.go
+++ providers/network/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "network",
 	ID:              "go.mondoo.com/cnquery/v9/providers/network",
-	Version:         "13.2.8",
+	Version:         "13.3.0",
 	ConnectionTypes: []string{provider.HostConnectionType},
 	Platforms:       provider.Platforms,
 	CrossProviderTypes: []string{
