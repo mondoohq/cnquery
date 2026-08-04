8aac5e4b714669416cf2d60d83a1d752fc5db3dd
Mondoo Tools <tools@mondoo.com>
2026-08-04 03:22:27 +0000

    🎉 google-workspace-13.3.1, stackit-13.5.1

--- providers/stackit/config/config.go
+++ providers/stackit/config/config.go
@@ -16,7 +16,7 @@ import (
 var Config = plugin.Provider{
 	Name:            "stackit",
 	ID:              "go.mondoo.com/mql/providers/stackit",
-	Version:         "13.5.0",
+	Version:         "13.5.1",
 	ConnectionTypes: []string{provider.DefaultConnectionType},
 	Platforms:       connection.Platforms,
 	Connectors: []plugin.Connector{
