d82675c048535db2a313d769da0d1f8dfc13edba
Dominik Richter <dominik.richter@gmail.com>
2026-08-03 23:31:59 -0700

    🎉 os-13.40.2 (#9541)

--- providers/os/config/config.go
+++ providers/os/config/config.go
@@ -15,7 +15,7 @@ import (
 var Config = plugin.Provider{
 	Name:    "os",
 	ID:      "go.mondoo.com/cnquery/v9/providers/os",
-	Version: "13.40.1",
+	Version: "13.40.2",
 	ConnectionTypes: []string{
 		shared.Type_Local.String(),
 		shared.Type_SSH.String(),
