e492f155d3aa8de0d8323631c10c7b9ea4822d01
Mondoo Tools <tools@mondoo.com>
2026-08-19 11:27:53 +0000

    🎉 databricks-13.3.1

--- providers/databricks/config/config.go
+++ providers/databricks/config/config.go
@@ -13,7 +13,7 @@ import (
 var Config = plugin.Provider{
 	Name:      "databricks",
 	ID:        "go.mondoo.com/mql/providers/databricks",
-	Version:   "13.3.0",
+	Version:   "13.3.1",
 	Platforms: connection.Platforms,
 	ConnectionTypes: []string{
 		provider.DefaultConnectionType,
