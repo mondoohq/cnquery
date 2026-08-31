82b34833dd632efd3cff25b3c334a3e5f6fb8614
Maximilian Rink <maximilian.rink@telekom.de>
2026-08-31 02:05:07 +0200

    test: use current module path for FRR coverage

--- mqlc/frr_config_test.go
+++ mqlc/frr_config_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"go.mondoo.com/mql/v13/mqlc"
-	"go.mondoo.com/mql/v13/providers-sdk/v1/testutils"
+	"go.mondoo.com/mql/mqlc"
+	"go.mondoo.com/mql/providers-sdk/v1/testutils"
 )
 
 // The FRR resources exist to answer three questions about a routing node:
